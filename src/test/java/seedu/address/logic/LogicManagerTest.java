5685d0cfe72e97c1df5ff1ee2c67c18bfd9a7256
Wang Leng Tan <tanwangleng@outlook.com>
2016-10-01 13:31:41 +0800

    Rename the package from '...model.person' to '...model.task'

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -15,9 +15,9 @@ import seedu.address.model.TaskList;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyTaskList;
-import seedu.address.model.person.*;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.UniqueTagList;
+import seedu.address.model.task.*;
 import seedu.address.storage.StorageManager;
 
 import java.util.ArrayList;
