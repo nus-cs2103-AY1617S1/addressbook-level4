67a4a3673d253c5c83f59f3224725d0b27aabb25
Wei Kang <weikangchia@gmail.com>
2016-10-04 17:22:26 +0800

    Add new model Status

---
+++ src/main/java/tars/model/task/Status.java
@@ -0,0 +1,37 @@
+package tars.model.task;
+
+/*
+ * Represents a Task's status in tars.
+ */
+public class Status {
+    private static final String MESSAGE_STATUS_DONE = "Done";
+    private static final String MESSAGE_STATUS_UNDONE = "Undone";
+
+    private boolean status;
+
+    public Status() {
+        status = false;
+    }
+
+    @Override
+    public String toString() {
+        if (status) {
+            return MESSAGE_STATUS_DONE;
+        } else {
+            return MESSAGE_STATUS_UNDONE;
+        }
+    }
+
+    public boolean isDone() {
+        return status;
+    }
+
+    public void setDone() {
+        this.status = true;
+    }
+    
+    public void setUndone() {
+        this.status = false;
+    }
+
+}
