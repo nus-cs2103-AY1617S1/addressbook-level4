67b3a1864cde9cc3bc7255c12f7fe85043f379b6
Wei Kang <weikangchia@gmail.com>
2016-10-04 17:34:30 +0800

    Add new model Priority

--- src/main/java/tars/model/task/Priority.java
+++ src/main/java/tars/model/task/Priority.java
@@ -1,8 +1,43 @@
 package tars.model.task;
 
+import tars.commons.exceptions.IllegalValueException;
+
 /**
  * Represents a Task's priority in tars.
  */
 public class Priority {
+    public static final String MESSAGE_PRIORITY_CONSTRAINTS = "Task priority should be h / m / l";
+    public static final String PRIORITY_VALIDATION_REGEX = "[\\p{Lower} ]+";
+    
+    private String priorityLevel;
+    
+    /**
+     * Validates given task priority level.
+     *
+     * @throws IllegalValueException if given priority level string is invalid.
+     */
+    public Priority(String priorityLevel) throws IllegalValueException {
+        assert priorityLevel != null;
+        priorityLevel = priorityLevel.trim();
+        if (!isValidPriorityLevel(priorityLevel)) {
+            throw new IllegalValueException(MESSAGE_PRIORITY_CONSTRAINTS);
+        }
+        this.priorityLevel = priorityLevel;
+    }
+    
+    /**
+     * Returns true if a given string is a valid task priority level.
+     */
+    public static boolean isValidPriorityLevel(String level) {
+        return level.matches(PRIORITY_VALIDATION_REGEX);
+    }
 
+    public String getLevel() {
+        return priorityLevel;
+    }
+
+    public void setLevel(String priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+    
 }
