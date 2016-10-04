c3e13d6453db8cf043acaf4e7c2d5c5809a5910f
Wei Kang <weikangchia@gmail.com>
2016-10-05 02:19:02 +0800

    Update back the correct version

--- src/main/java/tars/commons/events/model/TarsChangedEvent.java
+++ src/main/java/tars/commons/events/model/TarsChangedEvent.java
@@ -14,6 +14,6 @@ public class TarsChangedEvent extends BaseEvent {
 
     @Override
     public String toString() {
-        return "number of tasks " + data.getTaskList().size() + ", number of tags " + 0;
+        return "number of tasks " + data.getTaskList().size() + ", number of tags " + data.getTagList().size();
     }
 }
