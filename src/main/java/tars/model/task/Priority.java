d7bd90de1055e7bcc44c2e7530a84501f91cf905
Wei Kang <weikangchia@gmail.com>
2016-10-04 18:18:00 +0800

    Add toString method

--- src/main/java/tars/model/task/Priority.java
+++ src/main/java/tars/model/task/Priority.java
@@ -8,13 +8,22 @@ import tars.commons.exceptions.IllegalValueException;
 public class Priority {
     public static final String MESSAGE_PRIORITY_CONSTRAINTS = "Task priority should be h / m / l";
     public static final String PRIORITY_VALIDATION_REGEX = "[\\p{Lower} ]+";
-    
+
+    public static final String PRIORITY_HIGH = "high";
+    public static final String PRIORITY_MEDIUM = "medium";
+    public static final String PRIORITY_LOW = "low";
+
+    public static final String PRIORITY_H = "h";
+    public static final String PRIORITY_M = "m";
+    public static final String PRIORITY_L = "l";
+
     private String priorityLevel;
-    
+
     /**
      * Validates given task priority level.
      *
-     * @throws IllegalValueException if given priority level string is invalid.
+     * @throws IllegalValueException
+     *             if given priority level string is invalid.
      */
     public Priority(String priorityLevel) throws IllegalValueException {
         assert priorityLevel != null;
@@ -24,7 +33,7 @@ public class Priority {
         }
         this.priorityLevel = priorityLevel;
     }
-    
+
     /**
      * Returns true if a given string is a valid task priority level.
      */
@@ -32,12 +41,27 @@ public class Priority {
         return level.matches(PRIORITY_VALIDATION_REGEX);
     }
 
-    public String getLevel() {
-        return priorityLevel;
+    @Override
+    public String toString() {
+        String level = "";
+
+        switch (priorityLevel) {
+        case PRIORITY_H:
+            level = PRIORITY_HIGH;
+            break;
+        case PRIORITY_M:
+            level = PRIORITY_MEDIUM;
+            break;
+        case PRIORITY_L:
+            level = PRIORITY_LOW;
+            break;
+        }
+
+        return level;
     }
 
     public void setLevel(String priorityLevel) {
         this.priorityLevel = priorityLevel;
     }
-    
+
 }
