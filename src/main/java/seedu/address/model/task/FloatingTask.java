e1183d8bc46bc6bbd310036ade88608abee43cf4
Sriram Sami <frizensami@gmail.com>
2016-10-05 01:17:39 +0800

    Add FavoritableTask as a possible interface, implement in FloatingTask

--- src/main/java/seedu/address/model/task/FloatingTask.java
+++ src/main/java/seedu/address/model/task/FloatingTask.java
@@ -3,6 +3,21 @@ package seedu.address.model.task;
 /*
  * A simple Task implementation that does not have a deadline
  */
-public class FloatingTask extends Task {
+public class FloatingTask extends Task implements FavoritableTask {
+
+	private boolean isFavorite = false;
+	
+	@Override
+	public void setIsFavorite(boolean isFavorite) {
+		this.isFavorite = isFavorite;
+		
+	}
+
+	@Override
+	public boolean getIsFavorite() {
+		return isFavorite;
+	}
+
+	
 	
 }
