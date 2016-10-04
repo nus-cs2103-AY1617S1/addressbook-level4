f99a533b30442b1ed8a42f6ba81cb8c981506cd6
Aloisius Stephen <aloisius.s@u.nus.edu>
2016-10-05 00:59:25 +0800

    Rename Addressbook to Lifekeeper

--- src/main/java/seedu/address/commons/util/AppUtil.java
+++ src/main/java/seedu/address/commons/util/AppUtil.java
@@ -1,7 +1,7 @@
 package seedu.address.commons.util;
 
 import javafx.scene.image.Image;
-import seedu.address.MainApp;
+import seedu.lifekeeper.MainApp;
 
 /**
  * A container for App specific utility functions
