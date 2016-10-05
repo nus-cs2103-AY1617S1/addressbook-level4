152e4158613ce12b132126229108d12749a7cbc6
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-05 22:33:00 +0800

    fix tests

--- src/test/java/seedu/todo/logic/parser/TodoParserTest.java
+++ src/test/java/seedu/todo/logic/parser/TodoParserTest.java
@@ -4,53 +4,56 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TodoParserTest {
+import seedu.todo.commons.exceptions.IllegalValueException;
 
+public class TodoParserTest {
+    private ParserContract parser = new TodoParser();
+    
     @Test
-    public void testParse() {
+    public void testParse() throws IllegalValueException {
         ParseResult p;
         
-        p = new TodoParser("hello");
+        p = parser.parse("hello");
         assertEquals("hello", p.getComand());
         
-        p = new TodoParser("HeLLo");
+        p = parser.parse("HeLLo");
         assertEquals("hello", p.getComand());
         
-        p = new TodoParser("HELLO");
+        p = parser.parse("HELLO");
         assertEquals("hello", p.getComand());
         
-        p = new TodoParser("hello world");
+        p = parser.parse("hello world");
         assertEquals("hello", p.getComand());
     }
     
     @Test
-    public void testPositionalArgument() {
+    public void testPositionalArgument() throws IllegalValueException {
         ParseResult p;
         
-        p = new TodoParser("hello world");
+        p = parser.parse("hello world");
         assertEquals("world", p.getPositionalArgument().get());
         
-        p = new TodoParser("hello one two three");
+        p = parser.parse("hello one two three");
         assertEquals("one two three", p.getPositionalArgument().get());
     }
     
     @Test
-    public void testNamedArguments() {
+    public void testNamedArguments() throws IllegalValueException {
         ParseResult p;
         
-        p = new TodoParser("hello -f");
+        p = parser.parse("hello -f");
         assertEquals(1, p.getNamedArguments().size());
         assertTrue(p.getNamedArguments().containsKey("f"));
         
-        p = new TodoParser("hello -f Hello");
+        p = parser.parse("hello -f Hello");
         assertEquals(1, p.getNamedArguments().size());
         assertEquals("Hello", p.getNamedArguments().get("f"));
         
-        p = new TodoParser("hello --all Hello");
+        p = parser.parse("hello --all Hello");
         assertEquals(1, p.getNamedArguments().size());
         assertEquals("Hello", p.getNamedArguments().get("all"));
         
-        p = new TodoParser("hello -f Hello -p --all");
+        p = parser.parse("hello -f Hello -p --all");
         assertEquals(3, p.getNamedArguments().size());
         assertEquals("Hello", p.getNamedArguments().get("f"));
         assertTrue(p.getNamedArguments().containsKey("p"));
@@ -58,21 +61,21 @@ public class TodoParserTest {
     }
     
     @Test
-    public void testInvalidFlags() {
+    public void testInvalidFlags() throws IllegalValueException {
         ParseResult p;
         
-        p = new TodoParser("hello");
+        p = parser.parse("hello");
         assertFalse(p.getPositionalArgument().isPresent());
         assertEquals(0, p.getNamedArguments().size());
         
-        p = new TodoParser("hello --");
+        p = parser.parse("hello --");
         assertTrue(p.getPositionalArgument().isPresent());
         assertEquals(0, p.getNamedArguments().size());
         
-        p = new TodoParser("hello --a");
+        p = parser.parse("hello --a");
         assertEquals(0, p.getNamedArguments().size());
         
-        p = new TodoParser("hello -all");
+        p = parser.parse("hello -all");
         assertEquals(0, p.getNamedArguments().size());
     }
 
