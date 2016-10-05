c82c0c10b08c6172d6f50b3110f341dc58ffbed7
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-05 22:08:07 +0800

    add validateArguments function for custom validation logic

--- src/main/java/seedu/todo/logic/commands/BaseCommand.java
+++ src/main/java/seedu/todo/logic/commands/BaseCommand.java
@@ -5,11 +5,33 @@ import java.util.Map.Entry;
 import seedu.todo.commons.exceptions.IllegalValueException;
 import seedu.todo.logic.arguments.Parameter;
 import seedu.todo.logic.parser.ParseResult;
+import seedu.todo.model.TodoModel;
 
+/**
+ * The base class for commands. All commands need to implement an execute function 
+ * and a getArguments function that collects the command arguments for the use of 
+ * the help command. 
+ * 
+ * To perform additional validation on incoming arguments, override the validateArguments 
+ * function.
+ *
+ */
 public abstract class BaseCommand {
+    protected TodoModel model;
+    
     abstract protected Parameter[] getArguments();
     
-    abstract public void execute();
+    abstract public void execute() throws IllegalValueException;
+    
+    /**
+     * Hook allowing subclasses to implement their own validation logic for arguments
+     */
+    protected void validateArguments() throws IllegalValueException { 
+    }
+    
+    public void setModel(TodoModel model) {
+        this.model = model;
+    }
     
     public void setArguments(ParseResult parser) throws IllegalValueException {
         if (parser.getPositionalArgument().isPresent()) {
@@ -21,6 +43,7 @@ public abstract class BaseCommand {
         }
         
         checkRequiredArguments();
+        validateArguments();
     }
     
     private void setPositionalArgument(String argument) throws IllegalValueException {
