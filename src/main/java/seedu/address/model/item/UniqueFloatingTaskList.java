918e40fe18638e875204e5f7c7afe0b7785c09ad
linhan <yonglin12@hotmail.com>
2016-10-05 20:56:36 +0800

    Missed out committing 2 files? They suddenly show up in my source tree

---
+++ src/main/java/seedu/address/model/item/UniqueFloatingTaskList.java
@@ -0,0 +1,99 @@
+package seedu.address.model.item;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.model.person.Person;
+import seedu.address.commons.exceptions.DuplicateDataException;
+
+import java.util.*;
+
+/**
+ * A list of persons that enforces uniqueness between its elements and does not allow nulls.
+ *
+ * Supports a minimal set of list operations.
+ *
+ * @see Person#equals(Object)
+ * @see CollectionUtil#elementsAreUnique(Collection)
+ */
+public class UniqueFloatingTaskList implements Iterable<FloatingTask> {
+
+    /**
+     * Signals that an operation would have violated the 'no duplicates' property of the list.
+     */
+    public static class DuplicateFloatingTaskException extends DuplicateDataException {
+        protected DuplicateFloatingTaskException() {
+            super("Operation would result in duplicate floating tasks");
+        }
+    }
+
+    /**
+     * Signals that an operation targeting a specified person in the list would fail because
+     * there is no such matching person in the list.
+     */
+    public static class FloatingTaskNotFoundException extends Exception {}
+
+    private final ObservableList<FloatingTask> internalList = FXCollections.observableArrayList();
+
+    /**
+     * Constructs empty PersonList.
+     */
+    public UniqueFloatingTaskList() {}
+
+    /**
+     * Returns true if the list contains an equivalent person as the given argument.
+     */
+    public boolean contains(ReadOnlyFloatingTask toCheck) {
+        assert toCheck != null;
+        return internalList.contains(toCheck);
+    }
+
+    /**
+     * Adds a person to the list.
+     *
+     * @throws DuplicatePersonException if the person to add is a duplicate of an existing person in the list.
+     */
+    public void add(FloatingTask toAdd) throws DuplicateFloatingTaskException {
+        assert toAdd != null;
+        if (contains(toAdd)) {
+            throw new DuplicateFloatingTaskException();
+        }
+        internalList.add(toAdd);
+    }
+
+    /**
+     * Removes the equivalent person from the list.
+     *
+     * @throws PersonNotFoundException if no such person could be found in the list.
+     */
+    public boolean remove(ReadOnlyFloatingTask toRemove) throws FloatingTaskNotFoundException {
+        assert toRemove != null;
+        final boolean personFoundAndDeleted = internalList.remove(toRemove);
+        if (!personFoundAndDeleted) {
+            throw new FloatingTaskNotFoundException();
+        }
+        return personFoundAndDeleted;
+    }
+
+    public ObservableList<FloatingTask> getInternalList() {
+        return internalList;
+    }
+
+    @Override
+    public Iterator<FloatingTask> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof UniqueFloatingTaskList // instanceof handles nulls
+                && this.internalList.equals(
+                ((UniqueFloatingTaskList) other).internalList));
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+}
