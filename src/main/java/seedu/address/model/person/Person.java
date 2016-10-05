4efbacc720bea4affd3d5305576e6bf8c86c1299
Zavier\ZavierOng <zacohjayjay@gmail.com>
2016-10-05 20:08:16 +0800

    change all parameters of phone, email, address to date, start, end respectively

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -1,32 +1,32 @@
-package seedu.address.model.person;
+package seedu.end.model.person;
 
-import seedu.address.commons.util.CollectionUtil;
-import seedu.address.model.tag.UniqueTagList;
+import seedu.end.commons.util.CollectionUtil;
+import seedu.end.model.tag.UniqueTagList;
 
 import java.util.Objects;
 
 /**
- * Represents a Person in the address book.
+ * Represents a Person in the end book.
  * Guarantees: details are present and not null, field values are validated.
  */
-public class Person implements ReadOnlyPerson {
+public class Task implements ReadOnlyPerson {
 
     private Name name;
-    private Phone phone;
-    private Email email;
-    private Address address;
+    private Date date;
+    private Start start;
+    private End end;
 
     private UniqueTagList tags;
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, UniqueTagList tags) {
-        assert !CollectionUtil.isAnyNull(name, phone, email, address, tags);
+    public Person(Name name, Date date, Start start, End end, UniqueTagList tags) {
+        assert !CollectionUtil.isAnyNull(name, date, start, end, tags);
         this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.address = address;
+        this.date = date;
+        this.start = start;
+        this.end = end;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
     }
 
@@ -34,7 +34,7 @@ public class Person implements ReadOnlyPerson {
      * Copy constructor.
      */
     public Person(ReadOnlyPerson source) {
-        this(source.getName(), source.getPhone(), source.getEmail(), source.getAddress(), source.getTags());
+        this(source.getName(), source.getDate(), source.getStart(), source.getEnd(), source.getTags());
     }
 
     @Override
@@ -43,18 +43,18 @@ public class Person implements ReadOnlyPerson {
     }
 
     @Override
-    public Phone getPhone() {
-        return phone;
+    public Date getDate() {
+        return date;
     }
 
     @Override
-    public Email getEmail() {
-        return email;
+    public Start getStart() {
+        return start;
     }
 
     @Override
-    public Address getAddress() {
-        return address;
+    public End getEnd() {
+        return end;
     }
 
     @Override
@@ -79,7 +79,7 @@ public class Person implements ReadOnlyPerson {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name, date, start, end, tags);
     }
 
     @Override
