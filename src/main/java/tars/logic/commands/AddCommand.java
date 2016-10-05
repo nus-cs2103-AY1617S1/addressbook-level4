fcf29353d3da91194d390da9ab353ff1005a5b13
Calvin Yang <calvinyang98@hotmail.com>
2016-10-05 18:20:39 +0800

    Modify AddCommand.java

--- src/main/java/tars/logic/commands/AddCommand.java
+++ src/main/java/tars/logic/commands/AddCommand.java
@@ -18,7 +18,7 @@ public class AddCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a task to tars. "
             + "Parameters: NAME -dt DATETIME -p PRIORITY [t/TAG]...\n"
             + "Example: " + COMMAND_WORD
-            + " cs2103 project meeting -dt 5/9/2016 1400 to 5/9/2015 2200 -p h -t project";
+            + " cs2103 project meeting -dt 05/09/2016 1400 to 06/09/2016 2200 -p h -t project";
 
     public static final String MESSAGE_SUCCESS = "New task added: %1$s";
     public static final String MESSAGE_DUPLICATE_TASK= "This task already exists in tars";
