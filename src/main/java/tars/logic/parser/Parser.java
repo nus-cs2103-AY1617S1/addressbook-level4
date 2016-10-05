7915b8a763cc331ae8c6fa738f6d4be9d8fb8db4
Wei Kang <weikangchia@gmail.com>
2016-10-05 16:09:22 +0800

    Update parser Change date format

--- src/main/java/tars/logic/parser/Parser.java
+++ src/main/java/tars/logic/parser/Parser.java
@@ -36,10 +36,9 @@ public class Parser {
             Pattern.compile("(?<keywords>\\S+(?:\\s+\\S+)*)"); // one or more keywords separated by whitespace
     
     private static final Pattern TASK_DATA_ARGS_FORMAT = // '/' forward slashes are reserved for delimiter prefixes
-            Pattern.compile("(?<name>[^/]+)"
-                    + " (?<datetime>(-dt ([01]\\d|2[0-3])?[0-5]\\d (0?[1-9]|[12][0-9]|3[01])[//](0?[1-9]|1[012])[//]\\d{4})"
-                    + "|(-dt ([01]\\d|2[0-3])?[0-5]\\d (0?[1-9]|[12][0-9]|3[01])[//](0?[1-9]|1[012])[//]\\d{4} "
-                    + "to ([01]\\d|2[0-3])?[0-5]\\d (0?[1-9]|[12][0-9]|3[01])[//](0?[1-9]|1[012])[//]\\d{4})) "
+            Pattern.compile("(?<name>[^/]+) (?<datetime>(-dt (0?[1-9]|[12][0-9]|3[01])[//](0?[1-9]|1[012])[//]\\d{4} ([01]\\d|2[0-3])?[0-5]\\d)"
+                    + "|(-dt (0?[1-9]|[12][0-9]|3[01])[//](0?[1-9]|1[012])[//]\\d{4} ([01]\\d|2[0-3])?[0-5]\\d "
+                    + "to (0?[1-9]|[12][0-9]|3[01])[//](0?[1-9]|1[012])[//]\\d{4} ([01]\\d|2[0-3])?[0-5]\\d)) "
                     + "(?<priority>-p [hml])" 
                     + "(?<tagArguments>(?: -t [^/]+)*)"); // variable number of tags
 
