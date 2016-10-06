4dc53c806fc0fd836339ba3ee66bcf459362a7c2
myteo <mingyiteo@gmail.com>
2016-10-06 22:30:39 +0800

    Rename Address App to Daily Planner

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -45,7 +45,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing AddressBook ]===========================");
+        logger.info("=============================[ Initializing DailyPlanner ]===========================");
         super.init();
 
         config = initConfig(getApplicationParameter("config"));
@@ -159,13 +159,13 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting AddressBook " + MainApp.VERSION);
+        logger.info("Starting DailyPlanner " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
     @Override
     public void stop() {
-        logger.info("============================ [ Stopping Address Book ] =============================");
+        logger.info("============================ [ Stopping Daily Planner ] =============================");
         ui.stop();
         try {
             storage.saveUserPrefs(userPrefs);
