6ac87fae1c0093ec05b5340a7ff7c64bd58696dc
clarencechee <clarencechee94@gmail.com>
2016-10-06 23:04:36 +0800

    Refactor AddCommand to fit Jimi

--- src/main/java/seedu/jimi/logic/commands/AddCommand.java
+++ src/main/java/seedu/jimi/logic/commands/AddCommand.java
@@ -9,19 +9,19 @@ import seedu.jimi.model.tag.UniqueTagList;
 import seedu.jimi.model.task.*;
 
 /**
- * Adds a person to the address book.
+ * Adds a task to the Jimi.
  */
 public class AddCommand extends Command {
 
     public static final String COMMAND_WORD = "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a task to Jimi. "
             + "Parameters: NAME p/PHONE e/EMAIL a/ADDRESS  [t/TAG]...\n"
             + "Example: " + COMMAND_WORD
             + " John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
 
-    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+    public static final String MESSAGE_SUCCESS = "New task added: %1$s";
+    public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in Jimi";
 
     private final Task toAdd;
 
@@ -52,7 +52,7 @@ public class AddCommand extends Command {
             model.addTask(toAdd);
             return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
         } catch (UniqueTaskList.DuplicatePersonException e) {
-            return new CommandResult(MESSAGE_DUPLICATE_PERSON);
+            return new CommandResult(MESSAGE_DUPLICATE_TASK);
         }
 
     }
