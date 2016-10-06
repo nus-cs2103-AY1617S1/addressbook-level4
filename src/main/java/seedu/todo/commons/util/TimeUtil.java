59fecb2cf796756a0156aa7fed1cacbe88d329fc
Xien Dong Wang <xdrawks@gmail.com>
2016-10-06 13:07:11 +0800

    Add TimeUtil Class with the accompanying Test class

---
+++ src/main/java/seedu/todo/commons/util/TimeUtil.java
@@ -0,0 +1,8 @@
+package seedu.todo.commons.util;
+
+/**
+ * Utility methods that deals with time.
+ */
+public class TimeUtil {
+    
+}
---
+++ src/test/java/seedu/todo/commons/util/TimeUtilTest.java
@@ -0,0 +1,9 @@
+package seedu.todo.commons.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimeUtilTest {
+
+}
