7ab71bd4263cd446755efded6f7286814c95ab48
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-07 01:30:32 +0800

    tab to spaces Damn you Sublime Text!

--- docs/build/convertor.py
+++ docs/build/convertor.py
@@ -17,19 +17,19 @@ md = markdown.Markdown(extensions=[
 ])
 
 with open(path.join(base, "template.html"), encoding="utf-8") as f:
-	template = f.read()
+    template = f.read()
 
 for filename, title in files.items():
-	input_path = path.join(output_dir, filename + ".md")
-	output_path = path.join(output_dir, filename + ".html")
+    input_path = path.join(output_dir, filename + ".md")
+    output_path = path.join(output_dir, filename + ".html")
 
-	with open(input_path, encoding="utf-8") as input:
-		source = input.read()
+    with open(input_path, encoding="utf-8") as input:
+        source = input.read()
 
-	html = md.reset().convert(source)
-	output_content = template.format(title, html)
+    html = md.reset().convert(source)
+    output_content = template.format(title, html)
 
-	with open(output_path, mode="w", encoding="utf-8") as output:
-		output.write(output_content)
+    with open(output_path, mode="w", encoding="utf-8") as output:
+        output.write(output_content)
 
-	print("Converting {} to {}".format(input_path, output_path))
+    print("Converting {} to {}".format(input_path, output_path))
