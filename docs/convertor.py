3b9cd5a170262fc047a29c7c7e2fb4ab3374c54a
Kai <falconets25601@gmail.com>
2016-10-06 14:15:33 +0800

    Add headers programatically

--- docs/convertor.py
+++ docs/convertor.py
@@ -8,6 +8,22 @@ extensions = [
 	'markdown.extensions.sane_lists',
 ]
 
+HEADER = """<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link rel="stylesheet" type="text/css" href="normalize.css">
+  <link rel="stylesheet" type="text/css" href="skeleton.css">
+</head>
+<body>
+<div class="container">
+"""
+END = """
+</div>
+</body>
+</html>
+"""
+
 input_file = codecs.open("DeveloperGuide.md", mode="r", encoding="utf-8")
 text = input_file.read()
 html = markdown.markdown(text, extensions=extensions)
@@ -15,4 +31,6 @@ output_file = codecs.open("DeveloperGuide.html", "w",
                           encoding="utf-8",
                           errors="xmlcharrefreplace",
 )
+output_file.write(HEADER)
 output_file.write(html)
+output_file.write(END)
