649a01028ae26ce6f944a518d79b50dc6d160f07
Xien Dong Wang <xdrawks@gmail.com>
2016-10-06 13:15:04 +0800

    Declare printFormattedTime method for TimeUtils with requirements in JavaDocs

--- src/main/java/seedu/todo/commons/util/TimeUtil.java
+++ src/main/java/seedu/todo/commons/util/TimeUtil.java
@@ -1,8 +1,24 @@
 package seedu.todo.commons.util;
 
+import java.time.LocalDateTime;
+
 /**
  * Utility methods that deals with time.
  */
 public class TimeUtil {
+    
+    /**
+     * Gets the formatted time for the UI, where if the user provides:
+     *     endTime only          - parsed as deadline format for tasks
+     *     startTime and endTime - parsed as duration format for events
+     *     any other combination - invalid input
+     * 
+     * @param startTime starting time (optional)
+     * @param endTime ending time (compulsory)
+     * @return a formatted time to be displayed in UI
+     */
+    public static String printFormattedTime(LocalDateTime startTime, LocalDateTime endTime) {
+        return null;
+    }
     
 }
