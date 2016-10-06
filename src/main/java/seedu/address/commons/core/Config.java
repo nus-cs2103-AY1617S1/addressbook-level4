0604e74db0973cbf70e1c8f3739f2f2afb4a7bb4
abellimz <abellim1309@gmail.com>
2016-10-06 17:18:50 +0800

    Change app name, saved filePath and UserGuideUrl to fit Task Manager

--- src/main/java/seedu/address/commons/core/Config.java
+++ src/main/java/seedu/address/commons/core/Config.java
@@ -11,11 +11,11 @@ public class Config {
     public static final String DEFAULT_CONFIG_FILE = "config.json";
 
     // Config values customizable through config file
-    private String appTitle = "Address App";
+    private String appTitle = "Taskle";
     private Level logLevel = Level.INFO;
     private String userPrefsFilePath = "preferences.json";
-    private String addressBookFilePath = "data/addressbook.xml";
-    private String addressBookName = "MyAddressBook";
+    private String taskManagerFilePath = "data/taskmanager.xml";
+    private String taskManagerName = "MyTaskManager";
 
 
     public Config() {
@@ -46,19 +46,19 @@ public class Config {
     }
 
     public String getTaskManagerFilePath() {
-        return addressBookFilePath;
+        return taskManagerFilePath;
     }
 
     public void setAddressBookFilePath(String addressBookFilePath) {
-        this.addressBookFilePath = addressBookFilePath;
+        this.taskManagerFilePath = addressBookFilePath;
     }
 
     public String getTaskManagerName() {
-        return addressBookName;
+        return taskManagerName;
     }
 
     public void setAddressBookName(String addressBookName) {
-        this.addressBookName = addressBookName;
+        this.taskManagerName = addressBookName;
     }
 
 
@@ -76,13 +76,13 @@ public class Config {
         return Objects.equals(appTitle, o.appTitle)
                 && Objects.equals(logLevel, o.logLevel)
                 && Objects.equals(userPrefsFilePath, o.userPrefsFilePath)
-                && Objects.equals(addressBookFilePath, o.addressBookFilePath)
-                && Objects.equals(addressBookName, o.addressBookName);
+                && Objects.equals(taskManagerFilePath, o.taskManagerFilePath)
+                && Objects.equals(taskManagerName, o.taskManagerName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(appTitle, logLevel, userPrefsFilePath, addressBookFilePath, addressBookName);
+        return Objects.hash(appTitle, logLevel, userPrefsFilePath, taskManagerFilePath, taskManagerName);
     }
 
     @Override
@@ -91,8 +91,8 @@ public class Config {
         sb.append("App title : " + appTitle);
         sb.append("\nCurrent log level : " + logLevel);
         sb.append("\nPreference file Location : " + userPrefsFilePath);
-        sb.append("\nLocal data file location : " + addressBookFilePath);
-        sb.append("\nAddressBook name : " + addressBookName);
+        sb.append("\nLocal data file location : " + taskManagerFilePath);
+        sb.append("\nTaskManager name : " + taskManagerName);
         return sb.toString();
     }
 
