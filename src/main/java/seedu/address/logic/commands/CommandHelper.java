6f9ee4d59babdebba6df0d933a1abaa5b65a6b86
Cherlynn\Cherlynn <cherlynn@live.com.sg>
2016-10-07 00:06:39 +0800

    Add convertStringToDate method

--- src/main/java/seedu/address/logic/commands/CommandHelper.java
+++ src/main/java/seedu/address/logic/commands/CommandHelper.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.lang.Exception;
 import org.ocpsoft.prettytime.nlp.PrettyTimeParser;
 //import org.ocpsoft.prettytime.PrettyTime;
 
@@ -10,9 +11,19 @@ import seedu.address.model.task.Recurrence;
 
 public class CommandHelper {
 
+    //TODO what if no valid dates found by parser
     public static List<Date> convertStringToMultipleDates(String dateInString){
         List<Date> dates = new PrettyTimeParser().parse(dateInString);
         return dates;
+    }
+
+    //TODO specify exception
+    public static Date convertStringToDate(String dateInString) throws Exception{
+        List<Date> dates = new PrettyTimeParser().parse(dateInString);
+        if(dates.size() != 1){
+            throw new Exception();
+        }
+        return dates.get(0);
     }
 
     public static Recurrence getRecurrence(String repeatParameter){
