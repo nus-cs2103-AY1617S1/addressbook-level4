5bb728cb1dec5d5f518b03e3f173a534dca7981f
Ken Loh <Kenneth_LJS@Live.com>
2016-10-07 01:11:49 +0800

    Refactor XmlAdaptedPerson to XmlAdaptedTask

--- src/main/java/seedu/oneline/storage/XmlAdaptedTask.java
+++ src/main/java/seedu/oneline/storage/XmlAdaptedTask.java
@@ -13,7 +13,7 @@ import java.util.List;
 /**
  * JAXB-friendly version of the Person.
  */
-public class XmlAdaptedPerson {
+public class XmlAdaptedTask {
 
     @XmlElement(required = true)
     private String name;
@@ -30,7 +30,7 @@ public class XmlAdaptedPerson {
     /**
      * No-arg constructor for JAXB use.
      */
-    public XmlAdaptedPerson() {}
+    public XmlAdaptedTask() {}
 
 
     /**
@@ -38,7 +38,7 @@ public class XmlAdaptedPerson {
      *
      * @param source future changes to this will not affect the created XmlAdaptedPerson
      */
-    public XmlAdaptedPerson(ReadOnlyTask source) {
+    public XmlAdaptedTask(ReadOnlyTask source) {
         name = source.getName().fullName;
         phone = source.getPhone().value;
         email = source.getEmail().value;
--- src/main/java/seedu/oneline/storage/XmlSerializableTaskBook.java
+++ src/main/java/seedu/oneline/storage/XmlSerializableTaskBook.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 public class XmlSerializableTaskBook implements ReadOnlyTaskBook {
 
     @XmlElement
-    private List<XmlAdaptedPerson> persons;
+    private List<XmlAdaptedTask> persons;
     @XmlElement
     private List<Tag> tags;
 
@@ -40,7 +40,7 @@ public class XmlSerializableTaskBook implements ReadOnlyTaskBook {
      * Conversion
      */
     public XmlSerializableTaskBook(ReadOnlyTaskBook src) {
-        persons.addAll(src.getTaskList().stream().map(XmlAdaptedPerson::new).collect(Collectors.toList()));
+        persons.addAll(src.getTaskList().stream().map(XmlAdaptedTask::new).collect(Collectors.toList()));
         tags = src.getTagList();
     }
 
@@ -58,7 +58,7 @@ public class XmlSerializableTaskBook implements ReadOnlyTaskBook {
     @Override
     public UniqueTaskList getUniqueTaskList() {
         UniqueTaskList lists = new UniqueTaskList();
-        for (XmlAdaptedPerson p : persons) {
+        for (XmlAdaptedTask p : persons) {
             try {
                 lists.add(p.toModelType());
             } catch (IllegalValueException e) {
