76e0581ce566240330e60bfca7823a9ab720621c
Cherlynn\Cherlynn <cherlynn@live.com.sg>
2016-10-06 19:13:44 +0800

    Add parse command

--- src/main/java/seedu/address/logic/parser/KeywordParser.java
+++ src/main/java/seedu/address/logic/parser/KeywordParser.java
@@ -5,20 +5,41 @@ import java.util.regex.Pattern;
 
 public class KeywordParser {
 
-    private List<String> keywords;
+    private ArrayList<String> keywords;
+    private String KEYWORD_PARSER_NO_MATCHES = "No matches found for given keywords";
 
     /**
-     * Constructor, input the keywords used to parse Strings
-     * @param keywords
+     * Constructor
+     * @param keywords used to pass strings
      */
     public KeywordParser(String... keywords){
         for(String key: keywords){
             this.keywords.add(key);
         }
     }
-
-    public List<Map<String, String>> parse(){
+    /**
+     * Parses input string arguments using keywords provided at construction
+     * Substring associated with keyword starts after keyword, and before the next keyword or end of line
+     * Keyword and associated substring put in a Map<Keyword, Associated substring>
+     * Returns list of Maps
+     * @param string to be parsed
+     * @return
+     */
+    public List<Map<String, String>> parse(String inputString){
         ArrayList<Map<String,String>> words = new ArrayList<Map<String, String>>();
+        for(int i = 0; i < keywords.size(); i++){
+            String keyword = keywords.get(i);
+            String patternString = keyword + "(?<returnString>[^/]+)";
+            if (i < keywords.size() - 1){
+                patternString = patternString + keywords.get(i + 1);
+            }
+            Pattern pattern = Pattern.compile(patternString);
+            Matcher matcher = pattern.matcher(inputString.trim());
+            if(matcher.matches()){
+                String returnString = matcher.group("returnString");
+            }
+
+        }
 
         return words;
     }
