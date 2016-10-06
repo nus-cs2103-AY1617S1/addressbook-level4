fecb77aaff525b9d5fd2c327f05a584e2ce70da2
cheo1994 <tehcy2@msn.com>
2016-10-06 10:59:24 +0800

    changed ReadOnlyPerson to ReadOnlyTask

--- src/test/java/guitests/AddressBookGuiTest.java
+++ src/test/java/guitests/AddressBookGuiTest.java
@@ -12,6 +12,7 @@ import seedu.address.TestApp;
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.model.AddressBook;
 import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.ReadOnlyTask;
 import seedu.address.testutil.TestUtil;
 import seedu.address.testutil.TypicalTestPersons;
 
@@ -96,7 +97,7 @@ public abstract class AddressBookGuiTest {
     /**
      * Asserts the person shown in the card is same as the given person
      */
-    public void assertMatching(ReadOnlyPerson person, PersonCardHandle card) {
+    public void assertMatching(ReadOnlyTask person, PersonCardHandle card) {
         assertTrue(TestUtil.compareCardAndPerson(card, person));
     }
 
