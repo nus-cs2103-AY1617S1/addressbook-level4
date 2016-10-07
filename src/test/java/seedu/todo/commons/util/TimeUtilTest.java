593979078be7e3620842c5d802af58820b190624
Xien Dong Wang <xdrawks@gmail.com>
2016-10-07 14:25:21 +0800

    Add test case hoursBeforeDeadline in TimeUtilTest

--- src/test/java/seedu/todo/commons/util/TimeUtilTest.java
+++ src/test/java/seedu/todo/commons/util/TimeUtilTest.java
@@ -2,8 +2,19 @@ package seedu.todo.commons.util;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
 import org.junit.Test;
 
 public class TimeUtilTest {
+    
+    @Test
+    public void printFormattedTime_hoursBeforeDeadlines() {
+        for (long hoursLeft = 2; hoursLeft < 24; hoursLeft++) {
+            LocalDateTime inputTime = LocalDateTime.now().plusHours(hoursLeft).plusMinutes(1);
+            String input = TimeUtil.printFormattedTime(null, inputTime);
+            String output = "in " + String.valueOf(hoursLeft) + " hours";
+            assertEquals(input, output);
+        }
+    }
 
 }
