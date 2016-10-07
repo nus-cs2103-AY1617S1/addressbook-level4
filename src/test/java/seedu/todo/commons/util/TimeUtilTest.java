e95b01ddb0c204c0efeca37e7e6140f3297771b3
Xien Dong Wang <xdrawks@gmail.com>
2016-10-07 15:18:05 +0800

    Add nullEndTime test to TimeUtilTest

--- src/test/java/seedu/todo/commons/util/TimeUtilTest.java
+++ src/test/java/seedu/todo/commons/util/TimeUtilTest.java
@@ -7,6 +7,11 @@ import org.junit.Test;
 
 public class TimeUtilTest {
     
+    @Test (expected = AssertionError.class)
+    public void printFormattedTime_nullEndTime() {
+        TimeUtil.printFormattedTime(LocalDateTime.now(), null);
+    }
+    
     @Test
     public void printFormattedTime_hoursBeforeDeadlines() {
         for (long hoursLeft = 2; hoursLeft < 24; hoursLeft++) {
