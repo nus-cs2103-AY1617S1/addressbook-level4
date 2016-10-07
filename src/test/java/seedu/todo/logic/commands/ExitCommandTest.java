89418190284b9bfa16fa238ec8555644bc35ebdd
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-07 19:56:43 +0800

    add test for exit command

---
+++ src/test/java/seedu/todo/logic/commands/ExitCommandTest.java
@@ -0,0 +1,24 @@
+package seedu.todo.logic.commands;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import seedu.todo.commons.events.ui.ExitAppRequestEvent;
+import seedu.todo.commons.exceptions.IllegalValueException;
+import seedu.todo.testutil.EventsCollector;
+
+public class ExitCommandTest extends CommandTest {
+    @Override
+    protected BaseCommand commandUnderTest() {
+        return new ExitCommand();
+    }
+    
+    @Test
+    public void testExecute() throws IllegalValueException {
+        EventsCollector eventCollector = new EventsCollector();
+        execute();
+        assertTrue(eventCollector.get(0) instanceof ExitAppRequestEvent);
+    }
+
+}
