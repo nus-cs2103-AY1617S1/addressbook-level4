066c11fc3247584615265d4e0d277238b2f858ae
liu zhendong <liuzhendong@lius-MacBook-Pro.local>
2016-10-07 21:50:54 +0800

    remove frequency and priority from all the files

--- src/main/java/seedu/ggist/storage/XmlAdaptedTask.java
+++ src/main/java/seedu/ggist/storage/XmlAdaptedTask.java
@@ -23,11 +23,7 @@ public class XmlAdaptedTask {
     private String startTime;
     @XmlElement(required = true)
     private String endTime;
-    @XmlElement(required = true)
-    private String priority;
-    @XmlElement(required = true)
-    private String frequency;
-
+ 
     @XmlElement
     private List<XmlAdaptedTag> tagged = new ArrayList<>();
 
@@ -47,8 +43,6 @@ public class XmlAdaptedTask {
         date = source.getDate().value;
         startTime = source.getStartTime().value;
         endTime = source.getEndTime().value;
-        priority = source.getPriority().value;
-        frequency = source.getFrequency().value;
         tagged = new ArrayList<>();
         for (Tag tag : source.getTags()) {
             tagged.add(new XmlAdaptedTag(tag));
@@ -69,9 +63,7 @@ public class XmlAdaptedTask {
         final Date date = new Date(this.date);
         final Time startTime = new Time(this.startTime);
         final Time endTime = new Time(this.endTime);
-        final Priority priority = new Priority(this.priority);
-        final Frequency frequency = new Frequency(this.frequency);
         final UniqueTagList tags = new UniqueTagList(taskTags);
-        return new Task(taskName, date, startTime, endTime, priority, frequency, tags);
+        return new Task(taskName, date, startTime, endTime, tags);
     }
 }
