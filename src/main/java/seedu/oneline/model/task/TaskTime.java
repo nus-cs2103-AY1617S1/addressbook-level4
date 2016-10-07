7c946d507f67a49eac346594564120c14ec60c83
Ken Loh <Kenneth_LJS@Live.com>
2016-10-07 19:48:43 +0800

    Add placeholder methods for TaskTime class

--- src/main/java/seedu/oneline/model/task/TaskTime.java
+++ src/main/java/seedu/oneline/model/task/TaskTime.java
@@ -1,5 +1,50 @@
 package seedu.oneline.model.task;
 
+import seedu.oneline.commons.exceptions.IllegalValueException;
+
 public class TaskTime {
 
+    public static final String MESSAGE_TASK_TIME_CONSTRAINTS =
+            "Task time should ...";
+
+    public final String value;
+
+    /**
+     * Validates given email.
+     *
+     * @throws IllegalValueException if given email address string is invalid.
+     */
+    public TaskTime(String email) throws IllegalValueException {
+        assert email != null;
+        email = email.trim();
+        if (!isValidTaskTime(email)) {
+            throw new IllegalValueException(MESSAGE_TASK_TIME_CONSTRAINTS);
+        }
+        this.value = email;
+    }
+
+    /**
+     * Returns if a given string is a valid task time.
+     */
+    public static boolean isValidTaskTime(String test) {
+        return true; // TODO
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TaskTime // instanceof handles nulls
+                && true); // state check TODO
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+    
 }
