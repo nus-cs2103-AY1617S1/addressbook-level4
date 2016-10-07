1f2bff20b1723d0b72471bd54c6ed51ff0c4b5d9
Dylan Chew <dylanchew@Dylans-MacBook-Pro-2.local>
2016-10-08 02:34:40 +0800

    add MarkCommand

---
+++ src/main/java/harmony/mastermind/logic/commands/MarkCommand.java
@@ -0,0 +1,48 @@
+package harmony.mastermind.logic.commands;
+
+import harmony.mastermind.commons.core.Messages;
+import harmony.mastermind.commons.core.UnmodifiableObservableList;
+import harmony.mastermind.model.task.ReadOnlyTask;
+import harmony.mastermind.model.task.UniqueTaskList.TaskNotFoundException;
+
+public class MarkCommand extends Command{
+
+    public static final String COMMAND_WORD = "mark";
+
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": mark a task as done "
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_SUCCESS = "%1$s has been archived";
+    public static final String MESSAGE_MARKED_TASK = "%1$s is already marked";
+
+    public final int targetIndex;
+
+    public MarkCommand(int targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+    
+    @Override
+    public CommandResult execute() {
+        UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
+
+        if (lastShownList.size() < targetIndex) {
+            indicateAttemptToExecuteIncorrectCommand();
+            return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        }
+
+        ReadOnlyTask taskToMark = lastShownList.get(targetIndex - 1);
+
+        try {
+            model.markTask(taskToMark);
+        } catch (TaskNotFoundException pnfe) {
+            assert false : "The target task cannot be missing";
+        }
+        
+        return new CommandResult(String.format(MESSAGE_SUCCESS, taskToMark));
+    }
+    
+    
+
+}
---
+++ src/main/java/harmony/mastermind/model/task/ArchiveTaskList.java
@@ -0,0 +1,78 @@
+package harmony.mastermind.model.task;
+
+import java.util.Iterator;
+
+import harmony.mastermind.model.task.UniqueTaskList.TaskNotFoundException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ * A list of completed tasks that does not allow nulls.
+ *
+ * Supports a minimal set of list operations.
+ * 
+ */
+
+public class ArchiveTaskList implements Iterable<ReadOnlyTask> {
+
+    private final ObservableList<ReadOnlyTask> internalList = FXCollections.observableArrayList();
+
+    /**
+     * Constructs empty ArchiveTaskList.
+     */
+    public ArchiveTaskList() {}
+
+    /**
+     * Returns true if the list contains an equivalent task as the given argument.
+     */
+    public boolean contains(ReadOnlyTask toCheck) {
+        assert toCheck != null;
+        return internalList.contains(toCheck);
+    }
+
+    /**
+     * Adds an archived task to the list.
+     */
+    public void add(ReadOnlyTask toAdd) {
+        assert toAdd != null;
+        
+        internalList.add(toAdd);
+    }
+
+    /**
+     * Removes the equivalent task from the list.
+     *
+     * @throws TaskNotFoundException if no such task could be found in the list.
+     */
+    public boolean remove(ReadOnlyTask toRemove) throws TaskNotFoundException {
+        assert toRemove != null;
+        final boolean taskFoundAndDeleted = internalList.remove(toRemove);
+        if (!taskFoundAndDeleted) {
+            throw new TaskNotFoundException();
+        }
+        return taskFoundAndDeleted;
+    }
+
+    public ObservableList<ReadOnlyTask> getInternalList() {
+        return internalList;
+    }
+
+    @Override
+    public Iterator<ReadOnlyTask> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ArchiveTaskList // instanceof handles nulls
+                && this.internalList.equals(
+                ((ArchiveTaskList) other).internalList));
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+
+}
