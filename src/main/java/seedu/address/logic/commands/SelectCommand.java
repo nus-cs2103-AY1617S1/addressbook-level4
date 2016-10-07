c71fe521d1b5ee316f9d64d54dd8b96f6be0b3c3
StylishGoldPen <stylishgoldpen@yahoo.com.sg>
2016-10-07 20:50:19 +0800

    update clear to clr, delete to del and select to sel

--- src/main/java/seedu/address/logic/commands/SelectCommand.java
+++ src/main/java/seedu/address/logic/commands/SelectCommand.java
@@ -13,7 +13,7 @@ public class SelectCommand extends Command {
 
     public final int targetIndex;
 
-    public static final String COMMAND_WORD = "select";
+    public static final String COMMAND_WORD = "sel";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Selects the person identified by the index number used in the last person listing.\n"
