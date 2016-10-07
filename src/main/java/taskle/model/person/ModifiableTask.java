6df398c13564618603fcae22b8fd8b23c56e8868
Zhi Yong <zhiyong.ong@hotmail.com>
2016-10-07 23:26:58 +0800

    Add ModifiableTask.java. For edit command to use to modify the task

---
+++ src/main/java/taskle/model/person/ModifiableTask.java
@@ -0,0 +1,55 @@
+package taskle.model.person;
+
+import taskle.model.tag.UniqueTagList;
+
+/**
+ * A modifiable interface for a Task in the taskmanager.
+ * Implementations should guarantee: details are present and not null, field values are validated.
+ */
+public interface ModifiableTask {
+
+    Name getName();
+    
+    void setName();
+
+    /**
+     * The returned TagList is a deep copy of the internal TagList,
+     * changes on the returned list will not affect the task's internal tags.
+     */
+    UniqueTagList getTags();
+
+    /**
+     * Returns true if both have the same state. (interfaces cannot override .equals)
+     */
+    default boolean isSameStateAs(ReadOnlyTask other) {
+        return other == this // short circuit if same object
+                || (other != null // this is first to avoid NPE below
+                && other.getName().equals(this.getName())); // state checks here onwards
+    }
+
+    /**
+     * Formats the task as text, showing all task details.
+     */
+    default String getAsText() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName())
+                .append(" Tags: ");
+        getTags().forEach(builder::append);
+        return builder.toString();
+    }
+
+    /**
+     * Returns a string representation of this Task's tags
+     */
+    default String tagsString() {
+        final StringBuffer buffer = new StringBuffer();
+        final String separator = ", ";
+        getTags().forEach(tag -> buffer.append(tag).append(separator));
+        if (buffer.length() == 0) {
+            return "";
+        } else {
+            return buffer.substring(0, buffer.length() - separator.length());
+        }
+    }
+    
+}
