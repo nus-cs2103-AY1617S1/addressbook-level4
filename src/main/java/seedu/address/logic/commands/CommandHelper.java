f74dd9c0e48243c4f9e160fe6a8649bc37eaf4ba
Cherlynn\Cherlynn <cherlynn@live.com.sg>
2016-10-08 01:14:28 +0800

    Add test cases, header for convertStringtoMultipleDates

--- src/main/java/seedu/address/logic/commands/CommandHelper.java
+++ src/main/java/seedu/address/logic/commands/CommandHelper.java
@@ -11,12 +11,29 @@ import seedu.address.model.task.Recurrence;
 
 public class CommandHelper {
 
-    //TODO what if no valid dates found by parser
+    /**
+     * Parses date(s) from an input String containing the dates
+     * Dates parsed include time of day, day of week, day of month, month of year, and year
+     * Time of day can be in numerical 12 hours or 24 hours or word form (7pm or 1900 hours or seven in the evening)
+     * Day of week can be in short or full (fri or friday)
+     * Day of month can be in numerical or english word form (13 or thirteenth)
+     * Month of year can only be in word form, and can be in short(january or jan, but not 1)
+     * Year must be in numerical form, and put after month and day
+     * @param dateInString
+     * @return List of dates parsed from dateInString
+     */
     public static List<Date> convertStringToMultipleDates(String dateInString){
         List<Date> dates = new PrettyTimeParser().parse(dateInString);
         return dates;
     }
 
+    /**
+     * Like convertStringToMultipleDates(String dateInString), but only returns one Date
+     * Throws exception if multiple dates can be parsed from one dateInString
+     * @param dateInString
+     * @return Date parsed from dateInString
+     * @throws Exception
+     */
     //TODO specify exception
     public static Date convertStringToDate(String dateInString) throws Exception{
         List<Date> dates = new PrettyTimeParser().parse(dateInString);
