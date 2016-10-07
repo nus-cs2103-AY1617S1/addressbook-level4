1d357cf8d8005e3c5d2b9a1d9289e7c6e77bb7c0
Zhi Yong <zhiyong.ong@hotmail.com>
2016-10-08 02:10:06 +0800

    Refined ModifiableTaskManager to get uniqueTag and uniqueTask lists

--- src/main/java/taskle/model/ModifiableTaskManager.java
+++ src/main/java/taskle/model/ModifiableTaskManager.java
@@ -3,9 +3,16 @@ package taskle.model;
 import java.util.List;
 
 import taskle.model.person.ModifiableTask;
+import taskle.model.person.UniqueTaskList;
 import taskle.model.tag.Tag;
+import taskle.model.tag.UniqueTagList;
 
 public interface ModifiableTaskManager {
+    
+    UniqueTagList getModifiableUniqueTagList();
+
+    UniqueTaskList getModifiableUniqueTaskList();
+    
     /**
      * Returns an unmodifiable view of tasks list
      */
