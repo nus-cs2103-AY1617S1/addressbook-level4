29c459182b26387fd256bdc84a1b25303808d17d
Irvin Lim <irvinlimweiquan@gmail.com>
2016-10-08 20:57:52 +0800

    Restore intialization of config, logging and events

--- src/main/java/seedu/todo/MainApp.java
+++ src/main/java/seedu/todo/MainApp.java
@@ -4,19 +4,17 @@ import com.google.common.eventbus.Subscribe;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
-import seedu.todo.commons.core.Config;
 import seedu.todo.commons.core.EventsCenter;
-import seedu.todo.commons.core.LogsCenter;
-import seedu.todo.commons.core.Version;
-import seedu.todo.commons.events.ui.ExitAppRequestEvent;
+import seedu.todo.ui.UiManager;
+import seedu.todo.commons.core.Config;
 import seedu.todo.commons.exceptions.DataConversionException;
 import seedu.todo.commons.util.ConfigUtil;
 import seedu.todo.commons.util.StringUtil;
-import seedu.todo.logic.Logic;
+import seedu.todo.commons.core.LogsCenter;
+import seedu.todo.commons.core.Version;
+import seedu.todo.commons.events.ui.ExitAppRequestEvent;
 import seedu.todo.ui.Ui;
-import seedu.todo.ui.UiManager;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
@@ -31,17 +29,30 @@ public class MainApp extends Application {
     public static final Version VERSION = new Version(1, 0, 0, true);
 
     protected Ui ui;
+    protected Config config;
 
     public MainApp() {}
 
     @Override
     public void init() throws Exception {
-
+        super.init();
+        
+        // Initialize config from config file, or create a new one.
+        config = initConfig(getApplicationParameter("config"));
+        
+        // Initialize logging
+        initLogging(config);
+        
+        // Initialize events center
+        initEventsCenter();
+        
+        // Initialize UI
+        ui = new UiManager(config);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        
+    	ui.start(primaryStage);
     }
 
     @Override
@@ -50,12 +61,71 @@ public class MainApp extends Application {
         Platform.exit();
         System.exit(0);
     }
+    
+    /** ================== UTILS ====================== **/
+
+    /**
+     * Gets command-line parameter by name.
+     * 
+     * @param parameterName   Name of parameter
+     * @return   Value of parameter
+     */
+    private String getApplicationParameter(String parameterName){
+        Map<String, String> applicationParameters = getParameters().getNamed();
+        return applicationParameters.get(parameterName);
+    }
+    
+    /** ================== INITIALIZATION ====================== **/
+
+    private void initLogging(Config config) {
+        LogsCenter.init(config);
+    }
+
+    private Config initConfig(String configFilePath) {
+        Config initializedConfig;
+        String configFilePathUsed;
+
+        configFilePathUsed = Config.DEFAULT_CONFIG_FILE;
+
+        if(configFilePath != null) {
+            logger.info("Custom Config file specified " + configFilePath);
+            configFilePathUsed = configFilePath;
+        }
+
+        logger.info("Using config file : " + configFilePathUsed);
+
+        try {
+            Optional<Config> configOptional = ConfigUtil.readConfig(configFilePathUsed);
+            initializedConfig = configOptional.orElse(new Config());
+        } catch (DataConversionException e) {
+            logger.warning("Config file at " + configFilePathUsed + " is not in the correct format. " +
+                    "Using default config properties");
+            initializedConfig = new Config();
+        }
+
+        // Update config file in case it was missing to begin with or there are new/unused fields
+        try {
+            ConfigUtil.saveConfig(initializedConfig, configFilePathUsed);
+        } catch (IOException e) {
+            logger.warning("Failed to save config file : " + StringUtil.getDetails(e));
+        }
+        
+        return initializedConfig;
+    }
+
+    private void initEventsCenter() {
+        EventsCenter.getInstance().registerHandler(this);
+    }
+    
+    /** ================== SUBSCRIPTIONS ====================== **/
 
     @Subscribe
     public void handleExitAppRequestEvent(ExitAppRequestEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
         this.stop();
     }
+    
+    /** ================== MAIN METHOD ====================== **/
 
     public static void main(String[] args) {
         launch(args);
