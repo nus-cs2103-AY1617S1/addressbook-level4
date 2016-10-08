2c10d9072aa5f409d7a7f744935de6f51a07a432
Henry\Henry <e0003801@u.nus.edu>
2016-10-08 10:34:33 +0800

    Add AddCommandParser, intended to seperate parsing responsbilities from AddCommand (Currently WIP) Add MasterParser, intended to replace Parser. Parser will still exist until migration is complete. (Currently WIP)

---
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser;
+
+import java.util.regex.Pattern;
+
+import seedu.address.logic.commands.AddCommand;
+
+public class AddCommandParser extends CommandParser<AddCommand> {
+    private static final String HEADER = "add";
+    private static final String READABLE_FORMAT = "add TASK_NAME [s/START_DATE] [st/START_TIME] " +
+            "[e/END_DATE] [et/END_TIME] [l/LOCATION] [p/PRIORITY_LEVEL] [r/RECURRING_TYPE] " +
+            "[n/NUMBER_OF_RECURRENCE] [c/CATEGORY] [d/DESCRIPTION]";
+    private static final Pattern REGEX_FORMAT = Pattern.compile("add\\s+(?<TASK_NAME>[^/]+)\\s+" +
+            "((s/(?<START_DATE>[^/]+)(?!.*\\ss/)\\s+)" +
+            "|(st/(?<START_TIME>[^/]+)(?!.*\\sst/)\\s+)" +
+            "){0,10}");
+    
+    @Override
+    protected String getHeader() {
+        return HEADER;
+    }
+
+    @Override
+    protected String getRequiredFormat() {
+        return READABLE_FORMAT;
+    }
+
+    @Override
+    protected AddCommand parse(String commandText) throws ParseException {
+        
+    }
+
+}
---
+++ src/main/java/seedu/address/logic/parser/MasterParser.java
@@ -0,0 +1,46 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.IncorrectCommand;
+
+public class MasterCommandParser {
+    private static final Pattern BASIC_COMMAND_FORMAT = 
+            Pattern.compile("(?<header>\\S+).*");
+    
+    private final Map<String, CommandParser<? extends Command>> commandParsers;
+    
+    public MasterCommandParser() {
+        this.commandParsers = new HashMap<String, CommandParser<? extends Command>>();
+    }
+
+    public Command parse(String userInput) {
+        final String trimmedUserInput = userInput.trim();
+        final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(trimmedUserInput);
+        if (!matcher.matches()) {
+            return new IncorrectCommand(String.format(
+                    MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
+        }
+        
+        final String header = matcher.group("header");
+        CommandParser<? extends Command> parser = commandParsers.get(header);
+        if (parser == null) {
+            return new IncorrectCommand(MESSAGE_UNKNOWN_COMMAND);
+        }
+        
+        try {
+            return parser.parse(trimmedUserInput);
+        } catch (ParseException pe) {
+            return new IncorrectCommand(String.format(
+                    MESSAGE_INVALID_COMMAND_FORMAT, parser.getRequiredFormat()));
+        }
+    }
+}
