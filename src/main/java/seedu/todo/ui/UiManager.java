141130ff837fa376d1fbe4b1ae1ae10b02dd68fe
Irvin Lim <irvinlimweiquan@gmail.com>
2016-10-08 21:08:19 +0800

    Change UiManager method signature

--- src/main/java/seedu/todo/ui/UiManager.java
+++ src/main/java/seedu/todo/ui/UiManager.java
@@ -1,20 +1,10 @@
 package seedu.todo.ui;
 
-import com.google.common.eventbus.Subscribe;
-import javafx.application.Platform;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import seedu.todo.MainApp;
 import seedu.todo.commons.core.ComponentManager;
 import seedu.todo.commons.core.Config;
 import seedu.todo.commons.core.LogsCenter;
-import seedu.todo.commons.events.storage.DataSavingExceptionEvent;
-import seedu.todo.commons.events.ui.JumpToListRequestEvent;
-import seedu.todo.commons.events.ui.ShowHelpRequestEvent;
-import seedu.todo.commons.util.StringUtil;
-import seedu.todo.logic.Logic;
+import seedu.todo.ui.utils.ImageUtil;
 
 import java.util.logging.Logger;
 
@@ -25,13 +15,11 @@ public class UiManager extends ComponentManager implements Ui {
     private static final Logger logger = LogsCenter.getLogger(UiManager.class);
     private static final String ICON_APPLICATION = "/images/address_book_32.png";
 
-    private Logic logic;
     private Config config;
     private MainWindow mainWindow;
 
-    public UiManager(Logic logic, Config config) {
+    public UiManager(Config config) {
         super();
-        this.logic = logic;
         this.config = config;
     }
 
@@ -39,6 +27,9 @@ public class UiManager extends ComponentManager implements Ui {
     public void start(Stage primaryStage) {
         logger.info("Starting UI...");
         primaryStage.setTitle(config.getAppTitle());
+
+        // Set the application icon.
+        primaryStage.getIcons().add(ImageUtil.getImage(ICON_APPLICATION));
     }
 
     @Override
