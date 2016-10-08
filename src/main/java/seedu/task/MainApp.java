41e7508a43b1531e7ad2e1a9b0821121a4621881
TiankaiChen <tiankai.chen@gmail.com>
2016-10-08 14:35:13 +0800

    Morph Commons Package and MainApp (#18)

--- src/main/java/seedu/task/MainApp.java
+++ src/main/java/seedu/task/MainApp.java
@@ -45,7 +45,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing AddressBook ]===========================");
+        logger.info("=============================[ Initializing TaskBook ]===========================");
         super.init();
 
         config = initConfig(getApplicationParameter("config"));
@@ -75,14 +75,14 @@ public class MainApp extends Application {
         try {
             taskBookOptional = storage.readTaskBook();
             if(!taskBookOptional.isPresent()){
-                logger.info("Data file not found. Will be starting with an empty AddressBook");
+                logger.info("Data file not found. Will be starting with an empty TaskBook");
             }
             initialData = taskBookOptional.orElse(new TaskBook());
         } catch (DataConversionException e) {
-            logger.warning("Data file not in the correct format. Will be starting with an empty AddressBook");
+            logger.warning("Data file not in the correct format. Will be starting with an empty TaskBook");
             initialData = new TaskBook();
         } catch (FileNotFoundException e) {
-            logger.warning("Problem while reading from the file. . Will be starting with an empty AddressBook");
+            logger.warning("Problem while reading from the file. . Will be starting with an empty TaskBook");
             initialData = new TaskBook();
         }
 
@@ -139,7 +139,7 @@ public class MainApp extends Application {
                     "Using default user prefs");
             initializedPrefs = new UserPrefs();
         } catch (IOException e) {
-            logger.warning("Problem while reading from the file. . Will be starting with an empty AddressBook");
+            logger.warning("Problem while reading from the file. . Will be starting with an empty TaskBook");
             initializedPrefs = new UserPrefs();
         }
 
@@ -159,13 +159,13 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting AddressBook " + MainApp.VERSION);
+        logger.info("Starting TaskBook " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
     @Override
     public void stop() {
-        logger.info("============================ [ Stopping Address Book ] =============================");
+        logger.info("============================ [ Stopping Task Book ] =============================");
         ui.stop();
         try {
             storage.saveUserPrefs(userPrefs);
--- src/main/java/seedu/task/commons/events/model/TaskBookChangedEvent.java
+++ src/main/java/seedu/task/commons/events/model/TaskBookChangedEvent.java
@@ -3,7 +3,7 @@ package seedu.task.commons.events.model;
 import seedu.task.commons.events.BaseEvent;
 import seedu.task.model.ReadOnlyTaskBook;
 
-/** Indicates the AddressBook in the model has changed*/
+/** Indicates the TaskBook in the model has changed*/
 public class TaskBookChangedEvent extends BaseEvent {
 
     public final ReadOnlyTaskBook data;
