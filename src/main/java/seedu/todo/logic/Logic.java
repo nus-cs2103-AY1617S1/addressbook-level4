a9e0e5da88cbf139c89d739504f965ff823c1685
louietyj <louietyj@gmail.com>
2016-10-08 18:51:23 +0800

    Remove damith.

--- src/main/java/seedu/todo/logic/Logic.java
+++ src/main/java/seedu/todo/logic/Logic.java
@@ -1,21 +1,10 @@
 package seedu.todo.logic;
 
-import javafx.collections.ObservableList;
-import seedu.todo.logic.commands.CommandResult;
-import seedu.todo.model.person.ReadOnlyPerson;
 
 /**
  * API of the Logic component
  */
 public interface Logic {
-    /**
-     * Executes the command and returns the result.
-     * @param commandText The command as entered by the user.
-     * @return the result of the command execution.
-     */
-    CommandResult execute(String commandText);
-
-    /** Returns the filtered list of persons */
-    ObservableList<ReadOnlyPerson> getFilteredPersonList();
+    
 
 }
