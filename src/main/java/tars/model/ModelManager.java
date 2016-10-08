46952345796604888b0f46a96deb7d80b65b6412
Johnervan Lee <johnervan@gmail.com>
2016-10-08 22:29:15 +0800

    Edit find command from a "OR" search to a "AND" search

--- src/main/java/tars/model/ModelManager.java
+++ src/main/java/tars/model/ModelManager.java
@@ -140,8 +140,7 @@ public class ModelManager extends ComponentManager implements Model {
         public boolean run(ReadOnlyTask task) {
             return nameKeyWords.stream()
                     .filter(keyword -> StringUtil.containsIgnoreCase(task.getName().taskName, keyword))
-                    .findAny()
-                    .isPresent();
+                    .count() == nameKeyWords.size();
         }
 
         @Override
