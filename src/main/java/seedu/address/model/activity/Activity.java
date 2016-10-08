ff37aee48280ef5d0a138061ef6011a0fca1802d
Lee Han Cheng <lhchchc@gmail.com>
2016-10-08 17:55:24 +0800

    add and implement ReadOnlyActivity interface

--- src/main/java/seedu/address/model/activity/Activity.java
+++ src/main/java/seedu/address/model/activity/Activity.java
@@ -1,10 +1,21 @@
 package seedu.address.model.activity;
 
-public class Activity {
+public class Activity implements ReadOnlyActivity {
 	public String name;
 	
 	public Activity(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Copy constructor
+	 */
+	public Activity(ReadOnlyActivity source) {
+	    this(source.getName());
+	}
+	
+	@Override
+	public String getName() {
+	    return name;
+	}
 }
