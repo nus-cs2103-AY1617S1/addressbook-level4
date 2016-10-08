6336cf70c348c42f52ca2e96878007f6e42ab2d7
Xien Dong Wang <xdrawks@gmail.com>
2016-10-08 18:23:34 +0800

    Remove white space

--- src/test/java/seedu/todo/commons/util/TimeUtilTest.java
+++ src/test/java/seedu/todo/commons/util/TimeUtilTest.java
@@ -142,7 +142,7 @@ public class TimeUtilTest {
             String expectedOutput = "in " + minutesLeft + " minutes";
             
             LocalDateTime dueTime1 = LocalDateTime.of(2016, Month.MARCH, 20, 12, 00, 00);
-            LocalDateTime currentTime1 = dueTime1.minusMinutes(minutesLeft);            
+            LocalDateTime currentTime1 = dueTime1.minusMinutes(minutesLeft);
             TimeUtil timeUtil1 = new ModifiedTimeUtil(currentTime1);
             String generatedOutput1 = timeUtil1.getTaskDeadlineText(dueTime1);
             assertEquals(expectedOutput, generatedOutput1);
