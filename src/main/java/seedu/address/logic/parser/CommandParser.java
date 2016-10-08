f46d9a839218fe1611907546b72921b447c8b27a
Henry\Henry <e0003801@u.nus.edu>
2016-10-08 09:55:58 +0800

    Add getRequiredFormat() to CommandParser abstract class Add further clarity to documentation of CommandParser class

--- src/main/java/seedu/address/logic/parser/CommandParser.java
+++ src/main/java/seedu/address/logic/parser/CommandParser.java
@@ -29,20 +29,31 @@ public abstract class CommandParser<T extends Command> {
      * CommandParser the command text will be dispatched to. Subclasses should
      * implement this method to indicate their header so that any command text that
      * starts with such a header will be given to them. This method should always 
-     * return the same value, and must not return null or empty string.
+     * return the same value, must return a string as a single token with no whitespaces 
+     * in between, and must not return null or empty string.
      * 
      * @return header string
      */
     protected abstract String getHeader();
     
     /**
+     * Gets the human-readable format of the string required by this command parser to 
+     * parse correctly. Subclasses should implement this method to provide the above described.
+     * This method should not return null, and should return the same value every time.
+     * 
+     * @return a human-readable format required by this parser
+     */
+    protected abstract String getRequiredFormat();
+    
+    /**
      * Parses the command text and returns the resulting command built from the parse.
      * Subclasses should implement this method to do their parsing. If the command text
      * is not as what they expect, subclasses should throw a ParseException.
      * 
-     * @param commandText the command text
+     * @param commandText the full command text, including the header
      * @return the resulting command object which can then be executed
      * @throws ParseException if the commandText has an incorrect syntax
      */
     protected abstract T parse(String commandText) throws ParseException;
+    
 }
