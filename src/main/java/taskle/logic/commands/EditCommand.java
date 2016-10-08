f91fff4b799e124d7b1123836c9da197f45875c9
Zhi Yong <zhiyong.ong@hotmail.com>
2016-10-08 12:03:50 +0800

    Edit EditCommand.java to display changes on UI

--- src/main/java/taskle/logic/commands/EditCommand.java
+++ src/main/java/taskle/logic/commands/EditCommand.java
@@ -1,5 +1,14 @@
 package taskle.logic.commands;
 
+import taskle.commons.core.Messages;
+import taskle.commons.core.ModifiableObservableList;
+import taskle.commons.core.UnmodifiableObservableList;
+import taskle.commons.exceptions.IllegalValueException;
+import taskle.model.person.ModifiableTask;
+import taskle.model.person.Name;
+import taskle.model.person.ReadOnlyTask;
+import taskle.model.person.UniqueTaskList.TaskNotFoundException;
+
 /**
  * Edits a task identified using it's last displayed index from the task manager.
  */
@@ -9,23 +18,37 @@ public class EditCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Edits the Task identified by the index number used in the last Task listing.\n"
-            + "Format: edit task_number\n"
+            + "Format: edit task_number new_task\n"
             + "Example: " + COMMAND_WORD + " 1 Buy dinner";
 
-    public static final String MESSAGE_DELETE_TASK_SUCCESS = "Removed Task: %1$s";
+    public static final String MESSAGE_EDIT_TASK_SUCCESS = "Edited Task: %1$s";
 
     public final int targetIndex;
     
-    public final String newName;
+    public final Name newName;
     
-    public EditCommand(int targetIndex, String newName) {
+    public EditCommand(int targetIndex, String newName) throws IllegalValueException {
         this.targetIndex = targetIndex;
-        this.newName = newName;
+        this.newName = new Name(newName);
     }
+    
     @Override
     public CommandResult execute() {
-        // TODO Auto-generated method stub
-        return null;
+        ModifiableObservableList<ModifiableTask> lastShownList = model.getModifiableTaskList();
+        if (lastShownList.size() < targetIndex) {
+            indicateAttemptToExecuteIncorrectCommand();
+            return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        }
+
+        ModifiableTask taskToEdit = lastShownList.get(targetIndex - 1);
+
+        try {
+            model.editTask(taskToEdit, newName);
+        } catch (TaskNotFoundException pnfe) {
+            assert false : "The target task cannot be missing";
+        }
+
+        return new CommandResult(String.format(MESSAGE_EDIT_TASK_SUCCESS, taskToEdit));
     }
 
 }
