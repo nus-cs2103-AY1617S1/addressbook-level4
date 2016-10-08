bd72b520d828b4eb03b6306d43b0051770e0a853
Gary Goh <ggjy03@gmail.com>
2016-10-09 01:11:32 +0800

    more renaming of method getFilteredPersonList() to getFilteredTaskList()

--- src/main/java/seedu/unburden/ui/MainWindow.java
+++ src/main/java/seedu/unburden/ui/MainWindow.java
@@ -109,7 +109,7 @@ public class MainWindow extends UiPart {
 
     void fillInnerParts() {
         browserPanel = BrowserPanel.load(browserPlaceholder);
-        taskListPanel = TaskListPanel.load(primaryStage, getPersonListPlaceholder(), logic.getFilteredPersonList());
+        taskListPanel = TaskListPanel.load(primaryStage, getPersonListPlaceholder(), logic.getFilteredTaskList());
         resultDisplay = ResultDisplay.load(primaryStage, getResultDisplayPlaceholder());
         statusBarFooter = StatusBarFooter.load(primaryStage, getStatusbarPlaceholder(), config.getAddressBookFilePath());
         commandBox = CommandBox.load(primaryStage, getCommandBoxPlaceholder(), resultDisplay, logic);
