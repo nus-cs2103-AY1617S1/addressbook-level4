d1bfdad927284fec13b272a33e584d01e46a399d
li-kai <falconets25601@hotmail.com>
2016-10-08 21:28:50 +0800

    Add storage manager test

--- src/test/java/seedu/todo/storage/StorageManagerTest.java
+++ src/test/java/seedu/todo/storage/StorageManagerTest.java
@@ -1,34 +1,35 @@
 package seedu.todo.storage;
 
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
-import seedu.todo.model.UserPrefs;
-import seedu.todo.storage.StorageManager;
 
-import static junit.framework.TestCase.assertNotNull;
-import static org.junit.Assert.assertEquals;
+import seedu.todo.model.UserPrefs;
 
 public class StorageManagerTest {
 
     private StorageManager storageManager;
 
     @Rule
-    public TemporaryFolder testFolder = new TemporaryFolder();
+    public ExpectedException thrown = ExpectedException.none();
 
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
 
     @Before
     public void setup() {
         storageManager = new StorageManager(getTempFilePath("ab"), getTempFilePath("prefs"));
     }
 
-
     private String getTempFilePath(String fileName) {
         return testFolder.getRoot().getPath() + fileName;
     }
-
 
     /*
      * Note: This is an integration test that verifies the StorageManager is properly wired to the
@@ -46,7 +47,23 @@ public class StorageManagerTest {
     }
 
     @Test
-    public void getAddressBookFilePath(){
+    public void getTodoListFilePath() {
         assertNotNull(storageManager.getTodoListFilePath());
+    }
+
+    @Test
+    public void testSaveTodoListNull() {
+        thrown.expect(AssertionError.class);
+        storageManager.saveTodoList(null);
+    }
+
+    @Test
+    public void testReadEmptyTodoList() {
+        assertFalse(storageManager.readTodoList().isPresent());
+    }
+
+    @Test
+    public void testReadWrongPathTodoList() {
+        assertFalse(storageManager.readTodoList("test").isPresent());
     }
 }
