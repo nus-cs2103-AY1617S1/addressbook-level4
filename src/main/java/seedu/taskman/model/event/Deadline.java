2412b4805324b460e977340855fde164d946c291
Jia Yee <jiayeerawr@gmail.com>
2016-10-09 14:29:19 +0800

    Update DEADLINE_VALIDATION_REGEX
    ((tdy|tmr)|(((this)|(next))?\s(mon|tue|wed|thu|fri|sat|sun)))\s(((0|1)[0-9])|(2[0-3]))([0-5][0-9])
    
    [this/next] tdy/tmr/mon/tue/wed/thu/fri/sat/sun HHMM
    
    // Examples: tdy 2359, tmr 0000, mon 0400, this tue 1600, next thu 2200

--- src/main/java/seedu/taskman/model/event/Deadline.java
+++ src/main/java/seedu/taskman/model/event/Deadline.java
@@ -8,13 +8,15 @@ import seedu.taskman.commons.exceptions.IllegalValueException;
  */
 public class Deadline {
 
-    public static final String MESSAGE_DEADLINE_CONSTRAINTS = "Task deadline should only contain numbers";
-    public static final String DEADLINE_VALIDATION_REGEX = "\\d+";
+    public static final String MESSAGE_DEADLINE_CONSTRAINTS = "Task deadline should only contain dates and times in the format: [this/next] tdy/tmr/mon/tue/wed/thu/fri/sat/sun HHMM.";
+    public static final String DEADLINE_VALIDATION_REGEX = "((tdy|tmr)|(((this)|(next))?\\s(mon|tue|wed|thu|fri|sat|sun)))\\s(((0|1)[0-9])|(2[0-3]))([0-5][0-9])";
+    // ((tdy|tmr)|(((this)|(next))?\s(mon|tue|wed|thu|fri|sat|sun)))\s(((0|1)[0-9])|(2[0-3]))([0-5][0-9])
+    // Examples: tdy 2359, tmr 0000, mon 0400, this tue 1600, next thu 2200
 
     public final String value;
 
     /**
-     * Validates given deadline number.
+     * Validates given deadline string.
      *
      * @throws IllegalValueException if given deadline string is invalid.
      */
@@ -31,12 +33,13 @@ public class Deadline {
      * Returns true if a given string is a valid deadline.
      */
     public static boolean isValidDeadline(String test) {
-        // TODO: update validation
+        // TODO: improve on validation, see above
         return test.matches(DEADLINE_VALIDATION_REGEX);
     }
 
     @Override
     public String toString() {
+    	// TODO: check if direct return of original input without processing is OK
         return value;
     }
 
