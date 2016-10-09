3ad094e08df37a5e87ea682e10d810e7b7c65345
JingHe <he1994@hotmail.com>
2016-10-10 01:36:52 +0800

    Change occurrence of addressbook.xml to emeraldo.xml

--- src/test/java/seedu/address/commons/util/ConfigUtilTest.java
+++ src/test/java/seedu/address/commons/util/ConfigUtilTest.java
@@ -79,7 +79,7 @@ public class ConfigUtilTest {
         config.setAppTitle("Typical App Title");
         config.setLogLevel(Level.INFO);
         config.setUserPrefsFilePath("C:\\preferences.json");
-        config.setEmeraldoFilePath("addressbook.xml");
+        config.setEmeraldoFilePath("emeraldo.xml");
         config.setEmeraldoName("TypicalTaskManagerName");
         return config;
     }
