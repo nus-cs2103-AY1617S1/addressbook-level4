5c3cea61e37ee39dce27665b9f0df7f10573a0f7
nginyc <yunchuann@gmail.com>
2016-10-09 14:32:03 +0800

    Fix tests: `AppUtilTest`, `FileUtilTest` and `StringUtilTest`

--- src/test/java/seedu/address/commons/util/FileUtilTest.java
+++ src/test/java/seedu/address/commons/util/FileUtilTest.java
@@ -1,58 +1,65 @@
-//package seedu.address.commons.util;
-//
-//
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.junit.rules.ExpectedException;
-//import seedu.address.testutil.SerializableTestClass;
-//import seedu.address.testutil.TestUtil;
-//
-//import java.io.File;
-//import java.io.IOException;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class FileUtilTest {
-//    private static final File SERIALIZATION_FILE = new File(TestUtil.getFilePathInSandboxFolder("serialize.json"));
-//
-//
-//    @Rule
-//    public ExpectedException thrown = ExpectedException.none();
-//
-//    @Test
-//    public void getPath(){
-//
-//        // valid case
-//        assertEquals("folder" + File.separator + "sub-folder", FileUtil.getPath("folder/sub-folder"));
-//
-//        // null parameter -> assertion failure
-//        thrown.expect(AssertionError.class);
-//        FileUtil.getPath(null);
-//
-//        // no forwards slash -> assertion failure
-//        thrown.expect(AssertionError.class);
-//        FileUtil.getPath("folder");
-//    }
-//
-//    @Test
-//    public void serializeObjectToJsonFile_noExceptionThrown() throws IOException {
-//        SerializableTestClass serializableTestClass = new SerializableTestClass();
-//        serializableTestClass.setTestValues();
-//
-//        FileUtil.serializeObjectToJsonFile(SERIALIZATION_FILE, serializableTestClass);
-//
-//        assertEquals(FileUtil.readFromFile(SERIALIZATION_FILE), SerializableTestClass.JSON_STRING_REPRESENTATION);
-//    }
-//
-//    @Test
-//    public void deserializeObjectFromJsonFile_noExceptionThrown() throws IOException {
-//        FileUtil.writeToFile(SERIALIZATION_FILE, SerializableTestClass.JSON_STRING_REPRESENTATION);
-//
-//        SerializableTestClass serializableTestClass = FileUtil
-//                .deserializeObjectFromJsonFile(SERIALIZATION_FILE, SerializableTestClass.class);
-//
-//        assertEquals(serializableTestClass.getName(), SerializableTestClass.getNameTestValue());
-//        assertEquals(serializableTestClass.getListOfLocalDateTimes(), SerializableTestClass.getListTestValues());
-//        assertEquals(serializableTestClass.getMapOfIntegerToString(), SerializableTestClass.getHashMapTestValues());
-//    }
-//}
+package seedu.address.commons.util;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
+import seedu.address.testutil.SerializableTestClass;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class FileUtilTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void getPath_null(){
+        // null parameter -> assertion failure
+        thrown.expect(AssertionError.class);
+        FileUtil.getPath(null);
+    }
+
+    @Test
+    public void getPath_noForwardSlash(){
+        // no forwards slash -> assertion failure
+        thrown.expect(AssertionError.class);
+        FileUtil.getPath("folder");
+    }
+
+    @Test
+    public void getPath_valid() {
+        // valid case
+        assertEquals("folder" + File.separator + "sub-folder", FileUtil.getPath("folder/sub-folder"));
+    }
+
+    @Test
+    public void serializeObjectToJsonFile_noExceptionThrown() throws IOException {
+        SerializableTestClass serializableTestClass = new SerializableTestClass();
+        serializableTestClass.setTestValues();
+
+        File file = folder.newFile();
+        FileUtil.serializeObjectToJsonFile(file, serializableTestClass);
+
+        assertEquals(FileUtil.readFromFile(file), SerializableTestClass.JSON_STRING_REPRESENTATION);
+    }
+
+    @Test
+    public void deserializeObjectFromJsonFile_noExceptionThrown() throws IOException {
+        File file = folder.newFile();
+
+        FileUtil.writeToFile(file, SerializableTestClass.JSON_STRING_REPRESENTATION);
+
+        SerializableTestClass serializableTestClass = FileUtil
+                .deserializeObjectFromJsonFile(file, SerializableTestClass.class);
+
+        assertEquals(serializableTestClass.getName(), SerializableTestClass.getNameTestValue());
+        assertEquals(serializableTestClass.getListOfLocalDateTimes(), SerializableTestClass.getListTestValues());
+        assertEquals(serializableTestClass.getMapOfIntegerToString(), SerializableTestClass.getHashMapTestValues());
+    }
+}
