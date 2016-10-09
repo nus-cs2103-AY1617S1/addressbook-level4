b20890e2a70b5385a926eb32b71b67f34fa40d77
Zerc <syltaris@gmail.com>
2016-10-09 13:07:18 +0800

    Fix error in EditCommandTest

--- src/test/java/guitests/EditCommandTest.java
+++ src/test/java/guitests/EditCommandTest.java
@@ -58,7 +58,7 @@ public class EditCommandTest extends AddressBookGuiTest{
         
         //confirm the list now contains all previous persons except the deleted person
         //TODO: change from checking last index to previous index of changed task
-        assertTrue(taskListPanel.isListMatching(expectedTask));
+        assertTrue(taskListPanel.isListMatching(currentList));
 
         //confirm the result message is correct
         assertResultMessage(String.format(MESSAGE_DELETE_TASK_SUCCESS, expectedTask));
