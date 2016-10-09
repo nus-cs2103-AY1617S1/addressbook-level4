ef8261302f7df22671a55391931f85d31721ae1a
Xien Dong Wang <xdrawks@gmail.com>
2016-10-09 15:59:34 +0800

    Add helper function setCollapsed

--- src/main/java/seedu/todo/commons/util/FxViewUtil.java
+++ src/main/java/seedu/todo/commons/util/FxViewUtil.java
@@ -14,4 +14,12 @@ public class FxViewUtil {
         AnchorPane.setRightAnchor(node, right);
         AnchorPane.setTopAnchor(node, top);
     }
+    
+    /**
+     * Hides a specified UI element, and ensures that it does not occupy any space.
+     */
+    public static void setCollapsed(Node node, boolean isCollapsed) {
+        node.setVisible(!isCollapsed);
+        node.setManaged(!isCollapsed);
+    }
 }
