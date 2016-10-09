2530f37286c819c878c0e3c47779abe9fa19078d
Xien Dong Wang <xdrawks@gmail.com>
2016-10-09 18:40:53 +0800

    Create new UI element CommandFeedbackView

---
+++ src/main/java/seedu/todo/ui/io/CommandFeedbackView.java
@@ -0,0 +1,20 @@
+package seedu.todo.ui.io;
+
+import javafx.scene.Node;
+import seedu.todo.ui.UiPart;
+
+public class CommandFeedbackView extends UiPart {
+
+    @Override
+    public void setNode(Node node) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String getFxmlPath() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
