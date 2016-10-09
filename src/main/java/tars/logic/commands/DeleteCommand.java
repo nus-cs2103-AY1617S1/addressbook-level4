3d29cd978a92bbbe983d3385cac7fe507544c3fc
jaeoheeail <joelfooet@gmail.com>
2016-10-09 18:12:56 +0800

    Change exception name

--- src/main/java/tars/logic/commands/DeleteCommand.java
+++ src/main/java/tars/logic/commands/DeleteCommand.java
@@ -40,7 +40,7 @@ public class DeleteCommand extends Command {
 
         try {
             model.deleteTask(taskToDelete);
-        } catch (TaskNotFoundException pnfe) {
+        } catch (TaskNotFoundException tnfe) {
             assert false : "The target task cannot be missing";
         }
 
