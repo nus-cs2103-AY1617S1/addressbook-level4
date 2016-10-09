8388f2bdde0624da598e501cc73e3b8af60066c9
Samson <samsontmr@users.noreply.github.com>
2016-10-09 22:20:06 +0800

    Update EditCommand to support editing of tasks

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -2,7 +2,11 @@ package seedu.address.logic.commands;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.UnmodifiableObservableList;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.item.Item;
+import seedu.address.model.item.Name;
 import seedu.address.model.item.ReadOnlyItem;
+import seedu.address.model.item.UniquePersonList;
 import seedu.address.model.item.UniquePersonList.PersonNotFoundException;
 
 /**
@@ -14,18 +18,23 @@ public class EditCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Edits the item identified by the index number used in the last item listing.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + "Parameters: INDEX (must be a positive integer)" + "n/NAME" + "\n"
+            + "Example: " + COMMAND_WORD + " 1" + "buy milk";
 
     public static final String MESSAGE_EDIT_ITEM_SUCCESS = "Edited Item: %1$s";
-
+    
     public final int targetIndex;
+    public final Name newName;
 
     /*
      * Deletes deadline, task, or event by index.
+     *      
+     * @throws IllegalValueException if any of the raw values are invalid
      */
-    public EditCommand(int targetIndex) {
+    public EditCommand(int targetIndex, String name)
+            throws IllegalValueException {
         this.targetIndex = targetIndex;
+        this.newName = new Name(name);
     }
 
     @Override
@@ -38,15 +47,21 @@ public class EditCommand extends Command {
             return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
-        ReadOnlyItem personToDelete = lastShownList.get(targetIndex - 1);
-
+        ReadOnlyItem itemToDelete = lastShownList.get(targetIndex - 1);
+        Item itemToAdd = new Item(itemToDelete);
+        itemToAdd.setName(newName);
         try {
-            model.deleteItem(personToDelete);
+            model.deleteItem(itemToDelete);
         } catch (PersonNotFoundException pnfe) {
             assert false : "The target item cannot be missing";
         }
-
-        return new CommandResult(String.format(MESSAGE_EDIT_ITEM_SUCCESS, personToDelete));
+        
+        try {
+            model.addItem(itemToAdd);
+            return new CommandResult(String.format(MESSAGE_EDIT_ITEM_SUCCESS, itemToAdd));
+        } catch (UniquePersonList.DuplicatePersonException e) {
+            return new CommandResult(MESSAGE_DUPLICATE_ITEM);
+        }
     }
 
 }
