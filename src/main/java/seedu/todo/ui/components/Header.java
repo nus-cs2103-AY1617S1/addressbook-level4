81eab42feef01421bcdfd1d6c136c26a428053ea
Irvin Lim <irvinlimweiquan@gmail.com>
2016-10-09 23:23:09 +0800

    Added nice logo image

--- src/main/java/seedu/todo/ui/components/Header.java
+++ src/main/java/seedu/todo/ui/components/Header.java
@@ -1,11 +1,14 @@
 package seedu.todo.ui.components;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import seedu.todo.commons.util.FxViewUtil;
 
 public class Header extends Component {
 	
+	private static final String LOGO_IMAGE_PATH = "/images/logo-64x64.png";
 	private static final String FXML_PATH = "components/Header.fxml";
 	
 	// Props
@@ -14,6 +17,8 @@ public class Header extends Component {
 	// FXML
 	@FXML
 	private Text headerVersionText;
+	@FXML
+	private ImageView headerLogoImageView;
 
 	@Override
 	public String getFxmlPath() {
@@ -25,7 +30,12 @@ public class Header extends Component {
 		// Makes the Component full width wrt parent container.
 		FxViewUtil.makeFullWidth(this.mainNode);
 		
+		// Sets version number.
 		headerVersionText.setText("version " + versionString);
+		
+		// Sets logo image.
+		Image image = new Image(LOGO_IMAGE_PATH);
+		headerLogoImageView.setImage(image);
 	}
 
 }
