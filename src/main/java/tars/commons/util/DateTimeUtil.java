cde2b21d45aa3d47635b6f1d7064f77d6b2771e3
jaeoheeail <joelfooet@gmail.com>
2016-10-09 18:19:43 +0800

    Extract method into DateTimeUtil.java

---
+++ src/main/java/tars/commons/util/DateTimeUtil.java
@@ -0,0 +1,17 @@
+package tars.commons.util;
+
+public class DateTimeUtil {
+    /**
+     * Extracts the new task's dateTime from the add command's task arguments string.
+     */
+    public static String[] getDateTimeFromArgs(String taskArguments) {
+        if (taskArguments.contains("to")) {
+            int toIndex = taskArguments.indexOf("to");
+            String startDateTime = taskArguments.substring(0, toIndex).trim();
+            String endDateTime = taskArguments.substring(toIndex + 2).trim();
+            return new String[] {startDateTime, endDateTime};
+        } else {
+            return new String[] {null, taskArguments};
+        }
+    }
+}
