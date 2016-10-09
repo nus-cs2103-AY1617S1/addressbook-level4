27a07e0183b41cb6a7c5d936e2252a64e804e388
clarencechee <clarencechee94@gmail.com>
2016-10-09 13:18:49 +0800

    Style: wrap long arguments in FindCommandTest

--- src/test/java/guitests/FindCommandTest.java
+++ src/test/java/guitests/FindCommandTest.java
@@ -13,11 +13,16 @@ public class FindCommandTest extends AddressBookGuiTest {
     @Test
     public void find_nonEmptyList() {
         assertFindResult("find Mark"); //no results
-        assertFindResult("find to", TypicalTestFloatingTasks.airport, TypicalTestFloatingTasks.flight, TypicalTestFloatingTasks.beach); //multiple results
+        assertFindResult("find to", 
+                TypicalTestFloatingTasks.airport, 
+                TypicalTestFloatingTasks.flight,
+                TypicalTestFloatingTasks.beach); //multiple results
         
         //find after deleting one result
         commandBox.runCommand("delete 1");
-        assertFindResult("find to", TypicalTestFloatingTasks.flight, TypicalTestFloatingTasks.beach);
+        assertFindResult("find to", 
+                TypicalTestFloatingTasks.flight, 
+                TypicalTestFloatingTasks.beach);
     }
     
     @Test
