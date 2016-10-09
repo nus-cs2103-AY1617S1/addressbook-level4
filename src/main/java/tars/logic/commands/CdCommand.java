7cebcf61506d1cc9db198339e1378c13e916da71
Johnervan Lee <johnervan@gmail.com>
2016-10-09 11:27:17 +0800

    Change MESSAGE_SUCCESS string to be non-static

--- src/main/java/tars/logic/commands/CdCommand.java
+++ src/main/java/tars/logic/commands/CdCommand.java
@@ -14,18 +14,23 @@ public class CdCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Changes the directory of the "
             + "TARS storage file, tars.xml";
     
-    public static final String MESSAGE_SUCCESS = "Directory of tars.xml changed to: ";
+    public final String MESSAGE_SUCCESS = "Directory of tars.xml changed to: " + this.toString();
     
-    private final String filepath;
+    private final String filePath;
     
     public CdCommand(String filepath) {
-        this.filepath = filepath;
+        this.filePath = filepath;
+    }
+    
+    @Override
+    public String toString() {
+        return this.filePath;
     }
 
     @Override
     public CommandResult execute() {
         // TODO Auto-generated method stub
-        return null;
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 
 }
