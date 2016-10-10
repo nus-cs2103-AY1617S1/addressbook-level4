8a53c0e8baeb8245448498b9ba5e8f928c8dc8f9
Henry\Henry <e0003801@u.nus.edu>
2016-10-10 17:59:21 +0800

    Add ListType enum class Add ListCommandParser for parsing list command. (require integration) Add skeleton ModifyCommandParser for parsing modify command. Add skeleton TaskModelCommandParser abstract class for abstracting similar details for AddCommandParser and ModifyCommandParser

---
+++ src/main/java/seedu/address/logic/parser/TaskModelCommandParser.java
@@ -0,0 +1,7 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.Command;
+
+public abstract class TaskModelCommandParser<T extends Command> extends CommandParser<T> {
+
+}
