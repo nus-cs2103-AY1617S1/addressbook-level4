f3bba267b08658cf085f1704267719e925d3f5a0
Joycelyn Ng <Joycelyn Ng>
2016-10-10 18:24:08 +0800

    rewrite-error-messages-for-commands

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -7,7 +7,12 @@ package seedu.address.logic.commands;
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
-
+    
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all tasks filtered by specified parameters\n"
+            + "Event Parameters: [TASK_TYPE] [done| not-done] [dd-mm-yy] [hh:mm]\n"
+            + "Event Example: " + COMMAND_WORD
+            + " someday not-done\n";  
+    
     public static final String MESSAGE_SUCCESS = "Listed all tasks";
 
     public ListCommand() {}
