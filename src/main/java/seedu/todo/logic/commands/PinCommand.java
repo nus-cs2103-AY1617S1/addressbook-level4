b9828a6bda8dbc831956267059ed24b838575505
cricketer94 <e0011768@u.nus.edu>
2016-10-10 14:45:20 +0800

    Implement Pin Command class

---
+++ src/main/java/seedu/todo/logic/commands/PinCommand.java
@@ -0,0 +1,27 @@
+package seedu.todo.logic.commands;
+
+import seedu.todo.commons.exceptions.IllegalValueException;
+import seedu.todo.logic.arguments.Argument;
+import seedu.todo.logic.arguments.IntArgument;
+import seedu.todo.logic.arguments.Parameter;
+import seedu.todo.model.task.ImmutableTask;
+
+public class PinCommand extends BaseCommand {
+	
+	 private Argument<Integer> index = new IntArgument("index").required();
+
+	@Override
+	protected Parameter[] getArguments() {
+		return new Parameter[]{ index };
+	}
+
+	@Override
+	public void execute() throws IllegalValueException {
+		ImmutableTask toComplete=this.getTaskAt(index.getValue());
+		boolean isPinned=!(toComplete.isPinned());
+		this.model.update(toComplete, task->{task.setPinned(isPinned);});
+		
+
+	}
+
+}
