0d8caf056cec730ab600b78554a09318a1bffc22
Wei Kang <weikangchia@gmail.com>
2016-10-11 00:28:29 +0800

    Add new message for task cannot be found

--- src/main/java/tars/commons/core/Messages.java
+++ src/main/java/tars/commons/core/Messages.java
@@ -10,5 +10,5 @@ public class Messages {
     public static final String MESSAGE_INVALID_DATE = "Invalid date";
     public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d tasks listed!";
-
+    public static final String MESSAGE_TASK_CANNOT_BE_FOUND = "Task cannot be found!";
 }
