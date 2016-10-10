2068adef880bce6b8e42c8fb1b2b463bfe0bdb19
Low Jian Sheng <lowjiansheng@hotmail.sg>
2016-10-10 14:43:32 +0800

    Add checkEventArguments_returnsCorrectArguments. Pass

--- src/test/java/seedu/address/parser/AddParserTest.java
+++ src/test/java/seedu/address/parser/AddParserTest.java
@@ -67,4 +67,15 @@ public class AddParserTest {
 		
 		assertEquals("floatingTask", AddParser.parseCommand(arguments).get(0));
 	}
+	
+	@Test
+	public void checkEventArguments_returnsCorrectArguments(){
+		
+		String arguments = "from: 10-08-2016 1900 to: 11-08-2016 1900";
+		
+		assertEquals("10-08-2016", AddParser.parseCommand(arguments).get(1));
+		assertEquals("1900", AddParser.parseCommand(arguments).get(2));
+		assertEquals("11-08-2016", AddParser.parseCommand(arguments).get(3));
+		assertEquals("1900", AddParser.parseCommand(arguments).get(4));
+	}
 }
