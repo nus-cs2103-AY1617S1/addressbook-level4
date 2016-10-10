0d9316324acfb78ae98af90e2dff25fdcc5ac796
PankajB1997 <pankajbhootra@gmail.com>
2016-10-11 01:44:35 +0800

    Complete UpdateCommand (regex not working, to be fixed)

--- src/main/java/seedu/tasklist/logic/commands/UpdateCommand.java
+++ src/main/java/seedu/tasklist/logic/commands/UpdateCommand.java
@@ -2,6 +2,7 @@ package seedu.tasklist.logic.commands;
 
 import seedu.tasklist.commons.core.Messages;
 import seedu.tasklist.commons.core.UnmodifiableObservableList;
+import seedu.tasklist.commons.exceptions.IllegalValueException;
 import seedu.tasklist.model.task.EndTime;
 import seedu.tasklist.model.task.Priority;
 import seedu.tasklist.model.task.ReadOnlyTask;
@@ -16,50 +17,58 @@ public class UpdateCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Updates the task identified by the index number used in the last task listing.\n"
-            + "Parameters: INDEX [TASKNAME] [at/from [START_TIME][START_DATE]] [to/by [END_TIME][END_DATE]] [p/PRIORITY]\n" + "Example: " + COMMAND_WORD + " 1 at 13/09/2016 5pm";
+            + "Parameters: INDEX [TASKNAME] [at/from [START_TIME][START_DATE]] [to/by [END_TIME][END_DATE]] [p/PRIORITY]\n"
+            + "Example: " + COMMAND_WORD + " 1 at 13/09/2016 5pm";
 
-    
     public static final String MESSAGE_UPDATE_TASK_SUCCESS = "Task successfully updated: %1$s";
-    
-    
+
     private int targetIndex;
     private TaskDetails taskDetails;
     private StartTime startTime;
-//  private StartDate startDate;
+    // private StartDate startDate;
     private EndTime endTime;
-//  private EndDate endDate;
+    // private EndDate endDate;
     private Priority priority;
-    
-    public UpdateCommand(int targetIndex, 
-            TaskDetails taskDetails, 
-            StartTime startTime, 
-            //StartDate startDate,
-            EndTime endTime,
-            //EndDate endDate,
-            Priority priority){
+
+    public UpdateCommand(int targetIndex, String taskDetails, String startTime,
+            // String startDate,
+            String endTime,
+            // String endDate,
+            String priority) throws IllegalValueException {
         this.targetIndex = targetIndex;
-        this.taskDetails = taskDetails;
-        this.startTime = startTime;
-//      this.startDate = startDate;
-        this.endTime = endTime;
-//      this.endDate = endDate;
-        this.priority = priority;
+        if (taskDetails != null)
+            this.taskDetails = new TaskDetails(taskDetails);
+        if (startTime != null)
+            this.startTime = new StartTime(startTime);
+        // if (startDate != null)
+        // this.startDate = new StartDate(startDate);
+        if (endTime != null)
+            this.endTime = new EndTime(endTime);
+        // if (endDate != null)
+        // this.endDate = new EndDate(endDate);
+        if (priority != null)
+            this.priority = new Priority(priority);
     }
-    
+
     @Override
     public CommandResult execute() {
         UnmodifiableObservableList<Task> lastShownList = model.getModifiableTaskList();
-        if(targetIndex >= lastShownList.size() || targetIndex < 1){
+        if (targetIndex >= lastShownList.size() || targetIndex < 1) {
             return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-        }
-        else{
+        } else {
             Task taskToUpdate = lastShownList.get(targetIndex);
-            taskToUpdate.setTaskDetails(taskDetails);
-            taskToUpdate.setStartTime(startTime);
-//          taskToUpdate.setStartDate(startDate);
-            taskToUpdate.setEndTime(endTime);
-//          taskToUpdate.setEndDate(endTime);
-            taskToUpdate.setPriority(priority);
+            if (taskDetails != null)
+                taskToUpdate.setTaskDetails(taskDetails);
+            if (startTime != null)
+                taskToUpdate.setStartTime(startTime);
+            // if (startDate != null)
+            // taskToUpdate.setStartDate(startDate);
+            if (endTime != null)
+                taskToUpdate.setEndTime(endTime);
+            // if (endDate != null)
+            // taskToUpdate.setEndDate(endTime);
+            if (priority != null)
+                taskToUpdate.setPriority(priority);
             return new CommandResult(String.format(MESSAGE_UPDATE_TASK_SUCCESS, taskToUpdate));
         }
     }
