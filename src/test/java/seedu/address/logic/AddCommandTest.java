5e828f30a5c0183d69669e310bb68d5a936c18e5
Seow Weijie <sweijie94@gmail.com>
2016-10-10 15:39:02 +0800

    Remove TaskStub class

--- src/test/java/seedu/address/logic/AddCommandTest.java
+++ src/test/java/seedu/address/logic/AddCommandTest.java
@@ -31,23 +31,4 @@ public class AddCommandTest {
 		assertEquals(details.get(5), test.eventStub.endTime);
 		assertEquals(details.get(6), test.eventStub.notes);
 	}
-	
-	/*@Test
-	public void testIsTaskObjectCreatedSuccessfully() throws IllegalValueException {
-		ArrayList<String> details = new ArrayList<String>();
-		
-		details.add("task");
-		details.add("complete cs2103t");
-		details.add("10-08-2016");
-		details.add("1900");
-		details.add("must complete urgent");
-		
-		AddCommand test = new AddCommand(details);
-		
-		assertEquals(details.get(1), test.taskStub.name);
-		assertEquals(details.get(2), test.taskStub.deadlineDate);
-		assertEquals(details.get(3), test.taskStub.deadlineTime);
-		assertEquals(details.get(4), test.taskStub.notes);
-	}*/
-
 }
