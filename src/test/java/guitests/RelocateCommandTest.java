5959dd767a9d0de6b068e80159a84e754c473403
Wu Zefeng <flamesdesperado@gmail.com>
2016-10-10 15:57:32 +0800

    Added test cases in RelocateCommandTest.

---
+++ src/test/java/guitests/RelocateCommandTest.java
@@ -0,0 +1,27 @@
+package guitests;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.RelocateCommand;
+
+public class RelocateCommandTest extends AddressBookGuiTest {
+    @Test
+    public void invalid_relocate_input() {
+        
+        // Only in format "relocate/to/new/directory".
+        commandBox.runCommand("relocate /new");
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RelocateCommand.MESSAGE_USAGE));
+        
+        commandBox.runCommand("relocate /new/");
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RelocateCommand.MESSAGE_USAGE));
+        
+        commandBox.runCommand("relocate new/");
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RelocateCommand.MESSAGE_USAGE));
+        
+        // Should not append file name at the end.
+        commandBox.runCommand("relocate new/tasklist.xml");
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RelocateCommand.MESSAGE_USAGE));
+    }
+}
