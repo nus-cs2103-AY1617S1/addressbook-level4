036e8b6d03152562030f3da893399a8d57ef26be
Brehmer <Brehmerchan@gmail.com>
2016-10-11 21:17:11 +0800

    modify logic.getFilteredActivityList to logic.getFilteredTaskList

--- src/main/java/seedu/menion/ui/MainWindow.java
+++ src/main/java/seedu/menion/ui/MainWindow.java
@@ -109,7 +109,7 @@ public class MainWindow extends UiPart {
 
     void fillInnerParts() {
         browserPanel = BrowserPanel.load(browserPlaceholder);
-        taskListPanel = TaskListPanel.load(primaryStage, getTaskListPlaceholder(), logic.getFilteredActivityList());
+        taskListPanel = TaskListPanel.load(primaryStage, getTaskListPlaceholder(), logic.getFilteredTaskList());
         resultDisplay = ResultDisplay.load(primaryStage, getResultDisplayPlaceholder());
         statusBarFooter = StatusBarFooter.load(primaryStage, getStatusbarPlaceholder(), config.getActivityManagerFilePath());
         commandBox = CommandBox.load(primaryStage, getCommandBoxPlaceholder(), resultDisplay, logic);
