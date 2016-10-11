823492eeaeaa23f4e861ebea15b5ead8c31c08bc
louietyj <louietyj@gmail.com>
2016-10-11 17:48:45 +0800

    ListController

---
+++ src/main/java/seedu/todo/controllers/ListController.java
@@ -0,0 +1,24 @@
+package seedu.todo.controllers;
+
+import seedu.todo.models.TodoListDB;
+import seedu.todo.ui.views.IndexView;
+
+public class ListController implements Controller {
+
+    @Override
+    public float inputConfidence(String input) {
+        return (input.startsWith("list")) ? 1 : 0;
+    }
+
+    @Override
+    public void process(String input) {
+        TodoListDB db = TodoListDB.getInstance();
+        IndexView indexView = new IndexView();
+        indexView.passInProps(v -> {
+            IndexView view = (IndexView) v;
+            view.tasks = db.getAllTasks();
+            return view;
+        });
+    }
+
+}
