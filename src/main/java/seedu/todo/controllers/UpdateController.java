bc9108b6bf1fb725e42c850ecaa683caa1291f82
louietyj <louietyj@gmail.com>
2016-10-12 02:33:15 +0800

    UpdateController

--- src/main/java/seedu/todo/controllers/UpdateController.java
+++ src/main/java/seedu/todo/controllers/UpdateController.java
@@ -1,5 +1,21 @@
 package seedu.todo.controllers;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.joestelmach.natty.DateGroup;
+import com.joestelmach.natty.Parser;
+
+import seedu.todo.commons.EphemeralDB;
+import seedu.todo.models.Task;
+import seedu.todo.models.TodoListDB;
+import seedu.todo.ui.UiManager;
+import seedu.todo.ui.views.IndexView;
+
 public class UpdateController implements Controller {
 
     @Override
@@ -9,8 +25,46 @@ public class UpdateController implements Controller {
     }
 
     @Override
-    public void process(String input) {
-        // TODO Auto-generated method stub
+    public void process(String args) {
+        // TODO: Example of last minute work
+        
+        args = args.replaceFirst("update", "").trim();
+        
+        // Get index.
+        System.out.println(args);
+        
+        Matcher matcher = Pattern.compile("^\\d+").matcher(args);
+        matcher.find();
+        String indexStr = matcher.group();
+        int index = Integer.decode(indexStr.trim());
+        
+        // Parse name and date.
+        args = args.replaceFirst(indexStr, "").trim();
+        String[] splitted = args.split("( at | by )", 2);
+        String name = splitted[0].trim();
+        String naturalDate = splitted[1].trim();
+        
+        // Parse natural date using Natty.
+        Parser parser = new Parser();
+        List<DateGroup> groups = parser.parse(naturalDate);
+        Date date = groups.get(0).getDates().get(0);
+        LocalDateTime ldt = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        
+        // Get record
+        EphemeralDB edb = EphemeralDB.getInstance();
+        Task task = edb.getTaskByDisplayedId(index);
+        TodoListDB db = TodoListDB.getInstance();
+        if (task != null) {
+            task.setName(name);
+            task.setCalendarDT(ldt);
+            db.save();
+        }
+        
+        // Re-render
+        UiManager ui = UiManager.getInstance();
+        IndexView view = ui.loadView(IndexView.class);
+        view.tasks = db.getAllTasks();
+        view.render();
         
     }
 }
