55cb9dc4bb5dfc451d33f590438f0d73b1543fdf
Nikhil Suresh <nikhilsure@gmail.com>
2016-10-11 15:14:50 +0800

    [ListCommand] Change output of ListTaskCommand to feedback when there are no tasks to list Change output of ListTaskCommand to feedback how many tasks are listed

--- src/main/java/seedu/address/logic/commands/taskcommands/ListTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/taskcommands/ListTaskCommand.java
@@ -9,12 +9,16 @@ public class ListTaskCommand extends TaskCommand {
     
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all tasks";
+    public static final String MESSAGE_SUCCESS = "%1$d tasks listed";
+    public static final String MESSAGE_NOTASKS = "No tasks to list";
 
     @Override
     public CommandResult execute() {
         model.clearTasksFilter();
-        return new CommandResult(MESSAGE_SUCCESS);
+        if(model.getCurrentFilteredTasks().size() == 0) {
+            return new CommandResult(MESSAGE_NOTASKS);
+        }
+        return new CommandResult(String.format(MESSAGE_SUCCESS, model.getCurrentFilteredTasks().size() ));
     }
 
 }
