3e7630e551df7c25e02e1d7f198103d0e4ec68ca
Bernard <bernardified@gmail.com>
2016-10-11 17:33:25 +0800

    no message

--- src/main/java/seedu/ggist/logic/parser/Parser.java
+++ src/main/java/seedu/ggist/logic/parser/Parser.java
@@ -111,7 +111,6 @@ public class Parser {
                      );
                 }
             } else if (taskType.equals("deadlineTask")) {
-                System.out.println("deadline printed");
                 matcher = DEADLINE_TASK_DATA_ARGS_FORMAT.matcher(args.trim());
                 if (matcher.matches()) {
                     return new AddCommand(
@@ -122,7 +121,6 @@ public class Parser {
                      );
                 }
             } else if (taskType.equals("floatingTask")) {
-                System.out.println("floating");
                 matcher = FLOATING_TASK_DATA_ARGS_FORMAT.matcher(args.trim());
                 if (matcher.matches()) {
                     return new AddCommand(
