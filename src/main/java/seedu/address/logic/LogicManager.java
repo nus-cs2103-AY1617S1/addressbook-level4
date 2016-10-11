154304aab9b2fc7062e6d2441bb3d9bfd475d004
joycelynng <ng.joycelyn@dhs.sg>
2016-10-11 17:13:14 +0800

    Update LogicManager.java
    Changed TMReadOnlyTask back to ReadOnlyTask

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -8,7 +8,6 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.parser.Parser;
 import seedu.address.model.Model;
 import seedu.address.model.task.ReadOnlyTask;
-import seedu.address.model.task.TMReadOnlyTask;
 import seedu.address.storage.Storage;
 
 import java.util.logging.Logger;
@@ -36,7 +35,7 @@ public class LogicManager extends ComponentManager implements Logic {
     }
 
     @Override
-    public ObservableList<TMReadOnlyTask> getFilteredTaskList() {
+    public ObservableList<ReadOnlyTask> getFilteredTaskList() {
         return model.getFilteredTaskList();
     }
 }
