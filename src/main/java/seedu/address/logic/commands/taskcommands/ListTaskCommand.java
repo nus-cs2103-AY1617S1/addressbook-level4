f2d8ffce8c5e3f49e2d28cba0008f879001e561e
Nikhil Suresh <nikhilsure@gmail.com>
2016-10-11 11:58:39 +0800

    [Command] Add comments to ListTaskCommand and ListCommandParser

--- src/main/java/seedu/address/logic/commands/taskcommands/ListTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/taskcommands/ListTaskCommand.java
@@ -2,6 +2,9 @@ package seedu.address.logic.commands.taskcommands;
 
 import seedu.address.logic.commands.CommandResult;
 
+/**
+ * Lists all tasks in the TaskManager to the user.
+ */
 public class ListTaskCommand extends TaskCommand {
     
     public static final String COMMAND_WORD = "list";
--- src/main/java/seedu/address/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -3,6 +3,9 @@ package seedu.address.logic.parser;
 import seedu.address.logic.commands.taskcommands.ListTaskCommand;
 import seedu.address.logic.commands.taskcommands.TaskCommand;
 
+/*
+ * Parses the list command
+ */
 public class ListCommandParser extends CommandParser{
     public static final String COMMAND_WORD = ListTaskCommand.COMMAND_WORD;
 
