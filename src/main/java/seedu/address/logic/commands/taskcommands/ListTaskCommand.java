ae8d9cfbaf30c885e10984d1cd43d8e1f6ebf596
Nikhil Suresh <nikhilsure@gmail.com>
2016-10-11 16:49:56 +0800

    [List Command] Minor changes to ListTaskCommand and ListTaskCommandTest

--- src/main/java/seedu/address/logic/commands/taskcommands/ListTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/taskcommands/ListTaskCommand.java
@@ -9,7 +9,7 @@ public class ListTaskCommand extends TaskCommand {
     
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "%1$d tasks listed";
+    public static final String MESSAGE_SUCCESS = "Listed all tasks";
     public static final String MESSAGE_NOTASKS = "No tasks to list";
 
     @Override
@@ -18,7 +18,7 @@ public class ListTaskCommand extends TaskCommand {
         if(model.getCurrentFilteredTasks().size() == 0) {
             return new CommandResult(MESSAGE_NOTASKS);
         }
-        return new CommandResult(String.format(MESSAGE_SUCCESS, model.getCurrentFilteredTasks().size() ));
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 
 }
--- src/test/java/seedu/address/logic/commands/ListTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListTaskCommandTest.java
@@ -2,30 +2,22 @@ package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.logic.commands.taskcommands.AddTaskCommand;
 import seedu.address.logic.commands.taskcommands.ListTaskCommand;
 import seedu.address.model.task.InMemoryTaskList;
-import seedu.address.model.task.TaskManager;
+import seedu.address.testutil.TestUtil;
 
 public class ListTaskCommandTest {
-    
-    InMemoryTaskList model;
-    
-    @Before
-    public void setup() {
-        model = new TaskManager();
-    }
     
     @Test
     public void listCommand_noTasks() {
         /* CommandResult should return a string that denotes that 
          * there are no tasks to list.
          */
-        setupEmptyTaskList();
+        InMemoryTaskList model;
+        model = TestUtil.setupEmptyTaskList();
         ListTaskCommand command = new ListTaskCommand();
         command.setData(model);
         CommandResult result = command.execute();
@@ -38,12 +30,13 @@ public class ListTaskCommandTest {
         /* CommandResult should return a string that denotes that 
          * one task has been listed.
          */
-        setupOneTaskInTaskList();
+        InMemoryTaskList model;
+        model = TestUtil.setupSomeTasksInTaskList(1);
         ListTaskCommand command = new ListTaskCommand();
         command.setData(model);
         CommandResult result = command.execute();
         String feedback = result.feedbackToUser;
-        assertTrue(feedback.equals(String.format(ListTaskCommand.MESSAGE_SUCCESS, 1)));
+        assertTrue(feedback.equals(ListTaskCommand.MESSAGE_SUCCESS));
     }
     
     @Test
@@ -51,39 +44,13 @@ public class ListTaskCommandTest {
         /* CommandResult should return a string that denotes that 
          * one task has been listed.
          */
-        setupSomeTasksInTaskList();
+        InMemoryTaskList model;
+        model = TestUtil.setupSomeTasksInTaskList(3);
         ListTaskCommand command = new ListTaskCommand();
         command.setData(model);
         CommandResult result = command.execute();
         String feedback = result.feedbackToUser;
-        assertTrue(feedback.equals(String.format(ListTaskCommand.MESSAGE_SUCCESS, 3)));
-    }
-    
-    
-    /*
-     * Utility Functions
-     */
-    public void setupEmptyTaskList() {
-        model = new TaskManager();
-    }
-    
-    // Setting up one task in the TaskList in order to list it in the tests
-    public void setupOneTaskInTaskList() throws IllegalValueException {
-        model = new TaskManager();
-        AddTaskCommand command = new AddTaskCommand("test");
-        command.setData(model);
-        command.execute();
-    }
-    
-    // Setting up tasks in the TaskList in order to list them in the tests
-    public void setupSomeTasksInTaskList() throws IllegalValueException {
-        model = new TaskManager();
-        // Add 3 tasks into the task manager
-        for (int i = 0; i < 3; i++) {
-            AddTaskCommand command = new AddTaskCommand(String.format("Task %d", i));
-            command.setData(model);
-            command.execute();
-        }
+        assertTrue(feedback.equals(ListTaskCommand.MESSAGE_SUCCESS));
     }
     
 
