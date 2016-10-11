18fccb3afa34837a30448fc56a53ddf6df045e18
jaeoheeail <joelfooet@gmail.com>
2016-10-11 18:28:03 +0800

    Change prefix to flag

---
+++ src/main/java/tars/commons/flags/Flag.java
@@ -0,0 +1,38 @@
+package tars.commons.flags;
+
+/**
+ * Container for user input prefixes.
+ */
+public class Flag {
+
+    public static final int LENGTH_TWO = 2;
+    public static final int LENGTH_THREE = 3;
+    
+    public static final String NAME = "-n";
+    public static final String PRIORITY = "-p";
+    public static final String DATETIME = "-dt";
+    public static final String TAG = "-t";
+    public static final String ADDTAG = "-ta";
+    public static final String REMOVETAG = "-tr";
+        
+    public String prefix;
+    public boolean hasMultiple;
+    
+    public Flag(String prefix, boolean hasMultiple) {
+        this.prefix = prefix;
+        this.hasMultiple = hasMultiple;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Flag // instanceof handles nulls
+                && this.prefix.equals(((Flag) other).prefix)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return prefix.hashCode();
+    }
+    
+}
