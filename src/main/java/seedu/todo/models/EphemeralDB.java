778af961d829877e4a4838d5d809798a62447393
louietyj <louietyj@gmail.com>
2016-10-11 13:20:00 +0800

    EphemeralDB

---
+++ src/main/java/seedu/todo/models/EphemeralDB.java
@@ -0,0 +1,27 @@
+package seedu.todo.models;
+
+/**
+ * A bit like Redis, essentially a data store for things that should not be
+ * persisted to disk, but should be shared between all modules.
+ * 
+ * All variables should be public. In-place modifications of variables are
+ * encouraged.
+ * 
+ * @author louietyj
+ */
+public class EphemeralDB {
+
+    private static EphemeralDB instance = null;
+
+    protected EphemeralDB() {
+        // Prevent instantiation.
+    }
+
+    public static EphemeralDB getInstance() {
+        if (instance == null) {
+            instance = new EphemeralDB();
+        }
+        return instance;
+    }
+
+}
