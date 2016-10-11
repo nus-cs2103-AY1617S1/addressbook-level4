f030d576f513f07479fa162c583b7f61ee133ae4
Syed Abdullah <syed@syedabdullah.net>
2016-10-11 23:31:26 +0800

    Fix code formatting for UpdateCommand.java

--- src/main/java/seedu/address/logic/commands/UpdateCommand.java
+++ src/main/java/seedu/address/logic/commands/UpdateCommand.java
@@ -14,15 +14,17 @@ import seedu.address.model.task.UniqueTaskList.DuplicateTaskException;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Updates a task in the task list.
+ */
 public class UpdateCommand extends Command {
 
-	public static final String COMMAND_WORD = "update";
+    public static final String COMMAND_WORD = "update";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Update a task in the task list.\n "
             + "Parameters: INDEX (must be a positive integer) NAME \n"
             + "Example: " + COMMAND_WORD
             + " 1 cs2103 t/quiz";
     
-  //TODO: o/OPENTIME c/CLOSETIME i/IMPORTANCE t/tag
     public static final String MESSAGE_UPDATE_TASK_SUCCESS = "Updated Task: %1$s"; 
     
     public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the task list";
@@ -32,9 +34,7 @@ public class UpdateCommand extends Command {
     
     public String task_name;
     
-    public UpdateCommand(int targetIndex,String name,Set<String> tags)
-    		throws IllegalValueException{
-    	
+    public UpdateCommand(int targetIndex,String name,Set<String> tags) throws IllegalValueException{ 
         this.targetIndex = targetIndex;
         
         final Set<Tag> tagSet = new HashSet<>();
@@ -42,44 +42,31 @@ public class UpdateCommand extends Command {
             tagSet.add(new Tag(tagName));
         }
        
-         this.toUpdate = new Task(
+        this.toUpdate = new Task(
                 new Name(name),
-                //new DateTime(openTime),
-                //new DateTime(closeTime),
-                //new Boolean(importance),
-               new UniqueTagList(tagSet)
+                new UniqueTagList(tagSet)
         );
-        
-        
-       
     }
     
     @Override
     public CommandResult execute() {
-
-    	UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
+        UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
 
         if (lastShownList.size() < targetIndex) {
             indicateAttemptToExecuteIncorrectCommand();
             return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
         }
+
         ReadOnlyTask taskToUpdate = lastShownList.get(targetIndex - 1);
         
-		assert model != null;
-		try {
-			
-			
-			model.updateTask(taskToUpdate, toUpdate);
-			
-		} catch (DuplicateTaskException e) {
-			return new CommandResult(MESSAGE_DUPLICATE_TASK);
-		}catch (TaskNotFoundException pnfe) {
-			assert false : "The target task cannot be missing";
-		} 
-		return new CommandResult(String.format(MESSAGE_UPDATE_TASK_SUCCESS, toUpdate));
-        
-        
+        assert model != null;
+        try {
+            model.updateTask(taskToUpdate, toUpdate);
+        } catch (DuplicateTaskException e) {
+            return new CommandResult(MESSAGE_DUPLICATE_TASK);
+        } catch (TaskNotFoundException pnfe) {
+            assert false : "The target task cannot be missing";
+        } 
+        return new CommandResult(String.format(MESSAGE_UPDATE_TASK_SUCCESS, toUpdate));
     }
-    
-	
 }
