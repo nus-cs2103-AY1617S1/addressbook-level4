e0ed17ab60c1ee1f1bfca14987e76d572289cc6c
fionnatong <e0006117@u.nus.edu>
2016-10-11 19:45:11 +0800

    Introduce ShowCommand

---
+++ src/main/java/seedu/tasklist/logic/commands/ShowCommand.java
@@ -0,0 +1,27 @@
+package seedu.tasklist.logic.commands;
+
+/**
+ * Shows all tasks that fulfill the category keyword.
+ * Keyword matching is case insensitive.
+ */
+public class ShowCommand extends Command {
+
+    public static final String COMMAND_WORD = "find";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows all tasks under the requested category. "
+            + "The specified keywords (case-insensitive) are shown as a list with index numbers.\n"
+            + "Parameters: KEYWORD (all, complete, p/[PRIORITY]\n"
+            + "Example: " + COMMAND_WORD + " all";
+
+    public static final String MESSAGE_FIND_TASK_FAILURE = "No such task was found.";
+    private final String keyword;
+    
+    public ShowCommand(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Override
+    public CommandResult execute() {
+        return null;
+    }
+}
