e0540323e6909ab878d11ff63ff897c21b9be9ce
ShadowLoner17 <weiyinchong@hotmail.sg>
2016-10-12 16:36:35 +0800

    Create SaveAsCommand class

---
+++ src/main/java/seedu/jimi/logic/commands/SaveAsCommand.java
@@ -0,0 +1,28 @@
+package seedu.jimi.logic.commands;
+
+/**
+ *  
+ * @author Wei Yin 
+ * 
+ * Sets save directory for the tasks and events in Jimi.
+ */
+public class SaveAsCommand extends Command {
+
+    public static final String COMMAND_WORD = "saveas";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Set a new save directory for all your tasks and events in Jimi.\n"
+            + "Parameters: FILEPATH/FILENAME.xml \n"
+            + "Example: " + COMMAND_WORD
+            + " saveas C:/dropbox/taskbook.xml";
+
+    public static final String MESSAGE_SUCCESS = "Save directory changed: %1$s";
+    public static final String MESSAGE_DUPLICATE_TASK = "This save directory is originally used in Jimi";
+
+
+    @Override
+    public CommandResult execute() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
