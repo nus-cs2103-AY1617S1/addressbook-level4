be71994272627aeb04b4fecd40b2305b07ff757d
zerc <syltaris@gmail.com>
2016-10-13 01:02:29 +0800

    AgendaPanel: fix errors

--- src/main/java/seedu/jimi/ui/MainWindow.java
+++ src/main/java/seedu/jimi/ui/MainWindow.java
@@ -108,7 +108,7 @@ public class MainWindow extends UiPart {
     }
 
     void fillInnerParts() {
-        //agendaPanel = AgendaPanel.load(primaryStage, getAgendaPlaceholder(), logic.getFilteredTaskList());
+        agendaPanel = AgendaPanel.load(primaryStage, getAgendaPlaceholder(), logic.getFilteredTaskList());
         taskListPanel = TaskListPanel.load(primaryStage, getTaskListPlaceholder(), logic.getFilteredTaskList());
         resultDisplay = ResultDisplay.load(primaryStage, getResultDisplayPlaceholder());
         statusBarFooter = StatusBarFooter.load(primaryStage, getStatusbarPlaceholder(), config.getAddressBookFilePath());
