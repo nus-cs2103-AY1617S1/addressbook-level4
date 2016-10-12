116f7a758199d5a3a653050449ca4b00d81e5e21
zerc <syltaris@gmail.com>
2016-10-12 20:37:33 +0800

    Create AgendaPanel class

---
+++ src/main/java/seedu/jimi/ui/AgendaPanel.java
@@ -0,0 +1,19 @@
+package seedu.jimi.ui;
+
+import javafx.scene.Node;
+
+public class AgendaPanel extends UiPart{
+
+    @Override
+    public void setNode(Node node) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String getFxmlPath() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
