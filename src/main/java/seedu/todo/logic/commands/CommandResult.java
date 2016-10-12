c5b07cb69294473ec65ad10b3b1584f87d83940f
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-12 11:39:35 +0800

    update command result object

--- src/main/java/seedu/todo/logic/commands/CommandResult.java
+++ src/main/java/seedu/todo/logic/commands/CommandResult.java
@@ -1,15 +1,38 @@
 package seedu.todo.logic.commands;
 
+import seedu.todo.commons.core.LogsCenter;
+import seedu.todo.model.ErrorBag;
+
+import java.util.logging.Logger;
+
 /**
  * Represents the result of a command execution.
  */
 public class CommandResult {
+    private final String feedback;
+    private final ErrorBag errors; 
+    
+    private static final Logger logger = LogsCenter.getLogger(CommandResult.class);
 
-    public final String feedbackToUser;
-
-    public CommandResult(String feedbackToUser) {
-        assert feedbackToUser != null;
-        this.feedbackToUser = feedbackToUser;
+    public CommandResult(String feedback) {
+        this.feedback = feedback;
+        this.errors = null;
+    }
+    
+    public CommandResult(String feedback, ErrorBag errors) {
+        this.feedback = feedback;
+        this.errors = errors;
     }
 
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public ErrorBag getErrors() {
+        return errors;
+    }
+    
+    public boolean isSuccessful() {
+        return errors == null;
+    }
 }
