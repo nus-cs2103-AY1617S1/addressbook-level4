3391adbeccc41c8c34e92d33d0a52e4d677ccc3b
Weng Kiat <wengkiat916@gmail.com>
2016-10-12 23:12:24 +0800

    Added doneCommandTest

---
+++ src/test/java/guitests/DoneCommandTest.java
@@ -0,0 +1,57 @@
+package guitests;
+
+import org.junit.Test;
+import seedu.address.testutil.TestTask;
+import seedu.address.testutil.TestUtil;
+
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.DeleteCommand.MESSAGE_DELETE_TASK_SUCCESS;
+/*
+ * Successful testing for delete command, 12/10/16, A0141128R
+ */
+
+public class DoneCommandTest extends ToDoListGuiTest {
+
+    @Test
+    public void done() {
+
+        //done the first in the list
+        TestTask[] currentList = td.getTypicalTasks();
+        int targetIndex = 1;
+        assertdoneSuccess(targetIndex, currentList);
+
+        //delete the last in the list
+        currentList = TestUtil.removeTaskFromList(currentList, targetIndex);
+        targetIndex = currentList.length;
+        assertdoneSuccess(targetIndex, currentList);
+
+        //delete from the middle of the list
+        currentList = TestUtil.removeTaskFromList(currentList, targetIndex);
+        targetIndex = currentList.length/2;
+        assertdoneSuccess(targetIndex, currentList);
+
+        //invalid index
+        commandBox.runCommand("done " + currentList.length + 1);
+        assertResultMessage("The task index provided is invalid");
+
+    }
+
+    /**
+     * Runs the delete command to delete the task at specified index and confirms the result is correct.
+     * @param targetIndexOneIndexed e.g. to delete the first task in the list, 1 should be given as the target index.
+     * @param currentList A copy of the current list of tasks (before deletion).
+     */
+    private void assertdoneSuccess(int targetIndexOneIndexed, final TestTask[] currentList) {
+        TestTask taskToDone = currentList[targetIndexOneIndexed-1]; //-1 because array uses zero indexing
+        TestTask[] expectedRemainder = TestUtil.removeTaskFromList(currentList, targetIndexOneIndexed);
+
+        commandBox.runCommand("done " + targetIndexOneIndexed);
+
+        //confirm the list now contains all previous tasks except the done task
+        assertTrue(taskListPanel.isListMatching(expectedRemainder));
+
+        //confirm the result message is correct
+        assertResultMessage(String.format(MESSAGE_DELETE_TASK_SUCCESS, taskToDone));
+    }
+
+}
