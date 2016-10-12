6c4b837f412441979bb793f0b03a72dc90960152
Chase <tiongyaocong@gmail.com>
2016-10-12 20:14:53 +0800

    updated test case for new format

---
+++ src/main/java/seedu/todo/ui/components/TaskListEventItem.java
@@ -0,0 +1,36 @@
+package seedu.todo.ui.components;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import seedu.todo.models.Event;
+import seedu.todo.ui.UiPartLoader;
+
+public class TaskListEventItem extends MultiComponent {
+
+    private static final String FXML_PATH = "components/TaskListEventItem.fxml";
+
+    // Props
+    public Event event;
+    public int displayIndex;
+
+    // FXML
+    @FXML
+    private Text eventText;
+
+    public static TaskListEventItem load(Stage primaryStage, Pane placeholderPane) {
+        return UiPartLoader.loadUiPart(primaryStage, placeholderPane, new TaskListEventItem());
+    }
+
+    @Override
+    public String getFxmlPath() {
+        return FXML_PATH;
+    }
+
+    @Override
+    public void componentDidMount() {
+        eventText.setText(displayIndex + ". " + event.getName());
+    }
+
+}
