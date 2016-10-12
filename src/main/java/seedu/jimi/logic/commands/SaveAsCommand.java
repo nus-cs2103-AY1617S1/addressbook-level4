b700df2c878f6772e1d786805b2c344de98674d3
ShadowLoner17 <weiyinchong@hotmail.sg>
2016-10-13 03:18:19 +0800

    Further Implement execute of SaveAsCommand

--- src/main/java/seedu/jimi/logic/commands/SaveAsCommand.java
+++ src/main/java/seedu/jimi/logic/commands/SaveAsCommand.java
@@ -1,9 +1,14 @@
 package seedu.jimi.logic.commands;
 
+import java.io.IOException;
 import java.util.Optional;
 import seedu.jimi.commons.core.Config;
 import seedu.jimi.commons.exceptions.DataConversionException;
 import seedu.jimi.commons.util.ConfigUtil;
+import seedu.jimi.model.ReadOnlyTaskBook;
+import seedu.jimi.model.TaskBook;
+import seedu.jimi.storage.Storage;
+import seedu.jimi.storage.StorageManager;
 
 /**
  *  
@@ -22,7 +27,8 @@ public class SaveAsCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Save directory changed: %1$s";
     public static final String MESSAGE_DUPLICATE_TASK = "This save directory is originally used in Jimi";
-    public static final String CONFIG_FILE_NOT_FOUND = "Config file is not found. Using default config file properties...";
+    public static final String MESSAGE_CONFIG_FILE_NOT_FOUND = "Config file is not found. ";
+    public static final String MESSAGE_UPDATING_SAVE_DIR = "There is an error updating the new save directory.";
 
     private String taskBookFilePath;
 
@@ -30,22 +36,31 @@ public class SaveAsCommand extends Command {
      * Convenience constructor using raw values.
      */
     public SaveAsCommand(String filePath)  {
-        this.taskBookFilePath = filePath;
+        this.taskBookFilePath = filePath.concat(".xml");
     }
     
     @Override
     public CommandResult execute() {
         String defaultConfigFilePathUsed = Config.DEFAULT_CONFIG_FILE;
         try {
-            Optional<Config> configOptional = ConfigUtil.readConfig(defaultConfigFilePathUsed);
-            Config config = configOptional.orElse(new Config());
+            Config config = ConfigUtil.readConfig(defaultConfigFilePathUsed).orElse(new Config());
+            ConfigUtil.saveConfig(config, defaultConfigFilePathUsed);
+            
+            String oldTaskBookFilePath = config.getTaskBookFilePath();
             config.setTaskBookFilePath(taskBookFilePath);
-            return new CommandResult(String.format(MESSAGE_SUCCESS, taskBookFilePath));
+            StorageManager oldStorage = new StorageManager(oldTaskBookFilePath, config.getUserPrefsFilePath());
+            StorageManager newStorage = new StorageManager(taskBookFilePath, config.getUserPrefsFilePath());
+            
+            ReadOnlyTaskBook oldTaskBook = oldStorage.readTaskBook(oldTaskBookFilePath).orElse(new TaskBook());
+            newStorage.saveTaskBook(oldTaskBook);
+            
+            return new CommandResult(String.format(MESSAGE_SUCCESS, newStorage.getAddressBookFilePath()));
         } catch (DataConversionException e) {
-            Config config = new Config();
-            config.setTaskBookFilePath(taskBookFilePath);
-            return new CommandResult(String.format(CONFIG_FILE_NOT_FOUND, MESSAGE_SUCCESS, taskBookFilePath));
+            return new CommandResult(String.format(MESSAGE_CONFIG_FILE_NOT_FOUND));
+        } catch (IOException e) {
+            return new CommandResult(String.format(MESSAGE_UPDATING_SAVE_DIR));
         }
     }
+    
 
 }
