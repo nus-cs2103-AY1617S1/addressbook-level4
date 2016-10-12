8c39fdfdc639a64fd338f5586e1f537d129f6db0
Zavier\ZavierOng <zacohjayjay@gmail.com>
2016-10-12 20:46:36 +0800

    add wrong input message for Editcommand

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -31,7 +31,8 @@ public class EditCommand extends Command{
             +"Example: " + COMMAND_WORD + " 1 " + DESCRIPTION_WORD + " beach party\t\t"
             +"Example: " + COMMAND_WORD + " 1 " + DATE_WORD + " 120516\n"
             +"Example: " + COMMAND_WORD + " 1 " + START_WORD + " 1600\t\t\t"
-            +"Example: " + COMMAND_WORD + " 1 " + END_WORD + " 2300\n";
+            +"Example: " + COMMAND_WORD + " 1 " + END_WORD + " 2300\n"
+    		+"Example: " + COMMAND_WORD + " 1 " + TAG_WORD + " sentosa";
     
     public static final String MESSAGE_EDIT_TASK_SUCCESS = "Edited task: %1$s";
     
