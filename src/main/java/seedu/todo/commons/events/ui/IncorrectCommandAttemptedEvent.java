0769e941801b7c7e13aa0733f600f5a4217f129d
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-12 11:02:18 +0800

    All I wanted was to break your walls.

--- src/main/java/seedu/todo/commons/events/ui/IncorrectCommandAttemptedEvent.java
+++ src/main/java/seedu/todo/commons/events/ui/IncorrectCommandAttemptedEvent.java
@@ -1,14 +1,14 @@
 package seedu.todo.commons.events.ui;
 
 import seedu.todo.commons.events.BaseEvent;
-import seedu.todo.logic.commands.Command;
+import seedu.todo.logic.commands.BaseCommand;
 
 /**
  * Indicates an attempt to execute an incorrect command
  */
 public class IncorrectCommandAttemptedEvent extends BaseEvent {
 
-    public IncorrectCommandAttemptedEvent(Command command) {}
+    public IncorrectCommandAttemptedEvent(BaseCommand command) {}
 
     @Override
     public String toString() {
