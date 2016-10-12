4fef9fdbb62248f4c11a343e3afef9032e1cd095
Jihui Xie <xjh125470939@gmail.com>
2016-10-12 13:43:01 +0800

    Modify AddCommand Class

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -16,9 +16,9 @@ public class AddCommand extends Command {
     public static final String COMMAND_WORD = "add";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a task to the task manager. "
-            + "Parameters: DESCRIPTION [pr/PRIORITY] [time/TIME] [a/VENUE] [t/TAG]...\n"
+            + "Parameters: DESCRIPTION [pr/PRIORITY] [time/TIME] [d/Date] [t/TAG]...\n"
             + "Example: " + COMMAND_WORD
-            + "Lose Sleep pr/high time/23:35 a/311, Clementi Ave 2, #02-25 t/CS2103 t/WhatIsSleep";
+            + "Lose Sleep pr/high time/23:35 d/12.10.2016 t/CS2103 t/WhatIsSleep";
 
     public static final String MESSAGE_SUCCESS = "New task added: %1$s";
     public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the task manager";
@@ -30,7 +30,7 @@ public class AddCommand extends Command {
      *
      * @throws IllegalValueException if any of the raw values are invalid
      */
-    public AddCommand(String description, String priority, String time, String venue, Set<String> tags)
+    public AddCommand(String description, String priority, String time, String date, Set<String> tags)
             throws IllegalValueException {
         final Set<Tag> tagSet = new HashSet<>();
         for (String tagName : tags) {
@@ -40,7 +40,7 @@ public class AddCommand extends Command {
                 new Description(description),
                 new Priority(priority),
                 new Time(time),
-                new Venue(venue),
+                new Date(date),
                 new UniqueTagList(tagSet)
         );
     }
