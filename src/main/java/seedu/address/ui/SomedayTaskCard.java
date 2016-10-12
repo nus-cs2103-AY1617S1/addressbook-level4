94b6a8907116310d5aa56b57c2d26909fb052674
KnytJC <knyt.jc@hotmail.com>
2016-10-12 10:49:01 +0800

    Editing ui someday task display (#42)
    * edit ui for someday task
    
    * delete useless class
    
    * ui edit for displaying someday task
    
    * further edit

---
+++ src/main/java/seedu/address/ui/SomedayTaskCard.java
@@ -0,0 +1,57 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import seedu.address.model.task.TMReadOnlyTask;
+
+public class SomedayTaskCard extends UiPart{
+
+    private static final String FXML = "SomedayTaskCard.fxml";
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label taskName;
+    @FXML
+    private Label id;
+    @FXML
+    private Label type;
+
+    private TMReadOnlyTask task;
+    private int displayedIndex;
+
+    public SomedayTaskCard(){
+
+    }
+
+    public static SomedayTaskCard load(TMReadOnlyTask task, int displayedIndex){
+    	SomedayTaskCard card = new SomedayTaskCard();
+        card.task = task;
+        card.displayedIndex = displayedIndex;
+        return UiPartLoader.loadUiPart(card);
+    }
+
+    @FXML
+    public void initialize() {
+        taskName.setText(task.getName().fullName);
+        id.setText(displayedIndex + ". ");
+        type.setText(task.getTaskType());
+    }
+
+    public HBox getLayout() {
+        return cardPane;
+    }
+
+    @Override
+    public void setNode(Node node) {
+        cardPane = (HBox)node;
+    }
+
+    @Override
+    public String getFxmlPath() {
+        return FXML;
+    }
+}
+// Note: translate V-box Y: -18
