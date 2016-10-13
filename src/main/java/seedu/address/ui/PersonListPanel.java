f00e58d932cfbfafc4454a570ddcb727f7638e1d
Edmund Mok <edmundmok@outlook.com>
2016-10-13 19:13:37 +0800

    Change more stuff to allow doneTaskList

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -1,6 +1,7 @@
 package seedu.address.ui;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -14,6 +15,7 @@ import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.item.ReadOnlyTask;
 import seedu.address.commons.core.LogsCenter;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 /**
@@ -24,9 +26,13 @@ public class PersonListPanel extends UiPart {
     private static final String FXML = "PersonListPanel.fxml";
     private VBox panel;
     private AnchorPane placeHolderPane;
+    
+    // store reference to undone and done task list for switching between views
+    private ObservableList<ReadOnlyTask> undoneTaskList;
+    private ObservableList<ReadOnlyTask> doneTaskList;
 
     @FXML
-    private ListView<ReadOnlyTask> personListView;
+    private ListView<ReadOnlyTask> taskListView;
 
     public PersonListPanel() {
         super();
@@ -48,21 +54,27 @@ public class PersonListPanel extends UiPart {
     }
 
     public static PersonListPanel load(Stage primaryStage, AnchorPane personListPlaceholder,
-                                       ObservableList<ReadOnlyTask> personList) {
+                                       ObservableList<ReadOnlyTask> undoneTaskList, ObservableList<ReadOnlyTask> doneTaskList) {
         PersonListPanel personListPanel =
                 UiPartLoader.loadUiPart(primaryStage, personListPlaceholder, new PersonListPanel());
-        personListPanel.configure(personList);
+        personListPanel.configure(undoneTaskList, doneTaskList);
+
         return personListPanel;
     }
 
-    private void configure(ObservableList<ReadOnlyTask> personList) {
-        setConnections(personList);
+    private void configure(ObservableList<ReadOnlyTask> undoneTaskList, ObservableList<ReadOnlyTask> doneTaskList) {
+        //set up references
+        this.undoneTaskList = undoneTaskList;
+        this.doneTaskList = FXCollections.observableArrayList();
+        
+        setConnections(undoneTaskList);
         addToPlaceholder();
     }
 
+    // can use this for init and when switching between undone and done task view.
     private void setConnections(ObservableList<ReadOnlyTask> personList) {
-        personListView.setItems(personList);
-        personListView.setCellFactory(listView -> new PersonListViewCell());
+        taskListView.setItems(personList);
+        taskListView.setCellFactory(listView -> new PersonListViewCell());
         setEventHandlerForSelectionChangeEvent();
     }
 
@@ -72,18 +84,32 @@ public class PersonListPanel extends UiPart {
     }
 
     private void setEventHandlerForSelectionChangeEvent() {
-        personListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+        taskListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue != null) {
                 logger.fine("Selection in person list panel changed to : '" + newValue + "'");
                 raise(new PersonPanelSelectionChangedEvent(newValue));
             }
         });
     }
+    
+    @FXML
+    private void handleSwitchTaskListView(){
+        System.out.println("hello");
+        if (taskListView.getItems() == undoneTaskList){
+            System.out.println("switch from undone to done");
+            taskListView.setItems(doneTaskList);
+        }
+        else{
+            System.out.println("switch from done to undone");
+            taskListView.setItems(undoneTaskList);
+        }
+            
+    }
 
     public void scrollTo(int index) {
         Platform.runLater(() -> {
-            personListView.scrollTo(index);
-            personListView.getSelectionModel().clearAndSelect(index);
+            taskListView.scrollTo(index);
+            taskListView.getSelectionModel().clearAndSelect(index);
         });
     }
 
