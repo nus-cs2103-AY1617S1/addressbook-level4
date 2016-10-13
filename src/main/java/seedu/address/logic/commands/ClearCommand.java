cd740859bc14af985dcff1b7343c996c2cdd2691
Yuchuan <yyc478@gmail.com>
2016-10-14 00:48:05 +0800

    Update ClearCommand to remove errors (can't be fixed at this stage)

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -1,7 +1,5 @@
 package seedu.address.logic.commands;
 
-import seedu.address.model.AddressBook;
-
 /**
  * Clears the address book.
  */
@@ -16,7 +14,7 @@ public class ClearCommand extends Command {
     @Override
     public CommandResult execute() {
         assert model != null;
-        model.resetData(AddressBook.getEmptyAddressBook());
+		// model.resetData(AddressBook.getEmptyAddressBook());
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
