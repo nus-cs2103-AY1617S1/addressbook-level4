e420847e61e0be8203f1d74cc70ccf8f97455115
Irvin Lim <irvinlimweiquan@gmail.com>
2016-10-13 12:39:19 +0800

    Update DateUtilTest for formatDay

--- src/test/java/seedu/address/commons/util/DateUtilTest.java
+++ src/test/java/seedu/address/commons/util/DateUtilTest.java
@@ -60,21 +60,16 @@ public class DateUtilTest {
 	@Test
 	public void formatDayTests() {
 	    LocalDateTime now = LocalDateTime.now();
-	    assertEquals(DateUtil.formatDay(now), "Today " + now.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.US).substring(0, 3) + " " + 
-	            now.getDayOfMonth() + " " + now.getMonth().getDisplayName(TextStyle.FULL, Locale.US).substring(0, 3));
-	    assertEquals(DateUtil.formatDay(now.plus(1, ChronoUnit.DAYS)), "Tomorrow " + 
-	            now.plus(1, ChronoUnit.DAYS).getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.US).substring(0, 3) + 
-	            " " + now.plus(1, ChronoUnit.DAYS).getDayOfMonth() + 
-	            " " + now.plus(1, ChronoUnit.DAYS).getMonth().getDisplayName(TextStyle.FULL, Locale.US).substring(0, 3));
-	    assertEquals(DateUtil.formatDay(now.plus(2, ChronoUnit.DAYS)), 
-	            now.plus(2, ChronoUnit.DAYS).getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.US) + 
-	            " " + now.plus(2, ChronoUnit.DAYS).getDayOfMonth() + 
-	            " " + now.plus(2, ChronoUnit.DAYS).getMonth().getDisplayName(TextStyle.FULL, Locale.US).substring(0, 3));
-	    assertEquals(DateUtil.formatDay(now.plus(6, ChronoUnit.DAYS)), 
-	            now.plus(6, ChronoUnit.DAYS).getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.US) + 
-	            " " + now.plus(6, ChronoUnit.DAYS).getDayOfMonth() + 
-	            " " + now.plus(6, ChronoUnit.DAYS).getMonth().getDisplayName(TextStyle.FULL, Locale.US).substring(0, 3));
-        assertEquals(DateUtil.formatDay(now.minus(1, ChronoUnit.DAYS)), "1 day ago");
+	    assertEquals(DateUtil.formatDay(now), "Today");
+	    assertEquals(DateUtil.formatDay(now.plus(1, ChronoUnit.DAYS)), "Tomorrow");
+	    
+	    // Show day of week for d = {n+2,...,n+6} where n = today
+	    for (int i = 2; i <= 6; i++) {
+	        String expected = now.plus(i, ChronoUnit.DAYS).getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.US);
+	        assertEquals(DateUtil.formatDay(now.plus(i, ChronoUnit.DAYS)), expected);
+	    }
+	    
+	    assertEquals(DateUtil.formatDay(now.minus(1, ChronoUnit.DAYS)), "1 day ago");
         assertEquals(DateUtil.formatDay(now.minus(6, ChronoUnit.DAYS)), "6 days ago");
         assertEquals(DateUtil.formatDay(now.minus(14, ChronoUnit.DAYS)), "14 days ago");
 	}
