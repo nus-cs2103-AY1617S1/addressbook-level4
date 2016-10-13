55e0bb8be00ba2b2212778d3d59ea8c907d0737c
Irvin Lim <irvinlimweiquan@gmail.com>
2016-10-14 05:52:34 +0800

    Added calendar icon to different events from task items

--- src/main/java/seedu/todo/ui/components/TaskListEventItem.java
+++ src/main/java/seedu/todo/ui/components/TaskListEventItem.java
@@ -1,6 +1,8 @@
 package seedu.todo.ui.components;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -10,7 +12,8 @@ import seedu.todo.ui.UiPartLoader;
 public class TaskListEventItem extends MultiComponent {
 
     private static final String FXML_PATH = "components/TaskListEventItem.fxml";
-
+    private static final String ICON_PATH = "/images/icon-calendar.png";
+    
     // Props
     public Event event;
     public int displayIndex;
@@ -20,6 +23,8 @@ public class TaskListEventItem extends MultiComponent {
     private Text eventText;
     @FXML
     private Text rowIndex;
+    @FXML
+    private ImageView rowIconImageView;
 
     public static TaskListEventItem load(Stage primaryStage, Pane placeholderPane) {
         return UiPartLoader.loadUiPart(primaryStage, placeholderPane, new TaskListEventItem());
@@ -34,6 +39,9 @@ public class TaskListEventItem extends MultiComponent {
     public void componentDidMount() {
         eventText.setText(event.getName());
         rowIndex.setText(displayIndex + "");
+        
+        // Set image
+        rowIconImageView.setImage(new Image(ICON_PATH));
     }
 
 }
