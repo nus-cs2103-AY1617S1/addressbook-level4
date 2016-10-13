aa8aaeafeac7c66081bb3a42dcf853e876b11673
Ian Teo <ian_teo@msn.com>
2016-10-13 15:39:14 +0800

    Change time display format

--- src/main/java/seedu/taskitty/model/task/TaskTime.java
+++ src/main/java/seedu/taskitty/model/task/TaskTime.java
@@ -18,7 +18,7 @@ public class TaskTime {
             "Task times should be in the format hh:mm"; //or hhmm
     public static final String MESSAGE_TIME_INVALID =
             "Task time provided is invalid!";
-    public static final String TIME_DISPLAY_FORMAT = "hh:mm";
+    public static final String TIME_DISPLAY_FORMAT = "kk:mm";
     
     //format: hh:mm
     private static final String TIME_VALIDATION_REGEX_FORMAT_1 = "[\\p{Digit}]{1,2}:[\\p{Digit}]{2}";
