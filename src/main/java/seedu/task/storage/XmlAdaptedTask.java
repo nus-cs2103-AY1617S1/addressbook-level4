cc64e9a5b9e4a8275b83020fb060d83ff2baa791
Advay Pal <advaypal@hotmail.com>
2016-10-13 19:52:55 +0800

    Remove 'person'

--- src/main/java/seedu/task/storage/XmlAdaptedTask.java
+++ src/main/java/seedu/task/storage/XmlAdaptedTask.java
@@ -37,7 +37,7 @@ public class XmlAdaptedTask {
     /**
      * Converts a given Task into this class for JAXB use.
      *
-     * @param source future changes to this will not affect the created XmlAdaptedPerson
+     * @param source future changes to this will not affect the created XmlAdaptedTask
      */
     public XmlAdaptedTask(ReadOnlyTask source) {
         name = source.getName().taskName;
@@ -51,7 +51,7 @@ public class XmlAdaptedTask {
     }
 
     /**
-     * Converts this jaxb-friendly adapted person object into the model's Task object.
+     * Converts this jaxb-friendly adapted task object into the model's Task object.
      *
      * @throws IllegalValueException if there were any data constraints violated in the adapted task
      */
--- src/main/java/seedu/task/ui/UiManager.java
+++ src/main/java/seedu/task/ui/UiManager.java
@@ -118,7 +118,7 @@ public class UiManager extends ComponentManager implements Ui {
     }
 
     @Subscribe
-    private void handlePersonPanelSelectionChangedEvent(TaskPanelSelectionChangedEvent event){
+    private void handleTaskPanelSelectionChangedEvent(TaskPanelSelectionChangedEvent event){
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
         mainWindow.loadTaskPage(event.getNewSelection());
     }
