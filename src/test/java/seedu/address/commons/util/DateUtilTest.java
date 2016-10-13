605d7e4befb92fe6a24fea6577382c61247a8e2d
Irvin Lim <irvinlimweiquan@gmail.com>
2016-10-13 12:49:36 +0800

    Write tests for formatShortDate

--- src/test/java/seedu/address/commons/util/DateUtilTest.java
+++ src/test/java/seedu/address/commons/util/DateUtilTest.java
@@ -3,6 +3,7 @@ package seedu.address.commons.util;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
@@ -70,8 +71,28 @@ public class DateUtilTest {
 	    }
 	    
 	    assertEquals(DateUtil.formatDay(now.minus(1, ChronoUnit.DAYS)), "1 day ago");
-        assertEquals(DateUtil.formatDay(now.minus(6, ChronoUnit.DAYS)), "6 days ago");
-        assertEquals(DateUtil.formatDay(now.minus(14, ChronoUnit.DAYS)), "14 days ago");
+	    assertEquals(DateUtil.formatDay(now.minus(6, ChronoUnit.DAYS)), "6 days ago");
+	    assertEquals(DateUtil.formatDay(now.minus(14, ChronoUnit.DAYS)), "14 days ago");
+	}
+	
+	@Test
+	public void formatShortDateTests() {
+	    LocalDateTime now = LocalDateTime.now();
+	    
+	    String expectedToday = now.format(DateTimeFormatter.ofPattern("E dd MMM"));
+	    assertEquals(DateUtil.formatShortDate(now), expectedToday);
+	    
+	    String expectedTomorrow = now.plus(1,  ChronoUnit.DAYS).format(DateTimeFormatter.ofPattern("E dd MMM"));
+	    assertEquals(DateUtil.formatShortDate(now.plus(1, ChronoUnit.DAYS)), expectedTomorrow);
+	    
+	    for (int i = 2; i <= 6; i++) {
+	        String expected = now.plus(i,  ChronoUnit.DAYS).format(DateTimeFormatter.ofPattern("dd MMM"));
+	        assertEquals(DateUtil.formatShortDate(now.plus(i, ChronoUnit.DAYS)), expected);
+	    }
+	    
+	    // Test dates in the past
+	    LocalDateTime pastDate = fromEpoch(946656000000l);
+	    assertEquals(DateUtil.formatShortDate(pastDate), "Sat 01 Jan");
 	}
 	
 	private static LocalDateTime fromEpoch(long epoch) {
