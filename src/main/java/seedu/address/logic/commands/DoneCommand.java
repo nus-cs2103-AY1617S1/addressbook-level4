7c16d50d1459ab3443b94c7c3a8303b4b732c008
njyjn <njyjustin@yahoo.com>
2016-10-13 12:13:44 +0800

    There is no need to display the entire task string when done.

--- src/main/java/seedu/address/logic/commands/DoneCommand.java
+++ src/main/java/seedu/address/logic/commands/DoneCommand.java
@@ -47,7 +47,7 @@ public class DoneCommand extends Command {
             assert false : "The target task cannot be missing";
         }
         
-        return new CommandResult(String.format(MESSAGE_DONE_TASK_SUCCESS, taskToComplete));
+        return new CommandResult(String.format(MESSAGE_DONE_TASK_SUCCESS, taskToComplete.getDetail().details));
     }
 
 }
