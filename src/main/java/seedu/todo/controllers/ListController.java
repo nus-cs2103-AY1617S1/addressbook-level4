33b09e238ba2146dc0567a52a2943ae75968132e
Irvin Lim <irvinlimweiquan@gmail.com>
2016-10-15 02:10:52 +0800

    Add ListController console message

--- src/main/java/seedu/todo/controllers/ListController.java
+++ src/main/java/seedu/todo/controllers/ListController.java
@@ -10,6 +10,8 @@ public class ListController implements Controller {
     private static String DESCRIPTION = "Lists all tasks and events.";
     private static String COMMAND_SYNTAX = "list";
     
+    private static final String MESSAGE_LISTING_WHAT = "Listing a total of %d tasks and %d events.";
+    
     private static CommandDefinition commandDefinition =
             new CommandDefinition(NAME, DESCRIPTION, COMMAND_SYNTAX); 
 
@@ -26,10 +28,15 @@ public class ListController implements Controller {
     public void process(String input) {
         TodoListDB db = TodoListDB.getInstance();
         
+        // Render
         IndexView view = UiManager.loadView(IndexView.class);
         view.tasks = db.getAllTasks();
         view.events = db.getAllEvents();
         UiManager.renderView(view);
+
+        // Update console message
+        String consoleMessage = String.format(MESSAGE_LISTING_WHAT, view.tasks.size(), view.events.size());
+        UiManager.updateConsoleMessage(consoleMessage);
     }
 
 }
