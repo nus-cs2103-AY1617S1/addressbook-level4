cd44ee837f220b50d642a2fc26860df824a9e505
Irvin Lim <irvinlimweiquan@gmail.com>
2016-10-15 02:40:03 +0800

    Use pluralizer for ListController consoleMessage

--- src/main/java/seedu/todo/controllers/ListController.java
+++ src/main/java/seedu/todo/controllers/ListController.java
@@ -1,5 +1,6 @@
 package seedu.todo.controllers;
 
+import seedu.todo.commons.util.StringUtil;
 import seedu.todo.models.TodoListDB;
 import seedu.todo.ui.UiManager;
 import seedu.todo.ui.views.IndexView;
@@ -10,7 +11,7 @@ public class ListController implements Controller {
     private static String DESCRIPTION = "Lists all tasks and events.";
     private static String COMMAND_SYNTAX = "list";
     
-    private static final String MESSAGE_LISTING_WHAT = "Listing a total of %d tasks and %d events.";
+    private static final String MESSAGE_LISTING_SUCCESS = "Listing a total of %d %s and %d %s.";
     
     private static CommandDefinition commandDefinition =
             new CommandDefinition(NAME, DESCRIPTION, COMMAND_SYNTAX); 
@@ -35,7 +36,11 @@ public class ListController implements Controller {
         UiManager.renderView(view);
 
         // Update console message
-        String consoleMessage = String.format(MESSAGE_LISTING_WHAT, view.tasks.size(), view.events.size());
+        int numTasks = db.getAllTasks().size();
+        int numEvents = db.getAllEvents().size();
+        String consoleMessage = String.format(MESSAGE_LISTING_SUCCESS,
+                numTasks, StringUtil.pluralizer(numTasks, "task", "tasks"),
+                numEvents, StringUtil.pluralizer(numEvents, "event", "events"));
         UiManager.updateConsoleMessage(consoleMessage);
     }
 
