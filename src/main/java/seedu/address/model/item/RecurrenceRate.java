e8912263e0914c235695cc2087656f50af46c9cc
ZHIYUAN-PC\Zhi Yuan <zhiyuan_1994@yahoo.com.sg>
2016-10-14 11:08:43 +0800

    update RecurrenceRate to allow input for days e.g "Monday", "Wednesday"

--- src/main/java/seedu/address/model/item/RecurrenceRate.java
+++ src/main/java/seedu/address/model/item/RecurrenceRate.java
@@ -11,7 +11,12 @@ public class RecurrenceRate {
             + "\"day(s)\", \"week(s)\", \"month(s)\", \"year(s)\", \"Monday\", \"Wed\"\n"
             + "For example: \"repeat every 3 days\", \"repeat every week\", \"repeat every Wed\"";
     
-    public static final String[] arr = {"hour", "hours", "day", "days", "week", "weeks", ""};
+    //TODO: Design decision?
+    public static final String[][] TIME_PERIOD_INPUT = {{"hour", "hours", "day", "days", "week", "weeks", "month", "months", 
+        "year", "years", "mon", "monday", "tues", "tuesday", "wed", "wednesday", "thur", "thurs", "thursday", 
+        "fri", "friday", "sat", "saturday", "sun", "sunday"}, {"hour", "hour", "day", "day", "week", "week", "month", "month", 
+            "year", "year", "monday", "monday", "tuesday", "tuesday", "wednesday", "wednesday", "thursday", "thursday", "thursday", 
+            "friday", "friday", "saturday", "saturday", "sunday", "sunday"}};
     
     public Integer rate;
     public TimePeriod timePeriod;
@@ -44,22 +49,51 @@ public class RecurrenceRate {
     }
 
     private void isValidTimePeriod(String timePeriod) throws IllegalValueException {
-        switch (timePeriod) {
+        String validTimePeriod = null;
+        
+        for (int j = 0; j < TIME_PERIOD_INPUT[0].length; j++) {
+            if (TIME_PERIOD_INPUT[0][j].equals(timePeriod.toLowerCase())) {
+                validTimePeriod = TIME_PERIOD_INPUT[1][j];
+                break;
+            }
+        }
+        
+        switch (validTimePeriod) {
+        case ("hour"):
+            this.timePeriod = TimePeriod.HOUR;
+            break;
         case ("day"):
-        case ("days"):
             this.timePeriod = TimePeriod.DAY; 
             break;
         case ("week"):
-        case ("weeks"):
             this.timePeriod = TimePeriod.WEEK;
             break;
         case ("month"):
-        case ("months"):
             this.timePeriod = TimePeriod.MONTH;
             break;
         case ("year"):
-        case ("years"):
             this.timePeriod = TimePeriod.YEAR;
+            break;
+        case ("monday"):
+            this.timePeriod = TimePeriod.MONDAY;
+            break;
+        case ("tuesday"):
+            this.timePeriod = TimePeriod.TUESDAY;
+            break;
+        case ("wednesday"):
+            this.timePeriod = TimePeriod.WEDNESDAY;
+            break;
+        case ("thursday"):
+            this.timePeriod = TimePeriod.THURSDAY;
+            break;
+        case ("friday"):
+            this.timePeriod = TimePeriod.FRIDAY;
+            break;
+        case ("saturday"):
+            this.timePeriod = TimePeriod.SATURDAY;
+            break;
+        case ("sunday"):
+            this.timePeriod = TimePeriod.SUNDAY;
             break;
         default:
             throw new IllegalValueException(MESSAGE_VALUE_CONSTRAINTS);
