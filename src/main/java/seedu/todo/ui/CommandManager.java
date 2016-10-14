401396f49a0dec3094e8fbda2a6f498ad746d43c
Xien Dong Wang <xdrawks@gmail.com>
2016-10-14 21:22:22 +0800

    Add java doc to CommandManager

---
+++ src/main/java/seedu/todo/ui/CommandManager.java
@@ -0,0 +1,14 @@
+package seedu.todo.ui;
+
+/**
+ * Manages the input command from {@link CommandInputView}, pass it to {@link seedu.todo.logic.Logic}
+ * and hands the {@link seedu.todo.logic.commands.CommandResult}
+ * to {@link CommandFeedbackView} and {@link CommandErrorView}
+ *
+ * @author Wang Xien Dong
+ */
+public class CommandManager {
+
+    
+
+}
