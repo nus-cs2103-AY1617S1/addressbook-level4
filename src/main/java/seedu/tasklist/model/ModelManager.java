a0e9c0b4b7b7ede069abac57302466eec4888c99
PankajB1997 <pankajbhootra@gmail.com>
2016-10-14 16:11:24 +0800

    Remove else from addToUndoStack(...) method

--- src/main/java/seedu/tasklist/model/ModelManager.java
+++ src/main/java/seedu/tasklist/model/ModelManager.java
@@ -26,21 +26,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Represents the in-memory model of the task list data.
- * All changes to any model should be synchronized.
+ * Represents the in-memory model of the task list data. All changes to any
+ * model should be synchronized.
  */
 public class ModelManager extends ComponentManager implements Model {
     private static final Logger logger = LogsCenter.getLogger(ModelManager.class);
 
     public static LinkedList<UndoInfo> undoStack = new LinkedList<UndoInfo>();
     public static LinkedList<UndoInfo> redoStack = new LinkedList<UndoInfo>();
-    
+
     private final TaskList taskList;
     private final FilteredList<Task> filteredTasks;
 
     /**
-     * Initializes a ModelManager with the given TaskList
-     * TaskList and its variables should not be null
+     * Initializes a ModelManager with the given TaskList TaskList and its
+     * variables should not be null
      */
     public ModelManager(TaskList src, UserPrefs userPrefs) {
         super();
@@ -83,8 +83,8 @@ public class ModelManager extends ComponentManager implements Model {
         taskList.removeTask(target);
         updateFilteredListToShowIncomplete();
         indicateTaskListChanged();
-        if(callDidNotComeFromUndoCommand())
-           addToUndoStack(UndoCommand.DEL_CMD_ID, (Task)target);
+        if (callDidNotComeFromUndoCommand())
+            addToUndoStack(UndoCommand.DEL_CMD_ID, (Task) target);
     }
 
     @Override
@@ -92,64 +92,63 @@ public class ModelManager extends ComponentManager implements Model {
         taskList.addTask(task);
         updateFilteredListToShowIncomplete();
         indicateTaskListChanged();
-        if(callDidNotComeFromUndoCommand())
-           addToUndoStack(UndoCommand.ADD_CMD_ID, task);
+        if (callDidNotComeFromUndoCommand())
+            addToUndoStack(UndoCommand.ADD_CMD_ID, task);
     }
-    
+
     @Override
-    public synchronized void updateTask(Task taskToUpdate, TaskDetails taskDetails, StartTime startTime, EndTime endTime, Priority priority, UniqueTagList tags) throws UniqueTaskList.DuplicateTaskException {
+    public synchronized void updateTask(Task taskToUpdate, TaskDetails taskDetails, StartTime startTime,
+            EndTime endTime, Priority priority, UniqueTagList tags) throws UniqueTaskList.DuplicateTaskException {
         taskList.updateTask(taskToUpdate, taskDetails, startTime, endTime, priority);
         updateFilteredListToShowIncomplete();
         indicateTaskListChanged();
         Task originalTask = new Task(taskDetails, startTime, endTime, priority, tags);
-        if(callDidNotComeFromUndoCommand()){
-           addToUndoStack(UndoCommand.UPD_CMD_ID, taskToUpdate, originalTask);
-           System.out.println("No undo");
+        if (callDidNotComeFromUndoCommand()) {
+            addToUndoStack(UndoCommand.UPD_CMD_ID, taskToUpdate, originalTask);
         }
-        else System.out.println("undo");
     }
-    
+
     @Override
     public synchronized void markTaskAsComplete(ReadOnlyTask task) throws TaskNotFoundException {
         taskList.markTaskAsComplete(task);
         updateFilteredListToShowIncomplete();
         indicateTaskListChanged();
-        if(callDidNotComeFromUndoCommand())
-           addToUndoStack(UndoCommand.DONE_CMD_ID, (Task)task);
+        if (callDidNotComeFromUndoCommand())
+            addToUndoStack(UndoCommand.DONE_CMD_ID, (Task) task);
     }
-    
+
     @Override
     public synchronized void markTaskAsIncomplete(ReadOnlyTask task) throws TaskNotFoundException {
         taskList.markTaskAsIncomplete(task);
         updateFilteredListToShowIncomplete();
         indicateTaskListChanged();
-        if(callDidNotComeFromUndoCommand())
-           addToUndoStack(UndoCommand.DONE_CMD_ID, (Task)task);
+        if (callDidNotComeFromUndoCommand())
+            addToUndoStack(UndoCommand.DONE_CMD_ID, (Task) task);
     }
 
     private boolean callDidNotComeFromUndoCommand() {
-        //TO-DO
-        //return !(Thread.currentThread().getStackTrace().getClass().getName().equals("UndoCommand"));
+        // TO-DO
+        // return
+        // !(Thread.currentThread().getStackTrace().getClass().getName().equals("UndoCommand"));
         return true;
     }
 
     private void addToUndoStack(int undoID, Task... tasks) {
-        if (undoStack.size()==3){
-            undoStack.remove(undoStack.size()-1);
+        if (undoStack.size() == 3) {
+            undoStack.remove(undoStack.size() - 1);
         }
-        else{
-            UndoInfo undoInfo = new UndoInfo(undoID, tasks);
-            undoStack.push(undoInfo);
-        }
+        UndoInfo undoInfo = new UndoInfo(undoID, tasks);
+        undoStack.push(undoInfo);
     }
 
-    //=========== Filtered Person List Accessors ===============================================================
+    // =========== Filtered Person List Accessors
+    // ===============================================================
 
     @Override
     public UnmodifiableObservableList<ReadOnlyTask> getFilteredTaskList() {
         return new UnmodifiableObservableList<>(filteredTasks);
     }
-    
+
     public UnmodifiableObservableList<Task> getModifiableTaskList() {
         return new UnmodifiableObservableList<>(filteredTasks);
     }
@@ -158,41 +157,41 @@ public class ModelManager extends ComponentManager implements Model {
     public void updateFilteredListToShowAll() {
         filteredTasks.setPredicate(null);
     }
-    
+
     @Override
     public void updateFilteredListToShowIncomplete() {
-    	updateFilteredListToShowAll();
-    	updateFilteredTaskList(new PredicateExpression(new DefaultDisplayQualifier()));
+        updateFilteredListToShowAll();
+        updateFilteredTaskList(new PredicateExpression(new DefaultDisplayQualifier()));
     }
-    
-    public void updateFilteredList(){
+
+    public void updateFilteredList() {
         updateFilteredListToShowIncomplete();
         indicateTaskListChanged();
     }
 
     @Override
-    public void updateFilteredTaskList(Set<String> keywords){
+    public void updateFilteredTaskList(Set<String> keywords) {
         updateFilteredTaskList(new PredicateExpression(new NameQualifier(keywords)));
     }
 
     private void updateFilteredTaskList(Expression expression) {
         filteredTasks.setPredicate(expression::satisfies);
     }
-    
-    public Set<String> getKeywordsFromList(List<ReadOnlyTask> tasks){
+
+    public Set<String> getKeywordsFromList(List<ReadOnlyTask> tasks) {
         Set<String> keywords = new HashSet<String>();
-        for(ReadOnlyTask task: tasks){
+        for (ReadOnlyTask task : tasks) {
             keywords.addAll(Arrays.asList(task.getTaskDetails().toString().split(" ")));
         }
         return keywords;
     }
-    
+
     @Override
     public void updateFilteredListToShowComplete() {
-    	updateFilteredListToShowAll();
-    	updateFilteredTaskList(new PredicateExpression(new CompletedQualifier()));
+        updateFilteredListToShowAll();
+        updateFilteredTaskList(new PredicateExpression(new CompletedQualifier()));
     }
-    
+
     @Override
     public void updateFilteredListToShowPriority(String priority) {
         updateFilteredListToShowAll();
@@ -203,6 +202,7 @@ public class ModelManager extends ComponentManager implements Model {
 
     interface Expression {
         boolean satisfies(ReadOnlyTask person);
+
         String toString();
     }
 
@@ -227,21 +227,22 @@ public class ModelManager extends ComponentManager implements Model {
 
     interface Qualifier {
         boolean run(ReadOnlyTask person);
+
         String toString();
     }
-    
+
     private class DefaultDisplayQualifier implements Qualifier {
 
-    	DefaultDisplayQualifier(){
-    		
-    	}
-    	
-		@Override
-		public boolean run(ReadOnlyTask person) {
-			return !person.isComplete();
-		}
+        DefaultDisplayQualifier() {
+
+        }
+
+        @Override
+        public boolean run(ReadOnlyTask person) {
+            return !person.isComplete();
+        }
     }
-    
+
     private class CompletedQualifier implements Qualifier {
         @Override
         public boolean run(ReadOnlyTask person) {
@@ -252,52 +253,53 @@ public class ModelManager extends ComponentManager implements Model {
     private class NameQualifier implements Qualifier {
         private Set<String> nameKeyWords;
         private Pattern NAME_QUERY;
-        
 
         NameQualifier(Set<String> nameKeyWords) {
             this.nameKeyWords = nameKeyWords;
             this.NAME_QUERY = Pattern.compile(getRegexFromString(), Pattern.CASE_INSENSITIVE);
         }
 
-        private String getRegexFromString(){
-        	String result = "";
-        	for(String keyword: nameKeyWords){
-        		for(char c: keyword.toCharArray()){
-        			switch(c){
-        			case '*':
-        				result += ".*";
-        				break;
-        			default:
-        				result += c;
-        			}
-        		}
-        	}
-        	return result;
+        private String getRegexFromString() {
+            String result = "";
+            for (String keyword : nameKeyWords) {
+                for (char c : keyword.toCharArray()) {
+                    switch (c) {
+                    case '*':
+                        result += ".*";
+                        break;
+                    default:
+                        result += c;
+                    }
+                }
+            }
+            return result;
         }
-        
+
         @Override
         public boolean run(ReadOnlyTask person) {
-//            return nameKeyWords.stream()
-//                    .filter(keyword -> StringUtil.containsIgnoreCase(person.getName().fullName, keyword))
-//                    .findAny()
-//                    .isPresent();
-        	Matcher matcher = NAME_QUERY.matcher(person.getTaskDetails().taskDetails);
-        	return matcher.matches() && !person.isComplete();
+            // return nameKeyWords.stream()
+            // .filter(keyword ->
+            // StringUtil.containsIgnoreCase(person.getName().fullName,
+            // keyword))
+            // .findAny()
+            // .isPresent();
+            Matcher matcher = NAME_QUERY.matcher(person.getTaskDetails().taskDetails);
+            return matcher.matches() && !person.isComplete();
         }
-        
+
         @Override
         public String toString() {
             return "name=" + String.join(", ", nameKeyWords);
         }
     }
-    
+
     private class PriorityQualifier implements Qualifier {
         private String priority;
-	    
+
         public PriorityQualifier(String priority) {
             this.priority = priority.replaceFirst("p/", "");
         }
-    	
+
         @Override
         public boolean run(ReadOnlyTask person) {
             return person.getPriority().priorityLevel.equals(this.priority);
