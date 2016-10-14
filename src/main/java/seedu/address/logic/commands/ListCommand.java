0794aa3feae272a79511073f204a99b6ac4ffa1d
linhan <yonglin12@hotmail.com>
2016-10-14 14:55:37 +0800

    Update ListCommand to support list done

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -11,11 +11,22 @@ public class ListCommand extends Command {
     public static final String TOOL_TIP = "list";
 
     public static final String MESSAGE_SUCCESS = "Listed all items";
+    public static final String DONE_MESSAGE_SUCCESS = "Listed all done items";
+    
 
-    public ListCommand() {}
+    Boolean isListDoneCommand;
+    
+    public ListCommand(Boolean isListDoneCommand) {
+        this.isListDoneCommand = isListDoneCommand;
+    }
 
     @Override
     public CommandResult execute() {
+        if (isListDoneCommand) {
+            // do whatever it takes to show the new view here
+            
+            return new CommandResult(DONE_MESSAGE_SUCCESS);
+        }
         model.updateFilteredListToShowAll();
         return new CommandResult(MESSAGE_SUCCESS);
     }
