1f007d9199a806e2d427136e8729abd76ba395cc
Irvin Lim <irvinlimweiquan@gmail.com>
2016-10-14 15:49:18 +0800

    Add UI elements to distinguish completed and incomplete tasks

--- src/main/java/seedu/todo/ui/components/TaskListTaskItem.java
+++ src/main/java/seedu/todo/ui/components/TaskListTaskItem.java
@@ -1,6 +1,8 @@
 package seedu.todo.ui.components;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
@@ -11,6 +13,7 @@ import seedu.todo.ui.UiPartLoader;
 
 public class TaskListTaskItem extends MultiComponent {
 
+    private static final String COMPLETED_ICON_PATH = "/images/icon-tick.png";
     private static final String FXML_PATH = "components/TaskListTaskItem.fxml";
     
     // Props
@@ -26,6 +29,8 @@ public class TaskListTaskItem extends MultiComponent {
     private Text rowIndex;
     @FXML
     private Circle taskCheckMarkCircle;
+    @FXML
+    private ImageView taskCheckMarkImage;
 
     public static TaskListTaskItem load(Stage primaryStage, Pane placeholderPane) {
         return UiPartLoader.loadUiPart(primaryStage, placeholderPane, new TaskListTaskItem());
@@ -41,6 +46,22 @@ public class TaskListTaskItem extends MultiComponent {
         taskText.setText(task.getName());
         taskTime.setText(DateUtil.formatTime(task.getCalendarDT()));
         rowIndex.setText(displayIndex.toString());
+        
+        if (task.isCompleted()) {
+            showCompleted();
+        } else {
+            showIncomplete();
+        }
+    }
+    
+    private void showCompleted() {
+        taskCheckMarkImage.setImage(new Image(COMPLETED_ICON_PATH));
+        taskCheckMarkCircle.setRadius(0);
+        taskText.getStyleClass().add("completed");
+    }
+    
+    private void showIncomplete() {
+        taskCheckMarkImage.setFitWidth(0);
     }
 
 }
