795997726680f900fd2e641c70464c7a671b4344
louietyj <louietyj@gmail.com>
2016-10-14 19:36:57 +0800

    Remove nonsense.

--- src/main/java/seedu/todo/controllers/AddController.java
+++ src/main/java/seedu/todo/controllers/AddController.java
@@ -3,9 +3,13 @@ package seedu.todo.controllers;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import com.joestelmach.natty.*;
 
+import seedu.todo.commons.exceptions.UnmatchedQuotesException;
 import seedu.todo.models.Event;
 import seedu.todo.models.Task;
 import seedu.todo.models.TodoListDB;
@@ -30,80 +34,30 @@ public class AddController implements Controller {
         // TODO
         return (input.startsWith("add")) ? 1 : 0;
     }
-    
-    private int splitByKeyword(String args, boolean isTask) {
-        int indexOfBy = args.lastIndexOf("by");
-        int indexOfOn = args.lastIndexOf("on");
-        
-        // support for event from date to date
-        if (!isTask) {
-            int indexOfFrom = args.lastIndexOf("from");
-            int indexOfTo = args.lastIndexOf("to");
-            if (indexOfTo == -1 || indexOfFrom == -1) {
-                indexOfFrom = -1;
-                indexOfTo = -1;
-            } else {
-                // To do : to support event keyword "from" and "to"
-                return indexOfFrom;
-            }
-        }
-        
-        return Math.max(indexOfBy, indexOfOn);
-        
-    }
 
     @Override
-    public void process(String args) {
-        // TODO: Example of last minute work
+    public void process(String input) {
         
-        args = args.replaceFirst("add", "").trim();
-        
-        // Task or event
-        boolean isTask = true; // Default to task.
-        if (args.startsWith("task")) {
-            isTask = true;
-            args = args.replaceFirst("task", "").trim();
-        } else if (args.startsWith("event")) {
-            isTask = false;
-            args = args.replaceFirst("event", "").trim();
-        }
-        
-        // Parse name and date.
-        int splitValue = splitByKeyword(args, isTask);
-        String naturalDate = "Today"; //Default to Today for floating task.
-        String name = args;
-        if (splitValue != -1) {
-            name = args.substring(0, splitValue);
-            naturalDate = args.substring(splitValue);
-        }
-        
-        // Parse natural date using Natty.
-        Parser parser = new Parser();
-        List<DateGroup> groups = parser.parse(naturalDate);
-        Date date = groups.get(0).getDates().get(0);
-        LocalDateTime ldt = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
         
         // Create and persist task / event.
         TodoListDB db = TodoListDB.getInstance();
         if (isTask) {
             Task newTask = db.createTask();
             newTask.setName(name);
-            newTask.setDueDate(ldt);
-            db.save();
+            newTask.setDueDate(dateFrom);
         } else {
             Event newEvent = db.createEvent();
             newEvent.setName(name);
-            newEvent.setStartDate(ldt);
-            newEvent.setEndDate(ldt); // TODO
-            db.save();
+            newEvent.setStartDate(dateFrom);
+            newEvent.setEndDate(dateTo);
         }
+        db.save();
         
         // Re-render
         IndexView view = UiManager.loadView(IndexView.class);
         view.tasks = db.getAllTasks();
         view.events = db.getAllEvents();
         view.render();
-        
     }
     
 }
