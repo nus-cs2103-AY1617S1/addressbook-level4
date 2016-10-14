f3e7755befd6586a57afa664acbfbea99ae6250d
clarencechee <clarencechee94@gmail.com>
2016-10-14 16:23:17 +0800

    AddCommand: add tip

--- src/main/java/seedu/jimi/logic/commands/AddCommand.java
+++ src/main/java/seedu/jimi/logic/commands/AddCommand.java
@@ -18,7 +18,8 @@ public class AddCommand extends Command {
     public static final String MESSAGE_USAGE = 
             COMMAND_WORD + ": Adds a task to Jimi with one optional tag.\n"
             + "Parameters: \"TASK_DETAILS\" [by DATE_TIME] [t/TAG]\n"
-            + "Example: " + COMMAND_WORD + " \"do dishes\" t/important";
+            + "Example: " + COMMAND_WORD + " \"do dishes\" t/important\n"
+            + "> Tip: typing `a` or `ad` instead of `add` works too.";
 
     public static final String MESSAGE_SUCCESS = "New task added: %1$s";
     public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in Jimi";
