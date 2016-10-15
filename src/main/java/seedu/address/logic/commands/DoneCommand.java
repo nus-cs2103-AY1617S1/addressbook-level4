abd7dcacb95501bca473c1ffb5d0182c80c7fda1
kelvinnpc <kelvinnpc@gmail.com>
2016-10-15 21:26:53 +0800

    add DoneCommand

---
+++ src/main/java/seedu/address/logic/commands/DoneCommand.java
@@ -0,0 +1,63 @@
+package seedu.address.logic.commands;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.UnmodifiableObservableList;
+import seedu.address.model.tag.UniqueTagList;
+import seedu.address.model.task.ReadOnlyTask;
+import seedu.address.model.task.Status;
+import seedu.address.model.task.Task;
+import seedu.address.model.task.TaskType;
+import seedu.address.model.task.UniqueTaskList.TaskNotFoundException;
+
+/**
+ * Set the tasks identified as done using it's last displayed index from the task manager.
+ */
+public class DoneCommand extends Command {
+
+    public static final String COMMAND_WORD = "done";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Set the task identified by the index number used in the last task listing as done.\n"
+            + "Parameters: INDEX (positive integer) [MORE_INDICES] ... \n"
+            + "Example: " + COMMAND_WORD + " 1 3";
+ 
+    public static final String MESSAGE_DONE_TASK_SUCCESS = "Task(s) done: %1$s";
+
+    public final int[] targetIndices;
+
+    public DoneCommand(int[] targetIndices) {
+        this.targetIndices = targetIndices;
+    }
+
+
+    @Override
+    public CommandResult execute() {
+
+        UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
+
+        ArrayList<ReadOnlyTask> tasksDoneList = new ArrayList<>();
+        Task taskDone;
+        
+        for (int i=0; i<targetIndices.length; i++) {
+        	if (lastShownList.size() < targetIndices[i]) {
+                indicateAttemptToExecuteIncorrectCommand();
+                return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+            }
+
+            tasksDoneList.add(lastShownList.get(targetIndices[i] - 1));
+        	taskDone = new Task(lastShownList.get(targetIndices[i] - 1));
+            taskDone.setStatus(new Status("done"));
+            
+            try {
+                model.editTask(targetIndices[i], taskDone);
+            } catch (TaskNotFoundException pnfe) {
+                assert false : "The target task cannot be missing";
+            }
+        }
+        return new CommandResult(String.format(MESSAGE_DONE_TASK_SUCCESS, tasksDoneList));
+    }
+
+}
