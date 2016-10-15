2c933619698aa0482745b2fe0e4524aaab67e562
Wei Kang <weikangchia@gmail.com>
2016-10-15 17:36:03 +0800

    Clear the undoableCmdHist

--- src/main/java/tars/logic/commands/ClearCommand.java
+++ src/main/java/tars/logic/commands/ClearCommand.java
@@ -17,6 +17,7 @@ public class ClearCommand extends Command {
     public CommandResult execute() {
         assert model != null;
         model.resetData(Tars.getEmptyTars());
+        model.getUndoableCmdHist().clear();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
