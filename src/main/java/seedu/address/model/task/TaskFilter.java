8eeb4d5a60e70f7aefddd860f11e766b677cd0d5
JC <knyt.jc@hotmail.com>
2016-10-15 22:13:07 +0800

    Complete filter for tasks

--- src/main/java/seedu/address/model/task/TaskFilter.java
+++ src/main/java/seedu/address/model/task/TaskFilter.java
@@ -1,23 +1,38 @@
 package seedu.address.model.task;
 
 import java.util.function.Predicate;
+import java.time.LocalDateTime;
 
 public class TaskFilter {
 
 	public static Predicate<ReadOnlyTask> isTodayTask() {
-		// check today date
+		return p -> (p.getTaskType().equals(TaskType.Type.EVENT) 
+				&& p.getStartDate().get().toLocalDate().equals(LocalDateTime.now().toLocalDate())
+				|| (p.getTaskType().equals(TaskType.Type.DEADLINE) 
+				&& p.getEndDate().get().toLocalDate().equals(LocalDateTime.now().toLocalDate())));
 	}
 	
 	public static Predicate<ReadOnlyTask> isTomorrowTask() {
-		// check tomorrow date
+		return p -> (p.getTaskType().equals(TaskType.Type.EVENT) 
+				&& p.getStartDate().get().toLocalDate().isAfter(LocalDateTime.now().toLocalDate())
+				&& p.getStartDate().get().toLocalDate().isBefore(LocalDateTime.now().plusDays(2).toLocalDate())
+				|| (p.getTaskType().equals(TaskType.Type.DEADLINE) 
+				&& p.getEndDate().get().toLocalDate().isAfter(LocalDateTime.now().toLocalDate())
+				&& p.getEndDate().get().toLocalDate().isBefore(LocalDateTime.now().plusDays(2).toLocalDate())));
 	}
 	
 	public static Predicate<ReadOnlyTask> isIn7DaysTask() {
-		// check the next 7 days
+		return p -> (p.getTaskType().equals(TaskType.Type.EVENT) 
+				&& p.getStartDate().get().toLocalDate().isBefore(LocalDateTime.now().plusDays(7).toLocalDate())
+				|| (p.getTaskType().equals(TaskType.Type.DEADLINE)
+				&& p.getEndDate().get().toLocalDate().isBefore(LocalDateTime.now().plusDays(7).toLocalDate())));
 	}
 	
 	public static Predicate<ReadOnlyTask> isIn30DaysTask() {
-		// check the next 30 days
+		return p -> (p.getTaskType().equals(TaskType.Type.EVENT) 
+				&& p.getStartDate().get().toLocalDate().isBefore(LocalDateTime.now().plusDays(30).toLocalDate())
+				|| (p.getTaskType().equals(TaskType.Type.DEADLINE)
+				&& p.getEndDate().get().toLocalDate().isBefore(LocalDateTime.now().plusDays(30).toLocalDate())));
 	}
 	
 	public static Predicate<ReadOnlyTask> isSomedayTask() {
