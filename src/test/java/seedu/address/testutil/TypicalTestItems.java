5ae40df7f6d61662c5fc9653b936fea1b4ec917e
Cara Leong <heycraa@gmail.com>
2016-10-16 02:26:23 +0800

    Edit TypicalTestItems with FloatingTask items - dependent on Item not being an abstract class

--- src/test/java/seedu/address/testutil/TypicalTestItems.java
+++ src/test/java/seedu/address/testutil/TypicalTestItems.java
@@ -1,61 +1,59 @@
 package seedu.address.testutil;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.AddressBook;
-import seedu.address.model.person.*;
+import seedu.address.model.TaskBook;
+import seedu.address.model.item.Item;
+import seedu.address.model.item.UniqueItemList;
 
 /**
  *
  */
-public class TypicalTestPersons {
+public class TypicalTestItems {
 
-    public static TestPerson alice, benson, carl, daniel, elle, fiona, george, hoon, ida;
+    public static TestItem always, bags, cs2103, dover, eating, frolick, grass, help, indeed;
 
-    public TypicalTestPersons() {
+    public TypicalTestItems() {
         try {
-            alice =  new PersonBuilder().withName("Alice Pauline").withAddress("123, Jurong West Ave 6, #08-111")
-                    .withEmail("alice@gmail.com").withPhone("85355255")
-                    .withTags("friends").build();
-            benson = new PersonBuilder().withName("Benson Meier").withAddress("311, Clementi Ave 2, #02-25")
-                    .withEmail("johnd@gmail.com").withPhone("98765432")
-                    .withTags("owesMoney", "friends").build();
-            carl = new PersonBuilder().withName("Carl Kurz").withPhone("95352563").withEmail("heinz@yahoo.com").withAddress("wall street").build();
-            daniel = new PersonBuilder().withName("Daniel Meier").withPhone("87652533").withEmail("cornelia@google.com").withAddress("10th street").build();
-            elle = new PersonBuilder().withName("Elle Meyer").withPhone("9482224").withEmail("werner@gmail.com").withAddress("michegan ave").build();
-            fiona = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427").withEmail("lydia@gmail.com").withAddress("little tokyo").build();
-            george = new PersonBuilder().withName("George Best").withPhone("9482442").withEmail("anna@google.com").withAddress("4th street").build();
-
+            always  = new ItemBuilder().withDescription("always brush your teeth123").build();
+            bags    = new ItemBuilder().withDescription("bag, bags, bags, bags").build();
+            cs2103  = new ItemBuilder().withDescription("I want to finish my CS2103 homework").build();
+            dover   = new ItemBuilder().withDescription("Dover Road").build();
+            eating  = new ItemBuilder().withDescription("eat a child").build();
+            frolick = new ItemBuilder().withDescription("frolick in the grass").build();
+            grass   = new ItemBuilder().withDescription("You are allergic to grass").build();
             //Manually added
-            hoon = new PersonBuilder().withName("Hoon Meier").withPhone("8482424").withEmail("stefan@mail.com").withAddress("little india").build();
-            ida = new PersonBuilder().withName("Ida Mueller").withPhone("8482131").withEmail("hans@google.com").withAddress("chicago ave").build();
+            help    = new ItemBuilder().withDescription("\"help\", she yelled!!").build();
+            indeed  = new ItemBuilder().withDescription("indeed I am a very long description but "
+                    + "perhaps this will not be a bad thing??????????????????? but who knows really")
+                    .build();
         } catch (IllegalValueException e) {
             e.printStackTrace();
             assert false : "not possible";
         }
     }
 
-    public static void loadAddressBookWithSampleData(AddressBook ab) {
+    public static void loadTaskBookWithSampleData(TaskBook tb) {
 
         try {
-            ab.addPerson(new Person(alice));
-            ab.addPerson(new Person(benson));
-            ab.addPerson(new Person(carl));
-            ab.addPerson(new Person(daniel));
-            ab.addPerson(new Person(elle));
-            ab.addPerson(new Person(fiona));
-            ab.addPerson(new Person(george));
-        } catch (UniquePersonList.DuplicatePersonException e) {
+            tb.addItem(new Item(always));
+            tb.addItem(new Item(bags));
+            tb.addItem(new Item(cs2103));
+            tb.addItem(new Item(dover));
+            tb.addItem(new Item(eating));
+            tb.addItem(new Item(frolick));
+            tb.addItem(new Item(grass));
+        } catch (UniqueItemList.DuplicateItemException e) {
             assert false : "not possible";
         }
     }
 
-    public TestPerson[] getTypicalPersons() {
-        return new TestPerson[]{alice, benson, carl, daniel, elle, fiona, george};
+    public TestItem[] getTypicalItems() {
+        return new TestItem[]{always, bags, cs2103, dover, eating, frolick, grass};
     }
 
-    public AddressBook getTypicalAddressBook(){
-        AddressBook ab = new AddressBook();
-        loadAddressBookWithSampleData(ab);
-        return ab;
+    public TaskBook getTypicalTaskBook(){
+        TaskBook tb = new TaskBook();
+        loadTaskBookWithSampleData(tb);
+        return tb;
     }
 }
