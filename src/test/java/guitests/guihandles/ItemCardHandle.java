1c09507a1b4d9520940e4981c6e58530fa0b41b8
Cara Leong <heycraa@gmail.com>
2016-10-16 02:43:31 +0800

    Refactor GUI test classes - dependent on Item not being abstract

--- src/test/java/guitests/guihandles/ItemCardHandle.java
+++ src/test/java/guitests/guihandles/ItemCardHandle.java
@@ -3,20 +3,17 @@ package guitests.guihandles;
 import guitests.GuiRobot;
 import javafx.scene.Node;
 import javafx.stage.Stage;
-import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.item.ReadOnlyItem;
 
 /**
  * Provides a handle to a person card in the person list panel.
  */
-public class PersonCardHandle extends GuiHandle {
-    private static final String NAME_FIELD_ID = "#name";
-    private static final String ADDRESS_FIELD_ID = "#address";
-    private static final String PHONE_FIELD_ID = "#phone";
-    private static final String EMAIL_FIELD_ID = "#email";
+public class ItemCardHandle extends GuiHandle {
+    private static final String DESC_FIELD_ID = "#desc";
 
     private Node node;
 
-    public PersonCardHandle(GuiRobot guiRobot, Stage primaryStage, Node node){
+    public ItemCardHandle(GuiRobot guiRobot, Stage primaryStage, Node node){
         super(guiRobot, primaryStage, null);
         this.node = node;
     }
@@ -25,39 +22,26 @@ public class PersonCardHandle extends GuiHandle {
         return getTextFromLabel(fieldId, node);
     }
 
-    public String getFullName() {
-        return getTextFromLabel(NAME_FIELD_ID);
+    public String getFullDescription() {
+        return getTextFromLabel(DESC_FIELD_ID);
     }
 
-    public String getAddress() {
-        return getTextFromLabel(ADDRESS_FIELD_ID);
-    }
-
-    public String getPhone() {
-        return getTextFromLabel(PHONE_FIELD_ID);
-    }
-
-    public String getEmail() {
-        return getTextFromLabel(EMAIL_FIELD_ID);
-    }
-
-    public boolean isSamePerson(ReadOnlyPerson person){
-        return getFullName().equals(person.getName().fullName) && getPhone().equals(person.getPhone().value)
-                && getEmail().equals(person.getEmail().value) && getAddress().equals(person.getAddress().value);
+    public boolean isSameItem(ReadOnlyItem item){
+        return getFullDescription().equals(item.getDescription().getFullDescription());
     }
 
     @Override
     public boolean equals(Object obj) {
-        if(obj instanceof PersonCardHandle) {
-            PersonCardHandle handle = (PersonCardHandle) obj;
-            return getFullName().equals(handle.getFullName())
-                    && getAddress().equals(handle.getAddress()); //TODO: compare the rest
+        if(obj instanceof ItemCardHandle) {
+            ItemCardHandle handle = (ItemCardHandle) obj;
+            return getFullDescription().equals(handle.getFullDescription());
+            //TODO: update with more things to compare
         }
         return super.equals(obj);
     }
 
     @Override
     public String toString() {
-        return getFullName() + " " + getAddress();
+        return getFullDescription();
     }
 }
