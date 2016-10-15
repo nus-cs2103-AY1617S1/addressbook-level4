3b0026cb24972deeff5469cc64a988c503812162
Cara Leong <heycraa@gmail.com>
2016-10-16 01:55:13 +0800

    Refactor AddressBookBuilder to TaskBookBuilder

--- src/test/java/seedu/address/testutil/TaskBookBuilder.java
+++ src/test/java/seedu/address/testutil/TaskBookBuilder.java
@@ -1,35 +1,38 @@
 package seedu.address.testutil;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.item.Item;
+import seedu.address.model.item.UniqueItemList;
 import seedu.address.model.tag.Tag;
-import seedu.address.model.AddressBook;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.TaskBook;
 
 /**
- * A utility class to help with building Addressbook objects.
+ * A utility class to help with building Taskbook objects.
  * Example usage: <br>
- *     {@code AddressBook ab = new AddressBookBuilder().withPerson("John", "Doe").withTag("Friend").build();}
+ *     {@code TaskBook ab = new TaskBookBuilder().withPerson("John", "Doe").withTag("Friend").build();}
  */
-public class AddressBookBuilder {
+public class TaskBookBuilder {
 
-    private AddressBook addressBook;
+    private TaskBook taskBook;
 
-    public AddressBookBuilder(AddressBook addressBook){
-        this.addressBook = addressBook;
+    public TaskBookBuilder(TaskBook taskBook){
+        assert taskBook != null;
+        this.taskBook = taskBook;
     }
 
-    public AddressBookBuilder withPerson(Person person) throws UniquePersonList.DuplicatePersonException {
-        addressBook.addPerson(person);
+    public TaskBookBuilder withItem(Item item) throws UniqueItemList.DuplicateItemException {
+        taskBook.addItem(item);
         return this;
     }
-
-    public AddressBookBuilder withTag(String tagName) throws IllegalValueException {
-        addressBook.addTag(new Tag(tagName));
+    
+    /*
+    public TaskBookBuilder withTag(String tagName) throws IllegalValueException {
+        taskBook.addTag(new Tag(tagName));
         return this;
     }
-
-    public AddressBook build(){
-        return addressBook;
+    */
+    
+    public TaskBook build(){
+        return taskBook;
     }
 }
