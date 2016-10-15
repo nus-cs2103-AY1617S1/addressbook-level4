fcd56080682e3ddb27768ec1029bd844fb9aee2d
Wei Kang <weikangchia@gmail.com>
2016-10-15 17:04:19 +0800

    Add new messages for invalid end date

--- src/main/java/tars/commons/core/Messages.java
+++ src/main/java/tars/commons/core/Messages.java
@@ -8,6 +8,7 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_DATE = "Invalid date time entered!";
+    public static final String MESSAGE_INVALID_END_DATE = "End dateTime should be after start dateTime.";
     public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d tasks listed!";
     public static final String MESSAGE_TASK_CANNOT_BE_FOUND = "Task cannot be found!";
