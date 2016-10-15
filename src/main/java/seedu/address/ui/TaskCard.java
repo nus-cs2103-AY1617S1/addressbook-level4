c71246de87821540769758b02f85d13073243c69
Edmund Mok <edmundmok@outlook.com>
2016-10-15 13:58:33 +0800

    Temporary new task card for now

--- src/main/java/seedu/address/ui/TaskCard.java
+++ src/main/java/seedu/address/ui/TaskCard.java
@@ -26,11 +26,7 @@ public class TaskCard extends UiPart{
     @FXML
     private Label startDate;
     @FXML
-    private Label startTime;
-    @FXML
     private Label endDate;
-    @FXML
-    private Label endTime;
     @FXML
     private Label recurrenceRate;
     @FXML
@@ -72,37 +68,32 @@ public class TaskCard extends UiPart{
         
         id.setText(displayedIndex + ".");
         
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("EEE, d MMM yyyy");
-        SimpleDateFormat timeFormatter = new SimpleDateFormat("h:mm a");
-        String startDateText, endDateText, startTimeText, endTimeText, recurrenceRateText;
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("EEE, d MMM yyyy, h:mm a");
+        String startDateText, endDateText, recurrenceRateText;
         
         startDateText = "";
-        startTimeText = "";
         endDateText = "";
-        endTimeText = "";
         recurrenceRateText = "";
         
         if (task.getStartDate().isPresent()){
-            startDateText = dateFormatter.format(task.getStartDate().get());
-            startTimeText = timeFormatter.format(task.getStartDate().get());
+            startDateText = "Start: " + dateFormatter.format(task.getStartDate().get());
         }
         
         if (task.getEndDate().isPresent()){
-            endDateText = dateFormatter.format(task.getEndDate().get());
-            endTimeText = timeFormatter.format(task.getEndDate().get());
+            endDateText = "End: " + dateFormatter.format(task.getEndDate().get());
         }
         
         
         startDate.setText(startDateText);
-        startTime.setText(startTimeText);
         endDate.setText(endDateText);
-        endTime.setText(endTimeText);
         
         if (task.getRecurrenceRate().isPresent()){
             Integer recurrenceRateInteger = task.getRecurrenceRate().get().recurrenceRate;
             TimePeriod timePeriod = task.getRecurrenceRate().get().timePeriod;
             if (recurrenceRateInteger != null && timePeriod != null) {
-                recurrenceRateText = "every " + recurrenceRateInteger.toString() + " " + timePeriod.toString().toLowerCase() 
+                recurrenceRateText = "every " 
+                        + (recurrenceRateInteger == 1 ? "" : recurrenceRateInteger.toString() + " ")
+                        + timePeriod.toString().toLowerCase() 
                         + (recurrenceRateInteger.intValue() > 1 ? "s" : "");
             } else if (recurrenceRateInteger == null && timePeriod != null) {
                 recurrenceRateText = "every " + timePeriod.toString().toLowerCase();
