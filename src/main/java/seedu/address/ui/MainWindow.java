5861ccc0c68ffe47a2ffd5836fd31aca8e705d72
JingHe <he1994@hotmail.com>
2016-10-15 22:22:15 +0800

    Refactor getFilteredPersonList to getFilteredTaskList

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -104,7 +104,7 @@ public class MainWindow extends UiPart {
     }
 
     void fillInnerParts() {     
-        taskListPanel = TaskListPanel.load(primaryStage, getTaskListPlaceholder(), logic.getFilteredPersonList());
+        taskListPanel = TaskListPanel.load(primaryStage, getTaskListPlaceholder(), logic.getFilteredTaskList());
         resultDisplay = ResultDisplay.load(primaryStage, getResultDisplayPlaceholder());
         statusBarFooter = StatusBarFooter.load(primaryStage, getStatusbarPlaceholder(), config.getAddressBookFilePath());
         commandBox = CommandBox.load(primaryStage, getCommandBoxPlaceholder(), resultDisplay, logic);
