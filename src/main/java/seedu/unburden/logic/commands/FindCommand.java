e938a96da286b99f53ca2cca6465165ab8ff5cca
Nathanael Chan <nathanaelchan94@gmail.com>
2016-10-15 21:40:47 +0800

    Updated the FindCommand and Model classes

--- src/main/java/seedu/unburden/logic/commands/FindCommand.java
+++ src/main/java/seedu/unburden/logic/commands/FindCommand.java
@@ -24,6 +24,7 @@ public class FindCommand extends Command {
 	@Override
     public CommandResult execute() {
         model.updateFilteredTaskList(keywords);
+        model.updateFilteredTaskListForDate(keywords);
         return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredTaskList().size()));
     }
 
