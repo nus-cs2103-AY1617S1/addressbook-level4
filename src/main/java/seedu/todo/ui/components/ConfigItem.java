898b941736e1c40185ce2b6e1026a8ae760a9354
Irvin Lim <irvinlimweiquan@gmail.com>
2016-10-15 23:39:55 +0800

    Added ConfigItem component

---
+++ src/main/java/seedu/todo/ui/components/ConfigItem.java
@@ -0,0 +1,40 @@
+package seedu.todo.ui.components;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import seedu.todo.commons.core.ConfigDefinition;
+import seedu.todo.ui.UiPartLoader;
+
+public class ConfigItem extends MultiComponent {
+
+    private static final String FXML_PATH = "components/ConfigItem.fxml";
+    
+    // Props
+    public ConfigDefinition configDefinition;
+    
+    // FXML
+    @FXML
+    private Text configSetting;
+    @FXML
+    private Text configValue;
+    
+    public static ConfigItem load(Stage primaryStage, Pane placeholder) {
+        return UiPartLoader.loadUiPart(primaryStage, placeholder, new ConfigItem());
+    }
+
+    @Override
+    public String getFxmlPath() {
+        return FXML_PATH;
+    }
+    
+    @Override
+    public void componentDidMount() {
+        if (configDefinition != null) {
+            configSetting.setText(configDefinition.getConfigDescription());
+            configValue.setText(configDefinition.getConfigValue());
+        }
+    }
+
+}
