d879bea65cb1723bbc018a1917a95b8f9b85b03f
Darren Wee <darren.wee@u.nus.edu>
2016-10-17 00:17:37 +0800

    Add constructor for DateTimeParser

--- src/main/java/seedu/address/logic/parser/DateTimeParser.java
+++ src/main/java/seedu/address/logic/parser/DateTimeParser.java
@@ -8,5 +8,19 @@ import com.joestelmach.natty.*;
  * @author darren
  */
 public class DateTimeParser {
+    // the part of the command that contains the temporal part of the command
+    private String datetime;
+    
+    // natty parser object
+    // careful of name collision with our own Parser object
+    private com.joestelmach.natty.Parser parser;
+    
+    DateTimeParser(String temporality) {
+        assert temporality != null;
+        assert temporality.isEmpty() != true;
 
+        this.datetime = temporality;
+        this.parser = new com.joestelmach.natty.Parser();
+    }
+    
 }
