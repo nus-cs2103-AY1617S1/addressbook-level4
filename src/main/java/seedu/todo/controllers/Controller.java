09e841e6bd12bc26e38a500851df298d25879a08
louietyj <louietyj@gmail.com>
2016-10-17 05:07:31 +0800

    Controller documentation.

--- src/main/java/seedu/todo/controllers/Controller.java
+++ src/main/java/seedu/todo/controllers/Controller.java
@@ -1,8 +1,31 @@
 package seedu.todo.controllers;
 
+/**
+ * Contains the logic required to appropriately interpret and process user input
+ * from the views.
+ * 
+ * @author louietyj
+ *
+ */
 public interface Controller {
     
+    /**
+     * Given a user input, returns a measure of confidence of whether the input
+     * should be processed by the controller.
+     * 
+     * @param input
+     *            User input
+     * @return confidence A float in the range [0, 1] where 1 means extremely
+     *         confident.
+     */
     public float inputConfidence(String input);
+    
+    /**
+     * Processes the user input.
+     * 
+     * @param input
+     *            User input
+     */
     public void process(String input);
 
 }
