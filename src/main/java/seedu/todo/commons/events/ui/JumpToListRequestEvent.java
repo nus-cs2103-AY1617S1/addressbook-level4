d0d329005b22691b1aa01f993b4575d402529c1c
jessidew95 <dewi-jessi-jiahui@hotmail.com>
2016-10-16 17:14:42 +0800

    Refactor person to task

--- src/main/java/seedu/todo/commons/events/ui/JumpToListRequestEvent.java
+++ src/main/java/seedu/todo/commons/events/ui/JumpToListRequestEvent.java
@@ -3,7 +3,7 @@ package seedu.todo.commons.events.ui;
 import seedu.todo.commons.events.BaseEvent;
 
 /**
- * Indicates a request to jump to the list of persons
+ * Indicates a request to jump to the list of tasks
  */
 public class JumpToListRequestEvent extends BaseEvent {
 
