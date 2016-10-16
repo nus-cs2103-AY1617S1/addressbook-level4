007deef7f368a08e69f2032b191dee3b2ed0a6fb
unusep <unusep@gmail.com>
2016-10-16 22:05:11 +0800

    Add support for null value in tasktime

--- src/main/java/seedu/oneline/model/task/TaskTime.java
+++ src/main/java/seedu/oneline/model/task/TaskTime.java
@@ -66,7 +66,7 @@ public class TaskTime {
     
     @Override
     public String toString() {
-        return value.toString();
+        return value == null ? "" : value.toString();
     }
 
     @Override
