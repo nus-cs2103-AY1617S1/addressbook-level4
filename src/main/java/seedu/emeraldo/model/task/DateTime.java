1c61df03915a617d0fbb968f2524e853936945ae
JingHe <he1994@hotmail.com>
2016-10-17 00:25:02 +0800

    Fix ArrayIndexOutOfBounds error in DateTime class

--- src/main/java/seedu/emeraldo/model/task/DateTime.java
+++ src/main/java/seedu/emeraldo/model/task/DateTime.java
@@ -11,11 +11,10 @@ import java.time.*;
 public class DateTime {
     
     public static final String MESSAGE_DATETIME_CONSTRAINTS = "Task date and time must follow this format DD/MM/YYYY HH:MM in 24 hours format";
-    //public static final String DATETIME_VALIDATION_REGEX = "(.+)?";
     public static final String DATETIME_VALIDATION_REGEX = "((0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|[1][0-2])/(([0-9][0-9])?[0-9][0-9]))"
                                                             + "( ([01][0-9]|[2][0-3]):([0-5][0-9]))?";
-
-    public final LocalDateTime value;
+    public final String value;
+    public final LocalDateTime valueDateTime;
     public final LocalDate valueDate;
     public final LocalTime valueTime;
 
@@ -30,33 +29,46 @@ public class DateTime {
             throw new IllegalValueException(MESSAGE_DATETIME_CONSTRAINTS);
         }
         
-        String [] dateTimeArr = dateTime.split(" ");
-        String [] dateArr = dateTimeArr[0].split("/");
-        int year = Integer.parseInt(dateArr[2]);
-        int month = Integer.parseInt(dateArr[1]);
-        int day = Integer.parseInt(dateArr[0]);
-        this.valueDate = LocalDate.of(year, month, day);
+        this.value = dateTime;
         
-        String [] timeArr = dateTimeArr[1].split(":");
-        int hour = Integer.parseInt(timeArr[0]);
-        int minute = Integer.parseInt(timeArr[1]);
-        this.valueTime = LocalTime.of(hour, minute);
+        if(dateTime.equals("")){            //Constructing an empty DateTime object 
+            this.valueDateTime = null;
+            this.valueDate = null;
+            this.valueTime = null;
+        }else if(!dateTime.contains(":")){           //Constructing a DateTime object with Date only
+            String [] dateTimeArr = dateTime.split(" ");
+            String [] dateArr = dateTimeArr[0].split("/");
+            int year = Integer.parseInt(dateArr[2]);
+            int month = Integer.parseInt(dateArr[1]);
+            int day = Integer.parseInt(dateArr[0]);
+            this.valueDate = LocalDate.of(year, month, day);
+            
+            this.valueDateTime = null;
+            this.valueTime = null;
+        } else {                                    //Constructing a DateTime object with Date and time
+            String [] dateTimeArr = dateTime.split(" ");
+            String [] dateArr = dateTimeArr[0].split("/");
+            int year = Integer.parseInt(dateArr[2]);
+            int month = Integer.parseInt(dateArr[1]);
+            int day = Integer.parseInt(dateArr[0]);
+            this.valueDate = LocalDate.of(year, month, day);
         
-        this.value = LocalDateTime.of(this.valueDate, this.valueTime);
+            String [] timeArr = dateTimeArr[1].split(":");
+            int hour = Integer.parseInt(timeArr[0]);
+            int minute = Integer.parseInt(timeArr[1]);
+            this.valueTime = LocalTime.of(hour, minute);
         
+            this.valueDateTime = LocalDateTime.of(this.valueDate, this.valueTime);
+        }
     }
 
     private static boolean isValidDateTime(String test) {
-        if(test.equals(""))
-            return true;
-        else
-            return test.matches(DATETIME_VALIDATION_REGEX);
+        return test.matches(DATETIME_VALIDATION_REGEX);
     }
 
     @Override
     public String toString() {
-    	
-    	return value.toString();
+        return value;
     }
 
     @Override
