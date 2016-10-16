ddb18b8ea4134e170a42b600d92c5ad449872ecb
louietyj <louietyj@gmail.com>
2016-10-17 04:48:53 +0800

    CalendarItem documentation

--- src/main/java/seedu/todo/models/CalendarItem.java
+++ src/main/java/seedu/todo/models/CalendarItem.java
@@ -4,11 +4,44 @@ import java.time.LocalDateTime;
 
 public interface CalendarItem {
     
+    /**
+     * Get the display name of the calendar item.
+     * @return name
+     */
     public String getName();
+    
+    /**
+     * Set the display name of the calendar item.
+     * @param name
+     */
     public void setName(String name);
 
+    /**
+     * Get the calendar date of the calendar item. This is mostly for display
+     * and sorting purposes.
+     * 
+     * @return datetime
+     */
     public LocalDateTime getCalendarDT();
+    
+    /**
+     * Set the calendar date of the calendar item. The behavior of this will
+     * depend on the implementation, but it is guaranteed that the variable
+     * being set by this method is the variable that is returned by
+     * <code>getCalendarDT()</code>.<br>
+     * 
+     * It is unclear why one would ever set a calendar item's datetime using
+     * this method, but it is here for completeness.
+     * 
+     * @param datetime
+     */
     public void setCalendarDT(LocalDateTime datetime);
+    
+    /**
+     * Returns true if the calendar item has passed, false otherwise.
+     * 
+     * @return isOver
+     */
     public boolean isOver();
 
 }
