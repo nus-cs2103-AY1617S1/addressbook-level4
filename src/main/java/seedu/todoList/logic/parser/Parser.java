35b34f7ce40946d252c1bb3a48a8d7fb35ef6176
Junsheng <jasoncool93@hotmail.com>
2016-10-16 20:22:52 +0800

    Minor changes to date regex

--- src/main/java/seedu/todoList/logic/parser/Parser.java
+++ src/main/java/seedu/todoList/logic/parser/Parser.java
@@ -29,18 +29,18 @@ public class Parser {
 
     private static final Pattern task_DATA_ARGS_FORMAT = // '/' forward slashes are reserved for delimiter prefixes
             Pattern.compile("(?<name>[^/]+)"
-                    + "d/^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$/"
+                    + "d/^[0-9]{2}/[0-9]{2}/[0-9]{4}$/"
                     + " (?<isPriorityPrivate>p?)p/(?<priority>[^/]+)");
     
     private static final Pattern event_DATA_ARGS_FORMAT = // '/' forward slashes are reserved for delimiter prefixes
             Pattern.compile("(?<name>[^/]+)"
-                    + "d/^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$/"
+                    + "d/^[0-9]{2}/[0-9]{2}/[0-9]{4}$/"
                     + " (?<StartTime>p?)s/(?<StartTime>[^/]+)"
                     + " (?<EndTime>p?)e/(?<EndTime>[^/]+)");
     
     private static final Pattern deadline_DATA_ARGS_FORMAT = // '/' forward slashes are reserved for delimiter prefixes
             Pattern.compile("(?<name>[^/]+)"
-                    + "d/^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$/"
+                    + "d/^[0-9]{2}/[0-9]{2}/[0-9]{4}$/"
                     + " (?<EndTime>p?)e/(?<EndTime>[^/]+)");
 
     public Parser() {}
