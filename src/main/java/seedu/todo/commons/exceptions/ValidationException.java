3c7c6cbe4ae8cf7431dc37903bc98fb49c273962
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-16 16:02:24 +0800

    create empty ErrorBag validation exception

--- src/main/java/seedu/todo/commons/exceptions/ValidationException.java
+++ src/main/java/seedu/todo/commons/exceptions/ValidationException.java
@@ -6,6 +6,11 @@ public class ValidationException extends Exception {
 
     private ErrorBag errors;
     
+    public ValidationException(String message) {
+        super(message);
+        this.errors = new ErrorBag();
+    }
+    
     public ValidationException(String message, ErrorBag errors) {
         super(message);
         this.errors = errors;
