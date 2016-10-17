fd62fe1ceeed83e61e3fd09cda6f73b9c47a3e40
cricketer94 <e0011768@u.nus.edu>
2016-10-17 22:36:06 +0800

    Modify enums with filters

--- src/main/java/seedu/todo/commons/enumerations/TaskViewFilters.java
+++ src/main/java/seedu/todo/commons/enumerations/TaskViewFilters.java
@@ -1,41 +1,36 @@
 package seedu.todo.commons.enumerations;
+import java.time.LocalDateTime;
 import java.util.function.Predicate;
 
 import seedu.todo.model.task.ImmutableTask;
 
 public enum TaskViewFilters {
-    INCOMPLETE {
-        @Override
-        public String toString() {
-          return "incomplete";
-        }
-        
-      },
-      COMPLETED {
-        @Override
-        public String toString() {
-          return "complete";
-        }
-      },
-      DUE_TODAY {
-        @Override
-        public String toString() {
-          return "due today";
-        }
-      },
-      DUE_THIS_WEEK {
-          @Override
-          public String toString() {
-            return "due this week";
-          }
-        },
-      OVERDUE {
-          @Override
-          public String toString() {
-            return "overdue";
-          }
-        }
-     
+    DEFAULT("default", null), 
+    COMPLETE("complete", (task) -> task.isCompleted()), 
+    INCOMPLETE ("incomplete", (task) -> !task.isCompleted()),
+    DUE_TODAY ("due today", (task) -> task.getEndTime().get().toLocalDate().
+               equals(LocalDateTime.now().toLocalDate()) 
+               && !task.isEvent());
+    
+    
+    private final String name;
+    
+    private final Predicate<ImmutableTask> filter;
 
-
+    TaskViewFilters(String name, Predicate<ImmutableTask> filter) {
+        this.name = name;
+        this.filter = filter;
+    }
+    
+    public String getViewName() {
+        return this.name;
+    }
+    
+    public Predicate<ImmutableTask> getFilter() {
+        return this.filter;
+    }
+    
+    
+    
+    
 }
