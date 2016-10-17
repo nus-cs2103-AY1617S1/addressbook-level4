3f3736ba89d70230eb6d1188ec4486c27307c438
Zavier\ZavierOng <zacohjayjay@gmail.com>
2016-10-17 22:13:50 +0800

    edit small errors in regex

--- src/main/java/seedu/address/model/task/Start.java
+++ src/main/java/seedu/address/model/task/Start.java
@@ -10,7 +10,7 @@ import seedu.address.commons.exceptions.IllegalValueException;
 public class Start {
 
     public static final String MESSAGE_START_CONSTRAINTS = "Task start time can be entered in 24hour or 12hour format.";
-    public static final String START_VALIDATION_REGEX = "([012][0-3][0-5]\\d{1})|" +
+    public static final String START_VALIDATION_REGEX = "([012]\\d{1}[0-5]\\d{1})|" +
     													"([1-9](?:pm|am|PM|AM))|" + 
     													"(1[0-2](?:pm|am|PM|AM))|" +
     													"([1-9]\\.[0-5]{1}\\d{1}(?:pm|am))|" +
