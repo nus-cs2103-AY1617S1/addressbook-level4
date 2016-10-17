0c8b991c67e03984a533d5bf6ae2094145055360
lim wei jie <limweijie1994@gmail.com>
2016-10-17 22:31:01 +0800

    Modify UndoCommand

--- src/main/java/seedu/whatnow/logic/commands/UndoCommand.java
+++ src/main/java/seedu/whatnow/logic/commands/UndoCommand.java
@@ -2,6 +2,14 @@ package seedu.whatnow.logic.commands;
 
 public class UndoCommand extends Command{
 
+	public static final String COMMAND_WORD = "undo";
+
+	public static final String MESSAGE_USAGE = COMMAND_WORD + ": Undo previous task in WhatNow "
+			+ "Parameters: No parameters"
+			+ "Example: " + COMMAND_WORD;
+
+	public static final String MESSAGE_SUCCESS = "Undo Successfully";
+	public static final String MESSAGE_FAIL = "Undo failure due to unexisting previous commands";
 	@Override
 	public CommandResult execute() {
 		// TODO Auto-generated method stub
