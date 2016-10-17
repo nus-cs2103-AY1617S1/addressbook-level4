5b623cf4b04e9d0bcc19aa6d6a5d708da2e6092a
Shwetha <shwetha.ravi3@gmail.com>
2016-10-18 00:13:43 +0800

    Pass test case: readAndSaveAddressBook_allinOrder_success
    Needed to change due date regex, don’t know if the previous one was
    working as it should have been.

--- src/test/java/seedu/gtd/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/gtd/testutil/TypicalTestTasks.java
@@ -14,21 +14,21 @@ public class TypicalTestTasks {
 
     public TypicalTestTasks() {
         try {
-            alice =  new TaskBuilder().withName("Alice Pauline").withDueDate("21-10-2016").withAddress("123, Jurong West Ave 6, #08-111")
+            alice =  new TaskBuilder().withName("Alice Pauline").withDueDate("alicegmailcom").withAddress("123, Jurong West Ave 6, #08-111")
                     .withPriority("85355255")
                     .withTags("friends").build();
-            benson = new TaskBuilder().withName("Benson Meier").withDueDate("21-10-2016").withAddress("311, Clementi Ave 2, #02-25")
+            benson = new TaskBuilder().withName("Benson Meier").withDueDate("johndgmailcom").withAddress("311, Clementi Ave 2, #02-25")
                     .withPriority("98765432")
                     .withTags("owesMoney", "friends").build();
-            carl = new TaskBuilder().withName("Carl Kurz").withPriority("95352563").withDueDate("21-10-2016").withAddress("wall street").build();
-            daniel = new TaskBuilder().withName("Daniel Meier").withPriority("87652533").withDueDate("21-10-2016").withAddress("10th street").build();
-            elle = new TaskBuilder().withName("Elle Meyer").withPriority("9482224").withDueDate("21-10-2016").withAddress("michegan ave").build();
-            fiona = new TaskBuilder().withName("Fiona Kunz").withPriority("9482427").withDueDate("21-10-2016").withAddress("little tokyo").build();
-            george = new TaskBuilder().withName("George Best").withPriority("9482442").withDueDate("21-10-2016").withAddress("4th street").build();
+            carl = new TaskBuilder().withName("Carl Kurz").withPriority("95352563").withDueDate("heinzyahoocom").withAddress("wall street").build();
+            daniel = new TaskBuilder().withName("Daniel Meier").withPriority("87652533").withDueDate("corneliagooglecom").withAddress("10th street").build();
+            elle = new TaskBuilder().withName("Elle Meyer").withPriority("9482224").withDueDate("wernergmailcom").withAddress("michegan ave").build();
+            fiona = new TaskBuilder().withName("Fiona Kunz").withPriority("9482427").withDueDate("lydiagmailcom").withAddress("little tokyo").build();
+            george = new TaskBuilder().withName("George Best").withPriority("9482442").withDueDate("annagooglecom").withAddress("4th street").build();
 
             //Manually added
-            hoon = new TaskBuilder().withName("Hoon Meier").withPriority("8482424").withDueDate("21-10-2016").withAddress("little india").build();
-            ida = new TaskBuilder().withName("Ida Mueller").withPriority("8482131").withDueDate("21-10-2016").withAddress("chicago ave").build();
+            hoon = new TaskBuilder().withName("Hoon Meier").withPriority("8482424").withDueDate("stefanmailcom").withAddress("little india").build();
+            ida = new TaskBuilder().withName("Ida Mueller").withPriority("8482131").withDueDate("hansgooglecom").withAddress("chicago ave").build();
         } catch (IllegalValueException e) {
             e.printStackTrace();
             assert false : "not possible";
