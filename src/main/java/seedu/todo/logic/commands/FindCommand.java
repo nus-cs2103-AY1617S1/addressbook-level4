06578bb07917b029a4afde03683e0e2c02927419
cricketer94 <e0011768@u.nus.edu>
2016-10-17 12:31:15 +0800

    Allow for case insensitive search

--- src/main/java/seedu/todo/logic/commands/FindCommand.java
+++ src/main/java/seedu/todo/logic/commands/FindCommand.java
@@ -4,13 +4,12 @@ package seedu.todo.logic.commands;
 
 
 import java.util.List;
-import java.util.function.Predicate;
+
 
 import seedu.todo.commons.exceptions.ValidationException;
 import seedu.todo.logic.arguments.Argument;
 import seedu.todo.logic.arguments.Parameter;
 import seedu.todo.logic.arguments.StringArgument;
-import seedu.todo.model.task.ImmutableTask;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
@@ -38,17 +37,20 @@ public class FindCommand extends BaseCommand {
     }
     @Override
     public CommandResult execute() throws ValidationException {
-        List<String> keywordArray = Lists.newArrayList(Splitter
+        List<String> keywordList = Lists.newArrayList(Splitter
                                     .on(" ")
                                     .trimResults()
                                     .omitEmptyStrings()
-                                    .split(keywords.getValue()));;
-        Predicate<ImmutableTask> p = (task)-> task.getTitle().contains(keywordArray.get(0));
-        for (String keyword: keywordArray){
-            Predicate<ImmutableTask> q = (task) -> task.getTitle().contains(keyword);
-            p = p.or(q);
-        }
-        model.view( p , null );
+                                    .split(keywords.getValue().toLowerCase()));;
+        
+        model.view( task -> {
+           for (String keyword : keywordList) {
+               if (task.getTitle().toLowerCase().contains(keyword)) {
+                   return true;
+                   }
+               }
+           return false;
+           }, null );
         int results = model.getObserveableList().size();
         return taskSuccessfulResult(Integer.toString(results), VERB);
     }
