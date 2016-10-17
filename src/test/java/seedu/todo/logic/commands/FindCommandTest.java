2ad2fc4510c1c296ea971ec292885e5ec7b2db16
cricketer94 <e0011768@u.nus.edu>
2016-10-17 12:31:50 +0800

    Implement FindCommandTest

---
+++ src/test/java/seedu/todo/logic/commands/FindCommandTest.java
@@ -0,0 +1,51 @@
+package seedu.todo.logic.commands;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.todo.commons.exceptions.ValidationException;
+
+public class FindCommandTest extends CommandTest {
+    
+    @Override
+    protected BaseCommand commandUnderTest() {
+        return new FindCommand();
+    }
+
+    
+    @Before
+    public void setUp() throws Exception {
+        model.add("CS2101 Project Task");
+        model.add("CS2103T project");
+        model.add("Unrelated task");
+        model.add("Unrelated CS2101 that expands");
+    }
+    
+
+
+    @Test
+    public void testFindSuccessful() throws ValidationException {
+        assertVisibleTaskCount(4);
+        setParameter("CS2101");
+        execute(true);
+        assertVisibleTaskCount(2);
+        }
+    
+    @Test
+    public void testCaseInsensitive() throws ValidationException{
+        assertVisibleTaskCount(4);
+        setParameter("project");
+        execute(true);
+        assertVisibleTaskCount(2);
+    }
+    
+    @Test
+    public void testMultipleParamters() throws ValidationException{
+        assertVisibleTaskCount(4);
+        setParameter("task expands");
+        execute(true);
+        assertVisibleTaskCount(3);
+    }
+
+}
