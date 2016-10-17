9783702dd577f7f8672aaccc1f3d42d8319cc8b6
njyjn <njyjustin@yahoo.com>
2016-10-18 01:57:06 +0800

    IMPT: Now requiring the use of ' ' to encapsulate details.

--- src/main/java/seedu/cmdo/logic/parser/MainParser.java
+++ src/main/java/seedu/cmdo/logic/parser/MainParser.java
@@ -16,9 +16,7 @@ import com.joestelmach.natty.*;
 import seedu.cmdo.commons.exceptions.IllegalValueException;
 import seedu.cmdo.commons.util.StringUtil;
 import seedu.cmdo.logic.commands.*;
-import seedu.cmdo.model.tag.UniqueTagList;
 import seedu.cmdo.model.task.Priority;
-import seedu.cmdo.model.task.UniqueTaskList.TaskNotFoundException;
 
 /**
  * Parses user input.
@@ -46,6 +44,7 @@ public class MainParser {
 	private static MainParser mainParser;
 	private ArrayList<LocalDateTime> datesAndTimes;
 	private String reducedArgs;
+	private static String detailToAdd;
 	
     /**
      * Private constructor
@@ -74,7 +73,7 @@ public class MainParser {
 
         final String commandWord = matcher.group("commandWord");
         String arguments = matcher.group("arguments");
-        arguments = getCleanString(arguments);
+//        arguments = getCleanString(arguments);
         switch (commandWord) {
 
         case AddCommand.COMMAND_WORD:
@@ -122,24 +121,47 @@ public class MainParser {
      * @return the prepared command
      */
     private Command prepareAdd(String args){
-        String[] splittedArgs = getCleanString(args).split(" ");
-        reducedArgs = extractDueByDateAndTime(args);
-        LocalDateTime dt;
-        if (datesAndTimes.size() != 0)
-        	dt = datesAndTimes.get(0);
-        else
-        	dt = LocalDateTime.of(LocalDate.parse(NO_DATE_DEFAULT, DateTimeFormatter.ISO_LOCAL_DATE), 
-        			LocalTime.MAX);
-        // For testing purposes
-        datesAndTimes.clear();
-    	
-    	try {
-    		return new AddCommand(
-    				extractDetail(reducedArgs),
-    				dt.toLocalDate(),
-    				dt.toLocalTime(),
-    				extractPriority(splittedArgs),
-    				getTagsFromArgs(splittedArgs));
+        try {
+        	extractDetail(args);	// Saves to detailToAdd
+        	reducedArgs = extractDueByDateAndTime(args);
+        	removeTagsAndPriorityMarkers(reducedArgs);
+        	LocalDateTime dt = LocalDateTime.MIN;
+        	LocalDateTime dtStart = LocalDateTime.MIN;
+        	LocalDateTime dtEnd = LocalDateTime.MIN;
+        	String[] splittedArgs = getCleanString(args).split(" ");
+        	// used as flag for task type. 0 for floating, 1 for non-range, 2 for range
+        	int dataMode;
+        	if (datesAndTimes.size() == 1) {
+        		dt = datesAndTimes.get(0);
+        		dataMode = 1;
+        	} else if (datesAndTimes.size() == 2) {
+        		dtStart = datesAndTimes.get(0);
+        		dtEnd = datesAndTimes.get(1);
+        		dataMode = 2;
+        	} else {
+        		dt = LocalDateTime.of(LocalDate.parse(NO_DATE_DEFAULT, DateTimeFormatter.ISO_LOCAL_DATE), 
+        								LocalTime.MAX);
+        		dataMode = 0;
+        	}
+        	// For testing purposes
+        	datesAndTimes.clear();
+    		if (dataMode <= 1) {
+    			return new AddCommand(
+    					detailToAdd,
+    					dt.toLocalDate(),
+    					dt.toLocalTime(),
+    					extractPriority(splittedArgs),
+    					getTagsFromArgs(splittedArgs));
+    		} else {
+    			return new AddCommand(
+    					detailToAdd,
+    					dtStart.toLocalDate(),
+    					dtStart.toLocalTime(),
+    					dtEnd.toLocalDate(),
+    					dtEnd.toLocalTime(),
+    					extractPriority(splittedArgs),
+    					getTagsFromArgs(splittedArgs));
+    		}
     	} catch (IllegalValueException ive) {
     		return new IncorrectCommand(ive.getMessage());
     	}
@@ -152,7 +174,8 @@ public class MainParser {
      * @return the prepared command
      */
     private Command prepareEdit(String args){
-        // Determine if edit command is input correctly
+    	try {
+    	// Determine if edit command is input correctly
     	Optional<Integer> checkForIndex = parseLooseIndex(args);
         if(!checkForIndex.isPresent()){
             return new IncorrectCommand(
@@ -170,6 +193,7 @@ public class MainParser {
         // Store index and remove
         int targetIndex = index;
         args = args.replaceFirst("[0-9]+\\s", "");
+        extractDetail(args);
         
         // Parse date and time
         reducedArgs = extractDueByDateAndTime(args);
@@ -182,17 +206,17 @@ public class MainParser {
         // For testing purposes
         datesAndTimes.clear();
     	
-    	try {
+    	
     		return new EditCommand(
     				targetIndex,
-    				extractDetail(reducedArgs),
+    				detailToAdd,
     				dt.toLocalDate(),
     				dt.toLocalTime(),
     				extractPriority(splittedArgs),
     				getTagsFromArgs(splittedArgs));
     	} catch (IllegalValueException ive) {
     		return new IncorrectCommand(ive.getMessage());
-    	} 
+    	}
     }
 
     /**
@@ -311,6 +335,29 @@ public class MainParser {
     }
     
 //    ============== HELPER METHODS
+    /**
+     * Extracts the detail embedded in user input ' '.
+     * 
+     * @throws IllegalValueException if only one ' found
+     * 
+     * @@author A0139661Y
+     */
+    
+    private static void extractDetail(String args) throws IllegalValueException {
+    	// Check if only one ' used
+    	if (args.lastIndexOf("'") == args.indexOf("'"))
+    		throw new IllegalValueException("Encapsulate your task detauls in ' '.");
+    	// Split into '  ...  '
+    	String[] details = args.split("^'(.+)'$");
+    	// Details only, get rid of anything after the '
+    	String output = new StringBuilder(details[0]).replace(details[0].lastIndexOf("'"), 
+    													details[0].length(), 
+    													"").toString();
+    	// Get rid of the first '
+    	output = output.replaceFirst("'","");
+    	// Save to instance
+    	detailToAdd = output;
+    }
     
     /**
      * Extracts the details out of the reduced args string (without date and time)
@@ -318,14 +365,11 @@ public class MainParser {
      * @param reducedArgs
      * @return a clean string without redundant words, tags, priority
      * 
-     * @author A0139661Y
+     * @@author A0139661Y
      */
-    private String extractDetail(String reducedArgs) {
-		return getCleanString(reducedArgs.replaceAll("\\sby\\s$", " ")
-											.replaceAll("\\son\\s$", " ")
-											.replaceAll("\\sat\\s$", " ")
-											.replaceAll("/[^ ]+", "")
-											.replaceAll("-[^ ]+", ""));
+    private String removeTagsAndPriorityMarkers(String reducedArgs) {
+		return getCleanString(reducedArgs.replaceAll("-[^ ]+", "")
+										.replaceAll("/[^ ]+", ""));
     }
     
 	/**
@@ -334,7 +378,7 @@ public class MainParser {
      * @param splittedArgs an array of split user input
      * @return priority level string.
      * 
-     * @author A0139661Y
+     * @@author A0139661Y
      */ 
     private String extractPriority(String[] splittedArgs) {
     	List<String> rawArgs = Arrays.asList(splittedArgs);
@@ -359,7 +403,7 @@ public class MainParser {
      * This snippet of code uses natty by Joel Ostenmach and its implementation was inspired by
      * https://github.com/cs2103aug2015-t16-1j/fini
      * 
-     * @author A0139661Y
+     * @@author A0139661Y
      */
     public String extractDueByDateAndTime(String dirtyArgs) {
     	Parser parser = new Parser();
@@ -367,22 +411,25 @@ public class MainParser {
     	String cleanArgs = dirtyArgs;
     	
     	try {
-    		DateGroup group = groups.get(0);
-    		List<Date> dateList = group.getDates(); 	// Extract date
-    		Map<String, List<ParseLocation>> parseMap = group.getParseLocations();
-    		if ((!parseMap.containsKey("explicit_time") && parseMap.containsKey("relative_date")) || 
-    				(!parseMap.containsKey("explicit_time") && parseMap.containsKey("formal_date"))) {
-    			for (Date date : dateList) {
-    				LocalDateTime temp = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
-    				datesAndTimes.add(LocalDateTime.of(temp.toLocalDate(), LocalTime.MAX));
-    			}
-    		} else {
-    			for (Date date : dateList) {
-    				datesAndTimes.add(LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault()));
-    			}
-    		}
-    		for (ParseLocation parsedWord : parseMap.get("parse")) {
-    			cleanArgs = cleanArgs.substring(0, parsedWord.getStart() - 1) + cleanArgs.substring(parsedWord.getEnd() -1);
+    		// This retrieves either the start date/time, or the only date/time.
+    		for (int i=0; i<groups.size(); i++) {
+	    		DateGroup group = groups.get(i);
+	    		List<Date> dateList = group.getDates(); 	// Extract date
+	    		Map<String, List<ParseLocation>> parseMap = group.getParseLocations();
+	    		if ((!parseMap.containsKey("explicit_time") && parseMap.containsKey("relative_date")) || 
+	    				(!parseMap.containsKey("explicit_time") && parseMap.containsKey("formal_date"))) {
+	    			for (Date date : dateList) {
+	    				LocalDateTime temp = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+	    				datesAndTimes.add(LocalDateTime.of(temp.toLocalDate(), LocalTime.MAX));
+	    			}
+	    		} else {
+	    			for (Date date : dateList) {
+	    				datesAndTimes.add(LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault()));
+	    			}
+	    		}
+	    		for (ParseLocation parsedWord : parseMap.get("parse")) {
+	    			cleanArgs = cleanArgs.substring(0, parsedWord.getStart() - 1) + cleanArgs.substring(parsedWord.getEnd() -1);
+	    		}
     		}
     		return cleanArgs;	// Return a cleaned up string
     	} catch (IndexOutOfBoundsException e) {
