fdeae3ff79ffd6a5cb30083b984e4912dfdae1e7
Xien Dong Wang <xdrawks@gmail.com>
2016-10-17 17:32:15 +0800

    Feed data to the help panel (command summary)

--- src/main/java/seedu/todo/ui/view/HelpPanel.java
+++ src/main/java/seedu/todo/ui/view/HelpPanel.java
@@ -5,6 +5,8 @@ import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import seedu.todo.commons.core.LogsCenter;
 import seedu.todo.commons.util.FxViewUtil;
@@ -22,6 +24,8 @@ public class HelpPanel extends UiPart {
 
     private final Logger logger = LogsCenter.getLogger(HelpPanel.class);
     private static final String FXML = "HelpPanel.fxml";
+    private static final String STYLE_TEXT_4 = "text4";
+    private static final String STYLE_CODE = "code";
 
     /*Layouts*/
     private AnchorPane placeholder;
@@ -63,15 +67,49 @@ public class HelpPanel extends UiPart {
      * Displays a list of commands into the helpPanelView
      */
     public void displayCommandSummaries(List<CommandSummary> commandSummaries) {
-        
+        this.showHelpPanel();
+        helpGrid.getChildren().clear();
+        int rowIndex = 0;
+        for (CommandSummary commandSummary : commandSummaries) {
+            System.out.println(rowIndex);
+            appendCommandSummary(rowIndex++, commandSummary);
+        }
+    }
+
+    /**
+     * Add a command summary to each row of the helpGrid
+     * @param rowIndex the row number to which the command summary should append to
+     * @param commandSummary to be displayed
+     */
+    private void appendCommandSummary(int rowIndex, CommandSummary commandSummary) {
+        Text commandScenario = FxViewUtil.constructText(commandSummary.scenario, STYLE_TEXT_4);
+        Text commandName = FxViewUtil.constructText(commandSummary.command, STYLE_TEXT_4);
+        Text commandArgument = FxViewUtil.constructText(" " + commandSummary.arguments, STYLE_TEXT_4);
+
+        FxViewUtil.addClassStyle(commandArgument, STYLE_CODE);
+        FxViewUtil.addClassStyle(commandName, STYLE_CODE);
+        FxViewUtil.addClassStyle(commandName, "bolder");
+
+        TextFlow combinedCommand = FxViewUtil.placeIntoTextFlow(commandName, commandArgument);
+        helpGrid.addRow(rowIndex, commandScenario, combinedCommand);
     }
 
     /* Ui Methods */
-    private void hideHelpPanel() {
+    public void hideHelpPanel() {
+        FxViewUtil.setCollapsed(helpPanelView, true);
+    }
+
+    private void showHelpPanel() {
+        FxViewUtil.setCollapsed(helpPanelView, false);
     }
 
 
     /* Override Methods */
+    @Override
+    public void setPlaceholder(AnchorPane placeholder) {
+        this.placeholder = placeholder;
+    }
+
     @Override
     public void setNode(Node node) {
         this.helpPanelView = (VBox) node;
