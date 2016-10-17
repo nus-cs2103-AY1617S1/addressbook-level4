a2fed590fa01777d725bbd56cd2c7b477a9466e6
AloisiusStephen <aloisius.s@u.nus.edu>
2016-10-17 22:59:51 +0800

    Added DoneCommand Class

---
+++ src/main/java/seedu/address/logic/commands/DoneCommand.java
@@ -0,0 +1,52 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.UnmodifiableObservableList;
+import seedu.address.model.task.ReadOnlyTask;
+import seedu.address.model.task.UniqueTaskList.TaskNotFoundException;
+
+/**
+ * Marks the completion of a task identified using it's last displayed index
+ * from the Lifekeeper.
+ */
+public class DoneCommand extends Command {
+
+	public static final String COMMAND_WORD = "done";
+
+	public static final String MESSAGE_USAGE = COMMAND_WORD
+			+ ": Marks the completion of the task identified by the index number shown in the last task listing.\n"
+			+ "Parameters: INDEX (must be a positive integer)\n" + "Example: " + COMMAND_WORD + " 1";
+
+	public static final String MESSAGE_DONE_TASK_SUCCESS = "Task Completed: %1$s";
+
+	public final int targetIndex;
+
+	public DoneCommand(int targetIndex) {
+		this.targetIndex = targetIndex;
+	}
+
+	@Override
+	public CommandResult execute() {
+
+		UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
+
+		if (lastShownList.size() < targetIndex) {
+			indicateAttemptToExecuteIncorrectCommand();
+			return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+		}
+
+		ReadOnlyTask taskToMark = lastShownList.get(targetIndex - 1);
+
+		try {
+			model.deleteTask(taskToMark);
+
+			PreviousCommand doneCommand = new PreviousCommand(COMMAND_WORD, taskToMark);
+			PreviousCommandsStack.push(doneCommand);
+
+		} catch (TaskNotFoundException tnfe) {
+			assert false : "The target task cannot be missing";
+		}
+
+		return new CommandResult(String.format(MESSAGE_DONE_TASK_SUCCESS, taskToMark));
+	}
+}
