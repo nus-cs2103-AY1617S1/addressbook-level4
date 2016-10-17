c6572105f6c9e1cbd004eab329e287ee671ac68c
Wu Zefeng <flamesdesperado@gmail.com>
2016-10-17 19:18:43 +0800

    RelocateCommand modification done.

---
+++ src/main/java/teamfour/tasc/commons/events/storage/RequestOriginalFilePathEvent.java
@@ -0,0 +1,12 @@
+package teamfour.tasc.commons.events.storage;
+
+import teamfour.tasc.commons.events.BaseEvent;
+
+public class RequestOriginalFilePathEvent extends BaseEvent {
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+}
