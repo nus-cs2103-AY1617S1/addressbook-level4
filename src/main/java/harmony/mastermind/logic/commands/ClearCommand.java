945df70350f899145531bf0826d100275c2a4d1c
Albert Yeoh <bertthings@gmail.com>
2016-10-18 01:17:53 +0800

    Revert "add CommandCanceledException"
    This reverts commit 62a690be3e1bd23de9f52ed0cbe2b49ac9c43022.

--- src/main/java/harmony/mastermind/logic/commands/ClearCommand.java
+++ src/main/java/harmony/mastermind/logic/commands/ClearCommand.java
@@ -1,17 +1,14 @@
 package harmony.mastermind.logic.commands;
 
-import harmony.mastermind.commons.exceptions.CommandCanceledException;
 import harmony.mastermind.model.TaskManager;
 
 /**
- * Clears the task manager. Calling this will wipe data, undo history and redo history
+ * Clears the task manager.
  */
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Mastermind has been cleared!";
-    public static final String MESSAGE_FAIL = "No changes made.";
-
     public static final String COMMAND_SUMMARY = "Clearing current tab's tasks:"
             + "\n" + COMMAND_WORD;
 
@@ -21,18 +18,7 @@ public class ClearCommand extends Command {
     @Override
     public CommandResult execute() {
         assert model != null;
-        try {
-            confirmRequest();
-        } catch (CommandCanceledException cce) {
-            return new CommandResult(MESSAGE_FAIL);
-        }
         model.resetData(TaskManager.getEmptyTaskManager());
         return new CommandResult(MESSAGE_SUCCESS);
-    }
-
-
-    private void confirmRequest() throws CommandCanceledException {
-        // TODO Auto-generated method stub
-        
     }
 }
