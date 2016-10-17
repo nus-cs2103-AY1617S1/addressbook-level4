a9dd9b3aa1c4e45ae8ef2ba1e7f99ac73d9d1088
Irvin Lim <irvinlimweiquan@gmail.com>
2016-10-18 03:13:17 +0800

    Abstract renderConfig into concern

--- src/main/java/seedu/todo/controllers/ConfigController.java
+++ src/main/java/seedu/todo/controllers/ConfigController.java
@@ -7,16 +7,16 @@ import seedu.todo.MainApp;
 import seedu.todo.commons.core.Config;
 import seedu.todo.commons.exceptions.CannotConfigureException;
 import seedu.todo.commons.util.ConfigUtil;
+import seedu.todo.controllers.concerns.Renderer;
 import seedu.todo.models.TodoListDB;
 import seedu.todo.ui.UiManager;
-import seedu.todo.ui.views.ConfigView;
 
 public class ConfigController implements Controller {
-    
+
     private static String NAME = "Configure";
     private static String DESCRIPTION = "Shows current configuration settings or updates them.";
     private static String COMMAND_SYNTAX = "config [<setting> <value>]";
-    
+
     private static final String MESSAGE_SHOWING = "Showing all settings.";
     private static final String MESSAGE_SUCCESS = "Successfully updated %s.";
     private static final String MESSAGE_FAILURE = "Could not update settings: %s";
@@ -25,7 +25,7 @@ public class ConfigController implements Controller {
     private static final String SPACE = " ";
     private static final int ARGS_LENGTH = 2;
     private static final String DB_FILE_EXTENSION = ".json";
-    
+
     private static CommandDefinition commandDefinition =
             new CommandDefinition(NAME, DESCRIPTION, COMMAND_SYNTAX);
 
@@ -42,104 +42,94 @@ public class ConfigController implements Controller {
     @Override
     public void process(String input) {
         String params = input.replaceFirst("config", "").trim();
-        
+
         if (params.length() <= 0) {
-            
-            // Update console message
-            UiManager.updateConsoleMessage(MESSAGE_SHOWING);
-        
+
+            // Showing all configs
+            Renderer.renderConfig(MESSAGE_SHOWING);
+
         } else {
-            
+
             String[] args = params.split(SPACE, ARGS_LENGTH);
-            
+
             // Check args length
             if (args.length != ARGS_LENGTH) {
-                failWithMessage(MESSAGE_INVALID_INPUT);
+                Renderer.renderConfig(MESSAGE_INVALID_INPUT);
                 return;
             }
-            
+
             assert args.length == ARGS_LENGTH;
-            
+
             // Split by args
             String configName = args[0];
             String configValue = args[1];
-            
+
             // Get current config
             Config config = MainApp.getConfig();
-            
+
             // Check name
             List<String> validConfigDefinitions = config.getDefinitionsNames();
             if (!validConfigDefinitions.contains(configName)) {
-                failWithMessage(MESSAGE_INVALID_INPUT);
+                Renderer.renderConfig(MESSAGE_INVALID_INPUT);
                 return;
             }
-            
+
             assert validConfigDefinitions.contains(configName);
-            
+
             // Update config value
             try {
                 config = updateConfigByName(config, configName, configValue);
             } catch (CannotConfigureException e) {
-                failWithMessage(e.getMessage());
+                Renderer.renderConfig(e.getMessage());
                 return;
             }
-            
+
             // Save config to file
             try {
                 ConfigUtil.saveConfig(config, MainApp.getConfigFilePath());
             } catch (IOException e) {
-                failWithMessage(String.format(MESSAGE_FAILURE, e.getMessage()));
+                Renderer.renderConfig(String.format(MESSAGE_FAILURE, e.getMessage()));
                 return;
             }
-            
-            // Update console
-            UiManager.updateConsoleMessage(String.format(MESSAGE_SUCCESS, configName));
+
+            // Update console for success
+            Renderer.renderConfig(String.format(MESSAGE_SUCCESS, configName));
         }
-        
-        // Re-render
-        ConfigView view = UiManager.loadView(ConfigView.class);
-        view.render();
     }
-    
-    private void failWithMessage(String message) {
-        ConfigView view = UiManager.loadView(ConfigView.class);
-        view.render();
-        UiManager.updateConsoleMessage(message);
-    }
-    
+
     private Config updateConfigByName(Config config, String configName, String configValue) throws CannotConfigureException {
         switch (configName) {
         case "appTitle" :
             // Updates MainWindow title
             UiManager.getInstance().getMainWindow().setTitle(configValue);
-            
+
             // Update config
             config.setAppTitle(configValue);
-            
+
             break;
-            
+
         case "databaseFilePath" :
             // Make sure the new path has a .json extension
             if (!configValue.endsWith(DB_FILE_EXTENSION)) {
                 throw new CannotConfigureException(String.format(MESSAGE_WRONG_EXTENSION, DB_FILE_EXTENSION));
             }
-            
+
             // Move the DB file to the new location
             try {
                 TodoListDB.getInstance().move(configValue);
             } catch (IOException e) {
                 throw new CannotConfigureException(e.getMessage());
             }
-            
+
             // Update config
             config.setDatabaseFilePath(configValue);
-            
+
             break;
-            
+
         default :
             break;
         }
-        
+
         return config;
     }
 
