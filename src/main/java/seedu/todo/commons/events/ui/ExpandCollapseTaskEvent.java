35e5e16dea9bd178c1c3db20ac52303ffddd7045
Xien Dong Wang <xdrawks@gmail.com>
2016-10-17 13:12:06 +0800

    Change the parameter of ExpandCollapseTaskEvent

--- src/main/java/seedu/todo/commons/events/ui/ExpandCollapseTaskEvent.java
+++ src/main/java/seedu/todo/commons/events/ui/ExpandCollapseTaskEvent.java
@@ -2,22 +2,20 @@ package seedu.todo.commons.events.ui;
 
 import seedu.todo.commons.events.BaseEvent;
 
-import java.util.List;
-
 //@@author A0135805H
 /**
- * An event to tell the Ui to collapse or expand a given list of tasks in the to-do list.
+ * An event to tell the Ui to collapse or expand a given task in the to-do list.
  */
-public class ExpandCollapseTasksEvent extends BaseEvent{
+public class ExpandCollapseTaskEvent extends BaseEvent{
 
-    public final List<Integer> tasksIndexList;
+    public final int taskIndex;
 
     /**
-     * Construct an event that tells the Ui to collapse or expend a given list of tasks in the to-do list.
-     * @param tasksIndexList a list of index of the task that is matching to the Ui (index 1 to num of tasks, inclusive)
+     * Construct an event that tells the Ui to collapse or expend a given task in the to-do list.
+     * @param taskIndex a single index of the task that is matching to the Ui (index 1 to num of tasks, inclusive)
      */
-    public ExpandCollapseTasksEvent(List<Integer> tasksIndexList) {
-        this.tasksIndexList = tasksIndexList;
+    public ExpandCollapseTaskEvent(int taskIndex) {
+        this.taskIndex = taskIndex;
     }
 
     @Override
