62a690be3e1bd23de9f52ed0cbe2b49ac9c43022
Albert Yeoh <bertthings@gmail.com>
2016-10-18 01:17:20 +0800

    add CommandCanceledException Prep prompting user for confirmation

---
+++ src/main/java/harmony/mastermind/commons/exceptions/CommandCanceledException.java
@@ -0,0 +1,13 @@
+package harmony.mastermind.commons.exceptions;
+
+//@author A0139194X
+/**
+ * Exception for when user cancels a command when prompted
+ */
+public class CommandCanceledException extends Exception {
+    
+    public CommandCanceledException() {
+        super();
+    }
+    
+}
--- src/main/java/harmony/mastermind/logic/commands/ClearCommand.java
+++ src/main/java/harmony/mastermind/logic/commands/ClearCommand.java
@@ -1,14 +1,17 @@
 package harmony.mastermind.logic.commands;
 
+import harmony.mastermind.commons.exceptions.CommandCanceledException;
 import harmony.mastermind.model.TaskManager;
 
 /**
- * Clears the task manager.
+ * Clears the task manager. Calling this will wipe data, undo history and redo history
  */
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Mastermind has been cleared!";
+    public static final String MESSAGE_FAIL = "No changes made.";
+
     public static final String COMMAND_SUMMARY = "Clearing current tab's tasks:"
             + "\n" + COMMAND_WORD;
 
@@ -18,7 +21,18 @@ public class ClearCommand extends Command {
     @Override
     public CommandResult execute() {
         assert model != null;
+        try {
+            confirmRequest();
+        } catch (CommandCanceledException cce) {
+            return new CommandResult(MESSAGE_FAIL);
+        }
         model.resetData(TaskManager.getEmptyTaskManager());
         return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+
+    private void confirmRequest() throws CommandCanceledException {
+        // TODO Auto-generated method stub
+        
     }
 }
