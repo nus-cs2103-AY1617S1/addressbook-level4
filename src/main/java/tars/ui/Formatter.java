971d66c2d056694007913a028fb916cce6dabd7f
Wei Kang <weikangchia@gmail.com>
2016-10-17 19:30:01 +0800

    Add formatter for tag

---
+++ src/main/java/tars/ui/Formatter.java
@@ -0,0 +1,57 @@
+package tars.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tars.model.tag.ReadOnlyTag;
+
+public class Formatter {
+    /** Format of indexed list item */
+    private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
+
+    /** A decorative prefix added to the beginning of lines printed by TARS */
+    private static final String LINE_PREFIX = " ";
+
+    /** A platform independent line separator. */
+    private static final String LS = System.lineSeparator();
+
+    /** Offset required to convert between 1-indexing and 0-indexing. */
+    private static final int DISPLAYED_INDEX_OFFSET = 1;
+
+    public String formatTags(List<? extends ReadOnlyTag> tags) {
+        final List<String> formattedTags = new ArrayList<>();
+        for (ReadOnlyTag tag : tags) {
+            formattedTags.add(tag.getAsText());
+        }
+        return format(asIndexedList(formattedTags));
+    }
+
+    /** Formats the given strings for displaying to the user. */
+    public String format(String... messages) {
+        StringBuilder sb = new StringBuilder();
+        for (String m : messages) {
+            sb.append(LINE_PREFIX + m.replace("\n", LS + LINE_PREFIX) + LS);
+        }
+        return sb.toString();
+    }
+
+    /** Formats a list of strings as an indexed list. */
+    private static String asIndexedList(List<String> listItems) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
+        for (String listItem : listItems) {
+            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
+            displayIndex++;
+        }
+        return formatted.toString();
+    }
+
+    /**
+     * Formats a string as an indexed list item.
+     *
+     * @param visibleIndex index for this listing
+     */
+    private static String getIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+}
