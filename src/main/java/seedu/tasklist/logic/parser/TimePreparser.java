f4fb8f1ac495a0d8fe01043d659cae5c365f8c36
lestertj <lester.tan@u.nus.edu>
2016-10-19 00:34:59 +0800

    Fixed time preparser

--- src/main/java/seedu/tasklist/logic/parser/TimePreparser.java
+++ src/main/java/seedu/tasklist/logic/parser/TimePreparser.java
@@ -10,15 +10,16 @@ public class TimePreparser {
         String result = "";
         if (tokens.length >= 1) {
             Pattern dateType = Pattern
-                    .compile("(?<day>[12][0-9]|3[01])[/.-](?<month>[1-9]|1[012])[/.-](?<year>(19|20)\\d\\d)");
+                    .compile("(?<day>[012][0-9]|3[01])[/.-](?<month>[0]?[1-9]|1[012])[/.-](?<year>(19|20)\\d\\d)");
             for (String token : tokens) {
                 Matcher matcher = dateType.matcher(token);
                 if (matcher.matches()) {
+                	System.out.println("token: "+token);
                     String rearrangedDate = matcher.group("month") + "/" + matcher.group("day") + "/"
                             + matcher.group("year");
-                    token = rearrangedDate;
-                }
-                result += token + " ";
+                    System.out.println("rearrangedDate: "+rearrangedDate);
+                    result += rearrangedDate + " ";
+                }   
             }
         } 
         else {
