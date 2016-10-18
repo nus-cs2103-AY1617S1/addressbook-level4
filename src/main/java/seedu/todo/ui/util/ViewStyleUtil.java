c0d663b62025c49e38759eb1597e13e9032aeac3
Xien Dong Wang <xdrawks@gmail.com>
2016-10-18 14:03:48 +0800

    Add new method addRemoveClassStyle in ViewStyleUtil

--- src/main/java/seedu/todo/ui/util/ViewStyleUtil.java
+++ src/main/java/seedu/todo/ui/util/ViewStyleUtil.java
@@ -25,15 +25,32 @@ public class ViewStyleUtil {
     }
 
     /**
+     * Adds or removes class style based on a boolean parameter
+     * @param node to add/remove style onto
+     * @param classStyle to add to/remove from the node
+     * @param isAdding true to add, false to remove
+     */
+    public static void addRemoveClassStyle(Node node, String classStyle, boolean isAdding) {
+        if (isAdding) {
+            addClassStyle(node, classStyle);
+        } else {
+            removeClassStyle(node, classStyle);
+        }
+    }
+
+    /**
      * Toggles the style class:
      *      If supplied style class is available, remove it.
      *      Else, add one instance of it.
+     *  @return true if toggled from OFF -> ON
      */
-    public static void toggleClassStyle(Node node, String classStyle) {
-        if (node.getStyleClass().contains(classStyle)) {
-            removeClassStyle(node, classStyle);
-        } else {
+    public static boolean toggleClassStyle(Node node, String classStyle) {
+        boolean wasPreviouslyOff = !node.getStyleClass().contains(classStyle);
+        if (wasPreviouslyOff) {
             addClassStyle(node, classStyle);
+        } else {
+            removeClassStyle(node, classStyle);
         }
+        return wasPreviouslyOff;
     }
 }
