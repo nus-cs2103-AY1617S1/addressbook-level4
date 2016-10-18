e78947da71a6d7850c27b99e0a0b1faa7bbf4eef
suki <sukitcl@yahoo.com.hk>
2016-10-18 12:48:32 +0800

    Add assertResultMessage and delete unused libraries in test

--- src/test/java/guitests/PinCommandTest.java
+++ src/test/java/guitests/PinCommandTest.java
@@ -1,6 +1,5 @@
 package guitests;
 
-import guitests.guihandles.TaskCardHandle;
 import static org.junit.Assert.assertTrue;
 import static seedu.task.logic.commands.PinCommand.MESSAGE_PIN_TASK_SUCCESS;
 
@@ -8,11 +7,9 @@ import org.junit.Test;
 
 import seedu.task.commons.core.Messages;
 import seedu.task.commons.exceptions.IllegalValueException;
-import seedu.task.logic.commands.UpdateCommand;
-import seedu.task.model.tag.Tag;
+import seedu.task.logic.commands.PinCommand;
 import seedu.task.model.task.*;
 import seedu.task.testutil.TestTask;
-import seedu.task.testutil.TestUtil;
 
 public class PinCommandTest extends TaskManagerGuiTest{
 	@Test
@@ -21,19 +18,23 @@ public class PinCommandTest extends TaskManagerGuiTest{
 	int targetIndex =1;
 	
 	//invalid index
-    commandBox.runCommand("pin " + currentList.length + 1);
+    commandBox.runCommand("pin " + (currentList.length + 1));
     assertResultMessage(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
 	
     //pin the first task
     commandBox.runCommand("pin "+targetIndex);
     ReadOnlyTask newTask = taskListPanel.getTask(targetIndex - 1);
     assertTrue(newTask.getImportance());
+  //confirm the result message is correct
+    assertResultMessage(String.format(MESSAGE_PIN_TASK_SUCCESS, newTask));
+    
     
     //pin another task
     targetIndex=3;
     commandBox.runCommand("pin "+targetIndex);
     ReadOnlyTask otherTask = taskListPanel.getTask(targetIndex - 1);
     assertTrue(otherTask.getImportance());
+    assertResultMessage(String.format(MESSAGE_PIN_TASK_SUCCESS, otherTask));
 	
 	
 	//pin the last task
@@ -41,10 +42,11 @@ public class PinCommandTest extends TaskManagerGuiTest{
 	commandBox.runCommand("pin "+targetIndex);
 	newTask=taskListPanel.getTask(targetIndex - 1);
 	assertTrue(newTask.getImportance());
+	assertResultMessage(String.format(MESSAGE_PIN_TASK_SUCCESS, newTask));
 	
 	//pin at an empty list
 	commandBox.runCommand("clear");
-    commandBox.runCommand("pin " + currentList.length + 1);
+    commandBox.runCommand("pin " + (currentList.length + 1));
     assertResultMessage(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
 	
 	}
