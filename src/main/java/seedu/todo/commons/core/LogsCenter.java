4b145ed32deb4a43fc4aa470f4b0d89053f4bfe8
Irvin Lim <irvinlimweiquan@gmail.com>
2016-10-18 14:15:18 +0800

    Fix codacy issues pt. 2

--- src/main/java/seedu/todo/commons/core/LogsCenter.java
+++ src/main/java/seedu/todo/commons/core/LogsCenter.java
@@ -47,7 +47,10 @@ public class LogsCenter {
     }
 
     private static void addConsoleHandler(Logger logger) {
-        if (consoleHandler == null) consoleHandler = createConsoleHandler();
+        if (consoleHandler == null) {
+            consoleHandler = createConsoleHandler();
+        }
+        
         logger.addHandler(consoleHandler);
     }
 
@@ -60,7 +63,10 @@ public class LogsCenter {
 
     private static void addFileHandler(Logger logger) {
         try {
-            if (fileHandler == null) fileHandler = createFileHandler();
+            if (fileHandler == null) {
+                fileHandler = createFileHandler();
+            }
+            
             logger.addHandler(fileHandler);
         } catch (IOException e) {
             logger.warning("Error adding file handler for logger.");
@@ -84,7 +90,10 @@ public class LogsCenter {
      * Creates a Logger for the given class name.
      */
     public static <T> Logger getLogger(Class<T> clazz) {
-        if (clazz == null) return Logger.getLogger("");
+        if (clazz == null) {
+            return Logger.getLogger("");
+        }
+        
         return getLogger(clazz.getSimpleName());
     }
 
