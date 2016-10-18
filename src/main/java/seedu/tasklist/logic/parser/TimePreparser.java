08d7a77e95e331363302094a8fb4d7718f5cd159
lestertj <lester.tan@u.nus.edu>
2016-10-18 16:14:30 +0800

    Fixed TimePreparser

--- src/main/java/seedu/tasklist/logic/parser/TimePreparser.java
+++ src/main/java/seedu/tasklist/logic/parser/TimePreparser.java
@@ -4,24 +4,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TimePreparser {
-	
+
 	public static String preparse(String input){
 		String[] tokens = input.split(" ");
 		String result = "";
-		if(tokens.length>1){
-		Pattern dateType = Pattern.compile("(?<day>[12][0-9]|3[01])[/.-](?<month>[1-9]|1[012])[/.-](?<year>(19|20)\\d\\d)");
-		for(String token: tokens){
-			Matcher matcher = dateType.matcher(token);
-			if(matcher.matches()){
-				String rearrangedDate = matcher.group("month") + "/" + matcher.group("day")
-									+ "/" + matcher.group("year");
-				token = rearrangedDate;
+		if(tokens.length>=1){
+			Pattern dateType = Pattern.compile("(?<day>[12][0-9]|3[01])[/.-](?<month>[1-9]|1[012])[/.-](?<year>(19|20)\\d\\d)");
+			for(String token: tokens){
+				Matcher matcher = dateType.matcher(token);
+				if(matcher.matches()){
+					String rearrangedDate = matcher.group("month") + "/" + matcher.group("day")
+					+ "/" + matcher.group("year");
+					token = rearrangedDate;
+				}
+				result += token+" ";
 			}
-			result += token+" ";
 		}
-	}
 		else{
-		result = tokens[0];
+			result = tokens[0];
 		}
 		return result.trim();
 	}
