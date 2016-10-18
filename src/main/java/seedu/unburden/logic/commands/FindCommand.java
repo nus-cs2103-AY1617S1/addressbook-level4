31203a93eac10c876a18fcf791553b10a092ee0e
Nathanael Chan <nathanaelchan94@gmail.com>
2016-10-19 00:05:05 +0800

    Edited some changes

--- src/main/java/seedu/unburden/logic/commands/FindCommand.java
+++ src/main/java/seedu/unburden/logic/commands/FindCommand.java
@@ -26,8 +26,8 @@ public class FindCommand extends Command {
 	@Override
     public CommandResult execute() {
 		switch(modeOfSearch){
-			case "date": model.updateFilteredTaskListForDate(keywords); System.out.println("DATE"); break;
-			case "name": model.updateFilteredTaskList(keywords); System.out.println("NAME"); break;
+			case "date": model.updateFilteredTaskListForDate(keywords); break;
+			case "name": model.updateFilteredTaskList(keywords);
 		}
         return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredTaskList().size()));
     }
