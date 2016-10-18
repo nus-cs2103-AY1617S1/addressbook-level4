09151f99ebb8894d6d41b371c4258309f99f77cb
Zavier\ZavierOng <zacohjayjay@gmail.com>
2016-10-18 13:18:27 +0800

    debug small deadline error

--- src/main/java/seedu/address/logic/parser/Parser.java
+++ src/main/java/seedu/address/logic/parser/Parser.java
@@ -36,7 +36,7 @@ public class Parser {
 
     private static final Pattern DEADLINE_DATA_ARGS_FORMAT = // '/' forward slashes are reserved for delimiter prefixes
             Pattern.compile("(?<name>[^;]+)"
-                    + "; (?<date>[^;]+)"
+                    + "; (?<date>[^;#]+)"
                     + "(; (?<end>[^#]+))?"
                     + "(?<tagArguments>(?: #[^#]+)*)"); // variable number of tags
 
