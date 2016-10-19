0fc2a9c48391811f354bce5933ea17da9bb2a43c
clarencechee <clarencechee94@gmail.com>
2016-10-19 22:52:57 +0800

    StoragePathChangedEvent: modify to take in old/new paths

--- src/main/java/seedu/jimi/commons/events/storage/StoragePathChangedEvent.java
+++ src/main/java/seedu/jimi/commons/events/storage/StoragePathChangedEvent.java
@@ -1,18 +1,19 @@
 package seedu.jimi.commons.events.storage;
 
-import seedu.jimi.commons.core.Config;
 import seedu.jimi.commons.events.BaseEvent;
 
 public class StoragePathChangedEvent extends BaseEvent {
     
-    public final Config data;
+    public final String oldPath;
+    public final String newPath;
     
-    public StoragePathChangedEvent(Config data){
-        this.data = data;
+    public StoragePathChangedEvent(String oldPath, String newPath) {
+        this.oldPath = oldPath;
+        this.newPath = newPath;
     }
-
+    
     @Override
     public String toString() {
-        return "Storage Path changed " + data.getTaskBookFilePath();
+        return "Storage Path changed " + newPath;
     }
 }
