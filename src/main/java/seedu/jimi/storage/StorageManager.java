6ce86304950dcc3b0f058d01e6de8c175fe37f8d
clarencechee <clarencechee94@gmail.com>
2016-10-19 22:56:39 +0800

    StorageManger: shift job of copying data to event handling

--- src/main/java/seedu/jimi/storage/StorageManager.java
+++ src/main/java/seedu/jimi/storage/StorageManager.java
@@ -1,21 +1,20 @@
 package seedu.jimi.storage;
 
+import java.io.IOException;
+import java.util.Optional;
+import java.util.logging.Logger;
+
 import com.google.common.eventbus.Subscribe;
 
 import seedu.jimi.commons.core.ComponentManager;
-import seedu.jimi.commons.core.Config;
 import seedu.jimi.commons.core.LogsCenter;
 import seedu.jimi.commons.events.model.AddressBookChangedEvent;
 import seedu.jimi.commons.events.storage.DataSavingExceptionEvent;
 import seedu.jimi.commons.events.storage.StoragePathChangedEvent;
 import seedu.jimi.commons.exceptions.DataConversionException;
-import seedu.jimi.commons.util.ConfigUtil;
 import seedu.jimi.model.ReadOnlyTaskBook;
+import seedu.jimi.model.TaskBook;
 import seedu.jimi.model.UserPrefs;
-
-import java.io.IOException;
-import java.util.Optional;
-import java.util.logging.Logger;
 
 /**
  * Manages storage of TaskBook data in local storage.
@@ -77,7 +76,6 @@ public class StorageManager extends ComponentManager implements Storage {
     public void saveTaskBook(ReadOnlyTaskBook taskBook, String filePath) throws IOException {
         logger.fine("Attempting to write to data file: " + filePath);
         taskBookStorage.saveTaskBook(taskBook, filePath);
-        indicateStoragePathChanged();
     }
 
 
@@ -96,21 +94,12 @@ public class StorageManager extends ComponentManager implements Storage {
     public void handleStoragePathChangedEvent(StoragePathChangedEvent spce) {
         logger.info(LogsCenter.getEventHandlingLogMessage(spce, "Storage file path changed"));
         try {
-            ConfigUtil.saveConfig(spce.data, Config.DEFAULT_CONFIG_FILE);
-        } catch (IOException e) {
+            // copying previous data from old save file to new save file
+            ReadOnlyTaskBook oldTaskBook = readTaskBook(spce.oldPath).orElse(new TaskBook());
+            ((XmlTaskBookStorage) taskBookStorage).setTaskBookFilePath(spce.newPath);
+            saveTaskBook(oldTaskBook);
+        } catch (IOException  | DataConversionException e) {
             raise(new DataSavingExceptionEvent(e));
-        }
+        } 
     }
-    
-    /** Raises an event to indicate the storage has changed */
-    private void indicateStoragePathChanged() {
-        Config config;
-        try {
-            config = ConfigUtil.readConfig(Config.DEFAULT_CONFIG_FILE).orElse(new Config());
-        } catch (DataConversionException e) {
-            config = new Config();
-        }
-        raise(new StoragePathChangedEvent(config));
-    }
-
 }
