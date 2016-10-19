c028fd03aa7b435df5aef2718b910b6adf8d5ca1
clarencechee <clarencechee94@gmail.com>
2016-10-19 22:57:07 +0800

    XmlTaskBookStorage: format and add filepath setter method

--- src/main/java/seedu/jimi/storage/XmlTaskBookStorage.java
+++ src/main/java/seedu/jimi/storage/XmlTaskBookStorage.java
@@ -28,26 +28,31 @@ public class XmlTaskBookStorage implements TaskBookStorage {
         return filePath;
     }
     
+    public void setTaskBookFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+    
     /**
      * Similar to {@link #readTaskBook()}
      * @param filePath location of the data. Cannot be null
      * @throws DataConversionException if the file is not in the correct format.
      */
-    public Optional<ReadOnlyTaskBook> readTaskBook(String filePath) throws DataConversionException, FileNotFoundException {
+    public Optional<ReadOnlyTaskBook> readTaskBook(String filePath)
+            throws DataConversionException, FileNotFoundException {
         assert filePath != null;
-
+        
         File addressBookFile = new File(filePath);
-
+        
         if (!addressBookFile.exists()) {
-            logger.info("TaskBook file "  + addressBookFile + " not found");
+            logger.info("TaskBook file " + addressBookFile + " not found");
             return Optional.empty();
         }
-
+        
         ReadOnlyTaskBook taskBookOptional = XmlFileStorage.loadDataFromSaveFile(new File(filePath));
-
+        
         return Optional.of(taskBookOptional);
     }
-
+    
     /**
      * Similar to {@link #saveTaskBook(ReadOnlyTaskBook)}
      * @param filePath location of the data. Cannot be null
@@ -55,19 +60,20 @@ public class XmlTaskBookStorage implements TaskBookStorage {
     public void saveTaskBook(ReadOnlyTaskBook taskBook, String filePath) throws IOException {
         assert taskBook != null;
         assert filePath != null;
-
+        
         File file = new File(filePath);
         FileUtil.createIfMissing(file);
         XmlFileStorage.saveDataToFile(file, new XmlSerializableTaskBook(taskBook));
     }
-
+    
     @Override
     public Optional<ReadOnlyTaskBook> readTaskBook() throws DataConversionException, IOException {
         return readTaskBook(filePath);
     }
-
+    
     @Override
     public void saveTaskBook(ReadOnlyTaskBook taskBook) throws IOException {
         saveTaskBook(taskBook, filePath);
     }
+    
 }
