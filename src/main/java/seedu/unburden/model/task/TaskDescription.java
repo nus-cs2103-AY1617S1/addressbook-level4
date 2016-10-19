d419d758f7817731500ccd183389bf39617a6edd
Gaurzz <gauri9117@gmail.com>
2016-10-20 04:10:54 +0800

    Created taskDescription command

---
+++ src/main/java/seedu/unburden/model/task/TaskDescription.java
@@ -0,0 +1,53 @@
+package seedu.unburden.model.task;
+
+import seedu.unburden.commons.exceptions.IllegalValueException;
+
+/**
+ * Represents a Task's description in the task manager.
+ * Guarantees: immutable; is valid as declared in {@link #isValidDescription(String)}
+ */
+
+public class TaskDescription {
+	private static final String MESSAGE_TASK_CONSTRAINTS = "Task descriptions should be spaces or alphanumeric characters.";
+	public static final String TASK_VALIDATION_REGEX = "[\\p{Alnum} ]+";
+	
+	public final String fullTaskDescriptions;
+
+	/**
+	 * 
+	 * @param details
+	 *            Validates the given String as a task description
+	 * @throws IllegalValueException
+	 *             if the string passed in is invalid
+	 */
+	public TaskDescription(String details) throws IllegalValueException {
+		assert details != null;
+		details = details.trim();
+		if (isValidDetails(details)) {
+			throw new IllegalValueException(MESSAGE_TASK_CONSTRAINTS);
+		}
+		this.fullTaskDescriptions = details;
+	}
+
+	private boolean isValidDetails(String details) {
+		return details.matches(TASK_VALIDATION_REGEX);
+	}
+
+	@Override
+	public String toString() {
+		return fullTaskDescriptions;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		return other == this // short circuit if same object
+				|| (other instanceof TaskDescription // instanceof handles nulls
+						&& this.fullTaskDescriptions.equals(((TaskDescription) other).fullTaskDescriptions)); 
+	}
+
+	@Override
+	public int hashCode() {
+		return fullTaskDescriptions.hashCode();
+	}
+
+}
