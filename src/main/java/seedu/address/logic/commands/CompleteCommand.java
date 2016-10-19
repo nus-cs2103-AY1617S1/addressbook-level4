aad02536421b3bfef09eba1d3241ee1d825382a5
Yip Bing You <leafytubistfriend@gmail.com>
2016-10-19 16:43:46 +0800

    Remove unused imports according to eclipse

--- src/main/java/seedu/address/logic/commands/CompleteCommand.java
+++ src/main/java/seedu/address/logic/commands/CompleteCommand.java
@@ -3,9 +3,7 @@ package seedu.address.logic.commands;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.UnmodifiableObservableList;
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.tag.UniqueTagList.DuplicateTagException;
 import seedu.address.model.task.ReadOnlyTask;
-import seedu.address.model.task.UniqueTaskList.TaskNotFoundException;
 
 /**
  * Deletes a person identified using it's last displayed index from the address book.
--- src/main/java/seedu/address/logic/commands/UpdateCommand.java
+++ src/main/java/seedu/address/logic/commands/UpdateCommand.java
@@ -4,13 +4,7 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.UnmodifiableObservableList;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.task.*;
-import seedu.address.model.task.UniqueTaskList.TaskNotFoundException;
-import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.UniqueTagList;
-import seedu.address.model.tag.UniqueTagList.DuplicateTagException;
-
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * update the details of a task.
