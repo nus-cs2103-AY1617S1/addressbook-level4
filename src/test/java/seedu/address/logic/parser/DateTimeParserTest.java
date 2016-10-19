cb34f595fadac297cc19c31ae00c22a16715e11d
Darren Wee <darren.wee@u.nus.edu>
2016-10-19 20:19:54 +0800

    Add missing end datetime test for extractors

--- src/test/java/seedu/address/logic/parser/DateTimeParserTest.java
+++ src/test/java/seedu/address/logic/parser/DateTimeParserTest.java
@@ -19,17 +19,33 @@ public class DateTimeParserTest {
     @Test
     public void extractExplicitStartDateTimeTest() {
         String input = "16 september 2016 5pm to 17 september 2016 6pm";
-        LocalDateTime start = LocalDateTime.of(LocalDate.of(2016, 9, 16), LocalTime.of(17, 0));
         DateTimeParser parser = new DateTimeParser(input);
+
+        LocalDateTime start = LocalDateTime.of(LocalDate.of(2016, 9, 16), LocalTime.of(17, 0));
+
         assertEquals(parser.extractStartDate(), start);
     }
     
     @Test
     public void extractExplicitEndDateTimeTest() {
         String input = "16 september 2016 5pm to 17 september 2016 6pm";
-        LocalDateTime end = LocalDateTime.of(LocalDate.of(2016, 9, 17), LocalTime.of(18, 0));
         DateTimeParser parser = new DateTimeParser(input);
+
+        LocalDateTime end = LocalDateTime.of(LocalDate.of(2016, 9, 17), LocalTime.of(18, 0));
+
         assertEquals(parser.extractEndDate(), end);
     }
 
+    @Test
+    public void extractMissingEndDateTimeTest() {
+        String input = "by 12 november 1996 at 5pm";
+        DateTimeParser parser = new DateTimeParser(input);
+
+        LocalDateTime deadline = LocalDateTime.of(LocalDate.of(1996, 11, 12), LocalTime.of(17, 0));
+
+        // extractStartDate and extractEndDate return the same thing if there's only
+        // one date token inside the input string
+        assertEquals(parser.extractStartDate(), deadline);
+        assertEquals(parser.extractEndDate(), deadline);
+    }
 }
