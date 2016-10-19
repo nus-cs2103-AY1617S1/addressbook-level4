9cf209cf68384ffc62e82302ed9cff11e0bad4ad
Kai Yan <kaiyan.yeo@gmail.com>
2016-10-19 22:31:45 +0800

    change SaveAsCommand to SaveCommand

--- src/main/java/seedu/taskmanager/logic/commands/SaveCommand.java
+++ src/main/java/seedu/taskmanager/logic/commands/SaveCommand.java
@@ -17,24 +17,23 @@ import seedu.taskmanager.storage.StorageManager;
 /**
  * Saves the program data file at the specified location
  */
-public class SaveAsCommand extends Command {
+public class SaveCommand extends Command {
     public static final String COMMAND_WORD = "save";
     
     public static final String MESSAGE_ERROR_CONVERTING_FILE = "Error reading from config file: " + Config.DEFAULT_CONFIG_FILE;
     public static final String MESSAGE_SUCCESS = "File path changed! Custom file path specified: %1$s";
     public static final String MESSAGE_SAME_FILE_PATH = "File path is already saved at the specified location!";
     public static final String MESSAGE_ERROR_SAVING_FILE = "Error occured saving to file.";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Saves task manager information to the specified storage file path. \n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Saves task manager information to the specified storage file path.\n"
             + "Parameters: " + COMMAND_WORD + " FILEPATH.xml \n"
             + "Example: " + COMMAND_WORD +  " " + " data/newtaskbook.xml" + "\n"
             + "Note: file path is limited to .xml format";
-//            + "Note: " + COMMAND_WORD + " can be replaced by " + SHORT_COMMAND_WORD + "\n"
     
-    private static final Logger logger = LogsCenter.getLogger(SaveAsCommand.class);
+    private static final Logger logger = LogsCenter.getLogger(SaveCommand.class);
 
     private String newTaskManagerFilePath;
 
-    public SaveAsCommand(String newTaskManagerFilePath) {
+    public SaveCommand(String newTaskManagerFilePath) {
         this.newTaskManagerFilePath = newTaskManagerFilePath;
     }
     
