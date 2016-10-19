049579586f7547dff79a1fe6518183a8fee42a7e
clarencechee <clarencechee94@gmail.com>
2016-10-19 22:56:02 +0800

    SaveAs: Refine code * shift indication of event to command class * shift the job of copying taskbook data to storagemanager * add file path validation check from fileutil * remove redundant messages * remove redundant method * format code

--- src/main/java/seedu/jimi/logic/commands/SaveAsCommand.java
+++ src/main/java/seedu/jimi/logic/commands/SaveAsCommand.java
@@ -5,9 +5,7 @@ import java.io.IOException;
 import seedu.jimi.commons.core.Config;
 import seedu.jimi.commons.exceptions.DataConversionException;
 import seedu.jimi.commons.util.ConfigUtil;
-import seedu.jimi.model.ReadOnlyTaskBook;
-import seedu.jimi.model.TaskBook;
-import seedu.jimi.storage.StorageManager;
+import seedu.jimi.commons.util.FileUtil;
 
 /**
  *  
@@ -19,21 +17,16 @@ public class SaveAsCommand extends Command {
 
     public static final String COMMAND_WORD = "saveas";
 
-    private static final String DEFAULT_FILE_PATH = (new Config()).getTaskBookFilePath();
-    
     public static final String MESSAGE_USAGE = 
             COMMAND_WORD + ": Set a new save directory for all your tasks and events in Jimi.\n"
             + "Parameters: FILEPATH/FILENAME.xml or FILEPATH\\FILENAME.xml \n"
-            + "Jimi rejects these characters in file paths: * & % ? : \" < > \n"
-            + "and these characters in file names: / : * \" < > ? | \\ \n"
             + "Example: " + COMMAND_WORD + " C:/dropbox/taskbook.xml";
 
+    public static final String MESSAGE_INVALID_PATH = "Specified directory is invalid!";
     public static final String MESSAGE_SUCCESS = "Save directory changed: %1$s";
-    public static final String MESSAGE_CONFIG_FILE_NOT_FOUND = "Config file not found. ";
-    public static final String MESSAGE_DUPLICATE_SAVE_DIRECTORY = "New save directory is the same as the old save directory.";
-    public static final String MESSAGE_ERROR_SAVING = 
-            "Error: %1$s \n"
-            + "Reverting back to default save file path: " + DEFAULT_FILE_PATH;
+    public static final String MESSAGE_CONFIG_FILE_NOT_FOUND = "Config file not found.";
+    public static final String MESSAGE_DUPLICATE_SAVE_DIRECTORY =
+            "New save directory is the same as the old save directory.";
     
     private static String configFilePath = Config.DEFAULT_CONFIG_FILE;
     
@@ -47,7 +40,7 @@ public class SaveAsCommand extends Command {
     /**
      * Convenience constructor using raw values.
      */
-    public SaveAsCommand(String filePath)  {
+    public SaveAsCommand(String filePath) {
         this.taskBookFilePath = filePath;
     }
     
@@ -57,6 +50,10 @@ public class SaveAsCommand extends Command {
     
     @Override
     public CommandResult execute() {
+        if (!FileUtil.isValidPath(taskBookFilePath)) {
+            return new CommandResult(MESSAGE_INVALID_PATH);
+        }
+        
         try {
             Config config = ConfigUtil.readConfig(configFilePath).orElse(new Config());
             
@@ -69,33 +66,16 @@ public class SaveAsCommand extends Command {
             
             ConfigUtil.saveConfig(config, configFilePath);
             
-            StorageManager oldStorage = new StorageManager(oldTaskBookFilePath, config.getUserPrefsFilePath());
-            StorageManager newStorage = new StorageManager(taskBookFilePath, config.getUserPrefsFilePath());
-            
-            ReadOnlyTaskBook oldTaskBook = oldStorage.readTaskBook(oldTaskBookFilePath).orElse(new TaskBook());
-            
-            newStorage.saveTaskBook(oldTaskBook);
+            indicateStoragePathChanged(oldTaskBookFilePath, taskBookFilePath);
             
             return new CommandResult(String.format(MESSAGE_SUCCESS, config.getTaskBookFilePath()));
         } catch (DataConversionException e) {
-            return new CommandResult(String.format(MESSAGE_CONFIG_FILE_NOT_FOUND));
+            return new CommandResult(MESSAGE_CONFIG_FILE_NOT_FOUND);
         } catch (IOException e) {
-            saveAsDefaultConfig();
-            return new CommandResult(String.format(MESSAGE_ERROR_SAVING, e.getMessage()));
+            return new CommandResult(e.getMessage());
         }
     }
-
-    /**
-     * Reverts current config to default config specified in Config Class.
-     */
-    private static void saveAsDefaultConfig() {
-        try {
-            ConfigUtil.saveConfig(new Config(), Config.DEFAULT_CONFIG_FILE);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }     
-    }
-
+    
     @Override
     public boolean isValidCommandWord(String commandWord) {
         return commandWord.equals(COMMAND_WORD);
