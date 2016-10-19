f54978c592a0174501bef467afa76d5494494422
Ian Teo <ian_teo@msn.com>
2016-10-19 16:20:23 +0800

    Add numArgs to TaskBuilder

--- src/test/java/seedu/taskitty/testutil/TaskBuilder.java
+++ src/test/java/seedu/taskitty/testutil/TaskBuilder.java
@@ -10,33 +10,40 @@ import seedu.taskitty.model.task.*;
 public class TaskBuilder {
 
     private TestTask task;
+    private int numArgs;
 
     public TaskBuilder() {
         this.task = new TestTask();
+        numArgs = 0;
     }
 
     public TaskBuilder withName(String name) throws IllegalValueException {
         this.task.setName(new Name(name));
+        numArgs++;
         return this;
     }
     
     public TaskBuilder withStartDate(String startDate) throws IllegalValueException {
         this.task.setStartDate(new TaskDate(startDate));
+        numArgs++;
         return this;
     }
     
     public TaskBuilder withStartTime(String startTime) throws IllegalValueException {
         this.task.setStartTime(new TaskTime(startTime));
+        numArgs++;
         return this;
     }
     
     public TaskBuilder withEndDate(String endDate) throws IllegalValueException {
         this.task.setEndDate(new TaskDate(endDate));
+        numArgs++;
         return this;
     }
     
     public TaskBuilder withEndTime(String endTime) throws IllegalValueException {
         this.task.setEndTime(new TaskTime(endTime));
+        numArgs++;
         return this;
     }
 
@@ -48,6 +55,7 @@ public class TaskBuilder {
     }
 
     public TestTask build() {
+        task.setNumArgs(numArgs);
         return this.task;
     }
 
