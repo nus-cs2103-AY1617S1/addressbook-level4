a51f91b21e481bc9897cd2723708e2f04670cd47
Johnervan Lee <johnervan@gmail.com>
2016-10-20 02:40:21 +0800

    Remove priority and tag attributes from RsvTask

--- src/main/java/tars/model/task/rsv/RsvTask.java
+++ src/main/java/tars/model/task/rsv/RsvTask.java
@@ -6,7 +6,6 @@ import java.util.Set;
 import tars.commons.util.CollectionUtil;
 import tars.model.task.DateTime;
 import tars.model.task.Name;
-import tars.model.task.Priority;
 import tars.model.task.Task;
 
 /**
@@ -16,43 +15,43 @@ import tars.model.task.Task;
  */
 
 public class RsvTask extends Task {
-    private Set<DateTime> dateTimeSet;
-    private Set<String> tempTagSet;
     
-    public RsvTask(Name name, Set<DateTime> dateTimeSet, Priority priority, Set<String> tempTagSet) {
-        assert !CollectionUtil.isAnyNull(name, dateTimeSet, priority, tempTagSet);
+    private Name name;
+    private Set<DateTime> dateTimeSet;
+    
+    public RsvTask(Name name, Set<DateTime> dateTimeSet) {
+        assert !CollectionUtil.isAnyNull(name, dateTimeSet);
         
         this.name = name;
         this.dateTimeSet = dateTimeSet;
-        this.priority = priority;
-        this.tempTagSet = tempTagSet;
+
     }
     
     /*
      * Accessors
-     * Note: Accessor methods for the other attributes can be found in parent class, Task.java
      */
-
+    
+    public Name getName() {
+        return name;
+    }
+    
     public Set<DateTime> getDateTimeSet() {
         return dateTimeSet;
     }
     
-    public Set<String> getTempTagSet() {
-        return tempTagSet;
-    }
     
     /*
      * Mutators
-     * Note: Mutators methods for the other attributes can be found in parent class, Task.java
      */
       
+    public void setName(Name name) {
+        this.name = name;
+    }
+    
     public void setDateTimeSet(Set<DateTime> dateTimeSet) {
         this.dateTimeSet = dateTimeSet;
     }
     
-    public void setTempTagSet(Set<String> tempTagSet) {
-        this.tempTagSet = tempTagSet;
-    }
     
     @Override
     public int hashCode() {
@@ -65,11 +64,7 @@ public class RsvTask extends Task {
         final StringBuilder builder = new StringBuilder();
         builder.append(getName())
                 .append(" DateTime: ")
-                .append(getDateTimeSet().toString())
-                .append(" Priority: ")
-                .append(priorityString())
-                .append(" Tags: ")
-                .append(getTempTagSet().toString());
+                .append(getDateTimeSet().toString());
         
         return builder.toString();
     }
