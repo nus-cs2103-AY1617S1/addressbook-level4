e0536a8355937317c01b349e5aace7288157a655
Samson <samsontmr@users.noreply.github.com>
2016-10-19 21:56:39 +0800

    Change app icon

--- src/main/java/seedu/taskmanager/ui/MainWindow.java
+++ src/main/java/seedu/taskmanager/ui/MainWindow.java
@@ -23,7 +23,7 @@ import seedu.taskmanager.model.item.ReadOnlyItem;
  */
 public class MainWindow extends UiPart {
 
-    private static final String ICON = "/images/address_book_32.png";
+    private static final String ICON = "/images/task_manager.png";
     private static final String FXML = "MainWindow.fxml";
     public static final int MIN_HEIGHT = 600;
     public static final int MIN_WIDTH = 450;
