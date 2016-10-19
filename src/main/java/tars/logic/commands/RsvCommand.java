d15a562daf9d2072a192935aa29eae39726b60dc
Johnervan Lee <johnervan@gmail.com>
2016-10-20 02:27:19 +0800

    Add Rsv Del method in RsvCommand

--- src/main/java/tars/logic/commands/RsvCommand.java
+++ src/main/java/tars/logic/commands/RsvCommand.java
@@ -1,6 +1,7 @@
 package tars.logic.commands;
 
 import java.time.DateTimeException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -22,18 +23,29 @@ import tars.model.task.rsv.RsvTask;
 public class RsvCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "rsv";
+    public static final String COMMAND_WORD_DEL = "rsv -d";
+    public static final String COMMAND_WORD_EDIT = "rsv -e";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Reserves one or more timeslot for a task.\n"
             + "Parameters: TASK [-dt DATETIME] [ADDITIONAL DATETIME]\n" + "Example: " + COMMAND_WORD
             + " Meet John Doe -dt 26/09/2016 0900 to 1030, 28/09/2016 1000 to 1130";
-    
+
+    public static final String MESSAGE_USAGE_DEL = COMMAND_WORD_DEL
+            + ": Deletes a reserved task in the last reserved task listing \n"
+            + "Parameters: INDEX (must be a positive integer)\n " + "Example: " + COMMAND_WORD_DEL + " 1\n" + "OR "
+            + COMMAND_WORD_DEL + " 1..3";
+
     public static final String MESSAGE_DATETIME_NOTFOUND = "At least one DateTime is required!\n" + MESSAGE_USAGE;
+    
+    public static final String MESSAGE_DEL_AND_EDIT_FLAG_DETECTED = "Both Delete and Edit Flag detected!";
 
     public static final String MESSAGE_SUCCESS = "New task reserved: %1$s";
+    public static final String MESSAGE_SUCCESS_DEL = "Deleted Reserved Tasks: %1$s";
     public static final String MESSAGE_UNDO = "Removed %1$s";
     public static final String MESSAGE_REDO = "Reserved %1$s";
 
-    private final RsvTask toReserve;
+    private RsvTask toReserve = null;
+    private String rangeIndexString = "";
 
     /**
      * Convenience constructor using raw values.
@@ -56,6 +68,10 @@ public class RsvCommand extends UndoableCommand {
 
     }
 
+    public RsvCommand(String rangeIndexString) {
+        this.rangeIndexString = rangeIndexString;
+    }
+
     @Override
     public CommandResult undo() {
         // TODO Auto-generated method stub
@@ -71,15 +87,21 @@ public class RsvCommand extends UndoableCommand {
     @Override
     public CommandResult execute() {
         assert model != null;
-        try {
-            model.addRsvTask(toReserve);
-            model.getUndoableCmdHist().push(this);
-            return new CommandResult(String.format(MESSAGE_SUCCESS, toReserve.toString()));
-        } catch (DuplicateTaskException e) {
-            return new CommandResult(Messages.MESSAGE_DUPLICATE_TASK);
+
+        if (toReserve != null) {
+            try {
+                model.addRsvTask(toReserve);
+                model.getUndoableCmdHist().push(this);
+                return new CommandResult(String.format(MESSAGE_SUCCESS, toReserve.toString()));
+            } catch (DuplicateTaskException e) {
+                return new CommandResult(Messages.MESSAGE_DUPLICATE_TASK);
+            }
+        } else {
+            ArrayList<RsvTask> tasksToDelete = new ArrayList<RsvTask>();
+
+            return new CommandResult(String.format(MESSAGE_SUCCESS_DEL, tasksToDelete.toString()));
         }
 
     }
 
 }
-
