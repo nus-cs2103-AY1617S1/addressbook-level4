8e127fb22f8b2195119c39d2c7b136122b4e626b
Khoo Kay Leng Desmond <a0126633@u.nus.edu>
2016-10-19 17:31:03 +0800

    Enhance find command to find only floating task, deadline or schedule

--- src/main/java/seedu/malitio/logic/commands/FindCommand.java
+++ src/main/java/seedu/malitio/logic/commands/FindCommand.java
@@ -9,24 +9,39 @@ import java.util.Set;
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
-
+    
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all tasks whose names contain any of "
             + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
     private final Set<String> keywords;
+    private final String type;
 
-    public FindCommand(Set<String> keywords) {
+    public FindCommand(String type, Set<String> keywords) {
         this.keywords = keywords;
+        this.type = type;
     }
 
     @Override
     public CommandResult execute() {
-        model.updateFilteredTaskList(keywords);
-        model.updateFilteredDeadlineList(keywords);
-        model.updateFilteredEventList(keywords);
-        
+
+        switch (type) {
+        case "f": 
+            model.updateFilteredTaskList(keywords);
+            break;
+        case "d":
+            model.updateFilteredDeadlineList(keywords);
+            break;
+        case "e":
+            model.updateFilteredEventList(keywords);
+            break;
+        default:
+            model.updateFilteredTaskList(keywords);
+            model.updateFilteredDeadlineList(keywords);
+            model.updateFilteredEventList(keywords);    
+        }
+ 
         return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredFloatingTaskList().size()));
     }
 
