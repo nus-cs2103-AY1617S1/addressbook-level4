fdebb77df1eacc563beefcfe8e0dd7a38e73e290
Khoo Kay Leng Desmond <a0126633@u.nus.edu>
2016-10-19 23:39:49 +0800

    Fixed error in DeadlineListPanel

--- src/main/java/seedu/malitio/ui/DeadlineListPanel.java
+++ src/main/java/seedu/malitio/ui/DeadlineListPanel.java
@@ -1,172 +1,107 @@
 package seedu.malitio.ui;
 
-
-import guitests.GuiRobot;
-import guitests.guihandles.DeadlineCardHandle;
-import javafx.geometry.Point2D;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import seedu.malitio.TestApp;
-import seedu.malitio.testutil.TestUtil;
-import seedu.malitio.model.task.Deadline;
+import seedu.malitio.commons.core.LogsCenter;
+import seedu.malitio.commons.events.ui.TaskPanelSelectionChangedEvent;
 import seedu.malitio.model.task.ReadOnlyDeadline;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
-import static org.junit.Assert.assertTrue;
+import java.util.logging.Logger;
 
 /**
- * Provides a handle for the panel containing the floating task list.
+ * Panel containing the list of deadlines.
  */
-public class DeadlineListPanel extends GuiHandle {
+public class DeadlineListPanel extends UiPart {
+    private final Logger logger = LogsCenter.getLogger(DeadlineListPanel.class);
+    private static final String FXML = "DeadlineListPanel.fxml";
+    private VBox panel;
+    private AnchorPane placeHolderPane;
 
-    public static final int NOT_FOUND = -1;
-    public static final String CARD_PANE_ID = "#cardPane";
+    @FXML
+    private ListView<ReadOnlyDeadline> deadlineListView;
 
-    private static final String TASK_LIST_VIEW_ID = "#taskListView";
-
-    public DeadlineListPanel(GuiRobot guiRobot, Stage primaryStage) {
-        super(guiRobot, primaryStage, TestApp.APP_TITLE);
+    public DeadlineListPanel() {
+        super();
     }
 
-    public List<ReadOnlyDeadline> getSelectedTasks() {
-        ListView<ReadOnlyDeadline> taskList = getListView();
-        return taskList.getSelectionModel().getSelectedItems();
+    @Override
+    public void setNode(Node node) {
+        panel = (VBox) node;
     }
 
-    public ListView<ReadOnlyDeadline> getListView() {
-        return (ListView<ReadOnlyDeadline>) getNode(TASK_LIST_VIEW_ID);
+    @Override
+    public String getFxmlPath() {
+        return FXML;
     }
 
-    /**
-     * Returns true if the list is showing the task details correctly and in correct order.
-     * @param tasks A list of task in the correct order.
-     */
-    public boolean isListMatching(ReadOnlyDeadline... tasks) {
-        return this.isListMatching(0, tasks);
-    }
-    
-    /**
-     * Clicks on the ListView.
-     */
-    public void clickOnListView() {
-        Point2D point= TestUtil.getScreenMidPoint(getListView());
-        guiRobot.clickOn(point.getX(), point.getY());
+    @Override
+    public void setPlaceholder(AnchorPane pane) {
+        this.placeHolderPane = pane;
     }
 
-    /**
-     * Returns true if the {@code tasks} appear as the sub list (in that order) at position {@code startPosition}.
-     */
-    public boolean containsInOrder(int startPosition, ReadOnlyDeadline... tasks) {
-        List<ReadOnlyDeadline> tasksInList = getListView().getItems();
+    public static DeadlineListPanel load(Stage primaryStage, AnchorPane deadlineListPanelPlaceholder,
+                                       ObservableList<ReadOnlyDeadline> deadlineList) {
+        DeadlineListPanel deadlineListPanel =
+                UiPartLoader.loadUiPart(primaryStage, deadlineListPanelPlaceholder, new DeadlineListPanel());
+        deadlineListPanel.configure(deadlineList);
+        return deadlineListPanel;
+    }
 
-        // Return false if the list in panel is too short to contain the given list
-        if (startPosition + tasks.length > tasksInList.size()){
-            return false;
-        }
+    private void configure(ObservableList<ReadOnlyDeadline> deadlineList) {
+        setConnections(deadlineList);
+        addToPlaceholder();
+    }
 
-        // Return false if any of the tasks doesn't match
-        for (int i = 0; i < tasks.length; i++) {
-            if (!tasksInList.get(startPosition + i).getName().fullName.equals(tasks[i].getName().fullName)){
-                return false;
+    private void setConnections(ObservableList<ReadOnlyDeadline> deadlineList) {
+        deadlineListView.setItems(deadlineList);
+        deadlineListView.setCellFactory(listView -> new DeadlineListViewCell());
+        setEventHandlerForSelectionChangeEvent();
+    }
+
+    private void addToPlaceholder() {
+        SplitPane.setResizableWithParent(placeHolderPane, false);
+        placeHolderPane.getChildren().add(panel);
+    }
+
+    private void setEventHandlerForSelectionChangeEvent() {
+        deadlineListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                logger.fine("Selection in task list panel changed to : '" + newValue + "'");
+                raise(new TaskPanelSelectionChangedEvent(newValue));
             }
-        }
-
-        return true;
-    }
-
-    /**
-     * Returns true if the list is showing the task details correctly and in correct order.
-     * @param startPosition The starting position of the sub list.
-     * @param tasks A list of task in the correct order.
-     */
-    public boolean isListMatching(int startPosition, ReadOnlyDeadline... tasks) throws IllegalArgumentException {
-        if (tasks.length + startPosition != getListView().getItems().size()) {
-            throw new IllegalArgumentException("List size mismatched\n" +
-                    "Expected " + (getListView().getItems().size() - 1) + " tasks");
-        }
-        assertTrue(this.containsInOrder(startPosition, tasks));
-        for (int i = 0; i < tasks.length; i++) {
-            final int scrollTo = i + startPosition;
-            guiRobot.interact(() -> getListView().scrollTo(scrollTo));
-            guiRobot.sleep(200);
-            if (!TestUtil.compareCardAndTask(getTaskCardHandle(startPosition + i), tasks[i])) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-
-    public DeadlineCardHandle navigateToTask(String name) {
-        guiRobot.sleep(500); //Allow a bit of time for the list to be updated
-        final Optional<ReadOnlyDeadline> task = getListView().getItems().stream().filter(p -> p.getName().fullName.equals(name)).findAny();
-        if (!task.isPresent()) {
-            throw new IllegalStateException("Name not found: " + name);
-        }
-
-        return navigateToTask(task.get());
-    }
-
-    /**
-     * Navigates the listview to display and select the task.
-     */
-    public DeadlineCardHandle navigateToTask(ReadOnlyDeadline task) {
-        int index = getTaskIndex(task);
-
-        guiRobot.interact(() -> {
-            getListView().scrollTo(index);
-            guiRobot.sleep(150);
-            getListView().getSelectionModel().select(index);
         });
-        guiRobot.sleep(100);
-        return getTaskCardHandle(task);
     }
 
+    public void scrollTo(int index) {
+        Platform.runLater(() -> {
+            deadlineListView.scrollTo(index);
+            deadlineListView.getSelectionModel().clearAndSelect(index);
+        });
+    }
 
-    /**
-     * Returns the position of the task given, {@code NOT_FOUND} if not found in the list.
-     */
-    public int getTaskIndex(ReadOnlyDeadline targetTask) {
-        List<ReadOnlyDeadline> tasksInList = getListView().getItems();
-        for (int i = 0; i < tasksInList.size(); i++) {
-            if(tasksInList.get(i).getName().equals(targetTask.getName())){
-                return i;
+    class DeadlineListViewCell extends ListCell<ReadOnlyDeadline> {
+
+        public DeadlineListViewCell() {
+        }
+
+        @Override
+        protected void updateItem(ReadOnlyDeadline deadline, boolean empty) {
+            super.updateItem(deadline, empty);
+
+            if (empty || deadline == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(DeadlineCard.load(deadline, getIndex() + 1).getLayout());
             }
         }
-        return NOT_FOUND;
     }
 
-    /**
-     * Gets a task from the list by index
-     */
-    public ReadOnlyDeadline getTask(int index) {
-        return getListView().getItems().get(index);
-    }
-
-    public DeadlineCardHandle getTaskCardHandle(int index) {
-        return getTaskCardHandle(new Deadline(getListView().getItems().get(index)));
-    }
-
-    public DeadlineCardHandle getTaskCardHandle(ReadOnlyDeadline task) {
-        Set<Node> nodes = getAllCardNodes();
-        Optional<Node> taskCardNode = nodes.stream()
-                .filter(n -> new DeadlineCardHandle(guiRobot, primaryStage, n).isSameTask(task))
-                .findFirst();
-        if (taskCardNode.isPresent()) {
-            return new DeadlineCardHandle(guiRobot, primaryStage, taskCardNode.get());
-        } else {
-            return null;
-        }
-    }
-
-    protected Set<Node> getAllCardNodes() {
-        return guiRobot.lookup(CARD_PANE_ID).queryAll();
-    }
-
-    public int getNumberOfTasks() {
-        return getListView().getItems().size();
-    }
 }
