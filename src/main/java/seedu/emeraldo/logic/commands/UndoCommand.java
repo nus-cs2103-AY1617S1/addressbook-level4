b76da5f17101004fb28b4110bb7209f7269d4dc3
zhiminglim <i-jovial@hotmail.com>
2016-10-19 13:23:11 +0800

    Add UndoCommand.java

---
+++ src/main/java/seedu/emeraldo/logic/commands/UndoCommand.java
@@ -0,0 +1,13 @@
+package seedu.emeraldo.logic.commands;
+
+public class UndoCommand extends Command {
+
+    public UndoCommand(){}
+    
+    @Override
+    public CommandResult execute() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
