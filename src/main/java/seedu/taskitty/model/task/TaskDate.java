82c04bb213af395f5ab4b2f6660f620938216f31
Ian Teo <ian_teo@msn.com>
2016-10-20 15:32:53 +0800

    Improve model for TaskDate

--- src/main/java/seedu/taskitty/model/task/TaskDate.java
+++ src/main/java/seedu/taskitty/model/task/TaskDate.java
@@ -2,17 +2,18 @@ package seedu.taskitty.model.task;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 import seedu.taskitty.commons.exceptions.IllegalValueException;
 
 /**
  * Represents a Task's date in the task manager.
- * Guarantees: immutable; is valid as declared in {@link #isValidDate(String)}
+ * Guarantees: immutable; is valid as declared in {@link #isValidDateFormat(String)}
  */
 public class TaskDate {
 
     public static final String MESSAGE_DATE_CONSTRAINTS =
-            "Task dates should be in the format dd/mm/yyyy or ddmmyyyy or dd monthname yyyy";
+            "Task dates should be in the format dd/mm/yyyy";
     public static final String MESSAGE_DATE_INVALID =
             "Task date provided is invalid!";
     public static final String MESSAGE_DATE_MISSING =
@@ -20,7 +21,7 @@ public class TaskDate {
     public static final String DATE_FORMAT_STRING = "dd/MM/yyyy";
     public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_FORMAT_STRING);
     
-    //format: mm/dd/yyyy
+    //format: dd/mm/yyyy
     private static final String DATE_VALIDATION_REGEX = "[\\p{Digit}]{1,2}/[\\p{Digit}]{1,2}/[\\p{Digit}]{4}";
 
     public final LocalDate date;
@@ -36,17 +37,22 @@ public class TaskDate {
         }
         
         date = date.trim();
-        if (!isValidDate(date)) {
+      //This is not an assert because user can change the database and input wrong formats
+        if (!isValidDateFormat(date)) {
             throw new IllegalValueException(MESSAGE_DATE_CONSTRAINTS);
         }
         
-        this.date = LocalDate.parse(date, DATE_FORMATTER);
+        try {
+            this.date = LocalDate.parse(date, DATE_FORMATTER);
+        } catch (DateTimeParseException dtpe){
+            throw new IllegalValueException(MESSAGE_DATE_INVALID);
+        }
     }
 
     /**
      * Returns true if a given string is a valid person name.
      */
-    public static boolean isValidDate(String test) {
+    public static boolean isValidDateFormat(String test) {
         return test.matches(DATE_VALIDATION_REGEX);
     }
 
