be0a7eca753458584c2187d836de71149c9a46ae
cheo1994 <tehcy2@msn.com>
2016-10-20 14:59:02 +0800

    added test case for edit task name, startTime, endTime

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -374,8 +374,55 @@ public class LogicManagerTest {
                 expectedAB,
                 expectedList);
     }
-
-
+    
+    @Test
+    public void execute_edit_taskName() throws Exception {
+        TestDataHelper helper = new TestDataHelper();
+        Task pTarget1 = helper.generateTaskWithName("task 1");
+        Task pTarget2 = helper.generateTaskWithName("old name to change");
+        List<Task> twoTasks = helper.generateTaskList(pTarget1, pTarget2);
+        
+        TaskManager expectedAB = helper.generateTaskManager(twoTasks);
+        expectedAB.editTaskName(twoTasks.get(1), "new name");
+        helper.addToModel(model, twoTasks);
+        
+        assertCommandBehavior("edit 2 new name",
+                String.format(EditCommand.MESSAGE_EDIT_TASK_SUCCESS, twoTasks.get(1)),
+                expectedAB,
+                expectedAB.getTaskList());
+    }
+    
+    @Test
+    public void execute_edit_taskStartTime() throws Exception {
+        TestDataHelper helper = new TestDataHelper();
+        List<Task> threeTasks = helper.generateTaskList(3);
+        
+        TaskManager expectedAB = helper.generateTaskManager(threeTasks);
+        expectedAB.editTaskStartTime(threeTasks.get(1), "from 3pm tomorrow");
+        helper.addToModel(model, threeTasks);
+        
+        assertCommandBehavior("edit 2 from 3pm tomorrow",
+                String.format(EditCommand.MESSAGE_EDIT_TASK_SUCCESS, threeTasks.get(1)),
+                expectedAB,
+                expectedAB.getTaskList());
+    }
+    
+    @Test
+    public void execute_edit_taskEndTime() throws Exception {
+        TestDataHelper helper = new TestDataHelper();
+        List<Task> threeTasks = helper.generateTaskList(3);
+        
+        TaskManager expectedAB = helper.generateTaskManager(threeTasks);
+        expectedAB.editTaskEndTime(threeTasks.get(1), "by 3pm tomorrow");
+        helper.addToModel(model, threeTasks);
+        
+        assertCommandBehavior("edit 2 by 3pm tomorrow",
+                String.format(EditCommand.MESSAGE_EDIT_TASK_SUCCESS, threeTasks.get(1)),
+                expectedAB,
+                expectedAB.getTaskList());
+    } 
+    
+    
     /**
      * A utility class to generate test data.
      */
