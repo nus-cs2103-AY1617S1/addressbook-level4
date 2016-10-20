d12ba525754ae037bb56ee3603ddc356e7cf0a6c
KnytJC <knyt.jc@hotmail.com>
2016-10-20 09:03:26 +0800

    Update ListCommand.java

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -33,6 +33,6 @@ public class ListCommand extends Command {
     @Override
     public CommandResult execute() {
     	model.updateFilteredListToShowAll();
-	return new CommandResult(MESSAGE_SUCCESS);
+	 return new CommandResult(MESSAGE_SUCCESS);
     }
 }
