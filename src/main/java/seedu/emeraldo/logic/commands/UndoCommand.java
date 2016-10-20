f8933a2801ec14b835fff6ac3f6114aaafdd8782
Khong Yew <e0003227@u.nus.edu>
2016-10-20 19:26:36 +0800

    Add MESSAGE_UNSUCCESSFUL to UndoCommand

--- src/main/java/seedu/emeraldo/logic/commands/UndoCommand.java
+++ src/main/java/seedu/emeraldo/logic/commands/UndoCommand.java
@@ -4,6 +4,7 @@ public class UndoCommand extends Command {
 
     public static final String COMMAND_WORD = "undo";
     public static final String MESSAGE_SUCCESS = "Undo successful!";
+    public static final String MESSAGE_UNSUCCESSFUL =  "Cannot undo further";
 
     public UndoCommand() {}
     
