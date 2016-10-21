a059d43f8ffdae9f8dfcdd5a34e860d12fed409c
Ian Teo <ian_teo@msn.com>
2016-10-21 20:34:46 +0800

    Remove unused imports

--- src/test/java/seedu/taskitty/model/TaskTest.java
+++ src/test/java/seedu/taskitty/model/TaskTest.java
@@ -1,23 +1,15 @@
 package seedu.taskitty.model;
 
-import javafx.collections.FXCollections;
-import seedu.taskitty.commons.core.UnmodifiableObservableList;
-import seedu.taskitty.commons.exceptions.DataConversionException;
 import seedu.taskitty.commons.exceptions.IllegalValueException;
+import seedu.taskitty.model.tag.UniqueTagList;
+import seedu.taskitty.model.task.Name;
 import seedu.taskitty.model.task.Task;
 import seedu.taskitty.model.task.TaskDate;
 import seedu.taskitty.model.task.TaskTime;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
-import java.util.*;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-import static seedu.taskitty.testutil.TestUtil.assertThrows;
 
 public class TaskTest {
 
