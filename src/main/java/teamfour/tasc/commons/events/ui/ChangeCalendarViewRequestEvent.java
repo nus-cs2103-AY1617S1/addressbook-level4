2c00abf0f477db29be0b0f0b18a384b2063fa88b
initialshl <initialshl@outlook.com>
2016-10-21 22:37:52 +0800

    Add ChangeCalendarViewRequestEvent

---
+++ src/main/java/teamfour/tasc/commons/events/ui/ChangeCalendarViewRequestEvent.java
@@ -0,0 +1,24 @@
+package teamfour.tasc.commons.events.ui;
+
+import teamfour.tasc.commons.events.BaseEvent;
+
+/**
+ * An event requesting to change the calendar view.
+ */
+public class ChangeCalendarViewRequestEvent extends BaseEvent {
+
+    private final String calendarViewType;
+    
+    public ChangeCalendarViewRequestEvent(String calendarViewType) {
+        this.calendarViewType = calendarViewType;
+    }
+    
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+    public String getCalendarViewType() {
+        return calendarViewType;
+    }
+}
