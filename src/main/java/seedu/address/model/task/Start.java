ed95a1d8b5a095762efaf3b2bb198ec7326adc2e
Zavier\ZavierOng <zacohjayjay@gmail.com>
2016-10-21 15:39:41 +0800

    add changeTo24HrFormat in end to consistently add time of the same format to start time

--- src/main/java/seedu/address/model/task/Start.java
+++ src/main/java/seedu/address/model/task/Start.java
@@ -28,31 +28,60 @@ public class Start {
      * @throws IllegalValueException if given start time string is invalid.
      */
     public Start(String start) throws IllegalValueException {
-        //assert start != null;
+    	//assert start != null;
     	if (start == null) 
     		start = "default"; 
-        start = start.trim();
-        if (!isValidStart(start)) {
-            throw new IllegalValueException(MESSAGE_START_CONSTRAINTS);
-        }
-        if (start == "default") {
-        	String new_min = new String(LocalTime.now().getMinute() + "");
-        	String new_hr = new String(LocalTime.now().getHour() + "");
-        	if (new_hr.length() ==1)
-        		new_hr = "0" + new_hr;
-        	if (new_min.length() ==1 )
-        		new_min = "0" + new_min;
+    	start = start.trim();
+    	if (!isValidStart(start)) {
+    		throw new IllegalValueException(MESSAGE_START_CONSTRAINTS);
+    	}
+    	if (start.equals("default")) {
+    		String new_min = new String(LocalTime.now().getMinute() + "");
+    		String new_hr = new String(LocalTime.now().getHour() + "");
+    		if (new_hr.length() ==1)
+    			new_hr = "0" + new_hr;
+    		if (new_min.length() ==1 )
+    			new_min = "0" + new_min;
     		this.value = new_hr +""+ new_min;
-        }
-        else
-        	this.value = start;
+    	}
+    	else if (start.equals("no start"))
+    		this.value = "no start";
+    	else
+    		this.value = changeTo24HourFormat(start);
+    	
     }
 
-    /**
+    private String changeTo24HourFormat(String start) {
+		if (Character.isDigit(start.charAt(start.length()-1)))
+			return start;
+		else if (start.length() == 3) {
+			if (start.substring(1).equalsIgnoreCase("pm"))
+				return (Integer.parseInt(start.substring(0,1))+12) + "00";
+			else
+				return "0" + start.substring(0, 1) + "00";
+		}
+		else if (start.length() == 4) {
+			if (start.substring(2).equalsIgnoreCase("pm"))
+				return (Integer.parseInt(start.substring(0,2))+12) + "00";
+			else
+				return start.substring(0, 2) + "00";
+		}
+		else {
+			String[] time_cat = start.split("\\.");
+			if (time_cat[0].length() ==1)
+				time_cat[0] = "0" + time_cat[0];
+			if (time_cat[1].substring(2).equalsIgnoreCase("pm")) 
+				time_cat[0] = "" + (Integer.parseInt(time_cat[0]) + 12);
+			return time_cat[0] + time_cat[1].substring(0, 2);
+		}
+			
+	}
+
+	/**
      * Returns if a given string is a valid task start time.
      */
     public static boolean isValidStart(String test) {
-    	if (test.matches(START_VALIDATION_REGEX) || test == "default")
+    	if (test.matches(START_VALIDATION_REGEX) || test.equals("default"))
     		return true;
     	else
     		return false;
