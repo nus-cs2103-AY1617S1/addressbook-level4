2a3ab9fa6c9710d03e4d6c335018f1846f5596b5
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-21 16:20:07 +0800

    rename DEFAULT view to All, which is more user friendly

--- src/main/java/seedu/todo/commons/core/TaskViewFilter.java
+++ src/main/java/seedu/todo/commons/core/TaskViewFilter.java
@@ -16,20 +16,20 @@ public class TaskViewFilter {
     private static final Comparator<ImmutableTask> LAST_UPDATED = (a, b) -> 
         b.getCreatedAt().compareTo(a.getCreatedAt());
     
-    public static final TaskViewFilter DEFAULT = new TaskViewFilter("show all",
-        null, LAST_UPDATED, 5);
+    public static final TaskViewFilter DEFAULT = new TaskViewFilter("all",
+        null, LAST_UPDATED);
     
     public static final TaskViewFilter INCOMPLETE = new TaskViewFilter("incomplete",
-        task -> !task.isCompleted(), CHRONOLOGICAL, 0);
+        task -> !task.isCompleted(), CHRONOLOGICAL);
     
     public static final TaskViewFilter DUE_SOON = new TaskViewFilter("due soon", 
-        task -> !task.isCompleted() && !task.isEvent() && task.getEndTime().isPresent(), CHRONOLOGICAL, 0);
+        task -> !task.isCompleted() && !task.isEvent() && task.getEndTime().isPresent(), CHRONOLOGICAL);
     
     public static final TaskViewFilter EVENTS = new TaskViewFilter("events",
-        ImmutableTask::isEvent, CHRONOLOGICAL, 0);
+        ImmutableTask::isEvent, CHRONOLOGICAL);
     
     public static final TaskViewFilter COMPLETED = new TaskViewFilter("completed",
-        ImmutableTask::isCompleted, LAST_UPDATED, 0);
+        ImmutableTask::isCompleted, LAST_UPDATED);
 
     public final String name;
     
@@ -38,6 +38,10 @@ public class TaskViewFilter {
     public final Comparator<ImmutableTask> sort;
     
     public final int shortcutCharPosition;
+
+    private TaskViewFilter(String name, Predicate<ImmutableTask> filter, Comparator<ImmutableTask> sort) {
+        this(name, filter, sort, 0);
+    }
 
     private TaskViewFilter(String name, Predicate<ImmutableTask> filter, Comparator<ImmutableTask> sort, int underlineCharPosition) {
         this.name = name;
