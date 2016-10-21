a26f6ba9b428fa6b074454feb3387b0c68e70605
initialshl <initialshl@outlook.com>
2016-10-21 22:37:24 +0800

    Add and parse CalendarCommand

--- src/main/java/teamfour/tasc/logic/parser/Parser.java
+++ src/main/java/teamfour/tasc/logic/parser/Parser.java
@@ -109,6 +109,9 @@ public class Parser {
         case RelocateCommand.COMMAND_WORD:
             return prepareRelocate(arguments);
 
+        case CalendarCommand.COMMAND_WORD:
+            return prepareCalendar(arguments);
+
         default:
             return new IncorrectCommand(MESSAGE_UNKNOWN_COMMAND);
         }
@@ -326,6 +329,23 @@ public class Parser {
         }
 
         return new RelocateCommand(args.trim());
+    }
+    
+    /**
+     * Parses arguments in the context of the change calendar view command.
+     *
+     * @param args full command args string
+     * @return the prepared command
+     */
+    private Command prepareCalendar(String args) {
+        if (args.equals("")) {
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, CalendarCommand.MESSAGE_USAGE));
+        }
+        try {
+            return new CalendarCommand(args.trim());
+        } catch (IllegalValueException ive) {
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, CalendarCommand.MESSAGE_USAGE));
+        }
     }
 
     /**
