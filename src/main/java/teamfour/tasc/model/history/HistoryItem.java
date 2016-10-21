14ea0dde8a9ddcc4b11a01f01ce6d487d2bc89d5
initialshl <initialshl@outlook.com>
2016-10-21 11:57:52 +0800

    Update HistoryItem comments

--- src/main/java/teamfour/tasc/model/history/HistoryItem.java
+++ src/main/java/teamfour/tasc/model/history/HistoryItem.java
@@ -1,12 +1,12 @@
 package teamfour.tasc.model.history;
 
 /**
- * An item that can be reverted to past states,
- * must be constructed as a deep copy.
+ * An object that can be reverted to past states.
+ * Must be constructed as a deep copy.
  * 
  * @param <T> The class of the item
  */
 public interface HistoryItem<T> {
-    /** creates the state this object as a deep copy */
+    /** creates the state of this object as a deep copy */
     T createStateAsDeepCopy();
 }
