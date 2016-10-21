030cfc97ee88f64c5836b73367fcf440e3e6bfb9
lim wei jie <limweijie1994@gmail.com>
2016-10-21 17:56:09 +0800

    Fix minor bug i.e. can at least undo redo once in update

--- src/main/java/seedu/whatnow/logic/commands/UpdateCommand.java
+++ src/main/java/seedu/whatnow/logic/commands/UpdateCommand.java
@@ -129,10 +129,6 @@ public class UpdateCommand extends UndoAndRedo {
         toUpdate.setStatus(taskToUpdate.getStatus());
     }
     
-    private void undoUpdateTheCorrectField(ReadOnlyTask tasktoUndoUpdate) {
-    	
-    }
-    
     @Override
     public CommandResult execute() {
         UnmodifiableObservableList<ReadOnlyTask> lastShownList;
