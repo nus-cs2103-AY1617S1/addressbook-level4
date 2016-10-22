8a5ec42f8c163721d4cb7dabb6eec598f17887e7
marxlow <marx_low94@hotmail.com>
2016-10-22 20:19:03 +0800

    Remove debugging for ListCommand class. and minor typo fix.

--- src/main/java/seedu/menion/logic/commands/ListCommand.java
+++ src/main/java/seedu/menion/logic/commands/ListCommand.java
@@ -11,7 +11,7 @@ import seedu.menion.commons.exceptions.IllegalValueException;
 /**
  * Lists all tasks in the task manager to the user.
  */
-// @@author A0139277U
+//@@author: A0139277U
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
@@ -38,7 +38,6 @@ public class ListCommand extends Command {
     public ListCommand(String args){
     	argumentsToList = new HashSet<String>();
     	this.listArgument = args;
-    	System.out.println("args = " + args);
     }
 
     /**
