c6c2227b3b3f6aac39c1f5628d861aeefa6e4eb7
cheo1994 <tehcy2@msn.com>
2016-10-22 14:44:27 +0800

    ClearCommandTest still not working for 2/3 of the tests.

--- src/test/java/seedu/address/testutil/TestUtil.java
+++ src/test/java/seedu/address/testutil/TestUtil.java
@@ -327,8 +327,6 @@ public class TestUtil {
     }
 
     public static boolean compareCardAndTask(TaskCardHandle card, ReadOnlyTask task) {
-        System.out.println("!!" + card.toString());
-        System.out.println(task.toString() + "!!");
         return card.isSameTask(task);
     }
 
