84cad097a47fb17e34a17f84b0fa1a35ad99f119
Ian Teo <ian_teo@msn.com>
2016-10-22 16:45:29 +0800

    Add new test for AddCommand

--- src/test/java/guitests/AddCommandTest.java
+++ src/test/java/guitests/AddCommandTest.java
@@ -9,6 +9,7 @@ import seedu.taskitty.testutil.TestTask;
 import seedu.taskitty.testutil.TestTaskList;
 
 import static org.junit.Assert.assertTrue;
+import static seedu.taskitty.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 public class AddCommandTest extends TaskManagerGuiTest {
 
@@ -36,6 +37,10 @@ public class AddCommandTest extends TaskManagerGuiTest {
         //invalid command
         commandBox.runCommand("adds party");
         assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
+        
+      //natty read too many parameters
+        commandBox.runCommand("add party 6pm oct 3 or 4 to oct 5 or 6 7pm");
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
     }
 
     private void assertAddSuccess(TestTask taskToAdd, int type, TestTaskList currentList) {
