9ac150ca8d647bf0cac6afe903030c11b92ed7ba
jasminish <jasmineang@hotmail.com>
2016-10-24 01:47:10 +0800

    Change app title to OneLine

--- src/test/java/seedu/oneline/commons/core/ConfigTest.java
+++ src/test/java/seedu/oneline/commons/core/ConfigTest.java
@@ -16,7 +16,7 @@ public class ConfigTest {
 
     @Test
     public void toString_defaultObject_stringReturned() {
-        String defaultConfigAsString = "App title : Task App\n" +
+        String defaultConfigAsString = "App title : OneLine\n" +
                 "Current log level : INFO\n" +
                 "Preference file Location : preferences.json\n" +
                 "Local data file location : data/taskbook.xml\n" +
