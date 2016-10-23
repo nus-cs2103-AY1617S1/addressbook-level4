903f724d833dc005be42500aec6cf24dc167f53a
ChewZL <chewzl@yahoo.com.sg>
2016-10-23 21:30:12 +0800

    [Test] Added tests for Favorite parser

---
+++ src/test/java/seedu/address/parser/FavoriteCommandParserTest.java
@@ -0,0 +1,44 @@
+package seedu.address.parser;
+
+import static org.junit.Assert.assertEquals;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.taskcommands.FavoriteTaskCommand;
+import seedu.address.logic.commands.taskcommands.IncorrectTaskCommand;
+import seedu.address.logic.parser.FavoriteCommandParser;
+
+public class FavoriteCommandParserTest {
+	// Initialized to support the tests
+	FavoriteCommandParser parser = new FavoriteCommandParser();
+	
+	/**
+	 * Testing correct handling of invalid formats, indices, update types, or dates
+	 * 
+	 * Invalid EPs: Formats, Indices, Favorite types, Dates
+	 */
+	@Test
+	public void prepareCommand_invalidFormat() {
+		/*
+		 * Testing correct handling of invalid formats
+		 */
+		String expected = String.format(MESSAGE_INVALID_COMMAND_FORMAT, FavoriteTaskCommand.MESSAGE_USAGE);
+		
+		IncorrectTaskCommand command = (IncorrectTaskCommand) parser.prepareCommand("1favorite");
+		String feedback = command.feedbackToUser;
+		assertEquals(feedback, expected);
+	}
+	
+	@Test
+	public void prepareCommand_favoriteTask() {
+		/*
+		 * Testing correct handling of invalid update types
+		 */
+		String expected = "2";
+		
+		FavoriteTaskCommand command = (FavoriteTaskCommand) parser.prepareCommand("2");
+		String feedback = command.getIndex();
+		assertEquals(feedback, expected);
+	}
+}
