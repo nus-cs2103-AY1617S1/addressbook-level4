d6e0fbf9dddecf9a6c20c34ceaf51f0aa9adec0a
Ian Teo <ian_teo@msn.com>
2016-10-23 18:05:29 +0800

    Fix tests bug

--- src/test/java/guitests/guihandles/ResultDisplayHandle.java
+++ src/test/java/guitests/guihandles/ResultDisplayHandle.java
@@ -1,7 +1,7 @@
 package guitests.guihandles;
 
 import guitests.GuiRobot;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import seedu.taskitty.TestApp;
 
@@ -10,7 +10,7 @@ import seedu.taskitty.TestApp;
  */
 public class ResultDisplayHandle extends GuiHandle {
 
-    public static final String RESULT_DISPLAY_ID = "#resultDisplay";
+    public static final String RESULT_DISPLAY_ID = "#toolTipLabel";
 
     public ResultDisplayHandle(GuiRobot guiRobot, Stage primaryStage) {
         super(guiRobot, primaryStage, TestApp.APP_TITLE);
@@ -20,7 +20,7 @@ public class ResultDisplayHandle extends GuiHandle {
         return getResultDisplay().getText();
     }
 
-    private TextArea getResultDisplay() {
-        return (TextArea) getNode(RESULT_DISPLAY_ID);
+    private Label getResultDisplay() {
+        return (Label) getNode(RESULT_DISPLAY_ID);
     }
 }
