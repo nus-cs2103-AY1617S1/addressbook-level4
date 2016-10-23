b1efd713bbb9b85aaf4b8a77ec9eb39d8af499a7
fionnatong <e0006117@u.nus.edu>
2016-10-23 16:47:14 +0800

    Prevent done tasks from showing up during startup

--- src/main/java/seedu/tasklist/logic/LogicManager.java
+++ src/main/java/seedu/tasklist/logic/LogicManager.java
@@ -42,6 +42,7 @@ public class LogicManager extends ComponentManager implements Logic {
 
     @Override
     public ObservableList<ReadOnlyTask> getFilteredTaskList() {
+    	model.updateFilteredListToShowIncomplete();
         return model.getFilteredTaskList();
     }
     
