900b57b2d18bab2aadf6a6f476c054a31d3f197e
ongks <ongkashing@gmail.com>
2016-10-24 01:40:34 +0800

    fix done cmd attempt 1.1

--- src/main/java/seedu/oneline/storage/XmlAdaptedTask.java
+++ src/main/java/seedu/oneline/storage/XmlAdaptedTask.java
@@ -72,7 +72,9 @@ public class XmlAdaptedTask {
         final TaskRecurrence recurrence = TaskRecurrence.deserialize(this.recurrence);
         final UniqueTagList tags = new UniqueTagList(taskTags);
         Task task = new Task(name, startTime, endTime, deadline, recurrence, tags);
-        task.setCompleted(isCompleted);
+        if(isCompleted){
+            task.setCompleted(isCompleted);
+        }
         return task;
     }
 }
