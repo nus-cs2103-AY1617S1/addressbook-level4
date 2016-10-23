37225175af76a6b1dfdac0dfeea22fcc5e0a0dff
ChewZL <chewzl@yahoo.com.sg>
2016-10-23 14:42:05 +0800

    [Test]Added tests for Incorrect command and parser

---
+++ src/test/java/seedu/address/logic/commands/IncorrectTaskCommandTest.java
@@ -0,0 +1,21 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.taskcommands.IncorrectTaskCommand;
+
+public class IncorrectTaskCommandTest {
+	@Test
+	public void incorrectTask_validFeedback() {
+		/* CommandResult should return a string that denotes success in execution if description 
+		 * given to AddTaskCommand constructor is a string with size > 0
+		 */
+		IncorrectTaskCommand command = new IncorrectTaskCommand(Messages.MESSAGE_INVALID_COMMAND_FORMAT);
+		CommandResult result = command.execute();
+		String feedback = result.feedbackToUser;
+		assertTrue(feedback.equals(Messages.MESSAGE_INVALID_COMMAND_FORMAT));
+	}
+}
