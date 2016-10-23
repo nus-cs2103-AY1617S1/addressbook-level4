417d07be1f3bbb8c99f059e483a9e46233286ab5
Ang Shi Ya <shiya_95@hotmail.com>
2016-10-23 23:56:28 +0800

    edit delete command test to test for multiple indexes

--- src/test/java/guitests/DeleteCommandTest.java
+++ src/test/java/guitests/DeleteCommandTest.java
@@ -3,6 +3,7 @@ package guitests;
 import org.junit.Test;
 
 import seedu.task.testutil.TestTask;
+import seedu.task.testutil.TestTaskList;
 import seedu.task.testutil.TestUtil;
 
 import static org.junit.Assert.assertTrue;
@@ -14,42 +15,66 @@ public class DeleteCommandTest extends AddressBookGuiTest {
     public void delete() {
 
         //delete the first in the list
-        TestTask[] currentList = td.getTypicalTasks();
-        int targetIndex = 1;
-        assertDeleteSuccess(targetIndex, currentList);
+        TestTaskList currentList = new TestTaskList(td.getTypicalTasks());
+        int[] targetIndexes = new int[]{1};
+        assertDeleteSuccess(targetIndexes, currentList);
 
         //delete the last in the list
-        currentList = TestUtil.removeTaskFromList(currentList, targetIndex);
-        targetIndex = currentList.length;
-        assertDeleteSuccess(targetIndex, currentList);
+        currentList = TestUtil.removeTaskFromList(currentList, targetIndexes);
+        targetIndexes = new int[]{currentList.getIncompleteList().length};
+        assertDeleteSuccess(targetIndexes, currentList);
 
         //delete from the middle of the list
-        currentList = TestUtil.removeTaskFromList(currentList, targetIndex);
-        targetIndex = currentList.length/2;
-        assertDeleteSuccess(targetIndex, currentList);
+        currentList = TestUtil.removeTaskFromList(currentList, targetIndexes);
+        targetIndexes = new int[]{currentList.getIncompleteList().length/2};
+        assertDeleteSuccess(targetIndexes, currentList);
+        
+        //delete multiple
+        currentList = TestUtil.removeTaskFromList(currentList, targetIndexes);
+        targetIndexes = new int[]{1,2};
+        assertDeleteSuccess(targetIndexes, currentList);
 
         //invalid index
-        commandBox.runCommand("delete " + currentList.length + 1);
+        commandBox.runCommand("delete " + currentList.getIncompleteList().length + 1);
         assertResultMessage("The task index provided is invalid");
 
     }
 
     /**
      * Runs the delete command to delete the task at specified index and confirms the result is correct.
-     * @param targetIndexOneIndexed e.g. to delete the first task in the list, 1 should be given as the target index.
+     * @param targetIndexes a list of indexes to be deleted
      * @param currentList A copy of the current list of tasks (before deletion).
      */
-    private void assertDeleteSuccess(int targetIndexOneIndexed, final TestTask[] currentList) {
-        TestTask taskToDelete = currentList[targetIndexOneIndexed-1]; //-1 because array uses zero indexing
-        TestTask[] expectedRemainder = TestUtil.removeTaskFromList(currentList, targetIndexOneIndexed);
+    private void assertDeleteSuccess(int[] targetIndexes, final TestTaskList currentList) {
+        TestTaskList expectedRemainder = TestUtil.removeTaskFromList(currentList, targetIndexes);
 
-        commandBox.runCommand("delete " + targetIndexOneIndexed);
-
-        //confirm the list now contains all previous tasks except the deleted task
-        assertTrue(taskListPanel.isListMatching(expectedRemainder));
+        commandBox.runCommand(getCommand(targetIndexes));
+        
+        //confirm the incomplete list now contains all previous tasks except the deleted task
+        assertTrue(taskListPanel.isListMatching(expectedRemainder.getIncompleteList()));
+        
+        //confirm the complete list remains unchanged
+        assertTrue(completeTaskListPanel.isListMatching(expectedRemainder.getCompleteList()));
 
         //confirm the result message is correct
-        assertResultMessage(String.format(MESSAGE_DELETE_TASK_SUCCESS, taskToDelete));
+        assertResultMessage(MESSAGE_DELETE_TASK_SUCCESS);
+    }
+    
+    /**
+     * Returns the command to be entered
+     */
+    private String getCommand(int[] targetIndexes) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("delete ");
+        for (int i = 0; i < targetIndexes.length; i++) {
+            if (i == targetIndexes.length - 1) {
+                builder.append(targetIndexes[i]);
+            }
+            else {
+                builder.append(targetIndexes[i] + ", ");
+            }
+        }
+        return builder.toString();
     }
 
 }
