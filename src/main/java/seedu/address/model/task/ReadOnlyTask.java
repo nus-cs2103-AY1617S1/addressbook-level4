f50ebb75e974e48ce7e479d9fb402368c4080f68
AnniyaB <anniyablue@gmail.com>
2016-10-25 00:11:34 +0800

    Adapt ReadOnlyTask to optional priority

--- src/main/java/seedu/address/model/task/ReadOnlyTask.java
+++ src/main/java/seedu/address/model/task/ReadOnlyTask.java
@@ -31,8 +31,7 @@ public interface ReadOnlyTask {
     default boolean isSameStateAs(ReadOnlyTask other) {
         return other == this // short circuit if same object
                 || (other != null // this is first to avoid NPE below
-                && other.getName().equals(this.getName())) // state checks here onwards
-                && other.getPriority().equals(this.getPriority());
+                && other.getName().equals(this.getName())); // state checks here onwards
     }
 
     /**
