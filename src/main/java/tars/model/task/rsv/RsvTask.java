b3ccdfd983da2ab02e300b0b46ad11660db9427e
Johnervan Lee <johnervan@gmail.com>
2016-10-25 00:35:07 +0800

    Add "equals" method in RsvTask

--- src/main/java/tars/model/task/rsv/RsvTask.java
+++ src/main/java/tars/model/task/rsv/RsvTask.java
@@ -13,66 +13,70 @@ import tars.model.task.ReadOnlyTask;
  * @@author A0124333U
  */
 
-public class RsvTask{
-    
+public class RsvTask {
+
     private Name name;
     private ArrayList<DateTime> dateTimeList;
-    
+
     public RsvTask(Name name, ArrayList<DateTime> dateTimeList) {
         assert !CollectionUtil.isAnyNull(name, dateTimeList);
-        
+
         this.name = name;
         this.dateTimeList = dateTimeList;
 
     }
-    
+
     /**
      * Copy constructor.
      */
     public RsvTask(RsvTask source) {
         this(source.getName(), source.getDateTimeList());
     }
-    
+
     /*
      * Accessors
      */
-    
+
     public Name getName() {
         return name;
     }
-    
+
     public ArrayList<DateTime> getDateTimeList() {
         return dateTimeList;
     }
-    
-    
+
     /*
      * Mutators
      */
-      
+
     public void setName(Name name) {
         this.name = name;
     }
-    
+
     public void setDateTimeList(ArrayList<DateTime> dateTimeList) {
         this.dateTimeList = dateTimeList;
     }
-    
-    
+
     @Override
     public int hashCode() {
-        // use this method for custom fields hashing instead of implementing your own
+        // use this method for custom fields hashing instead of implementing
+        // your own
         return Objects.hash(name, dateTimeList);
-    }   
-    
+    }
+
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
-        builder.append(getName())
-                .append(" DateTime: ")
-                .append(getDateTimeList().toString());
-        
+        builder.append(getName()).append(" DateTime: ").append(getDateTimeList().toString());
+
         return builder.toString();
+    }
+
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof RsvTask // instanceof handles nulls
+                        && ((RsvTask) other).getName().equals(this.getName())
+                        && ((RsvTask) other).getDateTimeList().equals(this.getDateTimeList()));
     }
 
 }
