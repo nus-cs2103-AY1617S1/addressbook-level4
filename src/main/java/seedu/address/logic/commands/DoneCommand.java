ddabe4ce69d42ce410ff90cb3c9762b3b3078170
linhan <yonglin12@hotmail.com>
2016-10-24 17:30:47 +0800

    Add logging to done and list done classes

--- src/main/java/seedu/address/logic/commands/DoneCommand.java
+++ src/main/java/seedu/address/logic/commands/DoneCommand.java
@@ -3,8 +3,10 @@ package seedu.address.logic.commands;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import edu.emory.mathcs.backport.java.util.Collections;
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.UnmodifiableObservableList;
 import seedu.address.model.item.ReadOnlyTask;
@@ -15,6 +17,9 @@ import seedu.address.model.item.UniqueTaskList.TaskNotFoundException;
  * Archives a task identified using its last displayed index from the task manager.
  */
 public class DoneCommand extends UndoableCommand {
+    
+    private static final Logger logger = LogsCenter.getLogger(DoneCommand.class);
+
     public static final String COMMAND_WORD = "done";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
@@ -48,9 +53,14 @@ public class DoneCommand extends UndoableCommand {
             viewingDoneList = model.isCurrentListDoneList();
         }
         if (viewingDoneList) {
+            logger.warning("Invalid command, cannot do done command on done list.");
             indicateAttemptToExecuteIncorrectCommand();
             return new CommandResult(String.format(Messages.MESSAGE_DONE_LIST_RESTRICTION));
         }
+        
+        assert viewingDoneList == false;
+
+        logger.info("Preparing to archive tasks.");
         List<String> displayArchivedTasks = new ArrayList<String>();
         Collections.sort(targetIndexes);
         int adjustmentForRemovedTask = 0;
@@ -63,6 +73,7 @@ public class DoneCommand extends UndoableCommand {
             UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredUndoneTaskList();
     
             if (lastShownList.size() < targetIndex - adjustmentForRemovedTask) {
+                logger.warning("Task target index out of bounds detected.");
                 continue;      
             }
     
@@ -77,6 +88,7 @@ public class DoneCommand extends UndoableCommand {
             }
             
             if (taskToArchive.getRecurrenceRate().isPresent()) {
+                logger.info("Task is recurring. Updating task details.");
                 Task recurringTaskToReAdd = new Task(taskToArchive);
                 recurringTaskToReAdd.updateRecurringTask();
                 readdedRecurringTasks.add(recurringTaskToReAdd);
@@ -91,6 +103,7 @@ public class DoneCommand extends UndoableCommand {
         
         updateHistory();
         if (displayArchivedTasks.isEmpty()) {
+            logger.warning("No tasks archived. Non of the given task indexes are valid.");
             indicateAttemptToExecuteIncorrectCommand();
             return new CommandResult(Messages.MESSAGE_INVALID_ITEM_DISPLAYED_INDEX);
         }
--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,9 +1,13 @@
 package seedu.address.logic.commands;
 
 
+import java.util.logging.Logger;
+
 import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.ChangeToListDoneViewEvent;
 import seedu.address.commons.events.ui.ChangeToListUndoneViewEvent;
+import seedu.address.storage.StorageManager;
 
 /**
  * Lists all tasks in the task manager to the user.
@@ -11,6 +15,8 @@ import seedu.address.commons.events.ui.ChangeToListUndoneViewEvent;
  */
 public class ListCommand extends Command {
 
+    private static final Logger logger = LogsCenter.getLogger(ListCommand.class);
+    
     public static final String COMMAND_WORD = "list";
     
     public static final String TOOL_TIP = "list [done]";
@@ -26,11 +32,14 @@ public class ListCommand extends Command {
 
     @Override
     public CommandResult execute() {
+        logger.info("Updating lists, to show all tasks");
         model.updateFilteredListsToShowAll();
         if (isListDoneCommand) {
+            logger.info("Showing all done tasks");
             model.setCurrentListToBeDoneList();
             return new CommandResult(DONE_MESSAGE_SUCCESS);
         } else {
+            logger.info("Showing all undone tasks");
             model.setCurrentListToBeUndoneList();
             return new CommandResult(MESSAGE_SUCCESS);
         }
