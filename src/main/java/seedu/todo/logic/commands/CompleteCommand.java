8456fca423b72a9d35c6648f87ee9c38b6385c0c
cricketer94 <e0011768@u.nus.edu>
2016-10-24 22:31:12 +0800

    Update complete command to accept "all" flag.

--- src/main/java/seedu/todo/logic/commands/CompleteCommand.java
+++ src/main/java/seedu/todo/logic/commands/CompleteCommand.java
@@ -1,10 +1,12 @@
 package seedu.todo.logic.commands;
 
 import com.google.common.collect.ImmutableList;
+
 import seedu.todo.commons.exceptions.ValidationException;
 import seedu.todo.logic.arguments.Argument;
 import seedu.todo.logic.arguments.IntArgument;
 import seedu.todo.logic.arguments.Parameter;
+import seedu.todo.logic.arguments.StringArgument;
 import seedu.todo.model.task.ImmutableTask;
 
 import java.util.List;
@@ -14,18 +16,35 @@ public class CompleteCommand extends BaseCommand {
     private static final String VERB_INCOMPLETE = "marked incomplete";
     
 
-    private Argument<Integer> index = new IntArgument("index").required();
+    private Argument<Integer> index = new IntArgument("index");
+    
+    private Argument<String> updateAllFlag = new StringArgument("all").flag("all");
 
     @Override
     protected Parameter[] getArguments() {
-        return new Parameter[]{ index };
+        return new Parameter[]{ index, updateAllFlag  };
     }
-
     @Override
     public String getCommandName() {
         return "complete";
     }
+    
+    @Override
+    protected void validateArguments(){
+        if(updateAllFlag.hasBoundValue() && index.hasBoundValue()){
+            errors.put("You must either specify an index or an /all flag, not both!");
+        }
+        else if(!index.hasBoundValue() && !updateAllFlag.hasBoundValue()){
+            errors.put(" You must specify an index or a /all flag. You have specified none! ");
+        }
+        
+        else{
+            return;
+        }
+    }
 
+    
+    
     @Override
     public List<CommandSummary> getCommandSummary() {
         return ImmutableList.of(new CommandSummary("Mark task as completed", getCommandName(), 
@@ -34,9 +53,18 @@ public class CompleteCommand extends BaseCommand {
 
     @Override
     public CommandResult execute() throws ValidationException {
-        ImmutableTask task = this.model.update(index.getValue(), t -> t.setCompleted(!t.isCompleted()));
-        String feedback = task.isCompleted() ? CompleteCommand.VERB_COMPLETE : CompleteCommand.VERB_INCOMPLETE;
-        return taskSuccessfulResult(task.getTitle(), feedback);
+        if(index.hasBoundValue()){
+            ImmutableTask task = this.model.update(index.getValue(), t -> t.setCompleted(!t.isCompleted()));
+            String feedback = task.isCompleted() ? CompleteCommand.VERB_COMPLETE : CompleteCommand.VERB_INCOMPLETE;
+            return taskSuccessfulResult(task.getTitle(), feedback);
+        }
+        else {
+            this.model.updateAll(t -> t.setCompleted(!t.isCompleted()));
+            String feedback = "Complete all exceuted!";
+            return new CommandResult(feedback);
+        }
+        
+        
     }
 
 }
