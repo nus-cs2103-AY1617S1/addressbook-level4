097b102195b24920652f88bdffbf580c4e36ee8a
ChewZL <chewzl@yahoo.com.sg>
2016-10-24 21:55:23 +0800

    [Test]Modified favorite task test

--- src/test/java/seedu/address/logic/commands/FavoriteTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FavoriteTaskCommandTest.java
@@ -59,7 +59,7 @@ public class FavoriteTaskCommandTest {
 	}
 	
 	@Test
-	public void favoriteTask_validIndex() throws IllegalValueException {
+	public void favoriteTask_duplicateFavorite() throws IllegalValueException {
 		/* 
 		 * CommandResult should return a string that denotes success in execution if index given 
 		 * to favoriteTaskCommand constructor is within the range of added tasks.
@@ -70,6 +70,21 @@ public class FavoriteTaskCommandTest {
 		command.setData(model);
 		
 		String expected = String.format(FavoriteTaskCommand.MESSAGE_FAVORITE_TASK_SUCCESS, "[Floating Task][Description: Task 1]");
+		assertCommandFeedback(command, expected);
+	}
+	
+	@Test
+	public void favoriteTask_validIndex() throws IllegalValueException {
+		/* 
+		 * CommandResult should return a string that the task 
+		 * has already been favorited
+		 */
+		InMemoryTaskList model;
+		model = TestUtil.setupSomeFavoritedTasksInTaskList(3);
+		FavoriteTaskCommand command = new FavoriteTaskCommand(2);
+		command.setData(model);
+		
+		String expected = String.format(FavoriteTaskCommand.MESSAGE_TASK_ALR_FAVORITED);
 		assertCommandFeedback(command, expected);
 	}
 
