8db242261a09dc4559bf2e39e37ad2761f2ff0bd
marxlow <marx_low94@hotmail.com>
2016-10-24 15:27:16 +0800

    finish invalid tests for invalid inputs for DateTime

--- src/test/java/guitests/EditCommandTest.java
+++ src/test/java/guitests/EditCommandTest.java
@@ -8,7 +8,10 @@ import guitests.guihandles.EventCardHandle;
 import guitests.guihandles.FloatingTaskCardHandle;
 import guitests.guihandles.TaskCardHandle;
 import seedu.address.testutil.TestActivity;
+import seedu.menion.commons.util.DateChecker;
 import seedu.menion.logic.commands.EditCommand;
+import seedu.menion.model.activity.ActivityDate;
+import seedu.menion.model.activity.ActivityTime;
 
 //@@author: A0139164A
 
@@ -26,7 +29,7 @@ public class EditCommandTest extends ActivityManagerGuiTest {
         commandBox.runCommand(floating.getAddCommand());
         commandBox.runCommand(task.getAddCommand());
         commandBox.runCommand(event.getAddCommand());
-
+        /**
         assertFloatingEditNameSuccess(floating, 1, "Floating Hello World");
         assertTaskEditNameSuccess(task, 1, "Task Hello World");
         assertEventEditNameSuccess(event, 1, "Event Hello World");
@@ -52,9 +55,9 @@ public class EditCommandTest extends ActivityManagerGuiTest {
        
         // Edit From Time - For Event
         assertEventEditFromDateTimeSuccess(event, 1, "2359");
-
+    */
         // Edit Both, From Date/Time - For Event
-        assertEventEditFromDateTimeSuccess(event, 1, "01-01-2016 1000");
+        assertEventEditFromDateTimeSuccess(event, 1, "10-01-2016 1000");
         
         // Edit To Date - For Event
         assertEventEditToDateTimeSuccess(event, 1, "20-02-2016");
@@ -65,16 +68,22 @@ public class EditCommandTest extends ActivityManagerGuiTest {
         // Edit Both, To Date/Time - For Event
         assertEventEditToDateTimeSuccess(event, 1, "02-02-2016 1100");
        
-        
-        // Current from: 01-01-2016 1000. Current to: 02-02-2016 1100.
+
+        // Current from: 10-01-2016 1000. Current to: 02-02-2016 1100.
         // Edit invalid to/From date& Time.
         // From date/time cannot be after than to date/time. - For Event
-        assertinvalidEventFromDateTime(event, 1, "02-02-2016, 1101");
-        assertinvalidEventFromDateTime(event, 1, "03-02-2016, 1100");
-        assertinvalidEventToDateTime(event, 1, "01-01-2016 0959");
-        assertinvalidEventtoDateTime(event, 1, "02-01-2016 100");
+        assertInvalidEventFromDateTime(event, 1, "02-02-2016 1101"); // Invalid from time
+        assertInvalidEventFromDateTime(event, 1, "03-02-2016 1100"); // Invalid from date
+        assertInvalidEventToDateTime(event, 1, "10-01-2016 0959"); // Invalid to time
+        assertInvalidEventToDateTime(event, 1, "09-01-2016 1000"); // Invalid to date
         
         // Edit with invalid params for date/time
+        assertInvalidDateFormat(task, 1, "02-02-20160");
+        assertInvalidDateFormat(task, 1, "10-");
+        assertInvalidDateInput(task, 1, "02-13-2016"); // Invalid month
+        assertInvalidDateInput(task, 1, "32-02-2016"); //Invalid day
+        assertInvalidTimeInput(task, 1, "2400");
+        
         // Edit with invalid params for note 
         // Edit with invalid params for name
         
@@ -84,6 +93,36 @@ public class EditCommandTest extends ActivityManagerGuiTest {
         for (int i = 0; i < numTimes; i++) {
             commandBox.runCommand("undo");
         }
+    }
+    
+    private void assertInvalidTimeInput(TestActivity task, int index, String changes) {
+        commandBox.runCommand(task.getEditTaskDateTimeCommand(index, changes));
+        task = activityListPanel.returnsUpdatedTask(task.getActivityName().fullName); // Update Task with new changes
+        TaskCardHandle editedCard = activityListPanel.navigateToTask(task); // Check against card.
+        assertTaskMatching(task, editedCard);
+        
+        // Confirms the result message is correct
+        assertResultMessage(String.format(ActivityTime.ACTIVITY_TIME_CONSTRAINTS));
+    }
+    
+    private void assertInvalidDateFormat(TestActivity task, int index, String changes) {
+        commandBox.runCommand(task.getEditTaskDateTimeCommand(index, changes));
+        task = activityListPanel.returnsUpdatedTask(task.getActivityName().fullName); // Update Task with new changes
+        TaskCardHandle editedCard = activityListPanel.navigateToTask(task); // Check against card.
+        assertTaskMatching(task, editedCard);
+        
+        // Confirms the result message is correct
+        assertResultMessage(String.format(ActivityDate.MESSAGE_ACTIVITYDATE_CONSTRAINTS));
+    }
+    
+    private void assertInvalidDateInput(TestActivity task, int index, String changes) {
+        commandBox.runCommand(task.getEditTaskDateTimeCommand(index, changes));
+        task = activityListPanel.returnsUpdatedTask(task.getActivityName().fullName); // Update Task with new changes
+        TaskCardHandle editedCard = activityListPanel.navigateToTask(task); // Check against card.
+        assertTaskMatching(task, editedCard);
+        
+        // Confirms the result message is correct
+        assertResultMessage(String.format(ActivityDate.MESSAGE_ACTIVITYDATE_INVALID));
     }
     
     private void assertFloatingEditNameSuccess(TestActivity floating, int index, String changes) {
@@ -184,6 +223,25 @@ public class EditCommandTest extends ActivityManagerGuiTest {
         
         // Confirms the result message is correct
         assertResultMessage(String.format(EditCommand.MESSAGE_EDITTED_ACTIVITY_SUCCESS, event));
+    }
+    
+    private void assertInvalidEventFromDateTime(TestActivity event, int index, String changes) {
+        commandBox.runCommand(event.getEditEventFromDateTimeCommand(index, changes));
+        event = activityListPanel.returnsUpdatedEvent(event.getActivityName().fullName);
+        EventCardHandle editedCard = activityListPanel.navigateToEvent(event);
+        assertEventMatching(event, editedCard);
         
+        // Confirms the result message is correct
+        assertResultMessage(String.format(DateChecker.END_DATE_BEFORE_START_DATE_ERROR));
+    }
+    
+    private void assertInvalidEventToDateTime(TestActivity event, int index, String changes) {
+        commandBox.runCommand(event.getEditEventToDateTimeCommand(index, changes));
+        event = activityListPanel.returnsUpdatedEvent(event.getActivityName().fullName);
+        EventCardHandle editedCard = activityListPanel.navigateToEvent(event);
+        assertEventMatching(event, editedCard);
+        
+        // Confirms the result message is correct
+        assertResultMessage(String.format(DateChecker.END_DATE_BEFORE_START_DATE_ERROR));
     }
 }
