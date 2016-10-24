c4447775b5be7e53b08d3b7687ecaa18c323c4c8
jasminish <jasmineang@hotmail.com>
2016-10-25 00:12:05 +0800

    Remove menubar

--- src/main/java/seedu/oneline/ui/MainWindow.java
+++ src/main/java/seedu/oneline/ui/MainWindow.java
@@ -49,9 +49,6 @@ public class MainWindow extends UiPart {
     private AnchorPane commandBoxPlaceholder;
 
     @FXML
-    private MenuItem helpMenuItem;
-
-    @FXML
     private AnchorPane tagListPanelPlaceholder;
 
     @FXML
@@ -95,12 +92,6 @@ public class MainWindow extends UiPart {
         setWindowDefaultSize(prefs);
         scene = new Scene(rootLayout);
         primaryStage.setScene(scene);
-
-        setAccelerators();
-    }
-
-    private void setAccelerators() {
-        helpMenuItem.setAccelerator(KeyCombination.valueOf("F1"));
     }
 
     void fillInnerParts() {
