f714304cbe6f1e95b9733c4d7761177b90a3c7b2
Brehmer <Brehmerchan@gmail.com>
2016-10-25 22:27:30 +0800

    change delete example template

--- src/main/java/seedu/menion/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/menion/logic/commands/DeleteCommand.java
@@ -20,7 +20,9 @@ public class DeleteCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the activity identified by the activity type followed by the index number used in the last activity listing.\n"
             + "Parameters: ACTIVITY_TYPE(task,event,floating) INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " floating" + " 1";
+            + "Example: " + COMMAND_WORD + " floating" + " 1"
+            + "Example: " + COMMAND_WORD + " task" + " 2"
+            + "Example: " + COMMAND_WORD + " event" + " 3";
 
     public static final String MESSAGE_DELETE_ACTIVITY_SUCCESS = "Deleted Activity: %1$s";
 
