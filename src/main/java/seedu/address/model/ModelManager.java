fe401641d70eb2abe7c7ebd3b7e68a499b0b2c27
MUHAMMAD ARIF <halo3fanz@hotmail.com>
2016-10-25 13:42:43 +0800

    Fix ModelManager

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -115,10 +115,10 @@ public class ModelManager extends ComponentManager implements Model {
 		for (int i = 0; i < filteredPersons.size() - 1; i++) {
 			boolean isClashing = false;
 			Task task = filteredPersons.get(i);
-			Deadline deadline = task.getDeadlines().getDeadline();
+			Deadline deadline = task.getDeadlines();
 			for (int j = i + 1; j < filteredPersons.size(); j++) {
 				Task task2 = filteredPersons.get(j);
-				Deadline deadline2 = task2.getDeadlines().getDeadline();
+				Deadline deadline2 = task2.getDeadlines();
 				if (deadline != null && deadline2 != null && deadline.equals(deadline2)) {
 					if (!taskmanager.contains(task2))
 						taskmanager.addTask(task2);
