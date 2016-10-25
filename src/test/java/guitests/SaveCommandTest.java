7944ba5fd787afc94c1d1690f00dab6a267d51e5
Ian Teo <ian_teo@msn.com>
2016-10-26 02:14:48 +0800

    Fix SaveCommand tests

--- src/test/java/guitests/SaveCommandTest.java
+++ src/test/java/guitests/SaveCommandTest.java
@@ -4,6 +4,7 @@ import static seedu.taskitty.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMA
 
 import org.junit.Test;
 
+import seedu.taskitty.logic.commands.Command;
 import seedu.taskitty.logic.commands.SaveCommand;
 
 //@@author A0135793W
@@ -16,10 +17,12 @@ public class SaveCommandTest extends TaskManagerGuiTest {
         
         //no filepath
         commandBox.runCommand("save");
-        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SaveCommand.MESSAGE_USAGE));
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                Command.MESSAGE_FORMAT + SaveCommand.MESSAGE_PARAMETER));
         
         commandBox.runCommand("save ");
-        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SaveCommand.MESSAGE_USAGE));
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                Command.MESSAGE_FORMAT + SaveCommand.MESSAGE_PARAMETER));
     }
 
 }
