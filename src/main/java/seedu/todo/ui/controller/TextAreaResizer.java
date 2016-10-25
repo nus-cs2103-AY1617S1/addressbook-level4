b8b978a417bb2359d4228988ea220a0444ebd758
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-25 23:00:17 +0800

    attempt to account for padding offset

--- src/main/java/seedu/todo/ui/controller/TextAreaResizer.java
+++ src/main/java/seedu/todo/ui/controller/TextAreaResizer.java
@@ -4,13 +4,20 @@ import com.google.common.base.Strings;
 import javafx.application.Platform;
 import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
+
 /**
  * Automatically resize a text area object such that the height fits the content of the text area
  */
 public class TextAreaResizer {
+    // Constants
+    private static final int CHAR_AHEAD = 3; 
+    private static final int PADDING_OFFSET = 5;
+    
+    // FXML elements
     private final TextArea textArea;
     private final Text pseudoText = new Text();
     
+    // Internal variables
     private double lineHeight;
     private double charWidth;
     private double textAreaWidth;
@@ -25,7 +32,7 @@ public class TextAreaResizer {
         pseudoText.fontProperty().bind(textArea.fontProperty());
         
         textArea.widthProperty().addListener((observable, oldValue, newValue) -> {
-            textAreaWidth = (double) newValue;
+            textAreaWidth = (double) newValue - PADDING_OFFSET;
             updateTextArea();
         });
         
@@ -47,16 +54,14 @@ public class TextAreaResizer {
     
     private void updateTextArea() {
         // Silly hack to make sure lineHeight is available before continuing 
-        if (lineHeight == 0) {
-            lineHeight = textArea.getLayoutBounds().getHeight();
+        if (lineHeight <= 0) {
+            if ((lineHeight = textArea.getLayoutBounds().getHeight() - PADDING_OFFSET) <= 0) {
+                return;
+            }
         }
         
-        if (lineHeight == 0) {
-            return;
-        }
-        
-        int lines = (int) (((textAreaChars + 5) * charWidth) / textAreaWidth) + 1;
-        double height = lines * lineHeight;
+        int lines = (int) (((textAreaChars + CHAR_AHEAD) * charWidth) / textAreaWidth) + 1;
+        double height = lines * lineHeight + PADDING_OFFSET;
         
         Platform.runLater(() -> {
             textArea.setMaxHeight(height);
