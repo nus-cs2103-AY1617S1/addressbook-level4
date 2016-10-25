63030d9dc85843d321139c9c924c9b08318bc769
ShadowLoner17 <weiyinchong@hotmail.sg>
2016-10-25 08:56:24 +0800

    Implement Priority in FloatingTask

--- src/main/java/seedu/jimi/model/task/FloatingTask.java
+++ src/main/java/seedu/jimi/model/task/FloatingTask.java
@@ -1,6 +1,7 @@
 package seedu.jimi.model.task;
 
 import seedu.jimi.commons.util.CollectionUtil;
+import seedu.jimi.model.tag.Priority;
 import seedu.jimi.model.tag.UniqueTagList;
 
 import java.util.Objects;
@@ -14,27 +15,29 @@ public class FloatingTask implements ReadOnlyTask {
     private Name name;
     private UniqueTagList tags;
     private boolean isCompleted;
+    private Priority priority;
 
-    public FloatingTask(Name name, UniqueTagList tags, boolean isCompleted) {
-        this(name, tags);
+    public FloatingTask(Name name, UniqueTagList tags, boolean isCompleted, Priority priority) {
+        this(name, tags, priority);
         this.isCompleted = isCompleted;
     }
     
     /**
      * Every field must be present and not null.
      */
-    public FloatingTask(Name name, UniqueTagList tags) {
+    public FloatingTask(Name name, UniqueTagList tags, Priority priority) {
         assert !CollectionUtil.isAnyNull(name, tags);
         this.isCompleted = false;
         this.name = name;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        this.priority = priority;
     }
 
     /**
      * Copy constructor.
      */
     public FloatingTask(ReadOnlyTask source) {
-        this(source.getName(), source.getTags(), source.isCompleted());
+        this(source.getName(), source.getTags(), source.isCompleted(), source.getPriority());
     }
     
 
@@ -46,6 +49,11 @@ public class FloatingTask implements ReadOnlyTask {
     @Override
     public UniqueTagList getTags() {
         return new UniqueTagList(tags);
+    }
+    
+    @Override
+    public Priority getPriority() {
+        return priority;
     }
 
     /**
@@ -66,6 +74,10 @@ public class FloatingTask implements ReadOnlyTask {
      */
     public void setCompleted(boolean isCompleted) {
         this.isCompleted = isCompleted;
+    }
+    
+    public void setPriority(Priority priority)  {
+        this.priority = priority;
     }
     
     @Override
@@ -101,6 +113,8 @@ public class FloatingTask implements ReadOnlyTask {
         builder.append(getName())
                .append(" Tags: ");
         getTags().forEach(builder::append);
+        builder.append(" Priority: ")
+               .append(getPriority());
         return builder.toString();
     }
 
