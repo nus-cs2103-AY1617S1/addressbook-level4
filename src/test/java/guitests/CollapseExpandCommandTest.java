8bb07ca149195234f42b9795d1186cc8d12bed05
Cherlynn\Cherlynn <cherlynn@live.com.sg>
2016-10-25 20:45:30 +0800

    add test cases

--- src/test/java/guitests/CollapseExpandCommandTest.java
+++ src/test/java/guitests/CollapseExpandCommandTest.java
@@ -23,4 +23,20 @@ public class CollapseExpandCommandTest extends AddressBookGuiTest{
         assertResultMessage("Task view collapsed");
     }
 
+    @Test
+    public void expand_alreadyExpanded() {
+        commandBox.runCommand("expand");
+        assertResultMessage("Task view expanded");
+        commandBox.runCommand("expand");
+        assertResultMessage("Already in expanded view, type \"collapse\" to go into collapsed view");
+    }
+
+    @Test
+    public void expand_after_collapse() {
+        commandBox.runCommand("collapse");
+        assertResultMessage("Task view collapsed");
+        commandBox.runCommand("expand");
+        assertResultMessage("Task view expanded");
+    }
+
 }
