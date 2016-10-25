b3697b9b726a43d2c32d7b5bfcdde3dab22c8404
Joycelyn Ng <Joycelyn Ng>
2016-10-25 15:58:07 +0800

    Implemented basic structure for SetStorageCommandTest

---
+++ src/test/java/guitests/SetStorageCommand.java
@@ -0,0 +1,31 @@
+package guitests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SetStorageCommand extends TaskManagerGuiTest {
+
+    @Test
+    public void setStorage() {
+
+        //verify a non-empty list can be cleared
+        assertTrue(taskListPanel.isListMatching(td.getTypicalTasks()));
+        assertSetStorageCommandSuccess();
+
+        //verify other commands can work after a clear command
+        commandBox.runCommand(td.hoon.getAddCommand());
+        assertTrue(taskListPanel.isListMatching(td.hoon));
+        commandBox.runCommand("delete 1");
+        assertListSize(0);
+
+        //verify clear command works when the list is empty
+        assertSetStorageCommandSuccess();
+    }
+
+    private void assertSetStorageCommandSuccess() {
+        commandBox.runCommand("clear");
+        assertListSize(0);
+        assertResultMessage("Task manager has been cleared!");
+    }
+}
