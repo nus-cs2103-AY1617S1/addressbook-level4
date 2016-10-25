2eb07a823c3470a42ce882ea51cfd09792fce83a
Yip Bing You <leafytubistfriend@gmail.com>
2016-10-25 16:59:11 +0800

    Optimise Time regex
    Introduce optional <kbd>,</kbd> for demarcating separate values
    Eg. `start/tues, 25.10.2016, 17:00`

--- src/main/java/seedu/task/model/task/Time.java
+++ src/main/java/seedu/task/model/task/Time.java
@@ -27,13 +27,11 @@ public class Time {
             + "|^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$" // 24H Time
             // Day Of Week and Optional 24H Time
             + "|^(?i)(mon|tue(s)?|wed(nes)?|thu(r(s)?)?|fri|sat(ur)?|sun)(day)?"
-            + "( ([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9])?$"
-            // Date
-            + "|^([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1]).([1-9]|0[1-9]|1[0-2]).2\\d{3}$"
-            // Date, Day of Week and 24H Time
-            + "|^(?i)(mon|tue(s)?|wed(nes)?|thu(r(s)?)?|fri|sat(ur)?|sun)(day)?, "
-            + "([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1]).([1-9]|0[1-9]|1[0-2]).2\\d{3} "
-            + "([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$";
+            + "(,? ([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9])?$"
+            // Day of Week (Opt), Date and 24H Time (Opt)
+            + "|^((?i)(mon|tue(s)?|wed(nes)?|thu(r(s)?)?|fri|sat(ur)?|sun)(day)?,? )?"
+            + "([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1]).([1-9]|0[1-9]|1[0-2]).2\\d{3}"
+            + "(,? ([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9])?$";
 
 
     private LocalDateTime value;
@@ -52,28 +50,29 @@ public class Time {
             throw new IllegalValueException(MESSAGE_TIME_CONSTRAINTS);
         }
 
-        final Pattern DATEDAYTIME24H_FORMAT = Pattern.compile("^(?i)(mon|tue(s)?|wed(nes)?|thu(r(s)?)?|fri|sat(ur)?|sun)(day)?, "
-                                                            + "(?<Day>[1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1]).(?<Month>[1-9]|0[1-9]|1[0-2]).(?<Year>2\\d{3}) "
-                                                            + "(?<Hour>[0-9]|0[0-9]|1[0-9]|2[0-3]):(?<Minute>[0-5][0-9])$");
-        final Pattern DAYTIME24H_FORMAT = Pattern.compile("(?i)((?<Day>(mon|tue(s)?|wed(nes)?|thu(r(s)?)?|fri|sat(ur)?|sun)(day)?)) "
+        final Pattern DATETIME24H_FORMAT = Pattern.compile("^((?i)(mon|tue(s)?|wed(nes)?|thu(r(s)?)?|fri|sat(ur)?|sun)(day)?,? )?"
+                                                            + "(?<Day>[1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1]).(?<Month>[1-9]|0[1-9]|1[0-2]).(?<Year>2\\d{3})"
+                                                            + ",? (?<Hour>[0-9]|0[0-9]|1[0-9]|2[0-3]):(?<Minute>[0-5][0-9])$");
+        final Pattern DAYTIME24H_FORMAT = Pattern.compile("(?i)((?<Day>(mon|tue(s)?|wed(nes)?|thu(r(s)?)?|fri|sat(ur)?|sun)(day)?)),? "
                                                         + "(?<Hour>([0-9]|0[0-9]|1[0-9]|2[0-3])):(?<Minute>[0-5][0-9])");
         final Pattern TIME24H_FORMAT = Pattern.compile("(?<Hour>[0-9]|0[0-9]|1[0-9]|2[0-3]):(?<Minute>[0-5][0-9])");
         final Pattern DAY_FORMAT = Pattern.compile("(?i)(?<Day>(mon|tue(s)?|wed(nes)?|thu(r(s)?)?|fri|sat(ur)?|sun)(day)?)");
-        final Pattern DATE_FORMAT = Pattern.compile("^(?<Day>([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1])).(?<Month>([1-9]|0[1-9]|1[0-2])).(?<Year>2\\d{3})$");
+        final Pattern DATE_FORMAT = Pattern.compile("^((?i)(mon|tue(s)?|wed(nes)?|thu(r(s)?)?|fri|sat(ur)?|sun)(day)?,? )?"
+                                                  + "(?<Day>([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1])).(?<Month>([1-9]|0[1-9]|1[0-2])).(?<Year>2\\d{3})$");
 
-        final Matcher matcher_DATEDAYTIME24H = DATEDAYTIME24H_FORMAT.matcher(time);
+        final Matcher matcher_DATETIME24H = DATETIME24H_FORMAT.matcher(time);
         final Matcher matcher_DAYTIME24H = DAYTIME24H_FORMAT.matcher(time);
         final Matcher matcher_TIME24H = TIME24H_FORMAT.matcher(time);
         final Matcher matcher_DAY = DAY_FORMAT.matcher(time);
         final Matcher matcher_DATE = DATE_FORMAT.matcher(time);
 
         
-        if (matcher_DATEDAYTIME24H.matches()) {
-            Integer day = Integer.parseInt(matcher_DATEDAYTIME24H.group("Day"));
-            Integer month = Integer.parseInt(matcher_DATEDAYTIME24H.group("Month"));
-            Integer year = Integer.parseInt(matcher_DATEDAYTIME24H.group("Year"));
-            Integer hour = Integer.parseInt(matcher_DATEDAYTIME24H.group("Hour"));
-            Integer minute = Integer.parseInt(matcher_DATEDAYTIME24H.group("Minute"));
+        if (matcher_DATETIME24H.matches()) {
+            Integer day = Integer.parseInt(matcher_DATETIME24H.group("Day"));
+            Integer month = Integer.parseInt(matcher_DATETIME24H.group("Month"));
+            Integer year = Integer.parseInt(matcher_DATETIME24H.group("Year"));
+            Integer hour = Integer.parseInt(matcher_DATETIME24H.group("Hour"));
+            Integer minute = Integer.parseInt(matcher_DATETIME24H.group("Minute"));
             this.value = LocalDateTime.of(year, month, day, hour, minute);
         }
         
