5ec52a3bb78bd53ad98876504fdc9b03f371a219
Administrator <Administrator@N6QST2QZLU97Z9S>
2016-10-26 22:41:24 +0800

    change of Success message in GroupCommand

--- src/main/java/seedu/address/logic/commands/GroupCommand.java
+++ src/main/java/seedu/address/logic/commands/GroupCommand.java
@@ -1,5 +1,26 @@
 package seedu.address.logic.commands;
 
-public class GroupCommand {
+import java.util.Set;
+
+public class GroupCommand extends Command{
+	
+	public static final String COMMAND_WORD = "group";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all tasks under the same group "
+            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " CS2103";
+
+    private final String keywords;
+
+    public GroupCommand(String keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public CommandResult execute() {
+        model.updateFilteredPersonGroup(keywords);
+        return new CommandResult(getMessageForPersonListShownSummary(model.getFilteredPersonList().size()));
+    }
 
 }
