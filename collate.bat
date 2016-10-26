a91281cb19f0142b3b7d856e3f521ff06871bc87
clarencechee <clarencechee94@gmail.com>
2016-10-27 07:12:58 +0800

    collate.bat: @echo off

--- collate.bat
+++ collate.bat
@@ -1,4 +1,4 @@
-
+@echo off
 java -jar Collate-TUI.jar collate from src/main to collated/main include java, fxml, css
 
 java -jar Collate-TUI.jar collate from src/test to collated/test include java
