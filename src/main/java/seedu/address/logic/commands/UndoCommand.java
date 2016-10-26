3ec58c31b5556e53a85b4181636d1e7481c55447
Zhi Jie <zhijietan94@gmail.com>
2016-10-26 09:39:14 +0800

    Update Undo command failure message Add method comments

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import seedu.address.model.task.UniqueTaskList;
 import seedu.address.model.undo.UndoTask;
 
 //@@author A0139145E
@@ -10,10 +11,12 @@ public class UndoCommand extends Command {
 
     public static final String COMMAND_WORD = "undo";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Undo the last reversible action in the task book.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Undoes the last reversible action from Task Book in this session\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "Undid last action %1$s";
+    public static final String MESSAGE_SUCCESS = "Action \"%1$s\" has been reverted";
+
+    public static final String MESSAGE_UNDO_NOT_POSSIBLE = "There are no actions available for undo";
 
     public UndoCommand() {}
 
@@ -21,27 +24,38 @@ public class UndoCommand extends Command {
     public CommandResult execute() {
         assert model != null;
         UndoTask toUndo = model.undoTask();
+        
+        //No undoable action found.
+        if (toUndo == null) { 
+            return new CommandResult(MESSAGE_UNDO_NOT_POSSIBLE);
+        }
+        
         try {
             switch (toUndo.getCommand()){
-            
+
             case AddCommand.COMMAND_WORD:
                 model.deleteTask(toUndo.getPostData());
                 break;
-                
+
             case DeleteCommand.COMMAND_WORD:
                 model.addTask(toUndo.getPostData());
                 break;
-                
+
             case EditCommand.COMMAND_WORD:
                 model.deleteTask(toUndo.getPostData());
                 model.addTask(toUndo.getPreData());
                 break;
-                
+
             }
             return new CommandResult(String.format(MESSAGE_SUCCESS, toUndo.getCommand()));
         }
-        catch (Exception e){
-            return new CommandResult("Failed to Undo");
+        catch (UniqueTaskList.TaskNotFoundException tnfe){
+            //Not possible
+            return new CommandResult(MESSAGE_UNDO_NOT_POSSIBLE);
+        }
+        catch (UniqueTaskList.DuplicateTaskException dte){
+            //Not possible
+            return new CommandResult(MESSAGE_UNDO_NOT_POSSIBLE);
         }
 
     }
