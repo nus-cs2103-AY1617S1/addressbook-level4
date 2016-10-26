0ce800d55bfe0d619601b6d56eb75d43d7cff884
CARINA <carinackwu@gmail.com>
2016-10-27 00:02:00 +0800

    modify Parser class to add findType parameter to call FindCommand()

--- src/main/java/seedu/todolist/logic/parser/Parser.java
+++ src/main/java/seedu/todolist/logic/parser/Parser.java
@@ -329,7 +329,12 @@ public class Parser {
         // keywords delimited by whitespace
         final String[] keywords = matcher.group("keywords").split("\\s+");
         final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
-        return new FindCommand(keywordSet);
+        String findType = "null";
+        if (keywords[0].equals("all") || keywords[0].equals("exactly")) {
+        	findType = keywords[0];
+        	keywordSet.remove(keywords[0]);
+        }
+        return new FindCommand(keywordSet, findType);
     }
 
 }
