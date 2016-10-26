72d6008c8c645e3dc1778fc54088267ba8ced9ab
“Li <liwanghuan43@gmail.com>
2016-10-26 18:48:12 +0800

    rearrange collate

--- src/test/java/guitests/ClearCommandTest.java
+++ src/test/java/guitests/ClearCommandTest.java
@@ -14,14 +14,14 @@ public class ClearCommandTest extends TaskManagerGuiTest {
     public void clear() throws IllegalArgumentException, IllegalValueException {
 
         //verify a non-empty list can be cleared
-       // assertTrue(taskListPanel.isListMatching(td.getTypicalTasks()));
-       // assertClearCommandSuccess();
+        assertTrue(taskListPanel.isListMatching(td.getTypicalTasks()));
+        assertClearCommandSuccess();
 
         //verify other commands can work after a clear command
         commandBox.runCommand(td.dance.getAddCommand());
-        //assertTrue(taskListPanel.isListMatching(td.dance));
+        assertTrue(taskListPanel.isListMatching(td.dance));
         commandBox.runCommand("delete 0");
-        //assertListSize(0);
+        assertListSize(0);
 
         //verify clear command works when the list is empty
         assertClearCommandSuccess();
