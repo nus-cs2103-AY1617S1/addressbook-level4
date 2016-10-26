da14dcf384d44c74223b9705fdf3731bcfe8834b
Kevin Ong <Kevin Ong>
2016-10-26 14:49:25 +0800

    Implemeneted save feature, havent managed to save the location into user prefs yet

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -1,6 +1,7 @@
 package seedu.address.model;
 
 import seedu.address.commons.core.GuiSettings;
+import seedu.address.storage.XmlAddressBookStorage;
 
 import java.util.Objects;
 
@@ -10,6 +11,7 @@ import java.util.Objects;
 public class UserPrefs {
 
     public GuiSettings guiSettings;
+    public static String savePath;
 
     public GuiSettings getGuiSettings() {
         return guiSettings == null ? new GuiSettings() : guiSettings;
@@ -20,11 +22,17 @@ public class UserPrefs {
     }
 
     public UserPrefs(){
+        XmlAddressBookStorage xml = new XmlAddressBookStorage();
         this.setGuiSettings(500, 500, 0, 0);
+        this.setDataFilePath(xml.getAddressBookFilePath());
     }
 
     public void setGuiSettings(double width, double height, int x, int y) {
         guiSettings = new GuiSettings(width, height, x, y);
+    }
+    
+    public static void setDataFilePath(String filePath) {
+        savePath = filePath;
     }
 
     @Override
@@ -48,7 +56,10 @@ public class UserPrefs {
 
     @Override
     public String toString(){
-        return guiSettings.toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append(guiSettings.toString() + "\n");
+        sb.append("Save Path: " + savePath);
+        return sb.toString();
     }
 
 }
