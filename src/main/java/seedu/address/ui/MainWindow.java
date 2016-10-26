ebc9e96b3292d7f6d3d83ef2ab94cd701afb03e8
Kevin Ong <Kevin Ong>
2016-10-26 19:36:28 +0800

    Changed icon

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -27,7 +27,7 @@ import seedu.address.storage.XmlAddressBookStorage;
  */
 public class MainWindow extends UiPart {
 
-    private static final String ICON = "/images/address_book_32.png";
+    private static final String ICON = "/images/lifekeeper.png";
     private static final String FXML = "MainWindow.fxml";
     public static final int MIN_HEIGHT = 600;
     public static final int MIN_WIDTH = 450;
