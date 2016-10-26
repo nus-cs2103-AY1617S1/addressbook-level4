d05ad78c3498500387396349d56e1361d4a48b7d
Syed Abdullah <syed@syedabdullah.net>
2016-10-27 00:18:29 +0800

    Add event to switch omnibox state and add live search command handler and parser

---
+++ src/main/java/seedu/task/logic/parser/SearchParser.java
@@ -0,0 +1,13 @@
+package seedu.task.logic.parser;
+
+import seedu.task.logic.commands.Command;
+import seedu.task.logic.commands.SearchCommand;
+
+public class SearchParser extends BaseParser {
+
+    @Override
+    public Command parse(String command, String arguments) {
+        return new SearchCommand();
+    }
+
+}
