84a77e901a872cfd21253695c3328b0552e6fee1
louietyj <louietyj@gmail.com>
2016-10-27 20:00:30 +0800

    UnmatchedQuotesException extends from ParseError

--- src/main/java/seedu/todo/commons/exceptions/UnmatchedQuotesException.java
+++ src/main/java/seedu/todo/commons/exceptions/UnmatchedQuotesException.java
@@ -3,7 +3,7 @@ package seedu.todo.commons.exceptions;
 /**
  * Signals that some given data does not fulfill some constraints.
  */
-public class UnmatchedQuotesException extends Exception {
+public class UnmatchedQuotesException extends ParseException {
     /**
      * @param message should contain relevant information on the failed constraint(s)
      */
