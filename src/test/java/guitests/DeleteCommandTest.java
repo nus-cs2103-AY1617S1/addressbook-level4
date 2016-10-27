f23f54c09e53c79bafabaf4e70df616695bb14ae
Weng Kiat <wengkiat916@gmail.com>
2016-10-28 02:59:57 +0800

    edited delete test to pass

--- src/test/java/guitests/DeleteCommandTest.java
+++ src/test/java/guitests/DeleteCommandTest.java
@@ -57,7 +57,7 @@ public class DeleteCommandTest extends ToDoListGuiTest {
         assertTrue(taskListPanel.isListMatching(expectedRemainder));
 
         //confirm the result message is correct
-        assertResultMessage(MESSAGE_DELETE_TASK_SUCCESS);
+        assertResultMessage(String.format(MESSAGE_DELETE_TASK_SUCCESS,taskToDelete));
     }
 
 }
