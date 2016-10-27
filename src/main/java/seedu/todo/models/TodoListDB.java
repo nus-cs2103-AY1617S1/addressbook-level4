7e614f2e4c4bade0f8f5651a4ccb3d3c6db2689c
Chase <tiongyaocong@gmail.com>
2016-10-28 01:38:46 +0800

    adding comments for db filtering methods and authoring for V0.4 submission

--- src/main/java/seedu/todo/models/TodoListDB.java
+++ src/main/java/seedu/todo/models/TodoListDB.java
@@ -136,8 +136,8 @@ public class TodoListDB {
     /**
      * Destroys all Task in the DB and persists the commit.
      * 
-     * 
      * @return true if the save was successful, false otherwise
+     * @@author Tiong YaoCong A0139922Y
      */
     public void destroyAllTask() {
         tasks = new LinkedHashSet<Task>();
@@ -146,8 +146,8 @@ public class TodoListDB {
     /**
      * Destroys all Task in the DB by date
      * 
-     * 
      * @return true if the save was successful, false otherwise
+     * @@author Tiong YaoCong A0139922Y
      */
     public void destroyAllTaskByDate(LocalDateTime givenDate) {
         List<Task> selectedTasks = getTaskByDate(givenDate);
@@ -157,7 +157,7 @@ public class TodoListDB {
     /**
      * Destroys all Task in the DB by a range of date
      * 
-     * 
+     * @@author Tiong YaoCong A0139922Y
      */
     public void destroyAllTaskByRange(LocalDateTime dateFrom, LocalDateTime dateTo) {
         List<Task> selectedTasks = getTaskByRange(dateFrom, dateTo);
@@ -191,8 +191,7 @@ public class TodoListDB {
     /**
      * Destroys all Event in the DB and persists the commit.
      * 
-     * 
-     * 
+     * @@author Tiong YaoCong A0139922Y
      */
     public void destroyAllEvent() {
         events = new LinkedHashSet<Event>();
@@ -201,7 +200,7 @@ public class TodoListDB {
     /**
      * Destroys all Event in the DB by date
      * 
-     * 
+     * @@author Tiong YaoCong A0139922Y
      */
     public void destroyAllEventByDate(LocalDateTime givenDate) {
         List<Event> selectedEvents = getEventByDate(givenDate);
@@ -213,6 +212,7 @@ public class TodoListDB {
      * 
      * 
      * @return true if the save was successful, false otherwise
+     * @@author Tiong YaoCong A0139922Y
      */
     public void destroyAllEventByRange(LocalDateTime dateFrom, LocalDateTime dateTo) {
         List<Event> selectedEvents = getEventByRange(dateFrom, dateTo);
@@ -311,6 +311,12 @@ public class TodoListDB {
         }
     }
     
+    /**
+     * Get a list of events that are not over based on today date from the DB.
+     * 
+     * @return events
+     * @@author Tiong YaoCong A0139922Y
+     */   
     public List<Event> getAllCurrentEvents() {
         ArrayList<Event> currentEvents = new ArrayList<Event>();
         Iterator<Event> iterator = events.iterator();
@@ -324,9 +330,10 @@ public class TodoListDB {
     }
     
     /**
-     * Get a list of Incomplte Tasks in the DB.
+     * Get a list of Incomplete Tasks in the DB.
      * 
      * @return tasks
+     * @@author Tiong YaoCong A0139922Y
      */
     public List<Task> getIncompleteTasksAndTaskFromTodayDate() {
         ArrayList<Task> incompleteTasks = new ArrayList<Task>();
@@ -345,6 +352,16 @@ public class TodoListDB {
         return incompleteTasks;
     }
 
+    /**
+     * Filter a list of tasks with a provided item name list
+     * 
+     * @param tasks 
+     *            a list of tasks after searching condition
+     * @param itemNameList
+     *            a list of keyword to search based on task name
+     * @return tasks
+     * @@author Tiong YaoCong A0139922Y
+     */
     public List<Task> getTaskByName(List<Task> tasks, HashSet<String> itemNameList) {
         ArrayList<Task> taskByName = new ArrayList<Task>();
         Iterator<Task> iterator = tasks.iterator();
@@ -363,10 +380,19 @@ public class TodoListDB {
         return taskByName;
     }
 
-        /**
+   /**
      * Get a list of Task in the DB filtered by status , name and one date.
      * 
+     * @param givenDate
+     *               LocalDateTime parsed by Natty
+     * @param isCompleted
+     *               true if searching for completed task, else false for incomplete tasks  
+     * @param listAllStatus
+     *               true if searching for both completed and incomplete tasks, false if either one is been specify
+     * @param itemNameList
+     *               list of String to be used to search against task name                                        
      * @return list of tasks
+     * @@author Tiong YaoCong A0139922Y
      */
     public List<Task> getTaskByDateWithStatusAndName(LocalDateTime givenDate, boolean isCompleted, 
             boolean listAllStatus, HashSet<String> itemNameList) {
@@ -401,7 +427,11 @@ public class TodoListDB {
     /**
      * Get a list of Task in the DB filtered by a given date.
      * 
+     * @param givenDate 
+     *                LocalDateTime format parsed by Natty
+     *                
      * @return list of tasks
+     * @@author Tiong YaoCong A0139922Y
      */
     public List<Task> getTaskByDate(LocalDateTime givenDate) {
         ArrayList<Task> taskByDate = new ArrayList<Task>();
@@ -424,7 +454,14 @@ public class TodoListDB {
     /**
      * Get a list of Task in the DB filtered by status and one date.
      * 
+     * @param givenDate 
+     *                  LocalDateTime parsed by Natty
+     * @param isCompleted
+     *               true if searching for completed task, else false for incomplete tasks  
+     * @param listAllStatus
+     *               true if searching for both completed and incomplete tasks, false if either one is been specify
      * @return list of tasks
+     * @@author Tiong YaoCong A0139922Y
      */
     public List<Task> getTaskByDateWithStatus(LocalDateTime givenDate, boolean isCompleted, boolean listAllStatus) {
         ArrayList<Task> taskByDate = new ArrayList<Task>();
@@ -453,7 +490,17 @@ public class TodoListDB {
     /**
      * Get a list of Task in the DB filtered by status, name and range of date.
      * 
+     * @param fromDate
+     *                LocalDateTime parsed by Natty to be used to search as Start Date
+     * @param toDate
+     *                LocalDateTime parsed by Natty to be used to search as END Date               
+     * @param isCompleted
+     *               true if searching for completed task, else false for incomplete tasks  
+     * @param listAllStatus
+     *               true if searching for both completed and incomplete tasks, false if either one is been specify
+     *               
      * @return list of tasks
+     * @@author Tiong YaoCong A0139922Y
      */
     public List<Task> getTaskByRangeWithName (LocalDateTime fromDate , LocalDateTime toDate, boolean isCompleted, 
             boolean listAllStatus, HashSet<String> itemNameList) {
@@ -495,7 +542,12 @@ public class TodoListDB {
     /**
      * Get a list of Task in the DB filtered by range of date.
      * 
+     * @param fromDate
+     *                LocalDateTime parsed by Natty to be used to search as Start Date
+     * @param toDate
+     *                LocalDateTime parsed by Natty to be used to search as END Date   
      * @return list of tasks
+     * @@author Tiong YaoCong A0139922Y
      */
     public List<Task> getTaskByRange (LocalDateTime fromDate , LocalDateTime toDate) {
         ArrayList<Task> taskByRange = new ArrayList<Task>();
@@ -525,7 +577,17 @@ public class TodoListDB {
     /**
      * Get a list of Task in the DB filtered by status and range of date.
      * 
+     * @param fromDate
+     *                LocalDateTime parsed by Natty to be used to search as Start Date
+     * @param toDate
+     *                LocalDateTime parsed by Natty to be used to search as END Date               
+     * @param isCompleted
+     *               true if searching for completed task, else false for incomplete tasks  
+     * @param listAllStatus
+     *               true if status of task is not been specify else false if complete or incomplete is been specify
+     *                             
      * @return list of tasks
+     * @@author Tiong YaoCong A0139922Y
      */
     public List<Task> getTaskByRangeWithStatus (LocalDateTime fromDate , LocalDateTime toDate, 
             boolean isCompleted, boolean listAllStatus) {
@@ -563,7 +625,10 @@ public class TodoListDB {
     /**
      * Get a list of Event in the DB filtered by status, name and one date.
      * 
+     * @param givenDate
+     *                LocalDateTime parsed by Natty to be used to search event that start from this date
      * @return list of events
+     * @@author Tiong YaoCong A0139922Y
      */
     public List<Event> getEventbyDateWithName(LocalDateTime givenDate, HashSet<String> itemNameList) {
         ArrayList<Event> eventByDate = new ArrayList<Event>();
@@ -585,7 +650,12 @@ public class TodoListDB {
     /**
      * Get a list of Event in the DB filtered by status and one date.
      * 
+     * @param givenDate
+     *                LocalDateTime parsed by Natty to be used to search event that start from this date
+     * @param itemNameList
+     *                list of name keyword entered by user , to be used for match event name
      * @return list of events
+     * @@author Tiong YaoCong A0139922Y
      */
     public List<Event> getEventByDate(LocalDateTime givenDate) {
         ArrayList<Event> eventByDate = new ArrayList<Event>();
@@ -599,10 +669,17 @@ public class TodoListDB {
         return eventByDate;
     }
 
-        /**
+   /**
      * Get a list of Event in the DB filtered by status, name and range of date.
      * 
+     * @param fromDate
+     *                LocalDateTime parsed by Natty to be used to search as Start Date
+     * @param toDate
+     *                LocalDateTime parsed by Natty to be used to search as END Date               
+     * @param itemNameList
+     *                list of name keyword entered by user , to be used for match event name
      * @return list of events
+     * @@author Tiong YaoCong A0139922Y
      */
     public List<Event> getEventByRangeWithName (LocalDateTime fromDate , LocalDateTime toDate, HashSet<String> itemNameList) {
         ArrayList<Event> eventByRange = new ArrayList<Event>();
@@ -634,7 +711,13 @@ public class TodoListDB {
     /**
      * Get a list of Event in the DB filtered by status and range of date.
      * 
+     * @param fromDate
+     *                LocalDateTime parsed by Natty to be used to search as Start Date
+     * @param toDate
+     *                LocalDateTime parsed by Natty to be used to search as END Date               
+     *                           
      * @return list of events
+     * @@author Tiong YaoCong A0139922Y
      */
     public List<Event> getEventByRange (LocalDateTime fromDate , LocalDateTime toDate) {
         ArrayList<Event> eventByRange = new ArrayList<Event>();
@@ -658,6 +741,16 @@ public class TodoListDB {
         return eventByRange;
     }
 
+    /**
+     * Filter a list of event with a given name list
+     * 
+     * @param events
+     *                list of events to be used for filtering            
+     * @param itemNameList
+     *                list of name keyword entered by user , to be used for match event name
+     * @return list of events
+     * @@author Tiong YaoCong A0139922Y
+     */    
     public List<Event> getEventByName(List<Event> events, HashSet<String> itemNameList) {
         ArrayList<Event> eventByName = new ArrayList<Event>();
         Iterator<Event> iterator = events.iterator();
