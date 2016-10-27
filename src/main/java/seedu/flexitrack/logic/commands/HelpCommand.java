b147981a687f1fb3dabda83dccc9c70f89bfe1f7
SHIMIN\shimin <shimin.16@gmail.com>
2016-10-28 00:09:59 +0800

    update HelpCommand

--- src/main/java/seedu/flexitrack/logic/commands/HelpCommand.java
+++ src/main/java/seedu/flexitrack/logic/commands/HelpCommand.java
@@ -16,7 +16,7 @@ public class HelpCommand extends Command {
             + DeleteCommand.COMMAND_WORD + ", " + EditCommand.COMMAND_WORD + ", " + ExitCommand.COMMAND_WORD + ", "
             + FindCommand.COMMAND_WORD + ", " + ListCommand.COMMAND_WORD + ", " + MarkCommand.COMMAND_WORD + ", "
             + SelectCommand.COMMAND_WORD + ", " + UnmarkCommand.COMMAND_WORD + "\n" + "Example: " + COMMAND_WORD + " "
-            + ClearCommand.COMMAND_WORD;
+            + ClearCommand.COMMAND_WORD+ BlockCommand.COMMAND_WORD + UndoCommand.COMMAND_WORD;
 
     public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
     private String userInput;
@@ -59,6 +59,12 @@ public class HelpCommand extends Command {
         case ExitCommand.COMMAND_WORD:
             return new CommandResult(ExitCommand.MESSAGE_USAGE);
 
+        case BlockCommand.COMMAND_WORD:
+            return new CommandResult(BlockCommand.MESSAGE_USAGE);
+            
+        case UndoCommand.COMMAND_WORD:
+            return new CommandResult(UndoCommand.MESSAGE_USAGE);
+        
         default:
             return new CommandResult(HELP_MESSAGE_USAGE);
         }
