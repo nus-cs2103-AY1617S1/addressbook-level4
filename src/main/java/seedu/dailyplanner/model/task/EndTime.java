5221b51e9d1e7904d54ced7b54ce750c52537cec
myteo <mingyiteo@gmail.com>
2016-10-28 14:09:32 +0800

    Implement sorting

--- src/main/java/seedu/dailyplanner/model/task/EndTime.java
+++ src/main/java/seedu/dailyplanner/model/task/EndTime.java
@@ -1,55 +1,69 @@
 package seedu.dailyplanner.model.task;
 
-
 import seedu.dailyplanner.commons.exceptions.IllegalValueException;
 
 /**
- * Represents a Person's address in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ * Represents a Person's address in the address book. Guarantees: immutable; is
+ * valid as declared in {@link #isValidAddress(String)}
  */
-public class EndTime {
-    
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+public class EndTime implements Comparable<EndTime> {
 
-    public final String value;
+	public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
+	public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
-    /**
-     * Validates given address.
-     *
-     * @throws IllegalValueException if given address string is invalid.
-     */
-    //@@author A0146749N
-    public EndTime(String address) throws IllegalValueException {
-        assert address != null;
-        //if (!isValidAddress(address)) {
-          //  throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
-        //}
-        this.value = address;
-    }
+	public final String value;
+	public final int intValue;
 
-    /**
-     * Returns true if a given string is a valid person email.
-     */
-    public static boolean isValidAddress(String test) {
-        return test.matches(ADDRESS_VALIDATION_REGEX);
-    }
+	/**
+	 * Validates given address.
+	 *
+	 * @throws IllegalValueException
+	 *             if given address string is invalid.
+	 */
+	// @@author A0146749N
+	public EndTime(String address) throws IllegalValueException {
+		assert address != null;
+		// if (!isValidAddress(address)) {
+		// throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
+		// }
+		this.value = address;
+		if (address.equals(""))
+			this.intValue = 2400;
+		else {
+			String hour = address.substring(0, 2);
+			String minutes = address.substring(3, 5);
+			this.intValue = Integer.parseInt(hour + minutes);
+		}
+	}
 
-    @Override
-    public String toString() {
-        return value;
-    }
+	/**
+	 * Returns true if a given string is a valid person email.
+	 */
+	public static boolean isValidAddress(String test) {
+		return test.matches(ADDRESS_VALIDATION_REGEX);
+	}
 
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof EndTime // instanceof handles nulls
-                && this.value.equals(((EndTime) other).value)); // state check
-    }
+	@Override
+	public String toString() {
+		return value;
+	}
 
-    @Override
-    public int hashCode() {
-        return value.hashCode();
-    }
+	@Override
+	public boolean equals(Object other) {
+		return other == this // short circuit if same object
+				|| (other instanceof EndTime // instanceof handles nulls
+						&& this.value.equals(((EndTime) other).value)); // state
+																		// check
+	}
+
+	@Override
+	public int hashCode() {
+		return value.hashCode();
+	}
+
+	@Override
+	public int compareTo(EndTime o) {
+		return this.intValue - o.intValue;
+	}
 
 }
--- src/main/java/seedu/dailyplanner/model/task/Task.java
+++ src/main/java/seedu/dailyplanner/model/task/Task.java
@@ -6,115 +6,126 @@ import seedu.dailyplanner.commons.util.CollectionUtil;
 import seedu.dailyplanner.model.tag.UniqueTagList;
 
 /**
- * Represents a Person in the address book.
- * Guarantees: details are present and not null, field values are validated.
+ * Represents a Person in the address book. Guarantees: details are present and
+ * not null, field values are validated.
  */
-public class Task implements ReadOnlyTask {
+public class Task implements ReadOnlyTask, Comparable<Task> {
 
-    private Name name;
-    private Date phone;
-    private StartTime email;
-    private EndTime address;
-    private boolean isComplete;
+	private Name name;
+	private Date phone;
+	private StartTime email;
+	private EndTime address;
+	private boolean isComplete;
 
-    private UniqueTagList tags;
+	private UniqueTagList tags;
 
-    /**
-     * Every field must be present and not null.
-     */
-    public Task(Name name, Date phone, StartTime email, EndTime address, UniqueTagList tags) {
-        assert !CollectionUtil.isAnyNull(name, phone, email, address, tags);
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.address = address;
-        this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
-        this.isComplete = false;
-    }
+	/**
+	 * Every field must be present and not null.
+	 */
+	public Task(Name name, Date phone, StartTime email, EndTime address, UniqueTagList tags) {
+		assert !CollectionUtil.isAnyNull(name, phone, email, address, tags);
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+		this.address = address;
+		this.tags = new UniqueTagList(tags); // protect internal tags from
+												// changes in the arg list
+		this.isComplete = false;
+	}
 
-    /**
-     * Copy constructor.
-     */
-    public Task(ReadOnlyTask source) {
-        this(source.getName(), source.getPhone(), source.getEmail(), source.getAddress(), source.getTags());
-    }
+	/**
+	 * Copy constructor.
+	 */
+	public Task(ReadOnlyTask source) {
+		this(source.getName(), source.getPhone(), source.getEmail(), source.getAddress(), source.getTags());
+	}
 
-    @Override
-    public void setName(Name name) {
-        this.name = name;
-    }
+	@Override
+	public void setName(Name name) {
+		this.name = name;
+	}
 
-    @Override
-    public void setDate(Date date) {
-        this.phone = date;
-    }
+	@Override
+	public void setDate(Date date) {
+		this.phone = date;
+	}
 
-    @Override
-    public void setStartTime(StartTime time) {
-        this.email = time;
-    }
+	@Override
+	public void setStartTime(StartTime time) {
+		this.email = time;
+	}
 
-    @Override
-    public void setEndTime(EndTime time) {
-        this.address = time;
-    }
-    
-    public void markAsComplete() {
-	this.isComplete = true;
-    }
-    
-    @Override
-    public Name getName() {
-        return name;
-    }
+	@Override
+	public void setEndTime(EndTime time) {
+		this.address = time;
+	}
 
-    @Override
-    public Date getPhone() {
-        return phone;
-    }
+	public void markAsComplete() {
+		this.isComplete = true;
+	}
 
-    @Override
-    public StartTime getEmail() {
-        return email;
-    }
+	@Override
+	public Name getName() {
+		return name;
+	}
 
-    @Override
-    public EndTime getAddress() {
-        return address;
-    }
+	@Override
+	public Date getPhone() {
+		return phone;
+	}
 
-    @Override
-    public UniqueTagList getTags() {
-        return new UniqueTagList(tags);
-    }
+	@Override
+	public StartTime getEmail() {
+		return email;
+	}
 
-    /**
-     * Replaces this person's tags with the tags in the argument tag list.
-     */
-    public void setTags(UniqueTagList replacement) {
-        tags.setTags(replacement);
-    }
+	@Override
+	public EndTime getAddress() {
+		return address;
+	}
 
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof ReadOnlyTask // instanceof handles nulls
-                && this.isSameStateAs((ReadOnlyTask) other));
-    }
+	@Override
+	public UniqueTagList getTags() {
+		return new UniqueTagList(tags);
+	}
 
-    @Override
-    public int hashCode() {
-        // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags, isComplete);
-    }
+	/**
+	 * Replaces this person's tags with the tags in the argument tag list.
+	 */
+	public void setTags(UniqueTagList replacement) {
+		tags.setTags(replacement);
+	}
 
-    @Override
-    public String toString() {
-        return getAsText();
-    }
-    
-    public String getCompletion() {
-	return (isComplete) ? "COMPLETE" : "NOT COMPLETE"; 
-    }
+	@Override
+	public boolean equals(Object other) {
+		return other == this // short circuit if same object
+				|| (other instanceof ReadOnlyTask // instanceof handles nulls
+						&& this.isSameStateAs((ReadOnlyTask) other));
+	}
+
+	@Override
+	public int hashCode() {
+		// use this method for custom fields hashing instead of implementing
+		// your own
+		return Objects.hash(name, phone, email, address, tags, isComplete);
+	}
+
+	@Override
+	public String toString() {
+		return getAsText();
+	}
+
+	public String getCompletion() {
+		return (isComplete) ? "COMPLETE" : "NOT COMPLETE";
+	}
+
+	@Override
+	public int compareTo(Task o) {
+		if (this.getPhone().compareTo(o.getPhone()) != 0)
+			return this.getPhone().compareTo(o.getPhone());
+		else if (this.getEmail().compareTo(o.getEmail()) != 0)
+			return this.getEmail().compareTo(o.getEmail());
+		return this.getAddress().compareTo(o.getAddress());
+	}
 
 }
