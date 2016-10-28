b59a6e112e9957475271d745046e644b25c6587e
zhiminglim <i-jovial@hotmail.com>
2016-10-28 11:57:43 +0800

    Insert quotes

--- src/main/java/seedu/emeraldo/logic/commands/MotivateMeCommand.java
+++ src/main/java/seedu/emeraldo/logic/commands/MotivateMeCommand.java
@@ -3,15 +3,27 @@ package seedu.emeraldo.logic.commands;
 public class MotivateMeCommand extends Command {
 
     
-    private static final String MESSAGE_MOTIVATE_ME_SUCCESS = null;
+    public static final String MESSAGE_MOTIVATE_ME_SUCCESS = null;
+    public static final String [] MESSAGE_MOTIVATE_LIST = {
+            "Don't wish it was easier, wish you were better.", 
+            "If not now, when?",
+            "It does not matter how slowly you go as long as you do not stop.",
+            "Time will pass, will you?",
+            "When you feel like quitting, think about why you started."
+    };
     public static final String COMMAND_WORD = "motivate me";
 
+    public MotivateMeCommand() {}
+    
     public CommandResult execute() {
-
         
         Object motivateMe = null;
         return new CommandResult(String.format(MESSAGE_MOTIVATE_ME_SUCCESS, motivateMe));
 
+    }
+
+    public static String [] getMessageMotivateList() {
+        return MESSAGE_MOTIVATE_LIST;
     }
     
 }
