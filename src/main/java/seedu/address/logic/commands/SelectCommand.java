5c94dfca16d8a7a9b87f5c9f73b27554f3374487
Zavier\ZavierOng <zacohjayjay@gmail.com>
2016-10-28 13:54:14 +0800

    reedit select command

--- src/main/java/seedu/address/logic/commands/SelectCommand.java
+++ src/main/java/seedu/address/logic/commands/SelectCommand.java
@@ -12,34 +12,56 @@ import seedu.address.commons.core.UnmodifiableObservableList;
  */
 public class SelectCommand extends Command {
 
-    public final ArrayList<Integer> targetIndexes;
-
     public static final String COMMAND_WORD = "select";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Selects the task identified by the index number used in the last task listing.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + "Parameters: INDEX (must be a positive integer with either E, D or T in front.)\n"
+            + "Example: " + COMMAND_WORD + " E1\n"
+            + "Example: " + COMMAND_WORD + " D1\n"
+            + "Example: " + COMMAND_WORD + " T1";
 
     public static final String MESSAGE_SELECT_TASK_SUCCESS = "Selected Task: %1$s";
 
-    public SelectCommand(ArrayList<Integer> targetIndexes) {
-        this.targetIndexes = targetIndexes;
+    public final Integer targetIndex;
+    public final String Args;
+    public final char category;
+
+
+    public SelectCommand(Integer index, String args, char category) {
+    	this.targetIndex = index;
+        this.Args = args;
+        this.category = category;
     }
 
     @Override
     public CommandResult execute() {      
-
-        UnmodifiableObservableList<ReadOnlyTask> lastShownEventList = model.getFilteredEventList();
-        for(int i =0; i < targetIndexes.size(); i++){
-            if (lastShownEventList.size() < targetIndexes.get(i)) {
-                indicateAttemptToExecuteIncorrectCommand();
-                return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
-            }
-
-            EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndexes.get(i) - 1));
+    	
+    	UnmodifiableObservableList<ReadOnlyTask> lastShownEventList = model.getFilteredEventList();
+        UnmodifiableObservableList<ReadOnlyTask> lastShownDeadlineList = model.getFilteredDeadlineList();
+        UnmodifiableObservableList<ReadOnlyTask> lastShownTodoList = model.getFilteredTodoList();
+        if (category == 'E') {
+        	if (lastShownEventList.size() < targetIndex) {
+        		indicateAttemptToExecuteIncorrectCommand();
+        		return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        	}
+        	EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex - 1, category));
         }
-            return new CommandResult(String.format(MESSAGE_SELECT_TASK_SUCCESS, lastShownEventList.get(0).toString()));
+        else if (category == 'D') {
+        	if (lastShownDeadlineList.size() < targetIndex) {
+        		indicateAttemptToExecuteIncorrectCommand();
+        		return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        	}
+        	EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex - 1, category));
+        }
+        else {
+            if (lastShownTodoList.size() < targetIndex) {
+           		indicateAttemptToExecuteIncorrectCommand();
+           		return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+           	}
+           	EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex - 1, category));
+        }
+        return new CommandResult(String.format(MESSAGE_SELECT_TASK_SUCCESS, lastShownEventList.get(0).toString()));
         
     }
 
