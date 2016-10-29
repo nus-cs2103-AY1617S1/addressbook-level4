e45a24b9e11eb35c0fb96bc5c1bba703edb11916
Low Jian Sheng <lowjiansheng@hotmail.sg>
2016-10-29 18:14:59 +0800

    Add methods to compare current date and dates of activities.

---
+++ src/main/java/seedu/menion/background/SendEmailStub.java
@@ -0,0 +1,14 @@
+package seedu.menion.background;
+
+import seedu.menion.model.activity.ReadOnlyActivity;
+
+public class SendEmailStub {
+
+	public SendEmailStub(){};
+	
+	public static void send(ReadOnlyActivity activityToCheck){
+		
+		System.out.println("Email Sent");
+	}
+	
+}
