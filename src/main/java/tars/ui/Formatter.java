bc09dce073d23febf9ac7dc02804d8930694d5be
Wei Kang <weikangchia@gmail.com>
2016-10-29 14:43:22 +0800

    Refractor method for formatting taskList and rsvTaskList to Formatter class

--- src/main/java/tars/ui/Formatter.java
+++ src/main/java/tars/ui/Formatter.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import tars.model.tag.ReadOnlyTag;
 import tars.model.task.DateTime;
+import tars.model.task.ReadOnlyTask;
 import tars.model.task.rsv.RsvTask;
 
 /**
@@ -16,14 +17,8 @@ import tars.model.task.rsv.RsvTask;
 public class Formatter {
     /** Format of indexed list item */
     private static final String MESSAGE_INDEXED_LIST_ITEM = "%1$d.\t%2$s";
-
-    /** A decorative prefix added to the beginning of lines printed by TARS */
-    private static final String LINE_PREFIX = " ";
-
-    /** A platform independent line separator. */
-    private static final String LS = System.lineSeparator();
     
-    private static final String EMPTY_LIST_MESSAGE = "0 %1$s listed.";
+    public static final String EMPTY_LIST_MESSAGE = "0 %1$s listed.";
 
     /** Offset required to convert between 1-indexing and 0-indexing. */
     public static final int DISPLAYED_INDEX_OFFSET = 1;
@@ -38,25 +33,41 @@ public class Formatter {
         for (ReadOnlyTag tag : tags) {
             formattedTags.add(tag.getAsText());
         }
-        return format(asIndexedList(formattedTags));
+        return asIndexedList(formattedTags);
     }
-
-    /** Formats the given strings for displaying to the user. */
-    public String format(String... messages) {
-        StringBuilder sb = new StringBuilder();
-        for (String m : messages) {
-            sb.append(LINE_PREFIX + m.replace("\n", LS + LINE_PREFIX) + LS);
+    
+    public String formatTaskList(List<? extends ReadOnlyTask> taskList) {
+        final List<String> formattedTasks = new ArrayList<>();
+        
+        if(taskList.size() == 0) {
+            return String.format(EMPTY_LIST_MESSAGE, "tasks");
         }
-        return sb.toString();
+        
+        for (ReadOnlyTask task : taskList) {
+            formattedTasks.add(task.getAsText());
+        }
+        return asIndexedList(formattedTasks);
+    }
+    
+    public String formatRsvTaskList(List<? extends RsvTask> rsvTaskList) {
+        final List<String> formattedTasks = new ArrayList<>();
+        
+        if(rsvTaskList.size() == 0) {
+            return String.format(EMPTY_LIST_MESSAGE, "tasks");
+        }
+        
+        for (RsvTask task : rsvTaskList) {
+            formattedTasks.add(task.toString());
+        }
+        return asIndexedList(formattedTasks);
     }
 
     /** Formats a list of strings as an indexed list. */
     private static String asIndexedList(List<String> listItems) {
         final StringBuilder formatted = new StringBuilder();
-        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
+        int displayIndex = DISPLAYED_INDEX_OFFSET;
         for (String listItem : listItems) {
-            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
-            displayIndex++;
+            formatted.append(getIndexedListItem(displayIndex++, listItem)).append("\n");
         }
         return formatted.toString();
     }
