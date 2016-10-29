fa18e3e6307811a0b5669fdaaf67379bf26024e2
ChewZL <chewzl@yahoo.com.sg>
2016-10-29 22:33:31 +0800

    [Find command] Fixed find command with no string result

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,10 +1,13 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import seedu.address.logic.commands.taskcommands.FindTaskCommand;
+import seedu.address.logic.commands.taskcommands.IncorrectTaskCommand;
 import seedu.address.logic.commands.taskcommands.TaskCommand;
 
 /*
@@ -21,6 +24,9 @@ public class FindCommandParser extends CommandParser{
      */
     public TaskCommand prepareCommand(String arguments) {
         // keywords delimited by whitespace
+    	if(arguments.equals("")){
+    		return new IncorrectTaskCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTaskCommand.MESSAGE_USAGE));
+    	}
         final String[] keywords = arguments.split(" ");
         final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
         return new FindTaskCommand(keywordSet);
