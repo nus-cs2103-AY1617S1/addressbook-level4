53d01771848b0d942a297c912db7336157e20d08
PankajB1997 <pankajbhootra@gmail.com>
2016-10-29 16:54:30 +0800

    Add default message "Welcome to Lazyman's Friend!" to Messge Box

--- src/main/java/seedu/tasklist/commons/core/Messages.java
+++ src/main/java/seedu/tasklist/commons/core/Messages.java
@@ -4,7 +4,7 @@ package seedu.tasklist.commons.core;
  * Container for user visible messages.
  */
 public class Messages {
-
+    
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid";
