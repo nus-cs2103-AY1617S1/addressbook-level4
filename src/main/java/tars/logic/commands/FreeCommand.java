1e1f7d84f3bc52e3a2e5a9f21745fe0dbc2bb9d6
Johnervan Lee <johnervan@gmail.com>
2016-10-30 09:05:56 +0800

    Update MESSAGE_NO_FREE_TIMESLOTS and fix bug in FindCommand

--- src/main/java/tars/logic/commands/FreeCommand.java
+++ src/main/java/tars/logic/commands/FreeCommand.java
@@ -18,14 +18,16 @@ public class FreeCommand extends Command {
 
     public static final String MESSAGE_DATE_RANGE_DETECTED = "Range of datetime detected. Please only input a single datetime";
     public static final String MESSAGE_SUCCESS = "Free timeslots on %1$s";
-    public static final String MESSAGE_FREE_DAY = "You have no tasks or reserved tasks on %1$s";
+    public static final String MESSAGE_FREE_DAY = "You have no event tasks or reserved event tasks on %1$s";
     public static final String MESSAGE_NO_FREE_TIMESLOTS = "You have no free time slots on %1$s";
 
     private DateTime dateToCheck;
 
     public FreeCommand(DateTime dateToCheck) {
         this.dateToCheck = dateToCheck;
+        //Ensure that dateToCheck covers the whole day
         this.dateToCheck.setStartDateTime(dateToCheck.getEndDate().withHour(0).withMinute(0).withSecond(0));
+        this.dateToCheck.setEndDateTime(dateToCheck.getEndDate().withHour(23).withMinute(59).withSecond(59));
     }
 
     @Override
