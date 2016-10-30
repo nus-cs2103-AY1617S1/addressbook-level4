53d68aa69575babc563859f551ccfc838a14ec26
JingHe <he1994@hotmail.com>
2016-10-30 13:12:18 +0800

    Update UML Diagram for Model

--- src/main/java/seedu/emeraldo/model/Model.java
+++ src/main/java/seedu/emeraldo/model/Model.java
@@ -52,7 +52,6 @@ public interface Model {
     /** Updates the filter of the filtered task list to show all tasks */
     void updateFilteredListToShowAll();
 
-    
     /** Updates the filter of the filtered task list to filter by the given keywords*/
     void updateFilteredTaskList(Set<String> keywords);
     
