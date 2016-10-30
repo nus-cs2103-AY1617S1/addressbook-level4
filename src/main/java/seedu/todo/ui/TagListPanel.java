877cfe806f12d85794097038e65ae8286a5927d2
jessidew95 <dewi-jessi-jiahui@hotmail.com>
2016-10-30 23:42:38 +0800

    Fixed indentation and removed commented out code

--- src/main/java/seedu/todo/ui/TagListPanel.java
+++ src/main/java/seedu/todo/ui/TagListPanel.java
@@ -13,78 +13,76 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import seedu.todo.model.tag.Tag;
 
-public class TagListPanel extends UiPart{
+public class TagListPanel extends UiPart {
 	private static final String FXML = "TagListPanel.fxml";
-    private VBox panel;
-    private AnchorPane placeHolderPane;
+	private VBox panel;
+	private AnchorPane placeHolderPane;
 
-    @FXML
-    private ListView<Tag> tagListView;
-    
-    public TagListPanel() {
-    	super();
-    }
-    
-    @Override
-    public String getFxmlPath() {
-    	return FXML;
-    }
+	@FXML
+	private ListView<Tag> tagListView;
 
-    @Override
-    public void setNode(Node node) {
-        panel = (VBox) node;
-    }
-    
-    public void setPlaceholder(AnchorPane pane) {
-        this.placeHolderPane = pane;
-    }
+	public TagListPanel() {
+		super();
+	}
 
-    public static TagListPanel load(Stage primaryStage, AnchorPane tagListPanelPlaceholder,
-    								ObservableList<Tag> tagList){
-    	TagListPanel tagListPanel =
-        UiPartLoader.loadUiPart(primaryStage, tagListPanelPlaceholder, new TagListPanel());
-        tagListPanel.configure(tagList);
-        return tagListPanel;
-    }
+	@Override
+	public String getFxmlPath() {
+		return FXML;
+	}
 
-    private void configure(ObservableList<Tag> tagList) {
-        setConnections(tagList);
-        addToPlaceholder(); 
-    }
-    
-    private void addToPlaceholder() {
-        SplitPane.setResizableWithParent(placeHolderPane, false);
-        placeHolderPane.getChildren().add(panel);
-    }
-    
-    private void setConnections(ObservableList<Tag> tagList) {
-        tagListView.setItems(tagList);
-        tagListView.setCellFactory(listView -> new TagListViewCell());
-        //setEventHandlerForSelectionChangeEvent();
-    }
-    
-    public void scrollTo(int index) {
-        Platform.runLater(() -> {
-            tagListView.scrollTo(index);
-            tagListView.getSelectionModel().clearAndSelect(index);
-        });
-    }
-    
-    class TagListViewCell extends ListCell<Tag> {
+	@Override
+	public void setNode(Node node) {
+		panel = (VBox) node;
+	}
 
-        public TagListViewCell() {
-        }
+	public void setPlaceholder(AnchorPane pane) {
+		this.placeHolderPane = pane;
+	}
 
-        @Override
-        protected void updateItem(Tag tag, boolean empty) {
-            super.updateItem(tag, empty);
+	public static TagListPanel load(Stage primaryStage, AnchorPane tagListPanelPlaceholder,
+									ObservableList<Tag> tagList) {
+		TagListPanel tagListPanel = UiPartLoader.loadUiPart(primaryStage, tagListPanelPlaceholder, new TagListPanel());
+		tagListPanel.configure(tagList);
+		return tagListPanel;
+	}
 
-            if (empty || tag == null) {
-                setGraphic(null);
-                setText(null);
-            } else {
-                setGraphic(TagCard.load(tag).getLayout());
-            }
-        }
-    }
+	private void configure(ObservableList<Tag> tagList) {
+		setConnections(tagList);
+		addToPlaceholder();
+	}
+
+	private void addToPlaceholder() {
+		SplitPane.setResizableWithParent(placeHolderPane, false);
+		placeHolderPane.getChildren().add(panel);
+	}
+
+	private void setConnections(ObservableList<Tag> tagList) {
+		tagListView.setItems(tagList);
+		tagListView.setCellFactory(listView -> new TagListViewCell());
+	}
+
+	public void scrollTo(int index) {
+		Platform.runLater(() -> {
+			tagListView.scrollTo(index);
+			tagListView.getSelectionModel().clearAndSelect(index);
+		});
+	}
+
+	class TagListViewCell extends ListCell<Tag> {
+
+		public TagListViewCell() {
+		}
+
+		@Override
+		protected void updateItem(Tag tag, boolean empty) {
+			super.updateItem(tag, empty);
+
+			if (empty || tag == null) {
+				setGraphic(null);
+				setText(null);
+			} else {
+				setGraphic(TagCard.load(tag).getLayout());
+			}
+		}
+	}
 }
