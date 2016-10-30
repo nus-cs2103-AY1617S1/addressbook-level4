7ab5276bc603cf8cccf4be7cf39a137189612a42
jessidew95 <dewi-jessi-jiahui@hotmail.com>
2016-10-31 01:11:05 +0800

    Create Tag List Panel Handle

---
+++ src/test/java/guitests/guihandles/TagListPanelHandle.java
@@ -0,0 +1,34 @@
+//@@author A0142421X
+package guitests.guihandles;
+
+import java.util.List;
+
+import guitests.GuiRobot;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+import seedu.todo.TestApp;
+import seedu.todo.model.tag.Tag;
+
+/**
+ * Provides a handle for the panel containing tag List
+ */
+public class TagListPanelHandle extends GuiHandle{
+	
+	public static final int NOT_FOUND = -1;
+	public static final String CARD_PANE_ID = "#cardPane";
+
+    private static final String TAG_LIST_VIEW_ID = "#tagListView";
+
+    public TagListPanelHandle(GuiRobot guiRobot, Stage primaryStage) {
+        super(guiRobot, primaryStage, TestApp.APP_TITLE);
+    }
+
+    public List<Tag> getSelectedTasks() {
+        ListView<Tag> tagList = getListView();
+        return tagList.getSelectionModel().getSelectedItems();
+    }
+
+    public ListView<Tag> getListView() {
+        return (ListView<Tag>) getNode(TAG_LIST_VIEW_ID);
+    }
+}
