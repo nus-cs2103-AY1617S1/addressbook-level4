b2ea3928c7db87040fa350bb714b74cc2a65c18c
Nat <nathanaelchan94@gmail.com>
2016-10-30 22:10:19 +0800

    Should work now

--- src/main/java/seedu/unburden/model/task/Time.java
+++ src/main/java/seedu/unburden/model/task/Time.java
@@ -6,65 +6,68 @@ import java.util.regex.Pattern;
 import seedu.unburden.commons.exceptions.IllegalValueException;
 
 /**
- * Represents a Task's time in the Task Manager.
- * Guarantees: immutable; is valid as declared in {@link #isValidTime(String)}
+ * Represents a Task's time in the Task Manager. Guarantees: immutable; is valid
+ * as declared in {@link #isValidTime(String)}
+ * 
  * @@author A0143095H
  */
 
-//@@ Gauri Joshi A0143095H
+// @@ Gauri Joshi A0143095H
 public class Time {
 
-    public static final String MESSAGE_TIME_CONSTRAINTS = "Task time should be in the format XXYY where X represents the number of hours and Y represents the number of minutes";
-    public static final String TIME_VALIDATION_REGEX = "([01][0-9]|2[0-3])[0-5][0-9]";
-    
-    //\\[0-9]{2}[0-9]{2}
+	public static final String MESSAGE_TIME_CONSTRAINTS = "Task time should be in the format XXYY where X represents the number of hours and Y represents the number of minutes";
+	public static final String TIME_VALIDATION_REGEX = "([01][0-9]|2[0-3])[0-5][0-9]";
 
-    public final String fullTime;
+	// \\[0-9]{2}[0-9]{2}
 
-    /**
-     * Validates given time.
-     *
-     * @throws IllegalValueException if given time string is invalid.
-     */
-    public Time(String time) throws IllegalValueException {
-        assert time != null;
-       time = time.trim();
-        if (!isValidTime(time) && !time.equals("Thisisanemptystring")) {
-            throw new IllegalValueException(MESSAGE_TIME_CONSTRAINTS);
-        }
-        if(time.equals("Thisisanemptystring")){
-        	this.fullTime = "";
-        }
-        else{
-        	this.fullTime = time;
-        }
-    }
+	public final String fullTime;
 
-    /**
-     * Returns true if a given string is a valid time.
-     */
-    public static boolean isValidTime(String test) {
-    	final Pattern pattern = Pattern.compile(TIME_VALIDATION_REGEX);
-    	final Matcher matcher = pattern.matcher(test);
-        return matcher.matches();
-    }
+	/**
+	 * Validates given time.
+	 *
+	 * @throws IllegalValueException
+	 *             if given time string is invalid.
+	 */
+	public Time(String time) throws IllegalValueException {
+		assert time != null;
+		if (!time.equals("       ")) {
+			time = time.trim();
+			if (!isValidTime(time)) {
+				throw new IllegalValueException(MESSAGE_TIME_CONSTRAINTS);
+			}
+		}
+		if (time.equals("       ")) {
+			this.fullTime = "       ";
+		} else {
+			this.fullTime = time;
+		}
+	}
 
+	/**
+	 * Returns true if a given string is a valid time.
+	 */
+	public static boolean isValidTime(String test) {
+		final Pattern pattern = Pattern.compile(TIME_VALIDATION_REGEX);
+		final Matcher matcher = pattern.matcher(test);
+		return matcher.matches();
+	}
 
-    @Override
-    public String toString() {
-        return fullTime;
-    }
+	@Override
+	public String toString() {
+		return fullTime;
+	}
 
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof Time // instanceof handles nulls
-                && this.fullTime.equals(((Time) other).fullTime)); // state check
-    }
+	@Override
+	public boolean equals(Object other) {
+		return other == this // short circuit if same object
+				|| (other instanceof Time // instanceof handles nulls
+						&& this.fullTime.equals(((Time) other).fullTime)); // state
+																			// check
+	}
 
-    @Override
-    public int hashCode() {
-        return fullTime.hashCode();
-    }
+	@Override
+	public int hashCode() {
+		return fullTime.hashCode();
+	}
 
 }
