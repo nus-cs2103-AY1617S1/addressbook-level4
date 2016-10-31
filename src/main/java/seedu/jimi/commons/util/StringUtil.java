1f043a6c039c7ef6dfadf6fbf5bb25c362c239b8
clarencechee <clarencechee94@gmail.com>
2016-10-31 22:41:16 +0800

    StringUtil: add getFirstWord()

--- src/main/java/seedu/jimi/commons/util/StringUtil.java
+++ src/main/java/seedu/jimi/commons/util/StringUtil.java
@@ -33,4 +33,17 @@ public class StringUtil {
     public static boolean isUnsignedInteger(String s){
         return s != null && s.matches("^0*[1-9]\\d*$");
     }
+
+    /** 
+     * Returns the first word separated by spaces in {@code text}. 
+     * Adapted from StackOverflow because I'm too lazy to create my own method.
+     */
+    public static String getFirstWord(String text) {
+        String trimmed = new String(text.trim());
+        if (trimmed.indexOf(' ') > -1) { // Check if there is more than one word.
+            return trimmed.substring(0, trimmed.indexOf(' ')); // Extract first word.
+        } else {
+            return trimmed; // Text is the first word itself.
+        }
+    }
 }
