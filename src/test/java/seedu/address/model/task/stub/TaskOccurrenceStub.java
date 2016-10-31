da2f7ac1aa6c81cbf0fd07da72d8cffe7221874c
Brandon <brandonyeoxg@gmail.com>
2016-10-31 21:03:49 +0800

    Add unit test for UniqueTaskListTest.java.

---
+++ src/test/java/seedu/address/model/task/stub/TaskOccurrenceStub.java
@@ -0,0 +1,10 @@
+package seedu.address.model.task.stub;
+
+import seedu.address.model.task.TaskOccurrence;
+
+class TaskOccurrenceStub extends TaskOccurrence {
+
+    public TaskOccurrenceStub(TaskStub taskReference, TaskDateStub startDate, TaskDateStub endDate) {
+        super(taskReference, startDate, endDate);
+    }
+}
