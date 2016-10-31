55d54a7958d7854f2963b8a1ed315a0ebca6a34f
Nat <nathanaelchan94@gmail.com>
2016-10-31 13:55:18 +0800

    Updated the task description validation

--- src/main/java/seedu/unburden/model/task/TaskDescription.java
+++ src/main/java/seedu/unburden/model/task/TaskDescription.java
@@ -3,16 +3,17 @@ package seedu.unburden.model.task;
 import seedu.unburden.commons.exceptions.IllegalValueException;
 
 /**
- * Represents a Task's description in the task manager.
- * Guarantees: immutable; is valid as declared in {@link #isValidDescription(String)}
+ * Represents a Task's description in the task manager. Guarantees: immutable;
+ * is valid as declared in {@link #isValidDescription(String)}
+ * 
  * @@author A0143095H
  */
 
-//@@Gauri Joshi A0143095H
+// @@Gauri Joshi A0143095H
 public class TaskDescription {
 	private static final String MESSAGE_TASK_CONSTRAINTS = "Task descriptions should be spaces or alphanumeric characters.";
 	public static final String TASK_VALIDATION_REGEX = "[A-Za-z0-9 ,.?!\'\"]+";
-	
+
 	public final String fullTaskDescriptions;
 
 	/**
@@ -23,14 +24,18 @@ public class TaskDescription {
 	 *             if the string passed in is invalid
 	 */
 	public TaskDescription(String details) throws IllegalValueException {
-		/*
 		assert details != null;
-		details = details.trim();
-		if (isValidDetails(details)) {
-			throw new IllegalValueException(MESSAGE_TASK_CONSTRAINTS);
+		if (details.equals("       ")) {
+			details = details.trim();
+			if (!isValidDetails(details)) {
+				throw new IllegalValueException(MESSAGE_TASK_CONSTRAINTS);
+			}
 		}
-		*/
-		this.fullTaskDescriptions = details;
+		if (details.equals("       ")) {
+			this.fullTaskDescriptions = "       ";
+		} else {
+			this.fullTaskDescriptions = details;
+		}
 	}
 
 	private boolean isValidDetails(String details) {
@@ -46,7 +51,7 @@ public class TaskDescription {
 	public boolean equals(Object other) {
 		return other == this // short circuit if same object
 				|| (other instanceof TaskDescription // instanceof handles nulls
-						&& this.fullTaskDescriptions.equals(((TaskDescription) other).fullTaskDescriptions)); 
+						&& this.fullTaskDescriptions.equals(((TaskDescription) other).fullTaskDescriptions));
 	}
 
 	@Override
