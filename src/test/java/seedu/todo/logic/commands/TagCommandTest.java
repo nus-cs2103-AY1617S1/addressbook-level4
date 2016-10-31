cb0dca03d4807932b80f3a5314b643716f21d70a
Xien Dong Wang <xdrawks@gmail.com>
2016-10-31 14:52:57 +0800

    Create template for tag command test

---
+++ src/test/java/seedu/todo/logic/commands/TagCommandTest.java
@@ -0,0 +1,16 @@
+package seedu.todo.logic.commands;
+
+//@@author A0135805H
+/**
+ * Performs integration testing for Tag Command.
+ */
+public class TagCommandTest extends CommandTest {
+
+    /* Override Methods */
+    @Override
+    protected BaseCommand commandUnderTest() {
+        return new TagCommand();
+    }
+
+    
+}
