292547fcf64d6e8cba174eee41ff64360dc28f18
dheerajaraj <dheerajaraj@yahoo.com.sg>
2016-10-31 16:06:44 +0800

    overlapFixed

--- src/main/java/seedu/tasklist/model/task/UniqueTaskList.java
+++ src/main/java/seedu/tasklist/model/task/UniqueTaskList.java
@@ -18,151 +18,138 @@ import java.util.*;
  */
 public class UniqueTaskList implements Iterable<Task> {
 
-    /**
-     * Signals that an operation would have violated the 'no duplicates' property of the list.
-     */
-    public static class DuplicateTaskException extends DuplicateDataException {
-        protected DuplicateTaskException() {
-            super("Operation would result in duplicate tasks");
-        }
-    }
+	/**
+	 * Signals that an operation would have violated the 'no duplicates' property of the list.
+	 */
+	public static class DuplicateTaskException extends DuplicateDataException {
+		protected DuplicateTaskException() {
+			super("Operation would result in duplicate tasks");
+		}
+	}
 
-    /**
-     * Signals that an operation targeting a specified person in the list would fail because
-     * there is no such matching person in the list.
-     */
-    public static class TaskNotFoundException extends Exception {}
+	/**
+	 * Signals that an operation targeting a specified person in the list would fail because
+	 * there is no such matching person in the list.
+	 */
+	public static class TaskNotFoundException extends Exception {}
 
-    private final ObservableList<Task> internalList = FXCollections.observableArrayList();
+	private final ObservableList<Task> internalList = FXCollections.observableArrayList();
 
-    /**
-     * Constructs empty PersonList.
-     */
-    public UniqueTaskList() {}
+	/**
+	 * Constructs empty PersonList.
+	 */
+	public UniqueTaskList() {}
 
-    /**
-     * Returns true if the list contains an equivalent person as the given argument.
-     */
-    public boolean contains(ReadOnlyTask toCheck) {
-        assert toCheck != null;
-        return internalList.contains(toCheck);
-    }
+	/**
+	 * Returns true if the list contains an equivalent person as the given argument.
+	 */
+	public boolean contains(ReadOnlyTask toCheck) {
+		assert toCheck != null;
+		return internalList.contains(toCheck);
+	}
 
-    /**
-     * Adds a person to the list.
-     *
-     * @throws DuplicateTaskException if the person to add is a duplicate of an existing person in the list.
-     */
-    public void add(Task toAdd) throws DuplicateTaskException {
-        assert toAdd != null;
-        if (contains(toAdd)) {
-            throw new DuplicateTaskException();
-        }
-        internalList.add(toAdd);
-    }
-    
-    public boolean isOverlapping(Task toAdd) {
-    	assert toAdd != null;
-    	for (Task i: internalList) {
-    		if (toAdd.getEndTime().toCardString().equals("-")) {
-    			if (!toAdd.equals(i)
-            		&& !toAdd.getStartTime().toCardString().equals("-")
-            		//&& !task.getEndTime().toCardString().equals("-")
-    				&& !i.getStartTime().toCardString().equals("-")
-    				&& !i.getEndTime().toCardString().equals("-")
-    				&& !toAdd.getStartTime().getAsCalendar().after(i.getEndTime().getAsCalendar())
-    				&& (toAdd.getEndTime().time.getTime().compareTo(i.getEndTime().getAsCalendar().getTime())==0)
-    				&& (toAdd.getStartTime().time.getTime().compareTo(i.getStartTime().getAsCalendar().getTime())==0)
-    				&& !toAdd.getStartTime().getAsCalendar().before(i.getStartTime().getAsCalendar())) {
-    				return true;
-    			}
-    		}
-    		
-    		else if (!toAdd.equals(i)
-    				&& !toAdd.getStartTime().toCardString().equals("-")
-    				//&& !toAdd.getEndTime().toCardString().equals("-")
-    				&& !i.getStartTime().toCardString().equals("-")
-    				&& !i.getEndTime().toCardString().equals("-")
-    				&& !toAdd.getStartTime().getAsCalendar().after(i.getEndTime().getAsCalendar())
-    				&& (toAdd.getEndTime().time.getTime().compareTo(i.getEndTime().getAsCalendar().getTime())==0)
-    				&& (toAdd.getStartTime().time.getTime().compareTo(i.getStartTime().getAsCalendar().getTime())==0)
-    				&& !i.getStartTime().getAsCalendar().after(toAdd.getEndTime().getAsCalendar())) {
-    			return true;
-    		}
-    	}
-    	return false;
-    }
-    
-    /**
-     * Adds a person to the list.
-     *
-     * @throws TaskNotFoundException if no such person could be found in the list.
-     */
-    public boolean setComplete(ReadOnlyTask toComplete) throws TaskNotFoundException {
-        assert toComplete != null;
-        for (Task i: internalList){
-        	if(i.getUniqueID()==toComplete.getUniqueID()){
-        		i.markAsComplete();
-        		i.setRecurringTime();
-        		return true;
-        	}
-        }
-        throw new TaskNotFoundException();
-    }
-    
-    public boolean setIncomplete(ReadOnlyTask task) throws TaskNotFoundException {
-        assert task != null;
-        for (Task i: internalList){
-            if(i.getUniqueID()==task.getUniqueID()){
-                i.markAsIncomplete();
-                RecurringUtil.updateRecurringDate(i.getStartTime().time, i.getRecurringFrequency(), -1);
-                RecurringUtil.updateRecurringDate(i.getEndTime().time, i.getRecurringFrequency(), -1);
-                return true;
-            }
-        }
-        throw new TaskNotFoundException();
-    }
+	/**
+	 * Adds a person to the list.
+	 *
+	 * @throws DuplicateTaskException if the person to add is a duplicate of an existing person in the list.
+	 */
+	public void add(Task toAdd) throws DuplicateTaskException {
+		assert toAdd != null;
+		if (contains(toAdd)) {
+			throw new DuplicateTaskException();
+		}
+		internalList.add(toAdd);
+	}
 
-    /**
-     * Removes the equivalent person from the list.
-     *
-     * @throws TaskNotFoundException if no such person could be found in the list.
-     */
-    public boolean remove(ReadOnlyTask toRemove) throws TaskNotFoundException {
-        assert toRemove != null;
-        for (Task i: internalList){
-        	if(i.getUniqueID()==toRemove.getUniqueID()){
-        		internalList.remove(i);
-        		return true;
-        	}
-        }
-        throw new TaskNotFoundException();
-    }
+	public boolean isOverlapping(Task toAdd) {
+		assert toAdd != null;
+		if(!toAdd.isEvent()){
+			return false;
+		}
+		for (Task i: internalList) {
+			if(!i.equals(toAdd)){
+				if(i.isEvent()){
+					boolean cond1 = toAdd.getStartTime().compareTo(i.getEndTime()) < 0;
+					boolean cond2 = toAdd.getEndTime().compareTo(i.getStartTime()) > 0;
+					if(cond1 && cond2){
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
 
-    public ObservableList<Task> getInternalList() {
-        return internalList;
-    }
+	/**
+	 * Adds a person to the list.
+	 *
+	 * @throws TaskNotFoundException if no such person could be found in the list.
+	 */
+	public boolean setComplete(ReadOnlyTask toComplete) throws TaskNotFoundException {
+		assert toComplete != null;
+		for (Task i: internalList){
+			if(i.getUniqueID()==toComplete.getUniqueID()){
+				i.markAsComplete();
+				i.setRecurringTime();
+				return true;
+			}
+		}
+		throw new TaskNotFoundException();
+	}
 
-    @Override
-    public Iterator<Task> iterator() {
-        return internalList.iterator();
-    }
-    
-    public boolean isEmpty(){
-    	return internalList.isEmpty();
-    }
+	public boolean setIncomplete(ReadOnlyTask task) throws TaskNotFoundException {
+		assert task != null;
+		for (Task i: internalList){
+			if(i.getUniqueID()==task.getUniqueID()){
+				i.markAsIncomplete();
+				RecurringUtil.updateRecurringDate(i.getStartTime().time, i.getRecurringFrequency(), -1);
+				RecurringUtil.updateRecurringDate(i.getEndTime().time, i.getRecurringFrequency(), -1);
+				return true;
+			}
+		}
+		throw new TaskNotFoundException();
+	}
 
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof UniqueTaskList // instanceof handles nulls
-                && this.internalList.equals(
-                ((UniqueTaskList) other).internalList));
-    }
+	/**
+	 * Removes the equivalent person from the list.
+	 *
+	 * @throws TaskNotFoundException if no such person could be found in the list.
+	 */
+	public boolean remove(ReadOnlyTask toRemove) throws TaskNotFoundException {
+		assert toRemove != null;
+		for (Task i: internalList){
+			if(i.getUniqueID()==toRemove.getUniqueID()){
+				internalList.remove(i);
+				return true;
+			}
+		}
+		throw new TaskNotFoundException();
+	}
 
-    @Override
-    public int hashCode() {
-        return internalList.hashCode();
-    }
+	public ObservableList<Task> getInternalList() {
+		return internalList;
+	}
+
+	@Override
+	public Iterator<Task> iterator() {
+		return internalList.iterator();
+	}
+
+	public boolean isEmpty(){
+		return internalList.isEmpty();
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		return other == this // short circuit if same object
+				|| (other instanceof UniqueTaskList // instanceof handles nulls
+						&& this.internalList.equals(
+								((UniqueTaskList) other).internalList));
+	}
+
+	@Override
+	public int hashCode() {
+		return internalList.hashCode();
+	}
 
 }
