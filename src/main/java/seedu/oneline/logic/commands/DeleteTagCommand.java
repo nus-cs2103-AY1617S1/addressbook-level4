b27acd87044444d56a83c1b458b73685b281df36
unusep <unusep@gmail.com>
2016-11-02 02:02:02 +0800

    Implement execute delete tag

--- src/main/java/seedu/oneline/logic/commands/DeleteTagCommand.java
+++ src/main/java/seedu/oneline/logic/commands/DeleteTagCommand.java
@@ -1,10 +1,14 @@
+//@@author A0138848M
 package seedu.oneline.logic.commands;
 
+import java.util.List;
+
 import javafx.collections.transformation.FilteredList;
-import seedu.oneline.commons.core.UnmodifiableObservableList;
 import seedu.oneline.commons.exceptions.IllegalValueException;
 import seedu.oneline.model.tag.Tag;
 import seedu.oneline.model.task.ReadOnlyTask;
+import seedu.oneline.model.task.Task;
+import seedu.oneline.model.task.UniqueTaskList;
 
 public class DeleteTagCommand extends DeleteCommand {
 
@@ -23,14 +27,34 @@ public class DeleteTagCommand extends DeleteCommand {
         }
     }
     
+    /**
+     * Pre-condition: model.replaceTask removes the tag all together
+     * as long as all tasks tagged with the tag have their tags removed.
+     */
     @Override
     public CommandResult execute() {
         FilteredList<Tag> tagList = model.getTagList().filtered(tag -> tag.getTagName().equals(tagName));
         if (tagList.isEmpty()){
             return new CommandResult(String.format(Tag.MESSAGE_INVALID_TAG, tagName));
         } else {
-            // TODO
-            return new CommandResult(MESSAGE_DELETE_CAT_SUCCESS + " : " + tagName);
+            // remove tags from tasks that are currently tagged 
+            // model.replaceTask handles removing the tag
+            List<ReadOnlyTask> taskWithTag = 
+                    model.getFilteredTaskList().filtered(task -> task.getTag().getTagName().equals(tagName));
+            Tag defaultTag = Tag.getDefault();
+            while (!taskWithTag.isEmpty()){
+                ReadOnlyTask oldTask = taskWithTag.get(0);
+                Task newTask = oldTask.updateTag(defaultTag);
+                try {
+                    model.replaceTask(oldTask, newTask);
+                } catch (UniqueTaskList.TaskNotFoundException e) {
+                    assert false : "The target task cannot be missing";
+                } catch (UniqueTaskList.DuplicateTaskException e) {
+                    assert false : "The update task should not already exist";
+                }
+            }
+            
+            return new CommandResult(String.format(MESSAGE_DELETE_CAT_SUCCESS, tagName));
         }
     }
     
