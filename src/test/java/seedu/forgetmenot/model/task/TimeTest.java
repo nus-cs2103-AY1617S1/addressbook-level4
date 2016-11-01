dc993934fd2de25fac1682a8b0a7897633c4dc9e
nahata-shailesh <nahata.shailesh@gmail.com>
2016-11-02 02:19:13 +0800

    Add more tests to TimeTest.java

--- src/test/java/seedu/forgetmenot/model/task/TimeTest.java
+++ src/test/java/seedu/forgetmenot/model/task/TimeTest.java
@@ -7,9 +7,9 @@ import org.junit.Test;
 
 import seedu.forgetmenot.commons.exceptions.IllegalValueException;
 
-//@@A0139671X
 public class TimeTest {
     
+	//@@author A0139671X
     @Test
     public void isValidDate_trueIfDateFormatIsCorrect() {
 
@@ -40,6 +40,7 @@ public class TimeTest {
             assertFalse(Time.isValidDate(invalidDates.get(i)));
     }
     
+    //@@author A0147619W
 	@Test
 	public void parseTime_invalidTimes_throwsError() throws IllegalValueException {
 		
@@ -48,6 +49,35 @@ public class TimeTest {
 		assertInvalidTime("invalid");
 		assertInvalidTime("abc5pm");
 		assertInvalidTime("!@#");
+	}
+	
+	@Test
+	public void checkOrderOfDates_validStartAndEndTimes_trueIfStartTimeIsBeforeEndTime() throws IllegalValueException {
+		
+		assertTrue(Time.checkOrderOfDates("today", "tmr"));
+		assertTrue(Time.checkOrderOfDates("today", "day after tmr"));
+		assertTrue(Time.checkOrderOfDates("tmr", "5 days later"));
+		assertTrue(Time.checkOrderOfDates("7pm", "8pm"));
+		assertTrue(Time.checkOrderOfDates("2 days after the next tuesday", "1 week after the next tuesday"));
+		assertTrue(Time.checkOrderOfDates("28/2/17", "1/3/17"));
+		assertTrue(Time.checkOrderOfDates("christmas", "new year"));
+		assertTrue(Time.checkOrderOfDates("today 11:59pm", "1 week later"));
+		assertTrue(Time.checkOrderOfDates("today", "")); // If the end time is missing, it is still considered valid
+		assertTrue(Time.checkOrderOfDates("", "tmr")); // If the start time is missing, it is still considered valid
+		assertTrue(Time.checkOrderOfDates("", "")); // If both start time and end time is missing, it is still considered valid
+		
+	}
+	
+	@Test
+	public void checkOrderOfDates_invalidStartAndEndTimes_falseIfStartTimeIsAfterEndTime() throws IllegalValueException {
+		
+		assertFalse(Time.checkOrderOfDates("tmr", "today"));
+		assertFalse(Time.checkOrderOfDates("10 mins after today", "today"));
+		assertFalse(Time.checkOrderOfDates("4/4/20", "3 days later"));
+		assertFalse(Time.checkOrderOfDates("2 days after next mon", "next tue"));
+		assertFalse(Time.checkOrderOfDates("next month", "next week"));
+		assertFalse(Time.checkOrderOfDates("christmas", "christmas eve"));
+		
 	}
 
 	private void assertInvalidTime(String time) {
