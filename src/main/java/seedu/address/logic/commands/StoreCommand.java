8a6438e64c4e82949808d7d15034bd6bb7361b34
linhan <yonglin12@hotmail.com>
2016-11-01 20:47:18 +0800

    Improve functionality of store command and refactor

--- src/main/java/seedu/address/logic/commands/StoreCommand.java
+++ src/main/java/seedu/address/logic/commands/StoreCommand.java
@@ -1,12 +1,17 @@
 
 package seedu.address.logic.commands;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Optional;
 
 import seedu.address.commons.util.ConfigUtil;
+import seedu.address.model.ReadOnlyTaskManager;
 import seedu.address.commons.core.Config;
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.storage.ChangeStorageFilePathEvent;
+import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.storage.XmlTaskManagerStorage;
 
 //@@author A0139498J
@@ -20,7 +25,7 @@ public class StoreCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Changes the storage location. "
             + "Parameters: store [FILEPATH]\n" 
             + "Example: " + COMMAND_WORD 
-            + "data/dear_jim_backup";      
+            + "data/dearjim_backup";      
     public static final String TOOL_TIP = "store [FILEPATH]";
     public static final String MESSAGE_SUCCESS = "Storage location changed to %1$s.";
     public static final String MESSAGE_FAILURE = "Unable to write to the storage location specified. "
@@ -36,18 +41,61 @@ public class StoreCommand extends Command {
     public CommandResult execute() {        
         assert model != null;
         storageFilePath = storageFilePath + XML_FILE_EXTENSION;
-        Config config = new Config();
-        config.setTaskManagerFilePath(storageFilePath);
         XmlTaskManagerStorage xmlTaskManagerStorage = new XmlTaskManagerStorage(storageFilePath);
-        
+        File storageFile = new File(storageFilePath);
+
         try {
-            xmlTaskManagerStorage.saveTaskManager(model.getTaskManager(), storageFilePath);
-            ConfigUtil.saveConfig(config,  Config.DEFAULT_CONFIG_FILE);
-            EventsCenter.getInstance().post(new ChangeStorageFilePathEvent(storageFilePath));
+            if (storageFile.exists()) {
+                importDataFromExistingStorageFile(xmlTaskManagerStorage);
+            } else {
+                createNewFileWithExistingData(xmlTaskManagerStorage);
+            }
+            updateStorageFilePathOfConfigFile();
             return new CommandResult(String.format(MESSAGE_SUCCESS, storageFilePath));
         } catch (IOException ioe) {
             return new CommandResult(MESSAGE_FAILURE);
+        } catch (DataConversionException dce) {
+            return new CommandResult(MESSAGE_FAILURE);
         }
+    }
+
+    /**
+     * Updates the storage file path of config file with the new storage file path
+     * provided by user as part of the store command
+     * 
+     * @throws IOException if there was an error writing to the config file
+     */
+    private void updateStorageFilePathOfConfigFile() throws IOException {
+        Config config = new Config();
+        config.setTaskManagerFilePath(storageFilePath);
+        ConfigUtil.saveConfig(config, Config.DEFAULT_CONFIG_FILE);
+    }
+
+    /**
+     * Creates a new file, and copies all existing data in task manager over into it
+     * 
+     * @throws IOException if there was an error writing to the new file
+     */
+    private void createNewFileWithExistingData(XmlTaskManagerStorage xmlTaskManagerStorage) throws IOException {
+        xmlTaskManagerStorage.saveTaskManager(model.getTaskManager(), storageFilePath);
+        EventsCenter.getInstance().post(new ChangeStorageFilePathEvent(storageFilePath));
+    }
+
+    /**
+     * Imports data from the existing storage file
+     * Updates the model component of task manager with the imported data
+     * 
+     * @throws DataConversionException if target file is not in the correct format
+     * @throws FileNotFoundException if target file does not exist
+     */
+    private void importDataFromExistingStorageFile(XmlTaskManagerStorage xmlTaskManagerStorage)
+            throws DataConversionException, FileNotFoundException {
+        Optional <ReadOnlyTaskManager> readTaskManager = xmlTaskManagerStorage.readTaskManager(storageFilePath);
+        assert readTaskManager.isPresent();
+        ReadOnlyTaskManager readOnlyTaskManagerWithNewData = readTaskManager.get();
+        EventsCenter.getInstance().post(new ChangeStorageFilePathEvent(storageFilePath));
+        model.resetData(readOnlyTaskManagerWithNewData);
+        model.resetDoneData(readOnlyTaskManagerWithNewData);
     }
 
 }
