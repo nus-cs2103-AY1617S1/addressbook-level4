9c2b1cdb0e91fdd7d4a92e25af0ed81299f578a7
unusep <unusep@gmail.com>
2016-11-02 01:01:29 +0800

    Implement createfromargs

--- src/main/java/seedu/oneline/logic/commands/DeleteTagCommand.java
+++ src/main/java/seedu/oneline/logic/commands/DeleteTagCommand.java
@@ -1,27 +1,35 @@
 package seedu.oneline.logic.commands;
 
-import seedu.oneline.commons.core.Messages;
 import seedu.oneline.commons.core.UnmodifiableObservableList;
-import seedu.oneline.commons.exceptions.IllegalCmdArgsException;
 import seedu.oneline.commons.exceptions.IllegalValueException;
-import seedu.oneline.logic.parser.Parser;
+import seedu.oneline.model.tag.Tag;
 import seedu.oneline.model.task.ReadOnlyTask;
-import seedu.oneline.model.task.UniqueTaskList.TaskNotFoundException;
 
 public class DeleteTagCommand extends DeleteCommand {
 
-    public DeleteTagCommand(int targetIndex) {
-        super(targetIndex);
+    private final String tagName;
+    
+    public DeleteTagCommand(String tagName) {
+        this.tagName = tagName;
     }
     
     public static DeleteTagCommand createFromArgs(String args) throws IllegalValueException {
-
-        return null; //stub
+        String tagName = args.substring(1);
+        if (!Tag.isValidTagName(tagName)) {
+            throw new IllegalValueException(Tag.MESSAGE_TAG_CONSTRAINTS + " : " + args);
+        } else {
+            return new DeleteTagCommand(tagName);
+        }
     }
     
     @Override
     public CommandResult execute() {
         return null; //stub
+    }
+    
+    @Override
+    public boolean canUndo() {
+        return true;
     }
     
 }
