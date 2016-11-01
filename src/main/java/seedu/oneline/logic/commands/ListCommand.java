a7171854165c60c8a1538cdff1c3dc390cfd5999
jasminish <jasmineang@hotmail.com>
2016-11-02 01:23:20 +0800

    Initial implementation of help

--- src/main/java/seedu/oneline/logic/commands/ListCommand.java
+++ src/main/java/seedu/oneline/logic/commands/ListCommand.java
@@ -15,6 +15,11 @@ import seedu.oneline.logic.parser.Parser;
 public class ListCommand extends Command {
     
     public static final String COMMAND_WORD = "list";
+    
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows a list of undone tasks accordingly \n"
+            + "Parameters: [<today> / <week> / <float> / <done> / #<category>] \n"
+            + "Example: " + COMMAND_WORD
+            + "today";
 
     public static final String MESSAGE_SUCCESS = "Listed all tasks";
 
