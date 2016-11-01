a85787a768781c32a0351740cc4acf08077912c4
chuayupeng <chua.yupeng@dhs.sg>
2016-11-01 21:04:10 +0800

    Changed Logo

--- src/main/java/seedu/todo/ui/MainWindow.java
+++ src/main/java/seedu/todo/ui/MainWindow.java
@@ -21,7 +21,7 @@ import seedu.todo.model.task.ReadOnlyTask;
  */
 public class MainWindow extends UiPart {
 
-    private static final String ICON = "/images/address_book_32.png";
+    private static final String ICON = "/images/dodobirdlogo.png";
     private static final String FXML = "MainWindow.fxml";
     public static final int MIN_HEIGHT = 600;
     public static final int MIN_WIDTH = 450;
