1bf3f8d7e3695fa1f864972eb25ba44a559e1ee2
rainwindy <zhuangyufeng@gmail.com>
2016-11-02 22:36:11 +0800

    Update MESSAGE_USAGE for Add Command

--- src/main/java/seedu/dailyplanner/logic/commands/AddCommand.java
+++ src/main/java/seedu/dailyplanner/logic/commands/AddCommand.java
@@ -19,7 +19,7 @@ public class AddCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a task to the daily planner. "
             + "Parameters: NAME d/DATE s/STARTTIME e/ENDTIME [c/CATEGORY]...\n"
             + "Example: " + COMMAND_WORD
-            + " CS2103 Assignment d/11/11/2016 st/10pm et/11pm c/urgent c/important";
+            + " CS2103 Assignment d/11/11/2016 s/10pm e/11pm c/urgent c/important";
 
     public static final String MESSAGE_SUCCESS = "New task added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This task already exists in the daily planner";
