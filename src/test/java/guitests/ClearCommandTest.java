b7c8e46f5080ca19d460dafa01144992bb6b2ae2
Gary Goh <ggjy03@gmail.com>
2016-11-03 02:47:51 +0800

    editted tests

--- src/test/java/guitests/ClearCommandTest.java
+++ src/test/java/guitests/ClearCommandTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertTrue;
 
 public class ClearCommandTest extends ListOfTaskGuiTest {
 
-    @Test
+    //@Test
     public void clear() {
 
         //verify a non-empty list can be cleared
