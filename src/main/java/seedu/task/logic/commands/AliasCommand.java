892e9026825a52b8fd1b4f037b2d6de8eaa5ecb6
Advay Pal <advaypal@hotmail.com>
2016-11-02 19:43:42 +0800

    Check if valid alias

--- src/main/java/seedu/task/logic/commands/AliasCommand.java
+++ src/main/java/seedu/task/logic/commands/AliasCommand.java
@@ -1,6 +1,9 @@
 //@@author A0144939R
 package seedu.task.logic.commands;
 
+import seedu.task.commons.logic.CommandKeys;
+import seedu.task.commons.logic.CommandKeys.Commands;
+
 /**
  * Alias command
  */
@@ -16,31 +19,45 @@ public class AliasCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Command successfully aliased";
     public static final String MESSAGE_FAILURE = "Error, cannot alias command";
     
-    private String command;
+    private String commandString;
     private String alias;
     
     
     public AliasCommand(String command, String alias) {
-        this.command = command.trim();
+        this.commandString = command.trim();
         this.alias = alias.trim();
     }
     
 
     @Override
     public CommandResult execute() {
-      if(isValidAliasCommandPair(alias, command)) {
+      if(isValidAliasCommandPair(alias, commandString)) {
+          Commands command = CommandKeys.commandKeyMap.get(commandString);
           model.setMapping(command, alias);
           return new CommandResult(true, MESSAGE_SUCCESS);
       } else {
           return new CommandResult(false, MESSAGE_FAILURE);
       }
     }
-    
+    /**
+     * Checks if a given alias can be mapped to given command
+     * Note: one command can have multiple aliases
+     * @param alias Alias specified by user
+     * @param command Command the alias aliases to
+     * @return
+     */
     public boolean isValidAliasCommandPair(String alias, String command) {
         //check that alias is not null and that alias is not a command
         //checks that command is valid command
-        return true;
-    }
+        if(alias != null && command != null) {
+            if(CommandKeys.commandKeyMap.containsKey(commandString) && CommandKeys.commandKeyMap.get(commandString) == null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+   }
 
 }
 
