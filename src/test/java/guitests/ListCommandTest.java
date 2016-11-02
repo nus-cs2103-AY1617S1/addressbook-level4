6b3e369a6c89c5b787850c1e681ee4405736d2fc
Aloisius Stephen <aloisius.s@u.nus.edu>
2016-11-02 18:59:24 +0800

    Add ListCommandTest

--- src/test/java/guitests/ListCommandTest.java
+++ src/test/java/guitests/ListCommandTest.java
@@ -1,5 +1,19 @@
 package guitests;
 
-public class ListCommandTest {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
+import seedu.address.testutil.TestActivity;
+
+public class ListCommandTest extends AddressBookGuiTest  {
+
+    @Test
+    public void list_fromEmptyPanels() {
+        TestActivity[] currentList = td.getTypicalPersons();
+        commandBox.runCommand("find nothing");
+        commandBox.runCommand("list");
+        assertTrue(activityListPanel.isListMatching(currentList));  
+    }
+    
 }
