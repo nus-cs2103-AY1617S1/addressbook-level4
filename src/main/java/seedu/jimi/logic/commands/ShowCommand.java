b09dfb9ce2dd87043c93b87c1ea7c023c3ac28e7
clarencechee <clarencechee94@gmail.com>
2016-11-03 01:25:57 +0800

    Show: refactor + refine

--- src/main/java/seedu/jimi/logic/commands/ShowCommand.java
+++ src/main/java/seedu/jimi/logic/commands/ShowCommand.java
@@ -20,7 +20,7 @@ public class ShowCommand extends Command {
 
     public static final String COMMAND_WORD = "show";
     
-    public static final List<String> VALID_KEYWORDS = Arrays.asList(
+    private static final List<String> VALID_KEYWORDS = Arrays.asList(
             "overdue",
             "floating", 
             "incomplete", 
@@ -69,65 +69,17 @@ public class ShowCommand extends Command {
     public CommandResult execute() {
         ((ModelManager) model).showTaskPanelSection(userSelection);
         
-        ListId sectionToShow = null;
-        
-        if(userSelection.equals("all")) {
+        if (userSelection.equals("all")) {
             model.updateAllFilteredListsToNormalListing();
             return new CommandResult(MESSAGE_SUCCESS);
         }
         
-        switch (userSelection) {
-        case "overdue" :
-            sectionToShow = ListId.OVERDUE;
-            break;
-        case "floating" :
-            sectionToShow = ListId.FLOATING_TASKS;
-            break;
-        case "incomplete" :
-            sectionToShow = ListId.INCOMPLETE;
-            break;
-        case "completed" :
-            sectionToShow = ListId.COMPLETED;
-            break;
-        case "today" :
-            sectionToShow = ListId.DAY_AHEAD_0;
-            break;
-        case "tomorrow" :
-            sectionToShow = ListId.DAY_AHEAD_1;
-            break;
-        case "monday" :
-        case "tuesday" :
-        case "wednesday" :
-        case "thursday" :
-        case "friday" :
-        case "saturday" :
-        case "sunday" :
-            sectionToShow = findSectionToShow(userSelection);
-            break;
-        default :
-            break;
-        }
+        ListId sectionToShow = determineSectionToShow(userSelection);
         
         model.updateFilteredAgendaTaskList(sectionToShow);
         model.updateFilteredAgendaEventList(sectionToShow);
         
         return new CommandResult(MESSAGE_SUCCESS);
-    }
-
-    private ListId findSectionToShow(String userSelection) {
-        ListId sectionToShow = null;
-        
-        LocalDateTime dateTime = new DateTime().getLocalDateTime();
-        
-        for (int i = 0; i < 7; i++) {
-            String dayOfWeek = dateTime.getDayOfWeek().plus(i).getDisplayName(TextStyle.FULL, Locale.ENGLISH);
-            if (dayOfWeek.toLowerCase().equals(userSelection)) {
-                sectionToShow = ListId.values()[i];
-                break;
-            }
-        }
-        
-        return sectionToShow;
     }
     
     // @@author A0140133B
@@ -151,6 +103,12 @@ public class ShowCommand extends Command {
         return COMMAND_WORD;
     }
     
+    /*
+     * =========================================================
+     *                      Helper Methods
+     * =========================================================
+     */
+    
     /** Checks if {@code test} is a valid keyword as shown in {@code VALID_KEYWORDS}. */
     private boolean isValidSectionToShow(String test) {
         return ShowCommand.VALID_KEYWORDS.stream()
@@ -159,4 +117,40 @@ public class ShowCommand extends Command {
                 .isPresent();
     }
     // @@author
+    
+    private ListId determineSectionToShow(String sectionToShow) {
+        switch (userSelection) {
+        case "overdue" :
+            return ListId.OVERDUE;
+        case "floating" :
+            return ListId.FLOATING_TASKS;
+        case "incomplete" :
+            return ListId.INCOMPLETE;
+        case "completed" :
+            return ListId.COMPLETED;
+        case "today" :
+            return ListId.DAY_AHEAD_0;
+        case "tomorrow" :
+            return ListId.DAY_AHEAD_1;
+        default :
+            return determineDayOfWeekToShow(userSelection);
+        }
+    }
+
+    private ListId determineDayOfWeekToShow(String userSelection) {
+        ListId sectionToShow = null;
+        
+        LocalDateTime dateTime = new DateTime().getLocalDateTime();
+        
+        for (int i = 0; i < 7; i++) {
+            String dayOfWeek = dateTime.getDayOfWeek().plus(i).getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+            if (dayOfWeek.toLowerCase().equals(userSelection)) {
+                sectionToShow = ListId.values()[i];
+                break;
+            }
+        }
+        
+        return sectionToShow;
+    }
+    
 }
