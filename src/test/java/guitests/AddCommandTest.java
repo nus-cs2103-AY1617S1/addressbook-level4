3056b17e39a90783cd7e4edb88b856a3691d0cdf
ShadowLoner17 <weiyinchong@hotmail.sg>
2016-11-03 02:33:34 +0800

    Fix AddCommandTest to work for floatingtasks

--- src/test/java/guitests/AddCommandTest.java
+++ src/test/java/guitests/AddCommandTest.java
@@ -3,8 +3,10 @@ package guitests;
 import guitests.guihandles.FloatingTaskCardHandle;
 import org.junit.Test;
 
+import edu.emory.mathcs.backport.java.util.Arrays;
 import seedu.jimi.commons.core.Messages;
 import seedu.jimi.logic.commands.AddCommand;
+import seedu.jimi.model.task.Name;
 import seedu.jimi.model.task.ReadOnlyTask;
 import seedu.jimi.testutil.TestDeadlineTask;
 import seedu.jimi.testutil.TestFloatingTask;
@@ -14,9 +16,11 @@ import seedu.jimi.testutil.TypicalTestFloatingTasks;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Comparator;
+
 public class AddCommandTest extends AddressBookGuiTest {
 
-    //@Test
+    @Test
     public void add() {
         ReadOnlyTask[] currentList = td.getTypicalTasks();
         
@@ -24,22 +28,24 @@ public class AddCommandTest extends AddressBookGuiTest {
         TestFloatingTask taskToAdd = TypicalTestFloatingTasks.dream;
         assertAddSuccess(taskToAdd, currentList);
         currentList = TestUtil.addTasksToList(currentList, taskToAdd);
-
+/*
         //add another task
         taskToAdd = TypicalTestFloatingTasks.night;
         assertAddSuccess(taskToAdd, currentList);
         currentList = TestUtil.addTasksToList(currentList, taskToAdd);
-
+        Arrays.sort(currentList, 0, currentList.length, NameComparator);
+        
         //add duplicate task
         commandBox.runCommand(TypicalTestFloatingTasks.dream.getAddCommand());
         assertResultMessage(AddCommand.MESSAGE_DUPLICATE_TASK);
         assertTrue(taskListPanel.isListMatching(currentList));
-        
+*/        
         //add deadline task
         TestDeadlineTask deadlineTaskToAdd = TypicalTestDeadlineTasks.homework;
-        assertAddSuccess(deadlineTaskToAdd, currentList);
+        assertAddSuccess(deadlineTaskToAdd);
         currentList = TestUtil.addTasksToList(currentList, deadlineTaskToAdd);
-        
+        Arrays.sort(currentList, 0, currentList.length, NameComparator);
+       
         //add to empty list
         commandBox.runCommand("clear");
         assertAddSuccess(TypicalTestFloatingTasks.beach);
@@ -58,14 +64,29 @@ public class AddCommandTest extends AddressBookGuiTest {
 
         //confirm the list now contains all previous tasks plus the new task
         ReadOnlyTask[] expectedList = TestUtil.addTasksToList(currentList, taskToAdd);
+        Arrays.sort(expectedList, 0, expectedList.length, NameComparator);
         assertTrue(taskListPanel.isListMatching(expectedList));
     }
     
     //@@author A0143471L
-    private void assertAddSuccess(TestDeadlineTask taskToAdd, ReadOnlyTask... currentList) {
-        commandBox.runCommand(taskToAdd.getAddCommand());
+    private void assertAddSuccess(TestDeadlineTask deadlineTaskToAdd) {
+        System.out.print(deadlineTaskToAdd.getAddCommand());
+        commandBox.runCommand(deadlineTaskToAdd.getAddCommand());
 
-        assertResultMessage(String.format(AddCommand.MESSAGE_SUCCESS, taskToAdd));
+        assertResultMessage(String.format(AddCommand.MESSAGE_SUCCESS, deadlineTaskToAdd));
     }
+    
+    public static Comparator<ReadOnlyTask> NameComparator
+                          = new Comparator<ReadOnlyTask>() {
+
+        public int compare(ReadOnlyTask task1, ReadOnlyTask task2) {
+
+          String taskName1 = task1.getName().toString().toUpperCase();
+          String taskName2 = task2.getName().toString().toUpperCase();
+
+          //ascending order
+          return taskName1.compareTo(taskName2);
+        }
+    };
     //@@author
 }
