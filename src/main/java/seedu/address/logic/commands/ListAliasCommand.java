37f3a8db9f8664b2d51704d37a03e49f8e660270
Joycelyn Ng <Joycelyn Ng>
2016-11-03 02:15:30 +0800

    Functionality for ListAliasCommand implemented

--- src/main/java/seedu/address/logic/commands/ListAliasCommand.java
+++ src/main/java/seedu/address/logic/commands/ListAliasCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.ui.DisplayAliasListEvent;
-import seedu.address.model.TaskManager;
 
 /**
  * Lists all aliases in the alias manager to the user.
