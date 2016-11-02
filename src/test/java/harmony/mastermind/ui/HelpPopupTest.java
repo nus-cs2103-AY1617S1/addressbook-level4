b736b3f460207e29de66b3511af35336c686afc0
Albert Yeoh <bertthings@gmail.com>
2016-11-02 18:15:16 +0800

    fixed potential bug and made a ui test folder

---
+++ src/test/java/harmony/mastermind/ui/HelpPopupTest.java
@@ -0,0 +1,14 @@
+package harmony.mastermind.ui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelpPopupTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
