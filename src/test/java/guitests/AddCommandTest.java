4bcb31e9dc5e77514fa65799b4585fec31ed8540
ShadowLoner17 <weiyinchong@hotmail.sg>
2016-11-02 23:52:44 +0800

    Implement deadlinetask in AddCommandTest

--- src/test/java/guitests/AddCommandTest.java
+++ src/test/java/guitests/AddCommandTest.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 
 import seedu.jimi.commons.core.Messages;
 import seedu.jimi.logic.commands.AddCommand;
+import seedu.jimi.model.task.ReadOnlyTask;
+import seedu.jimi.testutil.TestDeadlineTask;
 import seedu.jimi.testutil.TestFloatingTask;
 import seedu.jimi.testutil.TestUtil;
+import seedu.jimi.testutil.TypicalTestDeadlineTasks;
 import seedu.jimi.testutil.TypicalTestFloatingTasks;
 
 import static org.junit.Assert.assertTrue;
@@ -15,8 +18,9 @@ public class AddCommandTest extends AddressBookGuiTest {
 
     @Test
     public void add() {
+        ReadOnlyTask[] currentList = td.getTypicalTasks();
+        
         //add one task
-        TestFloatingTask[] currentList = td.getTypicalTasks();
         TestFloatingTask taskToAdd = TypicalTestFloatingTasks.dream;
         assertAddSuccess(taskToAdd, currentList);
         currentList = TestUtil.addTasksToList(currentList, taskToAdd);
@@ -30,7 +34,12 @@ public class AddCommandTest extends AddressBookGuiTest {
         commandBox.runCommand(TypicalTestFloatingTasks.dream.getAddCommand());
         assertResultMessage(AddCommand.MESSAGE_DUPLICATE_TASK);
         assertTrue(taskListPanel.isListMatching(currentList));
-
+        
+        //add deadline task
+        TestDeadlineTask deadlineTaskToAdd = TypicalTestDeadlineTasks.homework;
+        assertAddSuccess(deadlineTaskToAdd, currentList);
+        currentList = TestUtil.addTasksToList(currentList, deadlineTaskToAdd);
+        
         //add to empty list
         commandBox.runCommand("clear");
         assertAddSuccess(TypicalTestFloatingTasks.beach);
@@ -40,7 +49,7 @@ public class AddCommandTest extends AddressBookGuiTest {
         assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
     }
 
-    private void assertAddSuccess(TestFloatingTask taskToAdd, TestFloatingTask... currentList) {
+    private void assertAddSuccess(TestFloatingTask taskToAdd, ReadOnlyTask... currentList) {
         commandBox.runCommand(taskToAdd.getAddCommand());
 
         //confirm the new card contains the right data
@@ -48,8 +57,15 @@ public class AddCommandTest extends AddressBookGuiTest {
         assertMatching(taskToAdd, addedCard);
 
         //confirm the list now contains all previous tasks plus the new task
-        TestFloatingTask[] expectedList = TestUtil.addTasksToList(currentList, taskToAdd);
+        ReadOnlyTask[] expectedList = TestUtil.addTasksToList(currentList, taskToAdd);
         assertTrue(taskListPanel.isListMatching(expectedList));
     }
+    
+    //@@author A0143471L
+    private void assertAddSuccess(TestDeadlineTask taskToAdd, ReadOnlyTask... currentList) {
+        commandBox.runCommand(taskToAdd.getAddCommand());
 
+        assertResultMessage(String.format(AddCommand.MESSAGE_SUCCESS, taskToAdd));
+    }
+    //@@author
 }
