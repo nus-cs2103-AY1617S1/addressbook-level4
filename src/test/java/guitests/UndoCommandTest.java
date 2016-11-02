4487ba028f70dbf89e803733aacef1c5905a1cd0
Aloisius Stephen <aloisius.s@u.nus.edu>
2016-11-02 18:19:43 +0800

    Add Edit test for UndoCommandTest

--- src/test/java/guitests/UndoCommandTest.java
+++ src/test/java/guitests/UndoCommandTest.java
@@ -2,8 +2,7 @@ package guitests;
 
 import org.junit.Test;
 import seedu.address.commons.core.Messages;
-import seedu.address.testutil.TestActivity;
-import seedu.address.testutil.TypicalTestActivities;
+import seedu.address.testutil.*;
 import static org.junit.Assert.assertTrue;
 
 //@@author A0125097A
@@ -31,9 +30,31 @@ public class UndoCommandTest extends AddressBookGuiTest {
         assertUndoDoneResult(index,currentList);
     }
 
+    @Test
+    public void undo_editCommand() {
+        TestActivity[] currentList = td.getTypicalPersons();
+        int index = 1;
+        String newName = "new name";
+        String newReminder = "29-12-2021 2200";
+        TestActivity activityBeforeEdit = new TestActivity(currentList[index-1]);
+        TestActivity activityAfterEdit = new TestActivity(currentList[index-1]);
+        activityAfterEdit.setName(newName);
+        activityAfterEdit.setReminder(newReminder);
+        String editCommand = "edit " + index + " n/" + newName + " r/" + newReminder;
+        assertUndoEditResult(editCommand,activityBeforeEdit,activityAfterEdit,currentList);
+    }
     
     
-    
+    private void assertUndoEditResult(String command, TestActivity activityAfterUndo, TestActivity activityBeforeUndo, TestActivity[] currentList) {
+        commandBox.runCommand(command);
+        commandBox.runCommand("undo"); 
+        assertResultMessage(String.format("Undo: Editting task from: %1$s\nto: %2$s",
+                activityAfterUndo.getAsText(),activityBeforeUndo.getAsText()));
+        assertTrue(activityListPanel.isListMatching(currentList));
+  
+        
+    }
+
     private void assertUndoAddResult(TestActivity activity,TestActivity... currentList){
         commandBox.runCommand(activity.getAddCommand());
         commandBox.runCommand("undo");
