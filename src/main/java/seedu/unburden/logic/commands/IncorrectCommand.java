ec095998dcaa2a151229885134101c21b6a4fc9b
Nat <nathanaelchan94@gmail.com>
2016-11-03 02:11:39 +0800

    Updated the incorrect command

--- src/main/java/seedu/unburden/logic/commands/IncorrectCommand.java
+++ src/main/java/seedu/unburden/logic/commands/IncorrectCommand.java
@@ -1,5 +1,11 @@
 package seedu.unburden.logic.commands;
 
+import java.util.List;
+
+import seedu.unburden.commons.exceptions.IllegalValueException;
+import seedu.unburden.model.tag.UniqueTagList.DuplicateTagException;
+import seedu.unburden.model.task.ReadOnlyTask;
+import seedu.unburden.model.task.Task;
 
 /**
  * Represents an incorrect command. Upon execution, produces some feedback to the user.
@@ -13,10 +19,22 @@ public class IncorrectCommand extends Command {
     }
 
     @Override
-    public CommandResult execute() {
+    public CommandResult execute() throws DuplicateTagException, IllegalValueException {
         indicateAttemptToExecuteIncorrectCommand();
+        overdueOrNot();
         return new CommandResult(feedbackToUser);
     }
-
+    //This method checks the entire list to check for overdue tasks
+   	private void overdueOrNot() throws IllegalValueException, DuplicateTagException {
+   		List<ReadOnlyTask> currentTaskList= model.getListOfTask().getTaskList();
+   		for(ReadOnlyTask task : currentTaskList){
+   			if(((Task) task).checkOverDue()){
+   				((Task) task).setOverdue();
+   			}
+   			else{
+   				((Task) task).setNotOverdue();
+   			}
+   		}
+   	}
 }
 
