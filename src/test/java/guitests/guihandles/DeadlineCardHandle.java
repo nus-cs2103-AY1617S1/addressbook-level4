16abee5451e85154d9e6789a127f8d530d223db4
Zavier\ZavierOng <zacohjayjay@gmail.com>
2016-11-03 01:23:53 +0800

    add deadlineCardHandle

---
+++ src/test/java/guitests/guihandles/DeadlineCardHandle.java
@@ -0,0 +1,61 @@
+package guitests.guihandles;
+
+import guitests.GuiRobot;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import seedu.address.model.task.ReadOnlyTask;
+
+/**
+ * @@author A0138993L
+ * Provides a handle to a deadline card in the deadline list panel.
+ */
+public class DeadlineCardHandle extends GuiHandle {
+    private static final String NAME_FIELD_ID = "#name";
+    private static final String END_FIELD_ID = "#end";
+    private static final String DATE_FIELD_ID = "#date";
+
+    private Node node;
+
+    public DeadlineCardHandle(GuiRobot guiRobot, Stage primaryStage, Node node){
+        super(guiRobot, primaryStage, null);
+        this.node = node;
+    }
+
+    protected String getTextFromLabel(String fieldId) {
+        return getTextFromLabel(fieldId, node);
+    }
+
+    public String getFullName() {
+        return getTextFromLabel(NAME_FIELD_ID);
+    }
+
+    public String getAddress() {
+        return getTextFromLabel(END_FIELD_ID);
+    }
+
+    public String getPhone() {
+        return getTextFromLabel(DATE_FIELD_ID);
+    }
+
+
+    public boolean isSamePerson(ReadOnlyTask person){
+        return getFullName().equals(person.getName()) && getPhone().equals(person.getDate())
+                && getAddress().equals(person.getEnd());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof DeadlineCardHandle) {
+            DeadlineCardHandle handle = (DeadlineCardHandle) obj;
+            return getFullName().equals(handle.getFullName())
+                    && getAddress().equals(handle.getAddress())
+                    && getPhone().equals(handle.getPhone()); 
+        }
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return getFullName() + " " + getAddress();
+    }
+}
