e661770693a811b9f18350ca642f68f36568b697
ShadowLoner17 <weiyinchong@hotmail.sg>
2016-11-02 23:40:35 +0800

    Add test cases for deadline tasks

---
+++ src/test/java/seedu/jimi/testutil/TypicalTestDeadlineTasks.java
@@ -0,0 +1,54 @@
+package seedu.jimi.testutil;
+
+import seedu.jimi.commons.exceptions.IllegalValueException;
+import seedu.jimi.model.TaskBook;
+import seedu.jimi.model.task.DeadlineTask;
+import seedu.jimi.model.task.FloatingTask;
+import seedu.jimi.model.task.ReadOnlyTask;
+import seedu.jimi.model.task.UniqueTaskList;
+
+/**
+ * @@author A0143471L
+ * 
+ * Example test cases for deadline tasks
+ *
+ */
+
+public class TypicalTestDeadlineTasks {
+    
+    public static TestDeadlineTask groceries, ideas, homework;
+    
+    public TypicalTestDeadlineTasks() {
+        try {
+            groceries =  new DeadlineTaskBuilder().withName("buy groceries").withTags("NTUC").withPriority("LOW").withDeadline("2016-10-31 19:00").build();
+            ideas = new DeadlineTaskBuilder().withName("brainstorm ideas").withTags("project").withPriority("LOW").withDeadline("2016-10-31 19:00").build();
+            
+            
+            //Manually added
+            homework = new DeadlineTaskBuilder().withName("finish homework").withPriority("HIGH").withDeadline("2016-10-31 19:00").build();
+        } catch (IllegalValueException e) {
+            e.printStackTrace();
+            assert false : "not possible";
+        }
+    }
+    
+    public static void loadTaskBookWithSampleData(TaskBook ab) {
+
+//        try {
+//            ab.addTask(new DeadlineTask(groceries));
+//            ab.addTask(new DeadlineTask(ideas));
+//        } catch (UniqueTaskList.DuplicateTaskException e) {
+//            assert false : "not possible";
+//        }
+    }
+    
+    public ReadOnlyTask[] getTypicalTasks() {
+        return new ReadOnlyTask[]{groceries};
+    }
+    
+    public TaskBook getTypicalTaskBook(){
+        TaskBook ab = new TaskBook();
+        loadTaskBookWithSampleData(ab);
+        return ab;
+    }
+}
