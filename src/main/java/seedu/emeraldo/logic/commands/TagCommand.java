6df6b46edd7694f7bdea207e298a788c8a8b6629
zhiminglim <i-jovial@hotmail.com>
2016-11-02 16:46:10 +0800

    Create new class for tag command

---
+++ src/main/java/seedu/emeraldo/logic/commands/TagCommand.java
@@ -0,0 +1,16 @@
+package seedu.emeraldo.logic.commands;
+
+public class TagCommand extends Command {
+
+    public static final String COMMAND_WORD = "tag";
+
+    
+    
+    
+    @Override
+    public CommandResult execute() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
