7ce149e490f114657127595c42602c81a303314c
ShadowLoner17 <weiyinchong@hotmail.sg>
2016-11-03 11:37:58 +0800

    Fix EditCommandTest to work

--- src/test/java/guitests/EditCommandTest.java
+++ src/test/java/guitests/EditCommandTest.java
@@ -3,20 +3,26 @@ package guitests;
 import static org.junit.Assert.*;
 import static seedu.jimi.logic.commands.EditCommand.MESSAGE_EDIT_SUCCESS;
 
+import java.util.Comparator;
+
 import org.junit.Test;
 
+import edu.emory.mathcs.backport.java.util.Arrays;
 import guitests.guihandles.FloatingTaskCardHandle;
+import seedu.jimi.commons.core.Messages;
 import seedu.jimi.commons.exceptions.IllegalValueException;
 import seedu.jimi.model.task.Name;
+import seedu.jimi.model.task.ReadOnlyTask;
 import seedu.jimi.testutil.TestFloatingTask;
 import seedu.jimi.testutil.TestUtil;
 //@@author A0138915X
 public class EditCommandTest extends AddressBookGuiTest{
 
-    //@Test
+    @Test
     public void edit() throws IllegalValueException {
         TestFloatingTask[] currentList = td.getTypicalTasks();
-        
+        Arrays.sort(currentList, 0, currentList.length, NameComparator);
+
         //edit the first task in list with all details
         int targetIndex = 1;
         
@@ -33,7 +39,7 @@ public class EditCommandTest extends AddressBookGuiTest{
 
         //invalid index
         commandBox.runCommand("edit t" + currentList.length + 1 + " \"Change invalid stuff\"");
-        assertResultMessage("The task index provided is invalid");
+        assertResultMessage(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
         
         //TODO: edit the first event in the list with all details
     }
@@ -67,10 +73,24 @@ public class EditCommandTest extends AddressBookGuiTest{
 
         //confirm the list now contains all previous tasks plus the edited task
         TestFloatingTask[] expectedList = TestUtil.replaceTaskFromList(currentList, expectedTask, targetIndex - 1);
+        Arrays.sort(expectedList, 0, expectedList.length, NameComparator);
         assertTrue(taskListPanel.isListMatching(expectedList));
         
         //confirm the result message is correct
         assertResultMessage(String.format(MESSAGE_EDIT_SUCCESS, expectedTask));
     }
+    
+    //@@author A0143471L
+    public static Comparator<ReadOnlyTask> NameComparator = new Comparator<ReadOnlyTask>() {
 
+        public int compare(ReadOnlyTask task1, ReadOnlyTask task2) {
+
+            String taskName1 = task1.getName().toString().toUpperCase();
+            String taskName2 = task2.getName().toString().toUpperCase();
+
+            // ascending order
+            return taskName1.compareTo(taskName2);
+        }
+    };
+    // @@author
 }
