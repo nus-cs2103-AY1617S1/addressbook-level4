906ac004f9a2cbf64ad3c65aacc87c078bc30cc9
clarencechee <clarencechee94@gmail.com>
2016-11-03 12:07:47 +0800

    Unused imports

--- src/main/java/seedu/jimi/commons/util/StringUtil.java
+++ src/main/java/seedu/jimi/commons/util/StringUtil.java
@@ -2,13 +2,11 @@ package seedu.jimi.commons.util;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 //@@author A0140133B
 /**
