889d3ef0e5c91daf69f86f1a540a4e06d684eeac
Zavier\ZavierOng <zacohjayjay@gmail.com>
2016-11-04 00:08:38 +0800

    create add deadline command test

---
+++ src/test/java/guitests/AddDeadlineCommandTest.java
@@ -0,0 +1,54 @@
+package guitests;
+
+import guitests.guihandles.DeadlineCardHandle;
+import org.junit.Test;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.commons.core.Messages;
+import seedu.address.testutil.TestDeadline;
+import seedu.address.testutil.TestUtil;
+import seedu.address.testutil.TypicalTestTasks;
+
+import static org.junit.Assert.assertTrue;
+
+public class AddDeadlineCommandTest extends AddressBookGuiTest {
+
+    @Test
+    public void add() {
+        //add one person
+        TestDeadline[] currentList = td.getTypicalDeadline();
+        TestDeadline deadlineToAdd = TypicalTestTasks.deadline8;
+        assertAddSuccess(deadlineToAdd, currentList);
+        currentList = TestUtil.addDeadlinesToList(currentList, deadlineToAdd);
+
+        //add another person
+        deadlineToAdd = TypicalTestTasks.deadline9;
+        assertAddSuccess(deadlineToAdd, currentList);
+        currentList = TestUtil.addDeadlinesToList(currentList, deadlineToAdd);
+
+        //add duplicate person
+        commandBox.runCommand(TypicalTestTasks.deadline8.getAddCommand());
+        assertResultMessage(AddCommand.MESSAGE_DUPLICATE_TASK);
+        assertTrue(deadlineListPanel.isListMatching(currentList));
+
+        //add to empty list
+        commandBox.runCommand("clear");
+        assertAddSuccess(TypicalTestTasks.deadline1);
+
+        //invalid command
+        commandBox.runCommand("adds dead");
+        assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
+    }
+
+    private void assertAddSuccess(TestDeadline personToAdd, TestDeadline... currentList) {
+        commandBox.runCommand(personToAdd.getAddCommand());
+
+        //confirm the new card contains the right data
+        DeadlineCardHandle addedCard = deadlineListPanel.navigateToPerson(personToAdd.getName().toString());
+        assertDeadlineMatching(personToAdd, addedCard);
+
+        //confirm the list now contains all previous persons plus the new person
+        TestDeadline[] expectedList = TestUtil.addDeadlinesToList(currentList, personToAdd);
+        assertTrue(deadlineListPanel.isListMatching(expectedList));
+    }
+
+}
