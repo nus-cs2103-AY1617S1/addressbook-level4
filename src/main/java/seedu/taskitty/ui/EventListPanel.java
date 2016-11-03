a64e51ee30e8e3c1ad4d4475dce404f91ce4da6e
Rachel Yang <frozensausage@gmail.com>
2016-11-03 20:24:52 +0800

    Remove date header in eventsListPanel

--- src/main/java/seedu/taskitty/ui/EventListPanel.java
+++ src/main/java/seedu/taskitty/ui/EventListPanel.java
@@ -12,16 +12,13 @@ import javafx.scene.control.ListView;
 import seedu.taskitty.commons.util.DateUtil;
 import seedu.taskitty.model.task.ReadOnlyTask;
 
-//@@author A0130853L
 
 /**
  * Panel containing the list of event tasks.
  */
 public class EventListPanel extends TaskListPanel {
     private static final String FXML = "EventListPanel.fxml";
-    
-    @FXML
-    private Label date;
+   
     
     @FXML
     private Label header;
@@ -44,39 +41,14 @@ public class EventListPanel extends TaskListPanel {
     public int getTaskCardID() {
         return EVENT_CARD_ID;
     }
-    
+    //@@author A0130853L
     @Override
     public void configure(ObservableList<ReadOnlyTask> eventList) {
     	header.setText("EVENTS [e]");
     	header.setStyle("-fx-text-fill: white");
-    	setDefaultDate();
         setConnections(eventListView, eventList);
         addToPlaceholder();
     }
+    //@@author
     
-    /**
-     * For when no view command is executed.
-     */
-    public void setEmptyDate() {
-    	date.setText("");
-    }
-    
-    /** 
-     * For when `view today` is executed, and today's date with the "(today)" label will be shown.
-     */
-    public void setDefaultDate() {
-    	
-    	date.setText(DateUtil.createDefaultDateString());
-    	date.setStyle("-fx-text-fill: black");
-    	date.setStyle("-fx-background-color: white");
-    }
-    
-    /**
-     * For when `view date` is executed, and that date will be shown.
-     * @param newDate
-     */
-    public void updateDate(LocalDate newDate) {
-    	DateFormat df = new SimpleDateFormat("dd MMM yyyy");
-    	date.setText(df.format(newDate));
-    }
 }
