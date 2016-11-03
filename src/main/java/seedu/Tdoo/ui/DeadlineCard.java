2fbb1d985829883449d14fce201455fbe02533fa
Junsheng <jasoncool93@hotmail.com>
2016-11-03 15:28:46 +0800

    Fix

--- src/main/java/seedu/Tdoo/ui/DeadlineCard.java
+++ src/main/java/seedu/Tdoo/ui/DeadlineCard.java
@@ -69,6 +69,22 @@ public class DeadlineCard extends UiPart {
 		}
 	}
 
+    //@@author A0132157M
+    public long dateToMilli(String s, String q) throws ParseException {
+        String string = s;
+        String[] parts = string.split(" ");
+        String part1 = parts[0]; 
+        String part2 = parts[1]; 
+        String part3 = parts[2];
+        String parta = part1.substring(0, 2);
+        String bstring = q;
+        String input = part3 + " " + part2 + " " + parta + " " + bstring;
+        Date date = new SimpleDateFormat("yyyy MMMM dd HH:mmaaa", Locale.ENGLISH).parse(input);
+        long milliseconds = date.getTime();
+        long ss = milliseconds - (new Date()).getTime();
+        return ss;
+    }
+    
 	public HBox getLayout() {
 		return cardPane;
 	}
