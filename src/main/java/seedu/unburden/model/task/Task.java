d75b81b6e6fdf1c4f8538458b4707d2a24579e9d
Gaurzzz <gauri.best@gmail.com>
2016-11-03 10:09:22 +0800

    Time error

--- src/main/java/seedu/unburden/model/task/Task.java
+++ src/main/java/seedu/unburden/model/task/Task.java
@@ -38,7 +38,7 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
     
     public Task(Name name, TaskDescription taskD, Date date, Time startTime, Time endTime, Boolean done,UniqueTagList tags) throws IllegalValueException {
         assert !CollectionUtil.isAnyNull(name, taskD, date, startTime, endTime, tags);
-        if(startTime.compareTo(endTime) < 0){
+        if(startTime.compareTo(endTime) > 0){
         	throw new IllegalValueException(Messages.MESSAGE_STARTTIME_AFTER_ENDTIME);
         }
         this.name = name;
@@ -57,7 +57,7 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
     // adds event
     public Task(Name name, TaskDescription taskD, Date date, Time startTime, Time endTime, UniqueTagList tags) throws IllegalValueException {
         assert !CollectionUtil.isAnyNull(name, taskD, date, startTime, endTime, tags);
-        if(startTime.compareTo(endTime) < 0){
+        if(startTime.compareTo(endTime) > 0){
         	throw new IllegalValueException(Messages.MESSAGE_STARTTIME_AFTER_ENDTIME);
         }
         this.name = name;
@@ -72,7 +72,7 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
     // adds event without description 
     public Task(Name name,Date date, Time startTime, Time endTime, UniqueTagList tags) throws IllegalValueException {
         assert !CollectionUtil.isAnyNull(name, date, startTime, endTime, tags);
-        if(startTime.compareTo(endTime) < 0){
+        if(startTime.compareTo(endTime) > 0){
         	throw new IllegalValueException(Messages.MESSAGE_STARTTIME_AFTER_ENDTIME);
         }
         this.name = name;
