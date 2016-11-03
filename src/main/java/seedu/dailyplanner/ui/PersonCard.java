c17baed3a24f1576051ba40c996cc6cb5e8246a9
myteo <mingyiteo@gmail.com>
2016-11-03 20:28:01 +0800

    Edit complete label

--- src/main/java/seedu/dailyplanner/ui/PersonCard.java
+++ src/main/java/seedu/dailyplanner/ui/PersonCard.java
@@ -6,67 +6,70 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import seedu.dailyplanner.model.task.ReadOnlyTask;
 
-public class PersonCard extends UiPart{
+public class PersonCard extends UiPart {
 
-    private static final String FXML = "PersonListCard.fxml";
+	private static final String FXML = "PersonListCard.fxml";
 
-    @FXML
-    private HBox cardPane;
-    @FXML
-    private Label name;
-    @FXML
-    private Label id;
-    @FXML
-    private Label phone;
-    @FXML
-    private Label endDate;
-    @FXML
-    private Label address;
-    @FXML
-    private Label email;
-    @FXML
-    private Label tags;
-    @FXML
-    private Label isComplete;
+	@FXML
+	private HBox cardPane;
+	@FXML
+	private Label name;
+	@FXML
+	private Label id;
+	@FXML
+	private Label phone;
+	@FXML
+	private Label endDate;
+	@FXML
+	private Label address;
+	@FXML
+	private Label email;
+	@FXML
+	private Label tags;
+	@FXML
+	private Label isComplete;
 
-    private ReadOnlyTask person;
-    private int displayedIndex;
+	private ReadOnlyTask person;
+	private int displayedIndex;
 
-    public PersonCard(){
+	public PersonCard() {
 
-    }
+	}
 
-    public static PersonCard load(ReadOnlyTask task, int displayedIndex){
-        PersonCard card = new PersonCard();
-        card.person = task;
-        card.displayedIndex = displayedIndex;
-        return UiPartLoader.loadUiPart(card);
-    }
+	public static PersonCard load(ReadOnlyTask task, int displayedIndex) {
+		PersonCard card = new PersonCard();
+		card.person = task;
+		card.displayedIndex = displayedIndex;
+		return UiPartLoader.loadUiPart(card);
+	}
 
-    @FXML
-    public void initialize() {
-        name.setText(person.getName().fullName);
-        id.setText(displayedIndex + ". ");
-        phone.setText(person.getPhone().value);
-        endDate.setText(person.getPhone().getEndDate());
-        address.setText(person.getAddress().value);
-        email.setText(person.getEmail().value);
-        tags.setText(person.tagsString());
-        isComplete.setText(person.getCompletion());
-        System.out.println("TESTING GUI ============== " + person.getCompletion());
-    }
+	@FXML
+	public void initialize() {
+		name.setText(person.getName().fullName);
+		id.setText(displayedIndex + ". ");
+		phone.setText(person.getPhone().value);
+		endDate.setText(person.getPhone().getEndDate());
+		address.setText(person.getAddress().value);
+		email.setText(person.getEmail().value);
+		tags.setText(person.tagsString());
+		if (person.getCompletion().equals("COMPLETE")) {
+			isComplete.setText(person.getCompletion());
+		} else {
+			isComplete.setText("");
+		}
+	}
 
-    public HBox getLayout() {
-        return cardPane;
-    }
+	public HBox getLayout() {
+		return cardPane;
+	}
 
-    @Override
-    public void setNode(Node node) {
-        cardPane = (HBox)node;
-    }
+	@Override
+	public void setNode(Node node) {
+		cardPane = (HBox) node;
+	}
 
-    @Override
-    public String getFxmlPath() {
-        return FXML;
-    }
+	@Override
+	public String getFxmlPath() {
+		return FXML;
+	}
 }
