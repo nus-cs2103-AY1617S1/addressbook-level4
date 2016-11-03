8946761dde00b26a41df7b9fecb3c575bf80c270
louietyj <louietyj@gmail.com>
2016-11-03 23:43:32 +0800

    Extract attemptRedo

--- src/main/java/seedu/todo/controllers/RedoController.java
+++ src/main/java/seedu/todo/controllers/RedoController.java
@@ -52,26 +52,45 @@ public class RedoController implements Controller {
         
         // We don't really have a nice way to support SQL transactions, so yeah >_<
         TodoListDB db = TodoListDB.getInstance();
-        if (numRedo <= 0 || db.redoSize() <= 0) {
-            UiManager.updateConsoleMessage(MESSAGE_FAILURE);
+        
+        // Attempt to redo DB. If fail, exit method.
+        if (attemptRedo(numRedo, db)) {
             return;
         }
-        if (db.redoSize() < numRedo) {
-            UiManager.updateConsoleMessage(String.format(MESSAGE_MULTIPLE_FAILURE,
-                    numRedo, StringUtil.pluralizer(numRedo, "command", "commands"),
-                    db.redoSize(), StringUtil.pluralizer(db.redoSize(), "command", "commands")));
-            return;
-        }
-        for (int i = 0; i < numRedo; i++) {
-            if (!db.redo()) {
-                UiManager.updateConsoleMessage(MESSAGE_FAILURE);
-                return;
-            }
-        }
+        
+        // Reload DB
         db = TodoListDB.getInstance();
         
         // Render
         Renderer.renderIndex(db, String.format(MESSAGE_SUCCESS, numRedo,
                 StringUtil.pluralizer(numRedo, "command", "commands")));
+    }
+
+    /**
+     * Attempt to redo the DB by <code>numRedo</code> steps. If this fails, this
+     * method will update the console message with appropriate error messages.
+     * 
+     * @param numRedo   Number of steps to redo
+     * @param db        TodoListDB instance
+     * @return          true if redo attempt successful, false otherwise
+     */
+    private boolean attemptRedo(int numRedo, TodoListDB db) {
+        if (numRedo <= 0 || db.redoSize() <= 0) {
+            UiManager.updateConsoleMessage(MESSAGE_FAILURE);
+            return false;
+        }
+        if (db.redoSize() < numRedo) {
+            UiManager.updateConsoleMessage(String.format(MESSAGE_MULTIPLE_FAILURE,
+                    numRedo, StringUtil.pluralizer(numRedo, "command", "commands"),
+                    db.redoSize(), StringUtil.pluralizer(db.redoSize(), "command", "commands")));
+            return false;
+        }
+        for (int i = 0; i < numRedo; i++) {
+            if (!db.redo()) {
+                UiManager.updateConsoleMessage(MESSAGE_FAILURE);
+                return false;
+            }
+        }
+        return true;
     }
 }
