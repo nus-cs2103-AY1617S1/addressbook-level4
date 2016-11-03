6fe53d1a2f2242fc6f80a5c84d83b690a71ccc4c
qhng <qhng@u.nus.edu>
2016-11-03 10:07:49 +0800

    Revert "Generate overdue, floating, daily and upcoming list in model manager and load them in mainWindow"
    This reverts commit 5624ef3e54c4ee575add4844179a883d7598f6ea.

--- src/main/java/seedu/savvytasker/logic/Logic.java
+++ src/main/java/seedu/savvytasker/logic/Logic.java
@@ -1,10 +1,8 @@
 package seedu.savvytasker.logic;
 
-import java.util.Date;
-
 import javafx.collections.ObservableList;
-import seedu.savvytasker.model.alias.AliasSymbol;
 import seedu.savvytasker.logic.commands.CommandResult;
+import seedu.savvytasker.model.alias.AliasSymbol;
 import seedu.savvytasker.model.task.ReadOnlyTask;
 
 /**
@@ -20,23 +18,10 @@ public interface Logic {
 
     /** Returns the filtered list of tasks */
     ObservableList<ReadOnlyTask> getFilteredTaskList();
-    
+
     /** Returns the filtered list of alias symbol */
     ObservableList<AliasSymbol> getAliasSymbolList();
     
     /** */
     boolean canParseHeader(String keyword);
-
-    /** Returns the list of tasks that are overdue */
-	ObservableList<ReadOnlyTask> getFilteredOverdueTasks();
-
-	/** Returns the list of floating tasks */
-	ObservableList<ReadOnlyTask> getFilteredFloatingTasks();
-
-	/** Returns the list of tasks on a specific day */
-	ObservableList<ReadOnlyTask> getFilteredDailyTasks(Date date);
-
-	/** Returns the list of tasks that occur after the selected week */
-	ObservableList<ReadOnlyTask> getFilteredUpcomingTasks(Date date);
-	
 }
