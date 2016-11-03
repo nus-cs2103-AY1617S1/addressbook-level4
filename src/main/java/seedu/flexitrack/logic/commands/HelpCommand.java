63f8f29d99ebe0c597c9bcb8514d37ca97bbbd4a
SHIMIN\shimin <shimin.16@gmail.com>
2016-11-03 15:36:18 +0800

    update help

--- src/main/java/seedu/flexitrack/logic/commands/HelpCommand.java
+++ src/main/java/seedu/flexitrack/logic/commands/HelpCommand.java
@@ -1,6 +1,9 @@
+//@@author A0138455Y
 package seedu.flexitrack.logic.commands;
 
-//@@author A0138455Y
+import seedu.flexitrack.commons.core.EventsCenter;
+import seedu.flexitrack.commons.events.ui.ShowHelpRequestEvent;
+
 /**
  * Format full help instructions for every command for display.
  */
@@ -16,8 +19,9 @@ public class HelpCommand extends Command {
             + "List of Commands: " + AddCommand.COMMAND_WORD + ", " + ClearCommand.COMMAND_WORD + ", "
             + DeleteCommand.COMMAND_WORD + ", " + EditCommand.COMMAND_WORD + ", " + ExitCommand.COMMAND_WORD + ", "
             + FindCommand.COMMAND_WORD + ", " + ListCommand.COMMAND_WORD + ", " + MarkCommand.COMMAND_WORD + ", "
-            + SelectCommand.COMMAND_WORD + ", " + UnmarkCommand.COMMAND_WORD + ", " + BlockCommand.COMMAND_WORD + ", " 
-            + ChangeStoragePathCommand.COMMAND_WORD + ", " + UndoCommand.COMMAND_WORD + "\n" + "Example: " + COMMAND_WORD + " "
+            + UnmarkCommand.COMMAND_WORD + ", " + BlockCommand.COMMAND_WORD + ", " 
+            + ChangeStoragePathCommand.COMMAND_WORD + "(Change Storage Path), " + UndoCommand.COMMAND_WORD 
+            + ", " + RedoCommand.COMMAND_WORD + ", " + GapCommand.COMMAND_WORD + "\n" + "Example: " + COMMAND_WORD + " "
             + ClearCommand.COMMAND_WORD;
 
     public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
@@ -28,14 +32,11 @@ public class HelpCommand extends Command {
     }
 
     @Override
-    public CommandResult execute() {
+    public CommandResult execute() {        
         switch (userInput) {
 
         case AddCommand.COMMAND_WORD:
             return new CommandResult(AddCommand.MESSAGE_USAGE);
-
-        case SelectCommand.COMMAND_WORD:
-            return new CommandResult(SelectCommand.MESSAGE_USAGE);
 
         case EditCommand.COMMAND_WORD:
             return new CommandResult(EditCommand.MESSAGE_USAGE);
@@ -66,6 +67,12 @@ public class HelpCommand extends Command {
             
         case UndoCommand.COMMAND_WORD:
             return new CommandResult(UndoCommand.MESSAGE_USAGE);
+            
+        case RedoCommand.COMMAND_WORD:
+            return new CommandResult(RedoCommand.MESSAGE_USAGE);
+            
+        case GapCommand.COMMAND_WORD:
+            return new CommandResult(GapCommand.MESSAGE_USAGE);
             
         case ChangeStoragePathCommand.COMMAND_WORD:
             return new CommandResult(ChangeStoragePathCommand.MESSAGE_USAGE);
