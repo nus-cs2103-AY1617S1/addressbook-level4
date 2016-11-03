32c7b4153ac7f2dc52e6866600d71dad3a257a55
Zavier\ZavierOng <zacohjayjay@gmail.com>
2016-11-04 00:08:47 +0800

    create add todo command test

---
+++ src/test/java/guitests/AddTodoCommandTest.java
@@ -0,0 +1,54 @@
+package guitests;
+
+import guitests.guihandles.TodoCardHandle;
+import org.junit.Test;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.commons.core.Messages;
+import seedu.address.testutil.TestTodo;
+import seedu.address.testutil.TestUtil;
+import seedu.address.testutil.TypicalTestTasks;
+
+import static org.junit.Assert.assertTrue;
+
+public class AddTodoCommandTest extends AddressBookGuiTest {
+
+    @Test
+    public void add() {
+        //add one person
+        TestTodo[] currentList = td.getTypicalTodo();
+        TestTodo todoToAdd = TypicalTestTasks.todo8;
+        assertAddSuccess(todoToAdd, currentList);
+        currentList = TestUtil.addTodosToList(currentList, todoToAdd);
+
+        //add another person
+        todoToAdd = TypicalTestTasks.todo9;
+        assertAddSuccess(todoToAdd, currentList);
+        currentList = TestUtil.addTodosToList(currentList, todoToAdd);
+
+        //add duplicate person
+        commandBox.runCommand(TypicalTestTasks.todo8.getAddCommand());
+        assertResultMessage(AddCommand.MESSAGE_DUPLICATE_TASK);
+        assertTrue(todoListPanel.isListMatching(currentList));
+
+        //add to empty list
+        commandBox.runCommand("clear");
+        assertAddSuccess(TypicalTestTasks.todo1);
+
+        //invalid command
+        commandBox.runCommand("adds dead");
+        assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
+    }
+
+    private void assertAddSuccess(TestTodo personToAdd, TestTodo... currentList) {
+        commandBox.runCommand(personToAdd.getAddCommand());
+
+        //confirm the new card contains the right data
+        TodoCardHandle addedCard = todoListPanel.navigateToPerson(personToAdd.getName().toString());
+        assertTodoMatching(personToAdd, addedCard);
+
+        //confirm the list now contains all previous persons plus the new person
+        TestTodo[] expectedList = TestUtil.addTodosToList(currentList, personToAdd);
+        assertTrue(todoListPanel.isListMatching(expectedList));
+    }
+
+}
