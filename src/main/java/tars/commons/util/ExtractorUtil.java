332313afc1c6305525bb6a12e6dbcd1772f8ddf0
Wei Kang <weikangchia@gmail.com>
2016-11-03 18:42:16 +0800

    Remove unused method

--- src/main/java/tars/commons/util/ExtractorUtil.java
+++ src/main/java/tars/commons/util/ExtractorUtil.java
@@ -1,11 +1,5 @@
 package tars.commons.util;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
 import tars.commons.exceptions.IllegalValueException;
 import tars.logic.parser.Prefix;
 
@@ -13,29 +7,6 @@ import tars.logic.parser.Prefix;
  * Container for methods which extract data from string
  */
 public class ExtractorUtil {
-
-    /**
-     * Extracts the new task's dateTimes from the rsv command's dateTime arguments string. Merges
-     * duplicate dateTime strings.
-     */
-    public static Set<String> getDateTimeStringSetFromArgs(
-            String dateTimeArguments, Prefix prefix)
-            throws IllegalValueException {
-        // no dateTime
-        if (StringUtil.EMPTY_STRING.equals(dateTimeArguments)) {
-            return Collections.emptySet();
-        }
-        // replace first delimiter prefix, then split
-        final Collection<String> dateTimeStrings =
-                Arrays.asList(dateTimeArguments
-                        .replaceFirst(
-                                prefix.value + StringUtil.STRING_WHITESPACE,
-                                StringUtil.EMPTY_STRING)
-                        .split(StringUtil.STRING_WHITESPACE + prefix.value
-                                + StringUtil.STRING_WHITESPACE));
-        return new HashSet<>(dateTimeStrings);
-    }
-
     /**
      * Extracts the new task's recurring args from add command.
      * 
