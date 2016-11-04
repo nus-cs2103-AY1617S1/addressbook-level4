8721ca335b7ab4d2040aef7a775d59b9e859db82
Weng Kiat <wengkiat916@gmail.com>
2016-11-05 01:17:29 +0800

    cleaned up tests

--- src/test/java/guitests/ClearCommandTest.java
+++ src/test/java/guitests/ClearCommandTest.java
@@ -2,15 +2,25 @@ package guitests;
 
 import org.junit.Test;
 
+import edu.emory.mathcs.backport.java.util.Arrays;
+import edu.emory.mathcs.backport.java.util.Collections;
+import seedu.cmdo.testutil.TestTask;
+
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 //@@author A0141006B
 public class ClearCommandTest extends ToDoListGuiTest {
 
     @Test
     public void clear() {
-
+    	
+    	TestTask[] currentList = td.getTypicalTasks();
+    	//sort unsorted list
+    	currentList = sortList(currentList);
+    	
         //verify a non-empty list can be cleared
-        assertTrue(taskListPanel.isListMatching(td.getTypicalTasks()));
+        assertTrue(taskListPanel.isListMatching(currentList));
         assertClearCommandSuccess();
 
         //verify other commands can work after a clear command
@@ -27,6 +37,13 @@ public class ClearCommandTest extends ToDoListGuiTest {
     //run commands other than add
     private void runCommand(String input){
     	commandBox.runCommand(input);
+    }
+    
+    //sort list
+    private TestTask[] sortList(TestTask... currentList){
+    	ArrayList<TestTask> list = new ArrayList<TestTask>(Arrays.asList(currentList));
+    	Collections.sort(list);
+    	return list.toArray(new TestTask[currentList.length]);
     }
 
     private void assertClearCommandSuccess() {
