b369cc5e7e05dcca133af4b040d7e632b296561e
njyjn <njyjustin@yahoo.com>
2016-11-05 01:49:24 +0800

    Added Marini's contribution.

--- src/main/java/seedu/cmdo/logic/parser/MainParser.java
+++ src/main/java/seedu/cmdo/logic/parser/MainParser.java
@@ -428,6 +428,33 @@ public class MainParser {
     	}
     	extractDetail();
     }
+     
+    //@@author A0138471A-unused
+    private void extractDetailUnused() throws IllegalValueException {
+    	checkValidDetailInput();
+    	// Split into '  ...  '
+    	String[] details = args.split("^ '(.+)'$");
+    	// Details only, get rid of anything after the '
+    	String output = new StringBuilder(details[0]).replace(details[0].lastIndexOf("'"), 
+   													details[0].length(), 
+   													"").toString();
+        // Details only, get rid of anything before the ' 
+        for(char o : output.toCharArray()) { 
+        	if(o =='\'') { 
+        		break; 
+        	} 
+        	else 
+        		output=output.replaceFirst(".", ""); 
+        } 
+        // Get rid of the first '
+        output = output.replaceFirst("'","");
+        // Save to instance
+        detailToAdd = output;
+    	// return rear end
+    	args = new StringBuilder(details[0]).substring(details[0].lastIndexOf("'")+1).toString();
+    	// return args without details 
+    	args = args.replace(output, ""); 
+      }
 	
     //@@author A0139661Y
     /**
