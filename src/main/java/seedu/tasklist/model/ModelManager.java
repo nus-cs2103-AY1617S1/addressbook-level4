8c955a9e5c9ef84a834e24cd50a1bab7b920d109
fionnatong <e0006117@u.nus.edu>
2016-11-05 21:41:37 +0800

    Improve indentation

--- src/main/java/seedu/tasklist/model/ModelManager.java
+++ src/main/java/seedu/tasklist/model/ModelManager.java
@@ -184,18 +184,19 @@ public class ModelManager extends ComponentManager implements Model {
 	}
 
 	@Override
-	public synchronized void updateTask(Task taskToUpdate, TaskDetails taskDetails, String startTime, String endTime, Priority priority, String frequency)
-					throws IllegalValueException {
+	public synchronized void updateTask(Task taskToUpdate, TaskDetails taskDetails,
+	        String startTime, String endTime, Priority priority, String frequency)
+	                throws IllegalValueException {
 	    StartTime originalStartTime = (taskToUpdate.getStartTime().isMissing()) ? new StartTime("") : new StartTime(taskToUpdate.getStartTime().toString());
 	    EndTime originalEndTime = (taskToUpdate.getEndTime().isMissing()) ? new EndTime("") : new EndTime(taskToUpdate.getEndTime().toString());
 	    
 	    Task originalTask = new Task(taskToUpdate.getTaskDetails(), originalStartTime, originalEndTime, taskToUpdate.getPriority(), taskToUpdate.getRecurringFrequency());
-		taskList.updateTask(taskToUpdate, taskDetails, startTime, endTime, priority, frequency);		
-        updateFilteredListToShowIncomplete();
-		indicateTaskListChanged();
-		addToUndoStack(UndoCommand.UPD_CMD_ID, null, taskToUpdate, originalTask);
-		clearRedoStack();
-		indicateTaskModified(taskToUpdate);
+	    taskList.updateTask(taskToUpdate, taskDetails, startTime, endTime, priority, frequency);		
+	    updateFilteredListToShowIncomplete();
+	    indicateTaskListChanged();
+	    addToUndoStack(UndoCommand.UPD_CMD_ID, null, taskToUpdate, originalTask);
+	    clearRedoStack();
+	    indicateTaskModified(taskToUpdate);
 	}
 
 	@Override
