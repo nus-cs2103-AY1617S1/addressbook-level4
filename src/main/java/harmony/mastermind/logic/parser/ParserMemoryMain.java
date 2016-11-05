831d6a1361c1aafcc57348f24fd18395b8ca00ba
Hui Qi <xxstarsgalorexx@gmail.com>
2016-11-05 15:20:49 +0800

    getCommand() for memory

---
+++ src/main/java/harmony/mastermind/logic/parser/ParserMemoryMain.java
@@ -0,0 +1,24 @@
+package harmony.mastermind.logic.parser;
+
+public class ParserMemoryMain { 
+    
+    protected static String command; 
+    protected static String taskName; 
+    protected static String description; 
+    
+    protected static int type;
+    protected static int length; 
+    
+    protected static boolean containsDescription; 
+    protected static boolean setProper;
+    
+    private static int day;
+    private static int month; 
+    private static int year;
+    
+    //@@author A0143378Y
+    //General getters and setters
+    public static String getCommand() { 
+        return command;
+    }
+}
