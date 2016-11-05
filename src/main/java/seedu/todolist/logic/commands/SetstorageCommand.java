b4925b6cb4ea11edaa3958d5ba868635b1cdffdc
TYRA-PC\tyra <tyrack99@gmail.com>
2016-11-05 09:43:56 +0800

    fix Indentation problem

--- src/main/java/seedu/todolist/logic/commands/SetstorageCommand.java
+++ src/main/java/seedu/todolist/logic/commands/SetstorageCommand.java
@@ -6,18 +6,16 @@ package seedu.todolist.logic.commands;
  */
 public class SetstorageCommand extends Command{
 
-public static final String COMMAND_WORD = "setstorage";
-    
-    public static final String MESSAGE_USAGE = COMMAND_WORD
-    		+ ": Sets the location of the storage file. \n"
-    		+ "Example: " + COMMAND_WORD
-    		+ " C://user/documents/todolist";
-    
-    public static final String MESSAGE_SUCCESS = "Successfully set the storage location!";
-    
-    public static final String MESSAGE_Invalid_Path = "Given location path is invalid.";
+	public static final String COMMAND_WORD = "setstorage";
 
-    private final String filepath;
+	public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sets the location of the storage file. \n"
+			+ "Example: " + COMMAND_WORD + " C://user/documents/todolist";
+
+	public static final String MESSAGE_SUCCESS = "Successfully set the storage location!";
+
+	public static final String MESSAGE_Invalid_Path = "Given location path is invalid.";
+
+	private final String filepath;
     
     public SetstorageCommand(String filepath) {
     	this.filepath = filepath;
