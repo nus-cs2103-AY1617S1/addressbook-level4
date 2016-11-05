b873ca6f02f2e62df664d05f4f93e7917a8a91b7
Sriram Sami <frizensami@gmail.com>
2016-11-05 17:21:29 +0800

    [GuiTests] Add gui tests for find command

--- src/test/java/guitests/FindCommandTest.java
+++ src/test/java/guitests/FindCommandTest.java
@@ -1,33 +1,53 @@
 package guitests;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import w15c2.tusk.commons.core.Messages;
+import w15c2.tusk.model.task.Task;
+import w15c2.tusk.testutil.TestUtil;
+
+import static org.junit.Assert.*;
+
 public class FindCommandTest extends TaskManagerGuiTest {
 
-//    @Test
-//    public void find_nonEmptyList() {
-//        assertFindResult("find Mark"); //no results
-//        assertFindResult("find Meier", td.benson, td.daniel); //multiple results
-//
-//        //find after deleting one result
-//        commandBox.runCommand("delete 1");
-//        assertFindResult("find Meier",td.daniel);
-//    }
-//
-//    @Test
-//    public void find_emptyList(){
-//        commandBox.runCommand("clear");
-//        assertFindResult("find Jean"); //no results
-//    }
-//
-//    @Test
-//    public void find_invalidCommand_fail() {
-//        commandBox.runCommand("findgeorge");
-//        assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
-//    }
-//
-//    private void assertFindResult(String command, TestPerson... expectedHits ) {
-//        commandBox.runCommand(command);
-//        assertListSize(expectedHits.length);
-//        assertResultMessage(expectedHits.length + " persons listed!");
-//        assertTrue(taskListPanel.isListMatching(expectedHits));
-//    }
+    @Test
+    public void find_nonEmptyList() {
+        List<Task> emptyResults = new ArrayList<>();
+        List<Task> currentTaskList = TestUtil.getInitialTasks().getInternalList();
+        
+        List<Task> results = new ArrayList<>();
+        results.add(currentTaskList.get(9));
+        results.add(currentTaskList.get(10));
+        
+        assertFindResult("find totallynonexistenttask", emptyResults); //no results
+        assertFindResult("find 10 11", results); //multiple results
+
+        //find after deleting one result
+        commandBox.runCommand("delete 2");
+        results.remove(1);
+        assertFindResult("find 10 11", results);
+    }
+
+    @Test
+    public void find_emptyList(){
+        List<Task> emptyResults = new ArrayList<>();
+        commandBox.runCommand("clear");
+        assertFindResult("find Jean", emptyResults); //no results
+    }
+
+    @Test
+    public void find_invalidCommand_fail() {
+        commandBox.runCommand("findgeorge");
+        assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
+    }
+
+    private void assertFindResult(String command, List<Task> expectedHits) {
+        commandBox.runCommand(command);
+        assertListSize(expectedHits.size());
+        assertResultMessage(expectedHits.size() + " tasks listed!");
+        assertTrue(taskListPanel.isListMatching(expectedHits));
+    }
 }
