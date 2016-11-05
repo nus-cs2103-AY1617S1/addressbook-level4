dcea280678497723805b7db9a027bee35193b6e7
Xien Dong <xdrawks@gmail.com>
2016-11-05 23:19:24 +0800

    Added test to fix the undo with rename bug

--- src/test/java/seedu/todo/logic/commands/UndoCommandTest.java
+++ src/test/java/seedu/todo/logic/commands/UndoCommandTest.java
@@ -3,6 +3,12 @@ package seedu.todo.logic.commands;
 import org.junit.Test;
 import seedu.todo.commons.exceptions.ValidationException;
 import seedu.todo.model.TodoModelTest;
+import seedu.todo.model.tag.Tag;
+import seedu.todo.model.task.ImmutableTask;
+
+import java.util.function.Consumer;
+
+import static org.junit.Assert.assertTrue;
 
 //@@author A0135817B
 public class UndoCommandTest extends CommandTest {
@@ -25,5 +31,34 @@ public class UndoCommandTest extends CommandTest {
     @Test(expected = ValidationException.class)
     public void testEmptyUndo() throws Exception {
         execute(false);
+    }
+
+    //@@author A0135805H
+    @Test
+    public void testUndo_undoTagRename_allTagsUndone() throws Exception {
+        String oldTag = "pikachu";
+        String newTag = "pichu";
+
+        for (int i = 1; i <= 3; i ++) {
+            model.add("Sample Task " + i);
+            Thread.sleep(10);
+            model.addTagsToTask(1, oldTag);
+        }
+
+        model.renameTag(oldTag, newTag);
+
+        model.getObservableList().forEach(task -> {
+            boolean hasOldTag = task.getTags().contains(new Tag(oldTag));
+            boolean hasNewTag = task.getTags().contains(new Tag(newTag));
+            assertTrue(hasNewTag && !hasOldTag);
+        });
+
+        model.undo();
+
+        model.getObservableList().forEach(task -> {
+            boolean hasOldTag = task.getTags().contains(new Tag(oldTag));
+            boolean hasNewTag = task.getTags().contains(new Tag(newTag));
+            assertTrue(!hasNewTag && hasOldTag);
+        });
     }
 }
