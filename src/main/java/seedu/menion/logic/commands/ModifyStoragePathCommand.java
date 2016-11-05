2df99f41012208078f8f596786b83a2cd5c43dea
Seow Weijie <sweijie94@gmail.com>
2016-11-06 00:36:11 +0800

    Updated ModifyStoragePathCommand to adapt to test storage files

--- src/main/java/seedu/menion/logic/commands/ModifyStoragePathCommand.java
+++ src/main/java/seedu/menion/logic/commands/ModifyStoragePathCommand.java
@@ -4,15 +4,19 @@ package seedu.menion.logic.commands;
 import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
+
+import seedu.menion.TestApp;
 import seedu.menion.commons.core.Config;
 import seedu.menion.commons.core.EventsCenter;
 import seedu.menion.commons.events.storage.StoragePathChangedEvent;
 import seedu.menion.commons.events.ui.ModifyStorageEvent;
 import seedu.menion.commons.exceptions.DataConversionException;
 import seedu.menion.commons.util.ConfigUtil;
+import seedu.menion.commons.util.FileUtil;
 import seedu.menion.model.ActivityManager;
 import seedu.menion.model.ReadOnlyActivityManager;
 import seedu.menion.storage.XmlActivityManagerStorage;
+import seedu.menion.testutil.TestUtil;
 
 //@@author A0139515A
 /**
@@ -22,6 +26,8 @@ public class ModifyStoragePathCommand extends Command {
 
     public static final String COMMAND_WORD = "modify";
     public static final String DEFAULT_STORAGE_PATH = new File(ModifyStoragePathCommand.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent() + File.separator + "data/menion.xml";
+    public static final String ORIGINAL_TEST_STORAGE_PATH = new File(ModifyStoragePathCommand.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent() + File.separator + FileUtil.getPath("src/test/data/sandbox/sampleData.xml");
+    public static final String TEST_STORAGE_PATH = new File(ModifyStoragePathCommand.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent() + File.separator + FileUtil.getPath("src/test/data/ModifyStoragePathTest/test.xml");
     public static final String MESSAGE_SUCCESS = "You have successfully changed Menion's storage location to %1$s \n";
     public static final String MESSAGE_FAILURE = "Please provide a valid storage path!\n" + 
     										"Examples to modify storage path: \n" + 
@@ -31,9 +37,13 @@ public class ModifyStoragePathCommand extends Command {
     private final String pathToChange;
     
     public ModifyStoragePathCommand(String pathToChange) {
+    	
     	if (!pathToChange.isEmpty()) {
     		if (pathToChange.toLowerCase().trim().equals("default")) {
     			this.pathToChange = DEFAULT_STORAGE_PATH;
+    		}
+    		else if (pathToChange.toLowerCase().trim().equals("test")) {
+    			this.pathToChange = TEST_STORAGE_PATH;
     		}
     		else {
     			this.pathToChange = pathToChange.trim();
@@ -51,20 +61,20 @@ public class ModifyStoragePathCommand extends Command {
     	model.updateRecentChangedActivity(null);
     	
     	ReadOnlyActivityManager before = new ActivityManager(model.getActivityManager());
-    	String newPath;
+		
+    	
+		String newPath;
     	
         if (pathToChange != null) {
-            Config config = new Config();
-            Optional<Config> configOptional = null;
-    		try {
-    			configOptional = ConfigUtil.readConfig(config.DEFAULT_CONFIG_FILE);
-    		} catch (DataConversionException e1) {
-    			return new CommandResult("Unable to read configuration file");
-    		}
+        
+        	model.addStoragePathToUndoStack(Config.getInstance().getActivityManagerFilePath());
+        	// Add old file path to undo stack
+        	if (pathToChange.equals(TEST_STORAGE_PATH)) {
+    			//for test
+    			model.addStoragePathToUndoStack(ORIGINAL_TEST_STORAGE_PATH);
+        	}
     		
-    		Config initializedConfig = configOptional.orElse(new Config());
-    		
-    		if (!pathToChange.equals(DEFAULT_STORAGE_PATH)) {
+    		if (!pathToChange.equals(DEFAULT_STORAGE_PATH) && !pathToChange.equals(TEST_STORAGE_PATH)) {
 	            String root = System.getProperty("user.home");
 	            newPath = root + File.separator + pathToChange;
     		}
@@ -72,14 +82,14 @@ public class ModifyStoragePathCommand extends Command {
     			newPath = pathToChange;
     		}
     		
-    		//deleting old files
-    		File oldStorage =  new File(initializedConfig.getActivityManagerFilePath());
+    		// Deleting old files
+    		File oldStorage =  new File(Config.getInstance().getActivityManagerFilePath());
     		oldStorage.delete();
     		
             // Saving configuration
-        	initializedConfig.setActivityManagerFilePath(newPath);
+    		Config.getInstance().setActivityManagerFilePath(newPath);
         	try {
-				ConfigUtil.saveConfig(initializedConfig, initializedConfig.DEFAULT_CONFIG_FILE);
+				ConfigUtil.saveConfig(Config.getInstance(), Config.getInstance().DEFAULT_CONFIG_FILE);
 			} catch (IOException e) {
 				return new CommandResult("Unable to save configuration");
 			}
@@ -94,16 +104,5 @@ public class ModifyStoragePathCommand extends Command {
         	return new CommandResult(String.format(MESSAGE_SUCCESS, pathToChange));
         }
         return new CommandResult(MESSAGE_FAILURE);
-    }
-
-    private void deleteFileOrFolder(File file){
-	    try {
-	        for (File f : file.listFiles()) {
-	            f.delete();
-	            deleteFileOrFolder(f);
-	        }
-	    } catch (Exception e) {
-	        e.printStackTrace(System.err);
-	    }
     }
 }
