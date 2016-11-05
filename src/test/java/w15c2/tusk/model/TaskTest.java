4f941a78d95ca7636d3ccee177c14036a4e05f27
Sriram Sami <frizensami@gmail.com>
2016-11-05 16:13:01 +0800

    [TaskTests] Add tests for Task

--- src/test/java/w15c2/tusk/model/TaskTest.java
+++ src/test/java/w15c2/tusk/model/TaskTest.java
@@ -1,9 +1,10 @@
 package w15c2.tusk.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
-import java.util.Date;
 import java.util.Calendar;
+import java.util.Date;
 
 import org.junit.Test;
 
@@ -15,6 +16,20 @@ import w15c2.tusk.model.task.Task;
 //@@author A0138978E
 public class TaskTest {
 
+    @Test
+    public void toString_noDescription() {
+        Task t1 = new FloatingTask("");
+        
+        assertEquals(t1.toString(), "");
+    }
+    
+    @Test
+    public void toString_hasDescription() {
+        Task t1 = new FloatingTask("test");
+        
+        assertEquals(t1.toString(), "test");
+    }
+    
     @Test
     public void compareTo_identicalTasks() {
         Task t1 = new FloatingTask("test");
@@ -85,6 +100,90 @@ public class TaskTest {
         Task t2 = getLaterEventTask();
         
         assertEquals(t1.compareTo(t2), -1);
+    }
+    
+    @Test
+    public void equals_twoFloatingTasks_sameDescription() {
+        Task t1 = new FloatingTask("hello world");
+        Task t2 = new FloatingTask("hello world");
+        
+        assertEquals(t1, t2);
+    }
+    
+    @Test
+    public void equals_twoFloatingTasks_differentDescriptions() {
+        Task t1 = new FloatingTask("hello world");
+        Task t2 = new FloatingTask("hello world 2");
+        
+        assertNotEquals(t1, t2);
+    }
+    
+    @Test
+    public void equals_twoFloatingTasks_differentPinStatus() {
+        Task t1 = new FloatingTask("hello world");
+        Task t2 = new FloatingTask("hello world");
+        t2.setAsPin();
+        
+        assertNotEquals(t1, t2);
+    }
+    
+    @Test
+    public void equals_twoFloatingTasks_differentCompletedStatus() {
+        Task t1 = new FloatingTask("hello world");
+        Task t2 = new FloatingTask("hello world");
+        t2.setAsComplete();
+        
+        assertNotEquals(t1, t2);
+    }
+    
+    @Test
+    public void equals_oneFloatingoneEvent() {
+        Task t1 = new FloatingTask("hello world");
+        Task t2 = getEventTask();
+        
+        assertNotEquals(t1, t2);
+    }
+    
+    
+    @Test
+    public void equals_oneFloatingoneDeadline() {
+        Task t1 = new FloatingTask("hello world");
+        Task t2 = getDeadlineTask();
+        
+        assertNotEquals(t1, t2);
+    }
+    
+    
+    @Test
+    public void equals_twoDeadlineTasks_sameDeadline() {
+        Task t1 = getDeadlineTask();
+        Task t2 = getDeadlineTask();
+        
+        assertEquals(t1, t2);
+    }
+    
+    @Test
+    public void equals_twoDeadlineTasks_differentDeadlines() {
+        Task t1 = getDeadlineTask();
+        Task t2 = getLaterDeadlineTask();
+        
+        assertNotEquals(t1, t2);
+    }
+    
+    @Test
+    public void equals_twoEventTasks_sameDates() {
+        Task t1 = getEventTask();
+        Task t2 = getEventTask();
+        
+        assertEquals(t1, t2);
+    }
+    
+    @Test
+    public void equals_twoEventTasks_differentDates() {
+        Task t1 = getEventTask();
+        Task t2 = getLaterEventTask();
+        
+        assertNotEquals(t1, t2);
     }
     
     
