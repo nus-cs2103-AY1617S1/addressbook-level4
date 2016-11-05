22694ac1ef52ec3547b96ed110d45db52941ce1c
Seow Weijie <sweijie94@gmail.com>
2016-11-06 00:37:00 +0800

    Implemented ModifyStoragePathTest

--- src/test/java/guitests/ModifyStoragePathCommandTest.java
+++ src/test/java/guitests/ModifyStoragePathCommandTest.java
@@ -8,36 +8,35 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import seedu.menion.logic.commands.ModifyStoragePathCommand;
 import seedu.menion.model.ActivityManager;
 import seedu.menion.model.ReadOnlyActivityManager;
 import seedu.menion.storage.XmlActivityManagerStorage;
 import seedu.menion.testutil.TypicalTestActivities;
 
 //@@author A0139515A
-/*
+
 public class ModifyStoragePathCommandTest extends ActivityManagerGuiTest {
-  
-	@Rule
-    public TemporaryFolder testFolder = new TemporaryFolder();
 	
 	@Test
 	public void modifyStoragePath() throws Exception {
 		
 		//testing if the file in the new location created has the same content
-		String filePath = testFolder.getRoot().getPath() + "testMenion.xml";		
+		String filePath = ModifyStoragePathCommand.TEST_STORAGE_PATH;		
 		TypicalTestActivities typicalTestActivities = new TypicalTestActivities();		
 		ActivityManager original = typicalTestActivities.getTypicalActivityManager();
-        String root = System.getProperty("user.home");
-        String newPath = root + File.separator + filePath;
+		
         commandBox.runCommand("modify " + filePath);         
-        XmlActivityManagerStorage xmlActivityManagerStorage = new XmlActivityManagerStorage(newPath);       
-        ReadOnlyActivityManager am =  xmlActivityManagerStorage.readActivityManager(newPath).get();       
+        XmlActivityManagerStorage xmlActivityManagerStorage = new XmlActivityManagerStorage(filePath);       
+        ReadOnlyActivityManager am =  xmlActivityManagerStorage.readActivityManager(filePath).get();       
         ActivityManager after = new ActivityManager(am);       
         assertEquals(original, after);
         
-        commandBox.runCommand("modify menionStorage/menion.xml");
+        commandBox.runCommand("modify default");
+        assertEquals(original, after);
+        
         //for invalid command
         commandBox.runCommand("modify"); 
-        assertResultMessage("Please provide a valid storage path!");
+        assertResultMessage(ModifyStoragePathCommand.MESSAGE_FAILURE);
 	}
-}*/
+}
