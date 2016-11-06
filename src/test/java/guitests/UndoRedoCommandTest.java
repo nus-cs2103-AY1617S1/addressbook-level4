68c8d29519ed7ba424850a9180fe257782b46770
Seow Weijie <sweijie94@gmail.com>
2016-11-06 11:18:21 +0800

    Passed Junit test for UndoRedo including undo and redo for modify storage path

--- src/test/java/guitests/UndoRedoCommandTest.java
+++ src/test/java/guitests/UndoRedoCommandTest.java
@@ -3,10 +3,14 @@ package guitests;
 import guitests.guihandles.TaskCardHandle;
 import seedu.menion.commons.core.Config;
 import seedu.menion.commons.exceptions.DataConversionException;
+import seedu.menion.commons.util.ConfigUtil;
 import seedu.menion.commons.util.FileUtil;
+import seedu.menion.commons.util.XmlUtil;
 import seedu.menion.logic.commands.ModifyStoragePathCommand;
 import seedu.menion.logic.commands.RedoCommand;
 import seedu.menion.logic.commands.UndoCommand;
+import seedu.menion.storage.XmlFileStorage;
+import seedu.menion.storage.XmlSerializableActivityManager;
 import seedu.menion.testutil.TestActivity;
 import seedu.menion.testutil.TestUtil;
 
@@ -17,12 +21,21 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.xml.bind.JAXBException;
 
 //@@author A0139515A
 public class UndoRedoCommandTest extends ActivityManagerGuiTest {
-
+	Config originalConfig;
+	File originalFile;
+	XmlSerializableActivityManager originalData;
+	
     @Test
-    public void undoRedo() throws FileNotFoundException, DataConversionException {
+    public void undoRedo() throws DataConversionException, JAXBException, IOException {
+ 
+    	saveOriginalConfig();
     	
         //add one activity
         TestActivity[] originalList = td.getTypicalTask();
@@ -31,6 +44,7 @@ public class UndoRedoCommandTest extends ActivityManagerGuiTest {
         assertAddSuccess(activityToAdd, originalList);
         TestActivity[] currentList = TestUtil.addActivitiesToList(originalList, activityToAdd);
 
+        
         //testing undo command for adding of task
         commandBox.runCommand("undo n");
         assertTrue(activityListPanel.isTaskListMatching(originalList));
@@ -56,8 +70,8 @@ public class UndoRedoCommandTest extends ActivityManagerGuiTest {
         commandBox.runCommand("redo n");
         assertTrue(activityListPanel.isTaskListMatching(originalList));
         assertResultMessage(RedoCommand.MESSAGE_SUCCESS);
-       
-    
+          
+        
         //testing undo for clear command
         commandBox.runCommand("clear");
         commandBox.runCommand("undo n");
@@ -69,17 +83,28 @@ public class UndoRedoCommandTest extends ActivityManagerGuiTest {
         assertListSize(0);
         assertResultMessage(RedoCommand.MESSAGE_SUCCESS);
         
+        
         //testing undo for modify storage path
         String originalTestFilePath = new File(ModifyStoragePathCommand.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent() + File.separator + FileUtil.getPath("src/test/data/sandbox/sampleData.xml");
         commandBox.runCommand("modify test");
+        
+        Config testConfig;
+    	testConfig = readFromCurrentConfig();
+    	String modifiedFilePath = testConfig.getActivityManagerFilePath();
+    	
         commandBox.runCommand("undo m");
-        assertEquals(originalTestFilePath, Config.getInstance().getActivityManagerFilePath());
+        
+    	testConfig = readFromCurrentConfig();
+    	String undoFilePath = testConfig.getActivityManagerFilePath();
+    	
+        assertEquals(originalTestFilePath, undoFilePath);
         assertResultMessage(UndoCommand.MESSAGE_SUCCESS);
         
         //testing redo command
         String testFilePath = new File(ModifyStoragePathCommand.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent() + File.separator + FileUtil.getPath("src/test/data/ModifyStoragePathTest/test.xml");
         commandBox.runCommand("redo m");
-        assertEquals(testFilePath, Config.getInstance().getActivityManagerFilePath());
+    	
+        assertEquals(testFilePath, modifiedFilePath);
         assertResultMessage(RedoCommand.MESSAGE_SUCCESS);
         
         //revert to original file path
@@ -101,10 +126,11 @@ public class UndoRedoCommandTest extends ActivityManagerGuiTest {
         commandBox.runCommand("redo n");
         commandBox.runCommand("redo n");
         commandBox.runCommand("redo n");
-        commandBox.runCommand("redo n");
         assertResultMessage(RedoCommand.MESSAGE_FAILURE);
         commandBox.runCommand("redo");
         assertResultMessage(RedoCommand.MESSAGE_FAILURE);
+        
+        restoreOriginalConfig();
     }
 
     private void assertAddSuccess(TestActivity activityToAdd, TestActivity... currentList) {
@@ -119,4 +145,31 @@ public class UndoRedoCommandTest extends ActivityManagerGuiTest {
         TestActivity[] expectedList = TestUtil.addActivitiesToList(currentList, activityToAdd);
         assertTrue(activityListPanel.isTaskListMatching(expectedList));
     }
+	private void restoreOriginalConfig() throws IOException, FileNotFoundException, JAXBException {
+		FileUtil.createIfMissing(originalFile);
+        XmlUtil.saveDataToFile(originalFile, originalData);
+	}
+
+	private void saveOriginalConfig() throws IOException, DataConversionException, FileNotFoundException {
+		try {
+            Optional<Config> configOptional = ConfigUtil.readConfig(Config.DEFAULT_CONFIG_FILE);
+            originalConfig = configOptional.orElse(Config.getInstance());
+        } catch (DataConversionException e) {
+            originalConfig = Config.getInstance();
+        }
+    	originalFile = new File(originalConfig.getActivityManagerFilePath());
+    	FileUtil.createIfMissing(originalFile);
+    	originalData = XmlFileStorage.loadDataFromSaveFile(originalFile);
+	}
+	
+	private Config readFromCurrentConfig() {
+		Config testConfig;
+		try {
+            Optional<Config> configOptional = ConfigUtil.readConfig(Config.DEFAULT_CONFIG_FILE);
+            testConfig = configOptional.orElse(Config.getInstance());
+        } catch (DataConversionException e) {
+        	testConfig = Config.getInstance();
+        }
+		return testConfig;
+	}
 }
