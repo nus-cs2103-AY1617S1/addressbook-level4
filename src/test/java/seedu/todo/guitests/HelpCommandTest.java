8df33723f2aa067762e6cfa56d29ebe458515e0a
Irvin Lim <irvinlimweiquan@gmail.com>
2016-11-06 23:53:28 +0800

    Add one more test case

--- src/test/java/seedu/todo/guitests/HelpCommandTest.java
+++ src/test/java/seedu/todo/guitests/HelpCommandTest.java
@@ -9,10 +9,22 @@ import seedu.todo.controllers.HelpController;
 import seedu.todo.guitests.guihandles.HelpItemHandle;
 
 public class HelpCommandTest extends GuiTest {
-
+    
     @Test
     public void help_show_success() {
         console.runCommand("help");
+        assertTrue(helpView.hasLoaded());
+        
+        // Make sure all command items have loaded
+        for (CommandDefinition commandDefinition : new HelpController().getAllCommandDefinitions()) {
+            HelpItemHandle helpItem = helpView.getHelpItem(commandDefinition);
+            assertNotNull(helpItem);
+        }
+    }
+
+    @Test
+    public void help_extraArguments_noDifference() {
+        console.runCommand("help me i'm drowning");
         assertTrue(helpView.hasLoaded());
         
         // Make sure all command items have loaded
