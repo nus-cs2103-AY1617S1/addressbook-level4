4971d7a248694e3925a2ee0ec1a837c27240336b
Joel Foo <joelfooet@gmail.com>
2016-11-06 16:58:06 +0800

    Update colors for task cards

--- src/main/java/tars/ui/UiColor.java
+++ src/main/java/tars/ui/UiColor.java
@@ -9,12 +9,14 @@ import javafx.scene.paint.Color;
  */
 public class UiColor {
 
-    public static final String STATUS_UNDONE_TEXT_FILL =
+    public static final String STATUS_UNDONE_TEXT_FILL_DARK =
             "-fx-text-fill: #212121";
+    public static final String STATUS_UNDONE_TEXT_FILL_LIGHT =
+            "-fx-text-fill: #757575";
     public static final String STATUS_DONE_TEXT_FILL = "-fx-text-fill: lightgrey";
     public static final String CIRCLE_LABEL_COLOR = "-fx-text-fill: white;";
-    public static final String TASK_CARD_NEWLY_ADDED_BORDER = "-fx-border-color: lightblue";
-    public static final String TASK_CARD_DEFAULT_BORDER = "-fx-border-color: #455A64";
+    public static final String TASK_CARD_NEWLY_ADDED_BORDER = "-fx-border-color: #2E8AF7";
+    public static final String TASK_CARD_DEFAULT_BORDER = "-fx-border-color: lightgrey";
     
     
     public enum CircleColor {
