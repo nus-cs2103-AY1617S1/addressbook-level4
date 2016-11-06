3c2384a3b0f0f15ef45a67103f91b34772290aed
Johnervan Lee <johnervan@gmail.com>
2016-11-06 15:22:32 +0800

    bug fix

--- src/main/java/tars/commons/util/DateTimeUtil.java
+++ src/main/java/tars/commons/util/DateTimeUtil.java
@@ -18,230 +18,237 @@ import tars.model.task.DateTime;
  */
 public class DateTimeUtil {
 
-    private static final String DATETIME_DAY = "day";
-    private static final String DATETIME_WEEK = "week";
-    private static final String DATETIME_MONTH = "month";
-    private static final String DATETIME_YEAR = "year";
-    private static final int DATETIME_INCREMENT = 1;
+  private static final String DATETIME_DAY = "day";
+  private static final String DATETIME_WEEK = "week";
+  private static final String DATETIME_MONTH = "month";
+  private static final String DATETIME_YEAR = "year";
+  private static final int DATETIME_INCREMENT = 1;
 
-    private static final DateTimeFormatter formatter =
-            DateTimeFormatter.ofPattern("d/M/uuuu HHmm");
-    private static final DateTimeFormatter stringFormatter =
-            DateTimeFormatter.ofPattern("dd/MM/uuuu HHmm");
-    private static final DateTimeFormatter stringFormatterWithoutTime =
-            DateTimeFormatter.ofPattern("dd/MM/uuuu");
-    private static final DateTimeFormatter stringFormatterWithoutDate =
-            DateTimeFormatter.ofPattern("HHmm");
+  private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/uuuu HHmm");
+  private static final DateTimeFormatter stringFormatter =
+      DateTimeFormatter.ofPattern("dd/MM/uuuu HHmm");
+  private static final DateTimeFormatter stringFormatterWithoutTime =
+      DateTimeFormatter.ofPattern("dd/MM/uuuu");
+  private static final DateTimeFormatter stringFormatterWithoutDate =
+      DateTimeFormatter.ofPattern("HHmm");
 
-    /**
-     * Extracts the new task's dateTime from the string arguments.
-     * 
-     * @@author A0139924W
-     * @return String[] with first index being the startDate time and second index being the end
-     *         date time
-     */
-    public static String[] parseStringToDateTime(String dateTimeArg) {
-        return NattyDateTimeUtil.parseStringToDateTime(dateTimeArg);
+  /**
+   * Extracts the new task's dateTime from the string arguments.
+   * 
+   * @@author A0139924W
+   * @return String[] with first index being the startDate time and second index being the end date
+   *         time
+   */
+  public static String[] parseStringToDateTime(String dateTimeArg) {
+    return NattyDateTimeUtil.parseStringToDateTime(dateTimeArg);
+  }
+
+  /**
+   * Checks if given endDateTime is within the start and end of this week
+   * 
+   * @@author A0121533W
+   */
+  public static boolean isWithinWeek(LocalDateTime endDateTime) {
+    if (endDateTime == null) {
+      return false;
+    } else {
+      LocalDateTime today = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0);
+      LocalDateTime startThisWeek = today.with(TemporalAdjusters.previous(DayOfWeek.SUNDAY));
+      LocalDateTime endThisWeek = today.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+      return endDateTime.isAfter(startThisWeek) && endDateTime.isBefore(endThisWeek);
+    }
+  }
+
+  /**
+   * Checks if given endDateTime is before the end of today
+   * 
+   * @@author A0121533W
+   */
+  public static boolean isOverDue(LocalDateTime endDateTime) {
+    if (endDateTime == null) {
+      return false;
+    } else {
+      LocalDateTime now = LocalDateTime.now();
+      return endDateTime.isBefore(now);
+    }
+  }
+
+  /**
+   * @@author A0124333U
+   * Checks whether the dateTimeQuery falls within the range of the dateTimeSource
+   * i.e. dateTimeQuery startDateTime is equals to or before the dateTimeSource endDateTime
+   * && dateTimeQuery endDateTime is equals to or after the dateTimeSource startDateTime
+   * 
+   * @param dateTimeSource
+   * @param dateTimeQuery
+   */
+  public static boolean isDateTimeWithinRange(DateTime dateTimeSource, DateTime dateTimeQuery) {
+
+    // Return false if task is a floating task (i.e. no start or end
+    // dateTime
+    if (dateTimeSource.getEndDate() == null) {
+      return false;
     }
 
-    /**
-     * Checks if given endDateTime is within the start and end of this week
-     * 
-     * @@author A0121533W
-     */
-    public static boolean isWithinWeek(LocalDateTime endDateTime) {
-        if (endDateTime == null) {
-            return false;
-        } else {
-            LocalDateTime today =
-                    LocalDateTime.now().withHour(0).withMinute(0).withSecond(0);
-            LocalDateTime startThisWeek =
-                    today.with(TemporalAdjusters.previous(DayOfWeek.SUNDAY));
-            LocalDateTime endThisWeek =
-                    today.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
-            return endDateTime.isAfter(startThisWeek)
-                    && endDateTime.isBefore(endThisWeek);
-        }
+    DateTime dateTime1 = fillDateTime(dateTimeSource);
+    DateTime dateTime2 = fillDateTime(dateTimeQuery);
+
+    return !dateTime1.getEndDate().isBefore(dateTime2.getStartDate())
+        && !dateTime1.getStartDate().isAfter(dateTime2.getEndDate());
+  }
+
+  /**
+   * Checks whether the dateTimeQuery conflicts with the dateTimeSource 
+   * i.e. dateTimeQuery endDateTime occurs after the dateTimeSource startDateTime
+   * && dateTimeQuery startDateTime occurs before the dateTimeSource endDateTime
+   * 
+   */
+  public static boolean isDateTimeConflicting(DateTime dateTimeSource, DateTime dateTimeQuery) {
+
+    // Return false if task is a floating task (i.e. no start or end
+    // dateTime
+    if (dateTimeSource.getEndDate() == null) {
+      return false;
     }
 
-    /**
-     * Checks if given endDateTime is before the end of today
-     * 
-     * @@author A0121533W
-     */
-    public static boolean isOverDue(LocalDateTime endDateTime) {
-        if (endDateTime == null) {
-            return false;
-        } else {
-            LocalDateTime now = LocalDateTime.now();
-            return endDateTime.isBefore(now);
-        }
+    DateTime dateTime1 = fillDateTime(dateTimeSource);
+    DateTime dateTime2 = fillDateTime(dateTimeQuery);
+
+    return dateTime1.getEndDate().isAfter(dateTime2.getStartDate())
+        && dateTime1.getStartDate().isBefore(dateTime2.getEndDate());
+  }
+
+  private static DateTime fillDateTime(DateTime filledDateTime) {
+    DateTime dateTimeToFill = new DateTime();
+
+    dateTimeToFill.setEndDateTime(filledDateTime.getEndDate());
+
+    if (filledDateTime.getStartDate() != null) {
+      dateTimeToFill.setStartDateTime(filledDateTime.getStartDate());
+    } else {
+      dateTimeToFill.setStartDateTime(filledDateTime.getEndDate());
+    }
+    return dateTimeToFill;
+  }
+
+  /**
+   * Returns an arraylist of free datetime slots in a specified date
+   * 
+   */
+  public static ArrayList<DateTime> getListOfFreeTimeSlotsInDate(DateTime dateToCheck,
+      ArrayList<DateTime> listOfFilledTimeSlotsInDate) {
+    ArrayList<DateTime> listOfFreeTimeSlots = new ArrayList<DateTime>();
+    LocalDateTime startDateTime = dateToCheck.getStartDate();
+    LocalDateTime endDateTime;
+
+    for (DateTime dt : listOfFilledTimeSlotsInDate) {
+      if (dt.getStartDate() == null) {
+        continue;
+      } else {
+        endDateTime = dt.getStartDate();
+      }
+
+      if (startDateTime.isBefore(endDateTime)) {
+        listOfFreeTimeSlots.add(new DateTime(startDateTime, endDateTime));
+      }
+
+      if (startDateTime.isBefore(dt.getEndDate())) {
+        startDateTime = dt.getEndDate();
+      }
     }
 
-    /**
-     * Checks whether the dateTimeQuery falls within the range of the dateTimeSource
-     * 
-     * @@author A0124333U
-     * @param dateTimeSource
-     * @param dateTimeQuery
-     */
-    public static boolean isDateTimeWithinRange(DateTime dateTimeSource, DateTime dateTimeQuery) {
-
-        DateTime dateTime1 = new DateTime();
-        DateTime dateTime2 = new DateTime();
-
-        // Return false if task is a floating task (i.e. no start or end
-        // dateTime
-        if (dateTimeSource.getEndDate() == null) {
-            return false;
-        }
-
-        fillDateTime(dateTime1, dateTimeSource);
-        fillDateTime(dateTime2, dateTimeQuery);
-
-        if (dateTime1.getEndDate().isBefore(dateTime2.getStartDate())
-                || dateTime1.getStartDate().isAfter(dateTime2.getEndDate())) {
-            return false;
-        } else {
-          return true;
-        }
+    if (startDateTime.isBefore(dateToCheck.getEndDate())) {   
+      listOfFreeTimeSlots.add(new DateTime(startDateTime, dateToCheck.getEndDate()));
     }
 
-    private static DateTime fillDateTime(DateTime dateTimeToFill, DateTime filledDateTime) {
-        dateTimeToFill.setEndDateTime(filledDateTime.getEndDate());
+    return listOfFreeTimeSlots;
+  }
 
-        if (filledDateTime.getStartDate() != null) {
-            dateTimeToFill.setStartDateTime(filledDateTime.getStartDate());
-        } else {
-            dateTimeToFill.setStartDateTime(filledDateTime.getEndDate());
-        }
-        return dateTimeToFill;
+  public static String getDayAndDateString(DateTime dateTime) {
+    StringBuilder sb = new StringBuilder();
+
+    sb.append(dateTime.getEndDate().getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH))
+        .append(StringUtil.STRING_COMMA)
+        .append(dateTime.getEndDate().format(stringFormatterWithoutTime));
+
+    return sb.toString();
+  }
+
+  public static String getStringOfFreeDateTimeInDate(DateTime dateToCheck,
+      ArrayList<DateTime> listOfFreeTimeSlotsInDate) {
+    StringBuilder sb = new StringBuilder();
+
+    sb.append(getDayAndDateString(dateToCheck)).append(StringUtil.STRING_COLON);
+
+    int counter = 1;
+
+    for (DateTime dt : listOfFreeTimeSlotsInDate) {
+      sb.append(StringUtil.STRING_NEWLINE).append(counter).append(StringUtil.STRING_FULLSTOP)
+          .append(dt.getStartDate().format(stringFormatterWithoutDate)).append("hrs to ")
+          .append(dt.getEndDate().format(stringFormatterWithoutDate)).append("hrs (")
+          .append(getDurationInMinutesBetweenTwoLocalDateTime(dt.getStartDate(), dt.getEndDate()))
+          .append(")");
+      counter++;
     }
 
-    /**
-     * Returns an arraylist of free datetime slots in a specified date
-     * 
-     */
-    public static ArrayList<DateTime> getListOfFreeTimeSlotsInDate(DateTime dateToCheck,
-            ArrayList<DateTime> listOfFilledTimeSlotsInDate) {
-        ArrayList<DateTime> listOfFreeTimeSlots = new ArrayList<DateTime>();
-        LocalDateTime startDateTime = dateToCheck.getStartDate();
-        LocalDateTime endDateTime;
+    return sb.toString();
 
-        for (DateTime dt : listOfFilledTimeSlotsInDate) {
-            if (dt.getStartDate() == null) {
-                continue;
-            } else {
-                endDateTime = dt.getStartDate();
-            }
+  }
 
-            if (startDateTime.isBefore(endDateTime)) {
-                listOfFreeTimeSlots
-                .add(new DateTime(startDateTime, endDateTime));
-            }
+  public static String getDurationInMinutesBetweenTwoLocalDateTime(LocalDateTime startDateTime,
+      LocalDateTime endDateTime) {
+    Duration duration = Duration.between(startDateTime, endDateTime);
+    long hours = duration.toHours();
+    long minutes = duration.toMinutes() % 60;
 
-            if (startDateTime.isBefore(dt.getEndDate())) {
-                startDateTime = dt.getEndDate();
-            }
-        }
+    return hours + " hr " + minutes + " min";
+  }
 
-        if (startDateTime.isBefore(dateToCheck.getEndDate())) {
-            listOfFreeTimeSlots
-            .add(new DateTime(startDateTime, dateToCheck.getEndDate()));
-        }
+  /**
+   * Modify the date based on the new hour, min and sec
+   * 
+   * @@author A0139924W
+   */
+  public static Date setDateTime(Date toBeEdit, int hour, int min, int sec) {
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(toBeEdit);
+    calendar.set(Calendar.HOUR_OF_DAY, hour);
+    calendar.set(Calendar.MINUTE, min);
+    calendar.set(Calendar.SECOND, sec);
+    toBeEdit = calendar.getTime();
 
-        return listOfFreeTimeSlots;
+    return toBeEdit;
+  }
+
+  /**
+   * Modifies the date based on the frequency for recurring tasks
+   * 
+   * @@author A0140022H
+   */
+  public static String modifyDate(String dateToModify, String frequency) {
+    LocalDateTime date = LocalDateTime.parse(dateToModify, formatter);
+
+    switch (frequency.toLowerCase()) {
+      case DATETIME_DAY:
+        date = date.plusDays(DATETIME_INCREMENT);
+        break;
+      case DATETIME_WEEK:
+        date = date.plusWeeks(DATETIME_INCREMENT);
+        break;
+      case DATETIME_MONTH:
+        date = date.plusMonths(DATETIME_INCREMENT);
+        break;
+      case DATETIME_YEAR:
+        date = date.plusYears(DATETIME_INCREMENT);
+        break;
     }
 
-    public static String getDayAndDateString(DateTime dateTime) {
-        StringBuilder sb = new StringBuilder();
+    dateToModify = date.format(stringFormatter);
+    return dateToModify;
+  }
 
-        sb.append(dateTime.getEndDate().getDayOfWeek()
-                .getDisplayName(TextStyle.FULL, Locale.ENGLISH)).append(StringUtil.STRING_COMMA)
-        .append(dateTime.getEndDate()
-                .format(stringFormatterWithoutTime));
-
-        return sb.toString();
-    }
-
-    public static String getStringOfFreeDateTimeInDate(DateTime dateToCheck,
-            ArrayList<DateTime> listOfFreeTimeSlotsInDate) {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(getDayAndDateString(dateToCheck)).append(StringUtil.STRING_COLON);
-
-        int counter = 1;
-
-        for (DateTime dt : listOfFreeTimeSlotsInDate) {
-            sb.append(StringUtil.STRING_NEWLINE).append(counter).append(StringUtil.STRING_FULLSTOP)
-            .append(dt.getStartDate()
-                    .format(stringFormatterWithoutDate))
-            .append("hrs to ")
-            .append(dt.getEndDate().format(stringFormatterWithoutDate))
-            .append("hrs (")
-            .append(getDurationInMinutesBetweenTwoLocalDateTime(
-                    dt.getStartDate(), dt.getEndDate()))
-            .append(")");
-            counter++;
-        }
-
-        return sb.toString();
-
-    }
-
-    public static String getDurationInMinutesBetweenTwoLocalDateTime(LocalDateTime startDateTime,
-            LocalDateTime endDateTime) {
-        Duration duration = Duration.between(startDateTime, endDateTime);
-        long hours = duration.toHours();
-        long minutes = duration.toMinutes() % 60;
-
-        return hours + " hr " + minutes + " min";
-    }
-
-    /**
-     * Modify the date based on the new hour, min and sec
-     * 
-     * @@author A0139924W
-     */
-    public static Date setDateTime(Date toBeEdit, int hour, int min, int sec) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(toBeEdit);
-        calendar.set(Calendar.HOUR_OF_DAY, hour);
-        calendar.set(Calendar.MINUTE, min);
-        calendar.set(Calendar.SECOND, sec);
-        toBeEdit = calendar.getTime();
-
-        return toBeEdit;
-    }
-
-    /**
-     * Modifies the date based on the frequency for recurring tasks
-     * 
-     * @@author A0140022H
-     */
-    public static String modifyDate(String dateToModify, String frequency) {
-        LocalDateTime date = LocalDateTime.parse(dateToModify, formatter);
-
-        switch (frequency.toLowerCase()) {
-            case DATETIME_DAY:
-                date = date.plusDays(DATETIME_INCREMENT);
-                break;
-            case DATETIME_WEEK:
-                date = date.plusWeeks(DATETIME_INCREMENT);
-                break;
-            case DATETIME_MONTH:
-                date = date.plusMonths(DATETIME_INCREMENT);
-                break;
-            case DATETIME_YEAR:
-                date = date.plusYears(DATETIME_INCREMENT);
-                break;
-        }
-
-        dateToModify = date.format(stringFormatter);
-        return dateToModify;
-    }
-
-    public static LocalDateTime setLocalTime(LocalDateTime dateTime, int hour, int min, int sec) {
-        return LocalDateTime.of(dateTime.getYear(), dateTime.getMonth(), dateTime.getDayOfMonth(), hour, min, sec);
-    }
+  public static LocalDateTime setLocalTime(LocalDateTime dateTime, int hour, int min, int sec) {
+    return LocalDateTime.of(dateTime.getYear(), dateTime.getMonth(), dateTime.getDayOfMonth(), hour,
+        min, sec);
+  }
 }
