2b4f495981f9bb3d257a72a06dff3b961c2a9a39
Nikhil Suresh <nikhilsure@gmail.com>
2016-11-06 23:39:14 +0800

    [UI][BugFix] Fix alias list wrap text and other small ui bugs

--- src/main/java/w15c2/tusk/ui/AliasCard.java
+++ src/main/java/w15c2/tusk/ui/AliasCard.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import w15c2.tusk.model.Alias;
@@ -21,7 +22,7 @@ public class AliasCard extends UiPart{
         @FXML
         private Label id;
         @FXML
-        private Rectangle colorTag;
+        private VBox colorTag;
 
         private Alias alias;
         private int displayedIndex;
@@ -30,7 +31,7 @@ public class AliasCard extends UiPart{
         private static final String CARD_ALIAS_CSS = "-fx-text-fill: rgba(247, 246, 239, 0.7);";
         private static final String ID_CSS = "-fx-text-fill: rgba(244, 244, 244, 1.0);";
         private static final String COMMAND_CSS = ID_CSS;
-        private static final Color  COLORTAG_CSS = Color.rgb(186, 143, 106);
+        private static final String  COLORTAG_CSS = "-fx-background-color: rgb(186, 143, 106)";
 
         public AliasCard(){
         }
@@ -52,7 +53,7 @@ public class AliasCard extends UiPart{
         	cardAlias.setStyle(CARD_ALIAS_CSS);
         	command.setStyle(COMMAND_CSS);
         	id.setStyle(ID_CSS);
-        	colorTag.setFill(COLORTAG_CSS);
+        	colorTag.setStyle(COLORTAG_CSS);
         }
 
         public HBox getLayout() {
