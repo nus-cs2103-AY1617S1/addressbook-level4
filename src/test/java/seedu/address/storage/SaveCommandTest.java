06693fe157327ac86a9028bfa4584e0274751493
Kevin Ong <Kevin Ong>
2016-11-06 15:25:06 +0800

    Added one more test, will increase coverage a tiny bit

--- src/test/java/seedu/address/storage/SaveCommandTest.java
+++ src/test/java/seedu/address/storage/SaveCommandTest.java
@@ -8,13 +8,21 @@ import static org.junit.Assert.assertTrue;
 
 import guitests.AddressBookGuiTest;
 
+//@@author A0125680H
 public class SaveCommandTest extends AddressBookGuiTest {
     private static final String TEST_SAVE_DIRECTORY = "src/test/data/testfile.xml";
+    private static final String TEST_SAVE_DIRECTORY_NO_EXTENSION = "src/test/data/testfile";
     
     @Test
     public void save_withDefaultTestPath() {
         saveFile(TEST_SAVE_DIRECTORY);
         assertSaveFileExists(TEST_SAVE_DIRECTORY);
+    }
+    
+    @Test
+    public void save_withDefaultTestPath_noXmlExtension() {
+        saveFile(TEST_SAVE_DIRECTORY_NO_EXTENSION);
+        assertSaveFileExists(TEST_SAVE_DIRECTORY_NO_EXTENSION + ".xml");
     }
     
     private void saveFile(String saveDirectory) {
