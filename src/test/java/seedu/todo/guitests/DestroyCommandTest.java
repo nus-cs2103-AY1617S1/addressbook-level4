8fffb47148f3473f11dfab9d3d5ff059e39d1148
louietyj <louietyj@gmail.com>
2016-11-06 22:47:54 +0800

    Fixtures

---
+++ src/test/java/seedu/todo/guitests/DestroyCommandTest.java
@@ -0,0 +1,56 @@
+package seedu.todo.guitests;
+
+import java.time.LocalDateTime;
+
+import org.junit.Before;
+
+import seedu.todo.commons.util.DateUtil;
+import seedu.todo.models.Event;
+import seedu.todo.models.Task;
+
+/**
+ * @@author A0093907W
+ */
+public class DestroyCommandTest extends GuiTest {
+    
+    private final LocalDateTime oneDayFromNow = LocalDateTime.now().plusDays(1);
+    private final String oneDayFromNowString = DateUtil.formatDate(oneDayFromNow);
+    private final String oneDayFromNowIsoString = DateUtil.formatIsoDate(oneDayFromNow);
+    private final LocalDateTime twoDaysFromNow = LocalDateTime.now().plusDays(2);
+    private final String twoDaysFromNowString = DateUtil.formatDate(twoDaysFromNow);
+    private final String twoDaysFromNowIsoString = DateUtil.formatIsoDate(twoDaysFromNow);
+    private final LocalDateTime oneDayToNow = LocalDateTime.now().minusDays(1);
+    private final String oneDayToNowString = DateUtil.formatDate(oneDayToNow);
+    private final String oneDayToNowIsoString = DateUtil.formatIsoDate(oneDayToNow);
+    
+    String commandAdd1 = String.format("add task Buy KOI by \"%s 8pm\"", oneDayToNowString);
+    Task task1 = new Task();
+    String commandAdd2 = String.format("add task Buy Milk by \"%s 9pm\"", oneDayFromNowString);
+    Task task2 = new Task();
+    String commandAdd3 = String.format("add event Some Event from \"%s 4pm\" to \"%s 5pm\"",
+            twoDaysFromNowString, twoDaysFromNowString);
+    Event event3 = new Event();
+    
+    public DestroyCommandTest() {
+        task1.setName("Buy KOI");
+        task1.setDueDate(DateUtil.parseDateTime(
+                String.format("%s 20:00:00", oneDayToNowIsoString)));
+        task2.setName("Buy Milk");
+        task2.setDueDate(DateUtil.parseDateTime(
+                String.format("%s 21:00:00", oneDayFromNowIsoString)));
+        event3.setName("Some Event");
+        event3.setStartDate(DateUtil.parseDateTime(
+                String.format("%s 16:00:00", twoDaysFromNowIsoString)));
+        event3.setEndDate(DateUtil.parseDateTime(
+                String.format("%s 17:00:00", twoDaysFromNowIsoString)));
+    }
+    
+    @Before
+    public void fixtures() {
+        console.runCommand("clear");
+        console.runCommand(commandAdd1);
+        console.runCommand(commandAdd2);
+        console.runCommand(commandAdd3);
+    }
+
+}
