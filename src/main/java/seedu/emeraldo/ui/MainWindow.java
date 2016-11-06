06fc66b7fae03d15efd633164934ac095ddfa593
JingHe <he1994@hotmail.com>
2016-11-06 18:08:56 +0800

    Decrease minWidth of main window

--- src/main/java/seedu/emeraldo/ui/MainWindow.java
+++ src/main/java/seedu/emeraldo/ui/MainWindow.java
@@ -24,7 +24,7 @@ public class MainWindow extends UiPart {
     private static final String ICON = "/images/Emeraldo_icon.png";
     private static final String FXML = "MainWindow.fxml";
     public static final int MIN_HEIGHT = 600;
-    public static final int MIN_WIDTH = 800;
+    public static final int MIN_WIDTH = 600;
 
     private Logic logic;
 
