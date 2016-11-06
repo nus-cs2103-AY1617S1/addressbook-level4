c6701337ebbfca5d65ba1dabe3c28f2b6dd2efd0
clarencechee <clarencechee94@gmail.com>
2016-11-06 20:08:05 +0800

    Help gui test: remove redundant click

--- src/test/java/guitests/HelpWindowTest.java
+++ src/test/java/guitests/HelpWindowTest.java
@@ -10,11 +10,7 @@ public class HelpWindowTest extends AddressBookGuiTest {
 
     @Test
     public void openHelpWindow() {
-
-        taskListPanel.clickOnListView();
-
         assertHelpWindowOpen(commandBox.runHelpCommand());
-
     }
 
     private void assertHelpWindowOpen(HelpWindowHandle helpWindowHandle) {
