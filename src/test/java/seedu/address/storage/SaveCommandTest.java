5636c626381ee5919d476b738cd105efa80c34d0
Kevin Ong <Kevin Ong>
2016-11-06 15:11:50 +0800

    Added SaveCommandTest (not sure if its in the correct folder)

--- src/test/java/seedu/address/storage/SaveCommandTest.java
+++ src/test/java/seedu/address/storage/SaveCommandTest.java
@@ -1,5 +1,34 @@
 package seedu.address.storage;
 
-public class SaveCommandTest {
+import java.io.File;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+import guitests.AddressBookGuiTest;
+
+public class SaveCommandTest extends AddressBookGuiTest {
+    private static final String TEST_SAVE_DIRECTORY = "src/test/data/testfile.xml";
+    
+    @Test
+    public void save_withDefaultTestPath() {
+        assertSave(TEST_SAVE_DIRECTORY);
+    }
+    
+    private void assertSave(String saveDirectory) {
+        String saveCommand = "save " + saveDirectory;
+        
+        commandBox.runCommand(saveCommand);
+        
+        assertSaveFileExists(saveDirectory);
+    }
+    
+    private void assertSaveFileExists(String saveDirectory) {
+        File testSaveFile = new File(saveDirectory);
+        
+        assertTrue(testSaveFile.exists());
+        
+        testSaveFile.delete();
+    }
 }
