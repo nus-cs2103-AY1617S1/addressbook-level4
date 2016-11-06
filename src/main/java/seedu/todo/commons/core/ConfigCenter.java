ff77649ff59d461b82ae55fb45bdbf520b46e05a
Irvin Lim <irvinlimweiquan@gmail.com>
2016-11-06 17:05:46 +0800

    Add ConfigCenter

---
+++ src/main/java/seedu/todo/commons/core/ConfigCenter.java
@@ -0,0 +1,52 @@
+package seedu.todo.commons.core;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import seedu.todo.MainApp;
+import seedu.todo.commons.exceptions.DataConversionException;
+import seedu.todo.commons.util.ConfigUtil;
+
+// @@author A0139812A
+public class ConfigCenter {
+    private static final Logger logger = LogsCenter.getLogger(MainApp.class);
+
+    private static ConfigCenter instance;
+    
+    private Config config;
+    private String configFilePath;
+
+    public static ConfigCenter getInstance() {
+        if (instance == null) {
+            instance = new ConfigCenter();
+        }
+        
+        return instance;
+    }
+    
+    public void setConfigFilePath(String path) {
+        configFilePath = path;
+    }
+    
+    public Config getConfig() {
+        if (config == null) {
+            Optional<Config> configOptional;
+            
+            try {
+                configOptional = ConfigUtil.readConfig(configFilePath);
+                config = configOptional.orElse(new Config());
+            } catch (DataConversionException e) {
+                logger.warning("Config file at " + configFilePath + " is not in the correct format. " +
+                        "Using default config properties");
+            }
+        }
+        
+        return config;
+    }
+    
+    public void saveConfig(Config config) throws IOException {
+        ConfigUtil.saveConfig(config, configFilePath);
+        this.config = config;
+    }
+}
