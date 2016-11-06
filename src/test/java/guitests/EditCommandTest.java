cd2b05a0ed7f2e0a259490af14361c9f0d6995e6
Kevin Ong <Kevin Ong>
2016-11-06 14:13:14 +0800

    Added tests for recurring reminders and events

--- src/test/java/guitests/EditCommandTest.java
+++ src/test/java/guitests/EditCommandTest.java
@@ -17,132 +17,171 @@ import seedu.address.testutil.*;
 
 //@@author A0125097A
 public class EditCommandTest extends AddressBookGuiTest {
+    
+    private static final boolean REMINDER_NOT_RECURRING = false;
+    private static final boolean REMINDER_IS_RECURRING = true;
 
     @Test
-    public void edit_activityParameters(){
+    public void edit_activityParameters() {
         TestActivity[] currentList = td.getTypicalActivities();
         int index = 1;
-        assertEditActivityResult(index,currentList);
+        assertEditActivityResult(index, currentList);
     }
-    
+
     @Test
-    public void edit_taskParameters(){
+    public void edit_taskParameters() {
         TestActivity[] currentList = td.getTypicalActivities();
         int index = 3;
-        assertEditTaskResult(index,currentList);
-    }
-    
-    @Test
-    public void edit_activityWithTaskParameters(){
-        TestActivity[] currentList = td.getTypicalActivities();
-        int index = 1;
-        assertEditTaskResult(index,currentList);
+        assertEditTaskResult(index, currentList);
     }
 
     @Test
-    public void edit_taskWithActivityParameters(){
+    public void edit_activityWithTaskParameters() {
+        TestActivity[] currentList = td.getTypicalActivities();
+        int index = 1;
+        assertEditTaskResult(index, currentList);
+    }
+
+    @Test
+    public void edit_taskWithActivityParameters() {
         TestActivity[] currentList = td.getTypicalActivities();
         int index = 4;
-        assertEditActivityResult(index,currentList);
+        assertEditActivityResult(index, currentList);
     }
 
-	@Test
-    public void edit_eventParameters(){
+    @Test
+    public void edit_eventParameters() {
         TestActivity[] currentList = td.getTypicalActivities();
         int index = 5;
-        assertEditEventResult(index,currentList);        
+        assertEditEventResult(index, currentList);
     }
-	
+
     @Test
-    public void edit_activityWithEventParameters(){
+    public void edit_activityWithEventParameters() {
         TestActivity[] currentList = td.getTypicalActivities();
         int index = 1;
-        assertEditEventResult(index,currentList);
+        assertEditEventResult(index, currentList);
+    }
+
+    @Test
+    public void edit_eventWithActivityParameters() {
+        TestActivity[] currentList = td.getTypicalActivities();
+        int index = 5;
+        assertEditActivityResult(index, currentList);
+    }
+
+    @Test
+    public void edit_activityWithRecurringReminder() {
+        TestActivity[] currentList = td.getTypicalActivities();
+        int index = 1;
+        assertEditRecurReminderResult(index, currentList);
     }
     
     @Test
-    public void edit_eventWithActivityParameters(){
+    public void edit_eventWithRecurringStartEndTimes() {
         TestActivity[] currentList = td.getTypicalActivities();
         int index = 5;
-        assertEditActivityResult(index,currentList);
+        assertEditRecurEventResult(index, currentList);
     }
-    
 
-
-	private void assertEditActivityResult(int index,TestActivity... currentList){
-    	assertEditResult(index,"activity",currentList);
+    private void assertEditActivityResult(int index, TestActivity... currentList) {
+        assertEditResult(index, "activity", REMINDER_NOT_RECURRING, currentList);
     }
-    
-	private void assertEditTaskResult(int index, TestActivity[] currentList) {
-		assertEditResult(index,"task",currentList);	
-	}
-    
+
+    private void assertEditTaskResult(int index, TestActivity[] currentList) {
+        assertEditResult(index, "task", REMINDER_NOT_RECURRING, currentList);
+    }
+
     private void assertEditEventResult(int index, TestActivity[] currentList) {
-    	assertEditResult(index,"event",currentList);
-	}
-    
-    	private void assertEditResult(int index,String type, TestActivity... currentList){
-    	String newName = "Totally New Name";
-    	String newReminder = "1-01-2020 1111";
-    	String newDuedate = "10-10-2020 1010";
-    	String newPriority = "3";
-    	String newStartTime = "2-02-2020 1212";
-    	String newEndTime = "10-10-2020 1010";
-        String editCommand = "edit " + index;
-    	
-        TestActivity activityBeforeEdit;
-			activityBeforeEdit = produceNewActivityObject(currentList[index-1]);
-
-    	TestActivity activityAfterEdit = null;
-
-        editCommand += " n/" + newName ;
-        switch (type) {
-        case "task":
-        {
-				activityAfterEdit = new TestTask(currentList[index-1]);
-        ((TestTask) activityAfterEdit).setDueDate(newDuedate);
-        ((TestTask) activityAfterEdit).setPriority(newPriority);
-        editCommand += " d/" + newDuedate  + " p/" + newPriority;
-        }
-        break;
-        
-        case "event":
-        {
-				activityAfterEdit = new TestEvent(currentList[index-1]);
-        ((TestEvent) activityAfterEdit).setStartTime(newStartTime);
-        ((TestEvent) activityAfterEdit).setEndTime(newEndTime);
-        editCommand += " s/" + newStartTime  + " e/" + newEndTime;
-        } 
-        break;
-        
-        case "activity":
-        	activityAfterEdit = currentList[index-1];
-        }
-        
-        activityAfterEdit.setName(newName);
-        activityAfterEdit.setReminder(newReminder);
-        editCommand += " r/" + newReminder;
-        commandBox.runCommand(editCommand);
-        
-        assertResultMessage(String.format("Edited Task from: %1$s\nto: %2$s",
-        		activityBeforeEdit.getAsText(),activityAfterEdit.getAsText()));
-        
-        assertTrue(activityListPanel.isListMatching(currentList));
- 
+        assertEditResult(index, "event", REMINDER_NOT_RECURRING, currentList);
     }
-    	
-    	
-        private TestActivity produceNewActivityObject(TestActivity original){
-            String type = original.getClass().getSimpleName().toLowerCase();
+    
+    private void assertEditRecurReminderResult(int index, TestActivity[] currentList) {
+        assertEditResult(index, "activity", REMINDER_IS_RECURRING, currentList);
+    }
+    
+    private void assertEditRecurEventResult(int index, TestActivity[] currentList) {
+        assertEditResult(index, "recur-event", REMINDER_NOT_RECURRING, currentList);
+    }
+
+    private void assertEditResult(int index, String type, boolean isReminderRecurring, TestActivity... currentList) {
+        String newName = "Totally New Name";
+        String newReminder = "1-01-2020 1111";
+        String newDuedate = "10-10-2020 1010";
+        String newPriority = "3";
+        String newStartTime = "2-02-2020 1212";
+        String newEndTime = "10-10-2020 1010";
+        String newRecurReminder = "every thu 1300";
+        String newRecurStartTime = "every fri 1400";
+        String newRecurEndTime = "every fri 1600";
+        String editCommand = "edit " + index;
+
+        TestActivity activityBeforeEdit;
+        activityBeforeEdit = produceNewActivityObject(currentList[index - 1]);
+
+        TestActivity activityAfterEdit = null;
+
+        editCommand += " n/" + newName;
+        switch (type) {
+        case "task": {
+            activityAfterEdit = new TestTask(currentList[index - 1]);
+            ((TestTask) activityAfterEdit).setDueDate(newDuedate);
+            ((TestTask) activityAfterEdit).setPriority(newPriority);
+            editCommand += " d/" + newDuedate + " p/" + newPriority;
+        }
+            break;
+
+        case "event": {
+            activityAfterEdit = new TestEvent(currentList[index - 1]);
+            ((TestEvent) activityAfterEdit).setStartTime(newStartTime);
+            ((TestEvent) activityAfterEdit).setEndTime(newEndTime);
+            editCommand += " s/" + newStartTime + " e/" + newEndTime;
+        }
+            break;
             
-            switch(type){
-            case "testactivity":
-            	return new TestActivity(original);
-            case "testtask":
-            	return new TestTask((TestTask) original);
-            default: //case "event":
-            	return new TestEvent((TestEvent) original);	
-            }
+        case "recur-event": {
+            activityAfterEdit = new TestEvent(currentList[index - 1]);
+            ((TestEvent) activityAfterEdit).setStartTime(newRecurStartTime);
+            ((TestEvent) activityAfterEdit).setEndTime(newRecurEndTime);
+            editCommand += " s/" + newRecurStartTime + " e/" + newRecurEndTime;
+        }
+            break;
+
+        case "activity":
+            activityAfterEdit = currentList[index - 1];
         }
         
+
+        activityAfterEdit.setName(newName);
+        
+        if (isReminderRecurring) {
+            activityAfterEdit.setReminder(newRecurReminder);
+            editCommand += " r/" + newRecurReminder;
+        } else {
+            activityAfterEdit.setReminder(newReminder);
+            editCommand += " r/" + newReminder;
+        }
+        
+        commandBox.runCommand(editCommand);
+
+        assertResultMessage(String.format("Edited Task from: %1$s\nto: %2$s", activityBeforeEdit.getAsText(),
+                activityAfterEdit.getAsText()));
+
+        assertTrue(activityListPanel.isListMatching(currentList));
+
+    }
+
+    private TestActivity produceNewActivityObject(TestActivity original) {
+        String type = original.getClass().getSimpleName().toLowerCase();
+
+        switch (type) {
+        case "testactivity":
+            return new TestActivity(original);
+        case "testtask":
+            return new TestTask((TestTask) original);
+        default: // case "event":
+            return new TestEvent((TestEvent) original);
+        }
+    }
+
 }
