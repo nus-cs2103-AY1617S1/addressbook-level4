6fe469c09692209397fcf0e7f64b23faff33c5a5
zavfel <zavfel@users.noreply.github.com>
2016-11-07 11:23:47 +0800

    fix configUtilTest

--- src/test/java/harmony/mastermind/commons/util/ConfigUtilTest.java
+++ src/test/java/harmony/mastermind/commons/util/ConfigUtilTest.java
@@ -77,8 +77,8 @@ public class ConfigUtilTest {
         Config config = new Config();
         config.setAppTitle("Typical App Title");
         config.setLogLevel(Level.INFO);
-        config.setUserPrefsFilePath("C:\\preferences.json");
-        config.setTaskManagerFilePath("mastermind.xml");
+        config.setUserPrefsFilePath("../src/test/data/sandbox/pref_testing.json");
+        config.setTaskManagerFilePath("../src/test/data/sandbox/tempTaskManager.xml");
         config.setTaskManagerName("TypicalTaskManagerName");
         return config;
     }
