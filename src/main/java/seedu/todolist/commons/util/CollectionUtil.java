f9b34180eb852d5026c1fdc7ff072d7aafa8ad8d
Ang Shi Ya <shiya_95@hotmail.com>
2016-11-07 20:51:03 +0800

    add test

--- src/main/java/seedu/todolist/commons/util/CollectionUtil.java
+++ src/main/java/seedu/todolist/commons/util/CollectionUtil.java
@@ -12,29 +12,29 @@ public class CollectionUtil {
     /**
      * Returns true if any of the given items are null.
      */
-    public static boolean isAnyNull(Object... items) {
+    /*public static boolean isAnyNull(Object... items) {
         for (Object item : items) {
             if (item == null) {
                 return true;
             }
         }
         return false;
-    }
+    }*/
 
 
 
     /**
      * Throws an assertion error if the collection or any item in it is null.
      */
-    public static void assertNoNullElements(Collection<?> items) {
+    /*public static void assertNoNullElements(Collection<?> items) {
         assert items != null;
         assert !isAnyNull(items);
-    }
+    }*/
 
     /**
      * Returns true if every element in a collection are unique by {@link Object#equals(Object)}.
      */
-    public static boolean elementsAreUnique(Collection<?> items) {
+    /*public static boolean elementsAreUnique(Collection<?> items) {
         final Set<Object> testSet = new HashSet<>();
         for (Object item : items) {
             final boolean itemAlreadyExists = !testSet.add(item); // see Set documentation
@@ -43,5 +43,5 @@ public class CollectionUtil {
             }
         }
         return true;
-    }
+    }*/
 }
