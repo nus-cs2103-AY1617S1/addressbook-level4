20d66d553003f8c8caf76e7a7c56501fe021ffac
RonaldLWM <Ronnyboy_123@hotmail.com>
2016-11-07 16:14:40 +0800

    changed indexes in edit message

--- src/main/java/seedu/simply/logic/commands/EditCommand.java
+++ src/main/java/seedu/simply/logic/commands/EditCommand.java
@@ -33,11 +33,11 @@ public class EditCommand extends Command{
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits an existing task in Simply. "
             +"Parameters: INDEX <section to delete> <edited information>\n" 
-            +"Example: " + COMMAND_WORD + " 1 " + DESCRIPTION_WORD + " beach party\t\t"
-            +"Example: " + COMMAND_WORD + " 1 " + DATE_WORD + " 120516\n"
-            +"Example: " + COMMAND_WORD + " 1 " + START_WORD + " 1600\t\t\t"
-            +"Example: " + COMMAND_WORD + " 1 " + END_WORD + " 2300\n"
-            +"Example: " + COMMAND_WORD + " 1 " + TAG_WORD + " sentosa";
+            +"Example: " + COMMAND_WORD + " T1 " + DESCRIPTION_WORD + " beach party\t\t"
+            +"Example: " + COMMAND_WORD + " D1 " + DATE_WORD + " 120516\n"
+            +"Example: " + COMMAND_WORD + " E1 " + START_WORD + " 1600\t\t\t"
+            +"Example: " + COMMAND_WORD + " E2 " + END_WORD + " 2300\n"
+            +"Example: " + COMMAND_WORD + " D2 " + TAG_WORD + " sentosa";
 
     public static final String MESSAGE_EDIT_TASK_SUCCESS = "Edited task: %1$s%2$s       Changes:  %3$s";
 
