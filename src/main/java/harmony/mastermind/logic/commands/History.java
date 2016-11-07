022f1929d4385fc6a59ec484456eb54584783523
Hui Qi <xxstarsgalorexx@gmail.com>
2016-11-07 22:29:47 +0800

    commeneted

--- src/main/java/harmony/mastermind/logic/commands/History.java
+++ src/main/java/harmony/mastermind/logic/commands/History.java
@@ -33,4 +33,69 @@ public class History {
                     memory.get(i).getState()));
         }
     }
+    
+    /*
+     * Unused code: 
+     * Reason: For future implementations of Memory 
+    //@@author A0143378Y-unused
+    // Retrieves most recent snapshot in the forward stack and swap it with current memory. Current memory gets pushed into back stack
+    public static void redo(Memory memory) {
+        if (forward.isEmpty() || (forward.peek() == null)) {
+            System.out.println("Nothing to redo!");
+            return;
+        }
+        
+        redoMemorySwap(memory);
+        StorageMemory.saveToStorage(memory);
+        System.out.println("Redo successful.");
+    }
+    
+    //@@author A0143378Y
+    // Swaps memory used for redo
+    private static void redoMemorySwap(Memory memory) {
+        assert forward.size() > 0;
+        back.push(current);
+        current = forward.pop();
+        memory.setList(current);
+    }
+
+    //@@author A0143378Y
+    // Retrieves most recent snapshot in the back stack and swap it with current memory. Current memory gets pushed into forward stack
+    public static void undo(Memory memory) {
+        if (back.isEmpty() || (back.peek() == null)) {
+            System.out.println("Nothing to undo!");
+            return;
+        }
+
+        ArrayList<GenericMemory> temp = duplicateTemp(memory);
+        undoMemorySwap(memory, temp);
+        StorageMemory.saveToStorage(memory);
+        System.out.println("Undo successful.");
+    }
+    
+    //@@author A0143378Y
+    // Swaps memory used for undo
+    private static void undoMemorySwap(Memory memory, ArrayList<GenericMemory> temp) {
+        forward.push(temp);
+        current = back.pop();
+        assert current != null;
+        memory.setList(current);
+    }
+
+    //@@author A0143378Y
+    // Duplicate current memory into temp ArrayList
+    private static ArrayList<GenericMemory> duplicateTemp(Memory memory) {
+        ArrayList<GenericMemory> temp = new ArrayList<GenericMemory>(); // Duplicating of memory into forward stack
+        for (int i = 0; i < memory.getSize(); i++) {
+            temp.add(new GenericMemory(memory.get(i).getType(),
+                    memory.get(i).getName(),
+                    memory.get(i).getDescription(),
+                    memory.get(i).getStart(),
+                    memory.get(i).getEnd(),
+                    memory.get(i).getState()));
+        }
+        return temp;
+    }
+    */
+
 }
