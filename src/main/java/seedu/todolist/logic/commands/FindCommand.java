3a425898dfb405d18742b0fe95e18a1f646d8e1a
CARINA <carinackwu@gmail.com>
2016-11-07 12:53:00 +0800

    add String constant and assertion to Command

--- src/main/java/seedu/todolist/logic/commands/FindCommand.java
+++ src/main/java/seedu/todolist/logic/commands/FindCommand.java
@@ -2,27 +2,32 @@ package seedu.todolist.logic.commands;
 
 import java.util.Set;
 
+//@@author A0153736B
 /**
- * Finds and lists all tasks in to-do list whose name contains any or all of the argument keywords.
- * Keyword matching is case sensitive.
+ * Finds and lists all tasks in to-do list whose name contains any/all/phrase of the argument keywords.
+ * Keyword matching is case insensitive.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
-
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all tasks whose names contain any or all of "
-            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: FINDTYPE KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " phrase alice bob charlie";
     
     public static final String MESSAGE_INVALID_FINDTYPE = "The findtype provided is invalid.";
     
     public static final String MESSAGE_KEYWORDS_NOT_PROVIDED = "Keywords are not provided.";
+    
+    public static final String FINDTYPE_EITHER = "either";
+    public static final String FINDTYPE_ALL = "all";
+    public static final String FINDTYPE_PHRASE = "phrase";
+    public static final String VALID_FINDTYPE = FINDTYPE_EITHER + "|" + FINDTYPE_ALL + "|" + FINDTYPE_PHRASE;
+    
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all tasks whose names contain any/all/phrase of "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: FINDTYPE KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " " + FINDTYPE_PHRASE + " alice bob charlie";
 
     private final Set<String> keywords;
     private final String findType;
   
-    //@@author A0153736B
     public FindCommand(Set<String> keywords, String findType) {
         this.keywords = keywords;
         this.findType = findType;
@@ -30,10 +35,12 @@ public class FindCommand extends Command {
 
     @Override
     public CommandResult execute() {
-    	if (!("either".equals(findType) || "all".equals(findType) || "phrase".equals(findType)))
+    	if (!findType.matches(VALID_FINDTYPE))
     		return new CommandResult(MESSAGE_INVALID_FINDTYPE);
     	if (keywords.isEmpty())
     		return new CommandResult(MESSAGE_KEYWORDS_NOT_PROVIDED);
+    	
+    	assert model != null;
         model.updateFilteredTaskList(keywords, findType);
         return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredAllTaskList().size()));
     }
