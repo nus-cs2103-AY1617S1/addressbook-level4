368520cb21ce86ed2448e47ab3bc1913113755c0
Shi Xiyue <shixiyue97@gmail.com>
2016-11-07 21:41:08 +0800

    Fix undo message.

--- src/main/java/seedu/toDoList/logic/commands/UndoCommand.java
+++ src/main/java/seedu/toDoList/logic/commands/UndoCommand.java
@@ -10,7 +10,7 @@ public class UndoCommand extends Command {
 
     public static final String COMMAND_WORD = "undo";
 
-    public static final String MESSAGE_UNDO_SUCCESS = "Undid the most recent action.\n%1$s";
+    public static final String MESSAGE_UNDO_SUCCESS = "Undid the most recent action.";
     public static final String MESSAGE_UNDO_FAILED = "No command to undo.";
 
     @Override
