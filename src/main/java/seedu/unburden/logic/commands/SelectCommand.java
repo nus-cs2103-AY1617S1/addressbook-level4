e480954bea86d462bdb382c12c251a4813159bdd
Luhua Yang <Luhua Yang>
2016-11-07 22:24:14 +0800

    no message

--- src/main/java/seedu/unburden/logic/commands/SelectCommand.java
+++ src/main/java/seedu/unburden/logic/commands/SelectCommand.java
@@ -8,37 +8,39 @@ import seedu.unburden.model.task.ReadOnlyTask;
 //@@author A0147986H-unused
 /**
  * Selects a task identified using it's last displayed index from the address book.
- * the user will be able to see the details in the specific task
+ * It will also show the details of the task selected, especially
+ * task descriptions that may not be able to show on the taskList panel
  */
 public class SelectCommand extends Command {
 
-    public final int targetIndex;
+    public final int taskIndex;
 
     public static final String COMMAND_WORD = "select";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Selects the task identified by the index number used in the last task listing.\n"
-            + "Format: select INDEX (must be a positive integer)\n"
+            + "Parameters: INDEX (must be a positive integer that does not exceed the maximum number of tasks)\n"
+            + "Format: select INDEX\n"
             + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_SELECT_TASK_SUCCESS = "Selected Task: %1$s";
 
-    public SelectCommand(int targetIndex) {
-        this.targetIndex = targetIndex;
+    public SelectCommand(int taskIndex) {
+        this.taskIndex = taskIndex;
     }
 
+    
     @Override
     public CommandResult execute() {
 
         UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
 
-        if (lastShownList.size() < targetIndex) {
+        if (lastShownList.size() < taskIndex) {
             indicateAttemptToExecuteIncorrectCommand();
             return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
         }
 
-        EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex - 1));
-        return new CommandResult(String.format(MESSAGE_SELECT_TASK_SUCCESS, lastShownList.get(targetIndex)));
-
+        EventsCenter.getInstance().post(new JumpToListRequestEvent(taskIndex - 1));
+        return new CommandResult(String.format(MESSAGE_SELECT_TASK_SUCCESS, lastShownList.get(taskIndex-1)));
     }
 }
